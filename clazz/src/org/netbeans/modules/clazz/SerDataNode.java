4c9d9ab188c909629bdac941c1be7b6885c8b5d1
David_Simonek <none@none>
1999-01-15 15:44:16 +0000

    #

---
+++ clazz/src/org/netbeans/modules/clazz/SerDataNode.java
@@ -0,0 +1,96 @@
+/*
+ *                 Sun Public License Notice
+ * 
+ * The contents of this file are subject to the Sun Public License
+ * Version 1.0 (the "License"). You may not use this file except in
+ * compliance with the License. A copy of the License is available at
+ * http://www.sun.com/
+ * 
+ * The Original Code is Forte for Java, Community Edition. The Initial
+ * Developer of the Original Code is Sun Microsystems, Inc. Portions
+ * Copyright 1997-2000 Sun Microsystems, Inc. All Rights Reserved.
+ */
+
+package com.netbeans.developer.modules.loaders.clazz;
+
+import java.awt.Image;
+import java.awt.Toolkit;
+import java.awt.datatransfer.Transferable;
+import java.beans.BeanInfo;
+
+import com.netbeans.ide.util.datatransfer.TransferableOwner;
+
+/** Exetends ClassDataNode, overrides one method
+* This class is final only for performance reasons,
+* can be happily unfinaled if desired.
+*
+* @author Jaroslav Tulach, Dafe Simonek
+*/
+final class SerDataNode extends ClassDataNode {
+  /** generated Serialized Version UID */
+  static final long serialVersionUID = -2645179282674800246L;
+
+  private static final String SER_BASE =
+    "com/netbeans/developer/modules/resources/class/ser";
+  private static final String SER_MAIN_BASE =
+    "com/netbeans/developer/modules/resources/class/serMain";
+  private static final String SER_ERROR_BASE =
+    "com/netbeans/developer/modules/resources/class/serError";
+
+  /** Constructs bean data node with asociated data object.
+  */
+  public SerDataNode(final BeanDataObject obj) {
+    super(obj);
+  }
+
+// ----------------------------------------------------------------------------------
+// methods
+
+  /** Returns initial icon base string for ser node.
+  */
+  protected String initialIconBase () {
+    return SER_BASE;
+  }
+
+  /** Called when an object is to be copied to clipboard.
+  * @return the transferable object dedicated to represent the
+  *    content of clipboard
+  * @exception NodeAccessException is thrown when the
+  *    operation cannot be performed
+  */
+  public TransferableOwner clipboardCopy () throws NodeAccessException {
+    try {
+      return obj.new BeanTransferableOwner (
+        super.clipboardCopy (),
+        obj.getPrimaryFile ().getPackageName ('.'),
+        obj.getBeanClass ()
+      );
+    } catch (Exception e) {
+      throw new NodeAccessException ();
+    }
+  }
+
+  void resolveIcons () {
+    try {
+      ClassDataObject dataObj = (ClassDataObject)getDataObject();
+      dataObj.getBeanClass(); // check exception
+      if (dataObj.getHasMainMethod()) {
+        setIconBase(SER_MAIN_BASE);
+      } else {
+        setIconBase(SER_BASE);
+      }
+    } catch (IOException ex) {
+      setIconBase(SER_ERROR_BASE);
+    } catch (ClassNotFoundException ex) {
+      setIconBase(SER_ERROR_BASE);
+    }
+    iconResolved = true;
+  }
+
+}
+
+/*
+ * Log
+ *  1    src-jtulach1.0         1/15/99  David Simonek   
+ * $
+ */
---
+++ clazz/src/org/netbeans/modules/clazz/SerDataObject.java
@@ -0,0 +1,66 @@
+/*
+ *                 Sun Public License Notice
+ * 
+ * The contents of this file are subject to the Sun Public License
+ * Version 1.0 (the "License"). You may not use this file except in
+ * compliance with the License. A copy of the License is available at
+ * http://www.sun.com/
+ * 
+ * The Original Code is Forte for Java, Community Edition. The Initial
+ * Developer of the Original Code is Sun Microsystems, Inc. Portions
+ * Copyright 1997-2000 Sun Microsystems, Inc. All Rights Reserved.
+ */
+
+package com.netbeans.developer.modules.loaders.clazz;
+
+import com.netbeans.ide.filesystems.*;
+import com.netbeans.ide.util.datatransfer.TransferableOwner;
+import com.netbeans.ide.loaders.DataObjectExistsException;
+import com.netbeans.ide.loaders.MultiFileLoader;
+import com.netbeans.ide.nodes.Node;
+
+/** DataObject which represents JavaBeans (".ser" files).
+* This class is final only for performance reasons,
+* can be happily unfinaled if desired.
+*
+* @author Jan Jancura, Ian Formanek, Dafe Simonek
+*/
+public final class SerDataObject extends ClassDataObject {
+  /** generated Serialized Version UID */
+  static final long serialVersionUID = 8229229209013849842L;
+
+  /** Constructs a new BeanDataObject */
+  public SerDataObject(FileObject fo, ClassDataLoader loader) throws DataObjectExistsException {
+    super (fo, loader);
+  }
+
+  /** Getter for move action.
+  * @return true if the object can be moved
+  */
+  public boolean isMoveAllowed () {
+    return !getPrimaryFile ().isReadOnly ();
+  }
+
+  /** Getter for rename action.
+  * @return true if the object can be renamed
+  */
+  public boolean isRenameAllowed () {
+    return !getPrimaryFile ().isReadOnly ();
+  }
+
+  /** Creates another delegate.
+  */
+  protected Node createNodeDelegate () {
+    return new SerDataNode (this);
+  }
+
+}
+
+/*
+ * Log
+ *  1    src-jtulach1.0         1/15/99  David Simonek   
+ * $
+ * Beta Change History:
+ *  0    Tuborg    0.20        --/--/98 Jan Formanek    SWITCHED TO NODES
+ *  0    Tuborg    0.21        --/--/98 Jan Formanek    icons tweak
+ */
