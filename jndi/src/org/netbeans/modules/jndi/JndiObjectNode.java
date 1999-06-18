903e2eb6315f45bc5315954dc76b095bf9558b82
Ales_Novak <none@none>
1999-06-18 10:06:11 +0000

    #

---
+++ jndi/src/org/netbeans/modules/jndi/JndiObjectNode.java
@@ -0,0 +1,125 @@
+/*
+ *                 Sun Public License Notice
+ * 
+ * The contents of this file are subject to the Sun Public License
+ * Version 1.0 (the "License"). You may not use this file except in
+ * compliance with the License. A copy of the License is available at
+ * http://www.sun.com/
+ * 
+ * The Original Code is NetBeans. The Initial Developer of the Original
+ * Code is Sun Microsystems, Inc. Portions Copyright 1997-2001 Sun
+ * Microsystems, Inc. All Rights Reserved.
+ */
+
+package com.netbeans.enterprise.modules.jndi;
+
+import javax.naming.CompositeName;
+import javax.naming.NamingException;
+import java.awt.datatransfer.Transferable;
+import java.awt.datatransfer.StringSelection;
+import java.io.IOException;
+
+import org.openide.TopManager;
+import org.openide.nodes.Children;
+import org.openide.nodes.Node;
+import org.openide.nodes.AbstractNode;
+import org.openide.nodes.Node.Cookie;
+import org.openide.nodes.NodeAdapter;
+import org.openide.nodes.NodeMemberEvent;
+import org.openide.util.actions.SystemAction;
+
+/** Common base class for JndiNode and JndiLeafNode.
+* The class provides copy (source generating)/delete actions.
+*
+* @author Ales Novak
+*/
+abstract class JndiObjectNode extends AbstractNode implements Cookie, TemplateCreator {   
+
+  
+  /** Set to true only if this node is being destroyed.
+  * If a node with removed set to true is removed then its parent calls refresh.
+  */
+  private boolean removed = false;
+ 
+  /** My actions */
+  private SystemAction[] jndiactions;
+
+  /**
+  * @param children
+  * @param name
+  */
+  public JndiObjectNode(Children children, String name) {
+    super (children);
+    setName(name);
+    getCookieSet().add(this);
+    if (! children.equals(Children.LEAF)) {
+      addNodeListener(new Refresher());
+    }
+  }
+  
+  /** @return actions */
+  public final SystemAction[] getActions() {
+    if (jndiactions == null) {
+      jndiactions = createActions();
+    }
+    return jndiactions;
+  }
+  
+  /** @creates actions */
+  protected abstract SystemAction[] createActions();
+  
+  /** @return true */
+  public final boolean canCopy() {
+      return true;
+  }
+
+  /** @return @link isRoot */
+  public final boolean canDestroy() {
+    return true;
+  } 
+
+  
+  public abstract String createTemplate() throws NamingException;
+  
+  /** Inserts generated text into the clipboard */
+  public final Transferable clipboardCopy() throws IOException {
+    try {
+      return new StringSelection(createTemplate());
+    } catch (NamingException ne) {
+      TopManager.getDefault().notifyException(ne);
+      return null;
+    }
+  }
+  
+  /** Marks this node as destroyed. */
+  public final void setRemoved() {
+    removed = true;
+  }
+  
+  /** Does nothing. */
+  public void refresh() {
+    throw new UnsupportedOperationException();
+  }
+  
+  /** Listens for changes of its subnodes. Calls refresh then. */
+  class Refresher extends NodeAdapter {
+    public void childrenRemoved(NodeMemberEvent ev) {
+      Node[] delta = ev.getDelta();
+      
+      for (int i = 0; i < delta.length; i++) {
+        JndiObjectNode child = (JndiObjectNode) delta[i].getCookie(JndiObjectNode.class);
+        if (child != null) {
+          if (child.removed) {
+            refresh();
+          }
+        }
+      }
+    }
+  }
+}
+
+/*
+* <<Log>>
+*  1    Gandalf   1.0         6/18/99  Ales Novak      
+* $
+*/
