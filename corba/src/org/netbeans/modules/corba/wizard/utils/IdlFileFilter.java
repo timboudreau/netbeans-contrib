6d7e6a0f76191dc0b003bddcafd790fbc995a0e1
tzezula@netbeans.org <tzezula@netbeans.org>
2000-04-27 13:23:07 +0000

    Filter for IDL Files Selection

---
+++ corba/src/org/netbeans/modules/corba/wizard/utils/IdlFileFilter.java
@@ -0,0 +1,42 @@
+/*
+ *                 Sun Public License Notice
+ * 
+ * The contents of this file are subject to the Sun Public License
+ * Version 1.0 (the "License"). You may not use this file except in
+ * compliance with the License. A copy of the License is available at
+ * http://www.sun.com/
+ * 
+ * The Original Code is NetBeans. The Initial Developer of the Original
+ * Code is Sun Microsystems, Inc. Portions Copyright 1997-2001 Sun
+ * Microsystems, Inc. All Rights Reserved.
+ */
+
+package org.netbeans.modules.corba.wizard.utils;
+import java.io.File;
+import javax.swing.filechooser.FileFilter;
+import org.netbeans.modules.corba.wizard.CorbaWizardAction;
+
+/** 
+ *
+ * @author  tzezula
+ * @version 
+ */
+public class IdlFileFilter extends FileFilter {
+
+  /** Creates new IdlFileFilter */
+  public IdlFileFilter() {
+  }
+  
+  public boolean accept (File file) {
+    if (file.isDirectory())
+      return false;
+    if (!file.canRead())
+      return false;
+    return file.getName().endsWith(".idl");
+  }
+  
+  public String getDescription () {
+    return CorbaWizardAction.getLocalizedString("TXT_IdlFile");
+  }
+  
+}
