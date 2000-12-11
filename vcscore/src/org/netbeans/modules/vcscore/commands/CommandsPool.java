ad68b4f9af6e2f2ed91c3e570fda73ca4d88ef9f
mentlicher@netbeans.org <mentlicher@netbeans.org>
2000-12-11 15:29:05 +0000

    Create the runtime nodes, do the command preprocessing.

--- vcscore/src/org/netbeans/modules/vcscore/commands/CommandsPool.java
+++ vcscore/src/org/netbeans/modules/vcscore/commands/CommandsPool.java
@@ -17,11 +17,12 @@ import java.util.*;
 
 import org.openide.TopManager;
 import org.openide.nodes.Node;
-import org.openide.nodes.AbstractNode;
-import org.openide.nodes.Children;
+//import org.openide.nodes.AbstractNode;
+//import org.openide.nodes.Children;
 
 import org.netbeans.modules.vcscore.VcsFileSystem;
 import org.netbeans.modules.vcscore.util.Table;
+import org.netbeans.modules.vcscore.runtime.RuntimeSupport;
 
 /**
  * This class is used as a container of all external commands which are either running or finished.
@@ -29,7 +30,24 @@ import org.netbeans.modules.vcscore.util.Table;
  */
 public class CommandsPool extends Object /*implements CommandListener */{
 
-    private static final String VCS_RUNTIME_NODE_NAME = "VcsRuntime";
+    /**
+     * The preprocessing of the command was cancelled. The command will not be executed.
+     */
+    public static final int PREPROCESS_CANCELLED = 0;
+    
+    /**
+     * When there are more files selected, the preprocessing needs to be done for
+     * next files again. The command will run on the first file, preprocessing will be
+     * done for the rest.
+     */
+    public static final int PREPROCESS_NEXT_FILE = 1;
+    
+    /**
+     * The preprocessing is done. When more files are selected, the command
+     * will not be preprocessed for the rest of them.
+     */
+    public static final int PREPROCESS_DONE = 2;
+    
     /** Contains instances of VcsCommandExecutor, which are to be run.  */
     private ArrayList commandsToRun;
     /** Contains pairs of instances of VcsCommandExecutor and threads in which are running. */
@@ -54,6 +72,8 @@ public class CommandsPool extends Object /*implements CommandListener */{
     private ArrayList commandListeners = new ArrayList();
     
     private VcsFileSystem fileSystem;
+    
+    private Node runtimeNode;
 
     /** Creates new CommandsPool */
     public CommandsPool(VcsFileSystem fileSystem) {
@@ -64,9 +84,10 @@ public class CommandsPool extends Object /*implements CommandListener */{
         outputVisualizers = new Hashtable();
         group = new ThreadGroup("VCS Commands Goup");
         this.fileSystem = fileSystem;
-        initRuntime();
+        runtimeNode = RuntimeSupport.initRuntime(fileSystem.getDisplayName());
     }
     
+    /*
     private void initRuntime() {
         Node runtime = TopManager.getDefault().getPlaces().nodes().environment();
         Children runtimeCh = runtime.getChildren();
@@ -84,6 +105,7 @@ public class CommandsPool extends Object /*implements CommandListener */{
         ch.add(new Node[] { vcsRuntime });
         return vcsRuntime;
     }
+     */
 
     /**
      * Set the number of finished commands to collect.
@@ -118,15 +140,46 @@ public class CommandsPool extends Object /*implements CommandListener */{
      */
 
     /**
-     * Add a new command.
-     * @param cmd The command to add
+     * Perform preprocessing of a new command. It will perform any needed input
+     * and update the execution string.
+     * @param vce the command to preprocess
+     * @return the preprocessing status, one of CommandExecutorSupport.PROPEROCESS_* constants
      */
-    public synchronized void add(VcsCommandExecutor cmd) {
-        commandsToRun.add(cmd);
+    public int preprocessCommand(VcsCommandExecutor vce, Hashtable vars) {
+        return preprocessCommand(vce, vars, null);
+    }
+    
+    /**
+     * Perform preprocessing of a new command. It will perform any needed input
+     * and update the execution string.
+     * @param vce the command to preprocess
+     * @return the preprocessing status, one of CommandExecutorSupport.PROPEROCESS_* constants
+     */
+    public int preprocessCommand(VcsCommandExecutor vce, Hashtable vars, boolean[] askForEachFile) {
+        synchronized (commandsToRun) {
+            commandsToRun.add(vce);
+        }
+        VcsCommand cmd = vce.getCommand();
+        String name = cmd.getDisplayName();
+        if (name == null) name = cmd.getName();
+        String exec = vce.getExec();
+        fileSystem.debug(g("MSG_Command_preprocessing", name, exec));
+        RuntimeSupport.addWaiting(runtimeNode, vce);
+        int preprocessStatus = CommandExecutorSupport.preprocessCommand(fileSystem, vce, vars, askForEachFile);
+        if (PREPROCESS_CANCELLED == preprocessStatus) {
+            fileSystem.debug(g("MSG_Command_cancelled", name));
+        }
+        return preprocessStatus;
     }
     
     private void commandStarted(VcsCommandExecutor vce) {
-        TopManager.getDefault().setStatusText(g("MSG_Command_name_running", vce.getCommand().getDisplayName()));
+        VcsCommand cmd = vce.getCommand();
+        waitToRun(cmd, vce.getFiles());
+        String name = cmd.getDisplayName();
+        if (name == null) name = cmd.getName();
+        TopManager.getDefault().setStatusText(g("MSG_Command_name_running", name));
+        fileSystem.debug(g("MSG_Command_started", name, vce.getExec()));
+        RuntimeSupport.addRunning(runtimeNode, vce);
         //System.out.println("command "+vce.getCommand()+" STARTED.");
         synchronized (commandListeners) {
             for(Iterator it = commandListeners.iterator(); it.hasNext(); ) {
@@ -150,16 +203,21 @@ public class CommandsPool extends Object /*implements CommandListener */{
     
     private void commandDone(VcsCommandExecutor vce) {
         //System.out.println("command "+vce.getCommand()+" DONE.");
+        VcsCommand cmd = vce.getCommand();
+        String name = cmd.getDisplayName();
+        if (name == null) name = cmd.getName();
         synchronized (this) {
             commands.remove(vce);
             commandsFinished.add(vce);
             notifyAll();
         }
+        RuntimeSupport.addDone(runtimeNode, vce);
         synchronized (commandsFinished) {
             //commandsFinished.removeRange(0, commandsFinished.size() - collectFinishedCmdsNum);
             while (commandsFinished.size() > collectFinishedCmdsNum) {
                 VcsCommandExecutor removedExecutor = (VcsCommandExecutor) commandsFinished.remove(0);
                 outputContainers.remove(removedExecutor);
+                RuntimeSupport.removeDone(removedExecutor);
             }
         }
         if (!isCollectOutput()) {
@@ -172,7 +230,7 @@ public class CommandsPool extends Object /*implements CommandListener */{
         }
         //System.out.println("command "+vce.getCommand()+" DONE, LISTENERS DONE.");
         int exit = vce.getExitStatus();
-        String name = vce.getCommand().getDisplayName();
+        //String name = vce.getCommand().getDisplayName();
         String message = "";
         switch (exit) {
             case VcsCommandExecutor.SUCCEEDED:
@@ -187,6 +245,7 @@ public class CommandsPool extends Object /*implements CommandListener */{
                 break;
         }
         TopManager.getDefault().setStatusText(message);
+        fileSystem.debug(message);
         VcsCommandVisualizer visualizer = vce.getVisualizer();
         if (visualizer != null) {
             visualizer.setExitStatus(exit);
@@ -314,17 +373,34 @@ public class CommandsPool extends Object /*implements CommandListener */{
      * Wait before the command can be run if necessary.
      * Check its concurrent property and other running commands.
      */
-    public synchronized void waitToRun(VcsCommand cmd, String filePath) {
-        boolean haveToWait = false;
+    public synchronized void waitToRun(VcsCommand cmd, Collection files) {
+        int concurrency = VcsCommandIO.getIntegerPropertyAssumeZero(cmd,
+                            VcsCommand.PROPERTY_CONCURRENT_EXECUTION);
+        if (concurrency == VcsCommand.EXEC_CONCURRENT_ALL) return ;
+        String name = cmd.getName();
+        boolean haveToWait = true;
         do {
             for(Enumeration enum = commands.keys(); enum.hasMoreElements(); ) {
                 VcsCommandExecutor ec = (VcsCommandExecutor) enum.nextElement();
                 VcsCommand uc = ec.getCommand();
+                String cmdName = uc.getName();
+                if ((concurrency & VcsCommand.EXEC_SERIAL_OF_COMMAND) != 0) {
+                    if (name.equals(cmdName)) {
+                        try {
+                            wait();
+                        } catch (InterruptedException intrexc) {
+                            // silently ignored
+                        }
+                        continue;
+                    }
+                }
+                Collection cmdFiles = ec.getFiles();
             //int index = commands.indexOf(cmd);
             //if (index >= 0) {
             //    VcsCommandExecutor executor = (VcsCommandExecutor) commands.get(index);
                 
             }
+            haveToWait = false;
         } while (haveToWait);
         // TODO
     }
@@ -373,7 +449,7 @@ public class CommandsPool extends Object /*implements CommandListener */{
             addExecutorsOfCommand(executors, cmd);
             for (Iterator itExecutors = executors.iterator(); itExecutors.hasNext(); ) {
                 VcsCommandExecutor executor = (VcsCommandExecutor) itExecutors.next();
-                Set execFiles = executor.getFiles();
+                Collection execFiles = executor.getFiles();
                 for (Iterator itFiles = execFiles.iterator(); itFiles.hasNext(); ) {
                     String file = (String) itFiles.next();
                     if (files.contains(file)) {
@@ -622,6 +698,10 @@ public class CommandsPool extends Object /*implements CommandListener */{
     
     private String  g(String s, Object obj) {
         return java.text.MessageFormat.format (g(s), new Object[] { obj });
+    }
+
+    private String  g(String s, Object obj, Object obj2) {
+        return java.text.MessageFormat.format (g(s), new Object[] { obj, obj2 });
     }
 
 }
