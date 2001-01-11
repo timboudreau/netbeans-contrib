9c461444aa14c7b85c99f6f41e18d2de954381d5
kgardas@netbeans.org <kgardas@netbeans.org>
2001-01-11 13:08:02 +0000

    - fixed for I18N - fixed for message when Orbix3.2 compiler isn't presented in a PATH

--- corba/src/org/netbeans/modules/corba/idl/compiler/orbix32/Compiler.java
+++ corba/src/org/netbeans/modules/corba/idl/compiler/orbix32/Compiler.java
@@ -19,50 +19,61 @@ import java.io.BufferedReader;
 
 class Compiler {
 
-    public static final String _S_cmd = "idlj";
+    public static final String _S_cmd = "idlj"; // NOI18N
     
     public static void main (String[] __args) throws Exception {
+	//System.out.println ("OrbixWeb 3.2c IDL Compiler Wrapper 1.0");
 	String[] __cmd = new String [__args.length + 1];
 	__cmd[0] = _S_cmd;
+	int __exit_status = 0;
 	for (int __i = 0; __i < __args.length; __i++) {
 	    __cmd[__i + 1] = __args[__i];
 	}
 	//for (int __i = 0; __i < __cmd.length; __i++) {
-	//    System.out.println (__i + " : " + __cmd[__i]);  
+	//    System.out.println (__i + " : " + __cmd[__i]); // NOI18N
 	//}	
-	Process __proc = Runtime.getRuntime ().exec (__cmd);
-	//OutputStream __out = __proc.getOutputStream ();
-	InputStream __tmp_in = __proc.getInputStream ();
-	InputStream __tmp_err = __proc.getErrorStream ();
-	BufferedReader __in = new BufferedReader (new InputStreamReader (__tmp_in));
-	BufferedReader __err = new BufferedReader (new InputStreamReader (__tmp_err));
-	
-	String __file_line, __err_line, __file_name = "";
-	for (;;) {
-	    if ((__file_line = __in.readLine ()) != null) {
-		//System.out.println (__file_line);
-		__file_name = __file_line.substring (14, __file_line.length ());
-	    }
-	    else 
-		break;
+	try {
+	    Process __proc = Runtime.getRuntime ().exec (__cmd);
+	    //OutputStream __out = __proc.getOutputStream ();
+	    InputStream __tmp_in = __proc.getInputStream ();
+	    InputStream __tmp_err = __proc.getErrorStream ();
+	    BufferedReader __in = new BufferedReader (new InputStreamReader (__tmp_in));
+	    BufferedReader __err = new BufferedReader (new InputStreamReader (__tmp_err));
+	    
+	    String __file_line, __err_line, __file_name = ""; // NOI18N
 	    for (;;) {
-		__err_line = __err.readLine ();
-		if (__err_line != null && !__err_line.equals ("")) {
-		    //System.out.println (__err_line);
-		    System.out.println (__file_name + ":" + __err_line);
+		if ((__file_line = __in.readLine ()) != null) {
+		    //System.out.println (__file_line);
+		    __file_name = __file_line.substring (14, __file_line.length ());
 		}
-		else
+		else 
 		    break;
-		/*
-		  if (__err_line.equals ("")) {
-		  //System.out.println ("new line...");
-		  break;
-		  }
-		*/
+		for (;;) {
+		    __err_line = __err.readLine ();
+		    if (__err_line != null && !__err_line.equals ("")) { // NOI18N
+			//System.out.println (__err_line);
+			System.out.println (__file_name + ":" + __err_line); // NOI18N
+		    }
+		    else
+			break;
+		    /*
+		      if (__err_line.equals ("")) {
+		      //System.out.println ("new line...");
+		      break;
+		      }
+		    */
+		}
 	    }
+	    __proc.waitFor ();
+	} catch (SecurityException __ex) {
+	    __ex.printStackTrace ();
+	    __exit_status = 2;
+	} catch (java.io.IOException __ex) {
+	    __ex.printStackTrace ();
+	    __exit_status = 1;
 	}
-	__proc.waitFor ();
-	//Print
+	
+	System.exit (__exit_status);
     }
 
 }
