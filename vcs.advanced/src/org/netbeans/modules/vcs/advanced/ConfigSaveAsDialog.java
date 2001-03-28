f16552ad591a344252bc039ebd44455a762361b0
gtichy@netbeans.org <gtichy@netbeans.org>
2001-03-28 08:16:43 +0000

    UI changes - spacing, layout, mnemonics added.

--- vcs.advanced/src/org/netbeans/modules/vcs/advanced/ConfigSaveAsDialog.java
+++ vcs.advanced/src/org/netbeans/modules/vcs/advanced/ConfigSaveAsDialog.java
@@ -38,8 +38,10 @@ public class ConfigSaveAsDialog extends javax.swing.JDialog {
         this.dir = dir;
         setTitle(org.openide.util.NbBundle.getBundle(ConfigSaveAsDialog.class).getString("ConfigSaveAsDialog.title"));
         initComponents ();
+        fileNameLabel.setDisplayedMnemonic (org.openide.util.NbBundle.getBundle(ConfigSaveAsDialog.class).getString ("ConfigSaveAsDialog.fileNameLabel.mnemonic").charAt (0));
+        configLabelLabel.setDisplayedMnemonic (org.openide.util.NbBundle.getBundle(ConfigSaveAsDialog.class).getString ("ConfigSaveAsDialog.configLabelLabel.mnemonic").charAt (0));
         fillFileList();
-        pack ();
+        this.setSize(340, 265);
     }
 
     /** This method is called from within the constructor to
@@ -54,29 +56,30 @@ public class ConfigSaveAsDialog extends javax.swing.JDialog {
         jPanel2 = new javax.swing.JPanel();
         fileNameLabel = new javax.swing.JLabel();
         fileNameTextField = new javax.swing.JTextField();
-        saveButton = new javax.swing.JButton();
-        cancelButton = new javax.swing.JButton();
         configLabelLabel = new javax.swing.JLabel();
         configLabelTextField = new javax.swing.JTextField();
+        jPanel1 = new javax.swing.JPanel();
+        saveButton = new javax.swing.JButton();
+        cancelButton = new javax.swing.JButton();
+        
         getContentPane().setLayout(new java.awt.GridBagLayout());
         java.awt.GridBagConstraints gridBagConstraints1;
+        
         addWindowListener(new java.awt.event.WindowAdapter() {
             public void windowClosing(java.awt.event.WindowEvent evt) {
                 closeDialog(evt);
             }
-        }
-        );
+        });
         
         listPanel.setLayout(new java.awt.GridBagLayout());
         java.awt.GridBagConstraints gridBagConstraints2;
-        
         
         fileList.addMouseListener(new java.awt.event.MouseAdapter() {
             public void mouseClicked(java.awt.event.MouseEvent evt) {
                 fileListMouseClicked(evt);
             }
-        }
-        );
+        });
+        
         fileScrollPane.setViewportView(fileList);
         
         gridBagConstraints2 = new java.awt.GridBagConstraints();
@@ -85,81 +88,77 @@ public class ConfigSaveAsDialog extends javax.swing.JDialog {
         gridBagConstraints2.weighty = 1.0;
         listPanel.add(fileScrollPane, gridBagConstraints2);
         
-        
         gridBagConstraints1 = new java.awt.GridBagConstraints();
         gridBagConstraints1.fill = java.awt.GridBagConstraints.BOTH;
-        gridBagConstraints1.insets = new java.awt.Insets(8, 8, 8, 8);
+        gridBagConstraints1.insets = new java.awt.Insets(12, 12, 0, 11);
         gridBagConstraints1.weightx = 1.0;
         gridBagConstraints1.weighty = 1.0;
         getContentPane().add(listPanel, gridBagConstraints1);
-        
         
         jPanel2.setLayout(new java.awt.GridBagLayout());
         java.awt.GridBagConstraints gridBagConstraints3;
         
         fileNameLabel.setText(org.openide.util.NbBundle.getBundle(ConfigSaveAsDialog.class).getString("ConfigSaveAsDialog.fileNameLabel.text"));
+        fileNameLabel.setLabelFor(fileNameTextField);
         gridBagConstraints3 = new java.awt.GridBagConstraints();
-        gridBagConstraints3.insets = new java.awt.Insets(0, 0, 0, 8);
+        gridBagConstraints3.fill = java.awt.GridBagConstraints.HORIZONTAL;
+        gridBagConstraints3.insets = new java.awt.Insets(0, 0, 0, 12);
         jPanel2.add(fileNameLabel, gridBagConstraints3);
-        
         
         fileNameTextField.setColumns(10);
         gridBagConstraints3 = new java.awt.GridBagConstraints();
         gridBagConstraints3.fill = java.awt.GridBagConstraints.HORIZONTAL;
-        gridBagConstraints3.insets = new java.awt.Insets(0, 0, 0, 8);
         gridBagConstraints3.weightx = 1.0;
         jPanel2.add(fileNameTextField, gridBagConstraints3);
         
+        configLabelLabel.setText(org.openide.util.NbBundle.getBundle(ConfigSaveAsDialog.class).getString("ConfigSaveAsDialog.configLabelLabel.text"));
+        configLabelLabel.setLabelFor(configLabelTextField);
+        gridBagConstraints3 = new java.awt.GridBagConstraints();
+        gridBagConstraints3.gridx = 0;
+        gridBagConstraints3.fill = java.awt.GridBagConstraints.HORIZONTAL;
+        gridBagConstraints3.insets = new java.awt.Insets(6, 0, 0, 12);
+        gridBagConstraints3.anchor = java.awt.GridBagConstraints.WEST;
+        jPanel2.add(configLabelLabel, gridBagConstraints3);
+        
+        gridBagConstraints3 = new java.awt.GridBagConstraints();
+        gridBagConstraints3.gridx = 1;
+        gridBagConstraints3.fill = java.awt.GridBagConstraints.HORIZONTAL;
+        gridBagConstraints3.insets = new java.awt.Insets(6, 0, 0, 0);
+        jPanel2.add(configLabelTextField, gridBagConstraints3);
+        
+        gridBagConstraints1 = new java.awt.GridBagConstraints();
+        gridBagConstraints1.gridy = 1;
+        gridBagConstraints1.fill = java.awt.GridBagConstraints.HORIZONTAL;
+        gridBagConstraints1.insets = new java.awt.Insets(12, 12, 17, 11);
+        gridBagConstraints1.weightx = 1.0;
+        getContentPane().add(jPanel2, gridBagConstraints1);
+        
+        jPanel1.setLayout(new java.awt.GridLayout(1, 0, 5, 0));
         
         saveButton.setText(org.openide.util.NbBundle.getBundle(ConfigSaveAsDialog.class).getString("ConfigSaveAsDialog.saveButton.text"));
         saveButton.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
                 saveButtonActionPerformed(evt);
             }
-        }
-        );
-        gridBagConstraints3 = new java.awt.GridBagConstraints();
-        gridBagConstraints3.fill = java.awt.GridBagConstraints.HORIZONTAL;
-        jPanel2.add(saveButton, gridBagConstraints3);
+        });
         
+        jPanel1.add(saveButton);
         
         cancelButton.setText(org.openide.util.NbBundle.getBundle(ConfigSaveAsDialog.class).getString("ConfigSaveAsDialog.cancelButton.text"));
         cancelButton.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
                 cancelButtonActionPerformed(evt);
             }
-        }
-        );
-        gridBagConstraints3 = new java.awt.GridBagConstraints();
-        gridBagConstraints3.gridx = 2;
-        gridBagConstraints3.gridy = 1;
-        gridBagConstraints3.fill = java.awt.GridBagConstraints.HORIZONTAL;
-        gridBagConstraints3.insets = new java.awt.Insets(8, 0, 0, 0);
-        gridBagConstraints3.anchor = java.awt.GridBagConstraints.EAST;
-        jPanel2.add(cancelButton, gridBagConstraints3);
+        });
         
-        
-        configLabelLabel.setText(org.openide.util.NbBundle.getBundle(ConfigSaveAsDialog.class).getString("ConfigSaveAsDialog.configLabelLabel.text"));
-        gridBagConstraints3 = new java.awt.GridBagConstraints();
-        gridBagConstraints3.gridx = 0;
-        gridBagConstraints3.insets = new java.awt.Insets(0, 0, 0, 8);
-        gridBagConstraints3.anchor = java.awt.GridBagConstraints.WEST;
-        jPanel2.add(configLabelLabel, gridBagConstraints3);
-        
-        
-        gridBagConstraints3 = new java.awt.GridBagConstraints();
-        gridBagConstraints3.gridx = 1;
-        gridBagConstraints3.fill = java.awt.GridBagConstraints.HORIZONTAL;
-        gridBagConstraints3.insets = new java.awt.Insets(0, 0, 0, 8);
-        jPanel2.add(configLabelTextField, gridBagConstraints3);
-        
+        jPanel1.add(cancelButton);
         
         gridBagConstraints1 = new java.awt.GridBagConstraints();
-        gridBagConstraints1.gridy = 1;
-        gridBagConstraints1.fill = java.awt.GridBagConstraints.HORIZONTAL;
-        gridBagConstraints1.insets = new java.awt.Insets(8, 8, 8, 8);
-        gridBagConstraints1.weightx = 1.0;
-        getContentPane().add(jPanel2, gridBagConstraints1);
+        gridBagConstraints1.gridx = 0;
+        gridBagConstraints1.gridy = 2;
+        gridBagConstraints1.insets = new java.awt.Insets(0, 12, 11, 11);
+        gridBagConstraints1.anchor = java.awt.GridBagConstraints.EAST;
+        getContentPane().add(jPanel1, gridBagConstraints1);
         
     }//GEN-END:initComponents
 
@@ -238,10 +237,11 @@ public class ConfigSaveAsDialog extends javax.swing.JDialog {
     private javax.swing.JPanel jPanel2;
     private javax.swing.JLabel fileNameLabel;
     private javax.swing.JTextField fileNameTextField;
-    private javax.swing.JButton saveButton;
-    private javax.swing.JButton cancelButton;
     private javax.swing.JLabel configLabelLabel;
     private javax.swing.JTextField configLabelTextField;
+    private javax.swing.JPanel jPanel1;
+    private javax.swing.JButton saveButton;
+    private javax.swing.JButton cancelButton;
     // End of variables declaration//GEN-END:variables
 
 }
