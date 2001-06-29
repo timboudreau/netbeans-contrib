ddd18d6b0ea8799fd22be81e9f8142e6d89d7ef5
mentlicher@netbeans.org <mentlicher@netbeans.org>
2001-06-29 15:40:42 +0000

    The commented parts that came from it's predecessor deleted.

--- vcscore/src/org/netbeans/modules/vcscore/versioning/impl/NumDotRevisionItem.java
+++ vcscore/src/org/netbeans/modules/vcscore/versioning/impl/NumDotRevisionItem.java
@@ -23,138 +23,17 @@ import org.netbeans.modules.vcscore.versioning.RevisionItem;
  */
 public class NumDotRevisionItem extends RevisionItem {
 
-    /*
-    private String revision;
-    private String revisionVCS; // the original VCS revision. May differ from "revision".
-    private Vector branches;
-    private String displayName;
-    private String message;
-    private String date;
-    private String author;
-    private Vector tagNames;
-     */
     private NumDotRevisionItem next;
-    /*
-    private boolean current;
-    private Hashtable additionalProperties;
-     */
 
     /** Creates new RevisionItem */
     public NumDotRevisionItem(String revision) {
         super(revision);
         next = null;
-        /*
-        this.revision = revision;
-        this.revisionVCS = revision;
-        branches = null;
-        message = null;
-        date = null;
-        author = null;
-        tagNames = new Vector();
-        next = null;
-        current = false;
-        additionalProperties = new Hashtable();
-        displayName = revision;
-         */
     }
-
-    //public void setRevision(String revision) {
-    //    this.revision = revision;
-    //}
-
-    /** Get the revision of that item.
-     *
-    public String getRevision() {
-        return this.revision;
-    }
-    
-    /** Get the original VCS revision of that item.
-     * That may differ from getRevision(), due to the fact, that odd-dotted denote
-     * revisions and even-dotted denote branches.
-     *
-    public String getRevisionVCS() {
-        return this.revisionVCS;
-    }
-    
-    public void setRevisionVCS(String revisionVCS) {
-        this.revisionVCS = revisionVCS;
-    }
-
-    public boolean isRevision() {
-        return (branches == null);
-    }
-     */
 
     public boolean isBranch() {
         return (evenDots());
     }
-    
-    /*
-    public void setDisplayName(String displayName) {
-        this.displayName = displayName;
-    }
-    
-    public String getDisplayName() {
-        return this.displayName;
-    }
-    
-    public void setMessage(String message) {
-        this.message = message;
-    }
-    
-    public String getMessage() {
-        return this.message;
-    }
-    
-    public void setDate(String date) {
-        this.date = date;
-    }
-    
-    public String getDate() {
-        return this.date;
-    }
-    
-    public void setAuthor(String author) {
-        this.author = author;
-    }
-    
-    public String getAuthor() {
-        return this.author;
-    }
-
-    public void addTagName(String tagName) {
-        tagNames.add(tagName);
-        if (isBranch()) displayName = revision+" ("+tagName+")";
-    }
-
-    public boolean removeTagName(String tagName) {
-        return tagNames.remove(tagName);
-    }
-
-    public void setTagNames(String[] tagNames) {
-        this.tagNames = new Vector(Arrays.asList(tagNames));
-    }
-
-    public String[] getTagNames() {
-        return (String[]) tagNames.toArray(new String[0]);
-    }
-    
-    public void setCurrent(boolean current) {
-        this.current = current;
-    }
-    
-    public boolean isCurrent() {
-        return current;
-    }
-
-    public void addProperty(String name, String value) {
-        additionalProperties.put(name, value);
-    }
-    
-    public Hashtable getAdditionalProperties() {
-        return additionalProperties;
-    }
-     */
     
     public static int numDots(String str) {
         int num = 0;
@@ -167,22 +46,6 @@ public class NumDotRevisionItem extends RevisionItem {
     private boolean evenDots() {
         return (NumDotRevisionItem.numDots(getRevision()) % 2) == 0;
     }
-
-    /*
-    private int cmpRev_OLD(String revision) {
-        int lastDot1 = this.revision.lastIndexOf('.');
-        int lastDot2 = revision.lastIndexOf('.');
-        int rev1 = 0;
-        int rev2 = 0;
-        try {
-            rev1 = Integer.parseInt(this.revision.substring(lastDot1+1));
-            rev2 = Integer.parseInt(revision.substring(lastDot2+1));
-        } catch (NumberFormatException e) {
-            return -1000;
-        }
-        return rev1 - rev2;
-    }
-    */
 
     protected int cmpRev(String revision) {
         StringTokenizer tokens1 = new StringTokenizer(getRevision(), ".");
@@ -265,31 +128,4 @@ public class NumDotRevisionItem extends RevisionItem {
         this.next = next;
     }
     
-    /*
-    public void putToList(Collection list) {
-        if (!list.contains(this)) list.add(this);
-        if (next != null) next.putToList(list);
-        if (branches != null) {
-            for(Enumeration enum = branches.elements(); enum.hasMoreElements(); ) {
-                ((RevisionItem) enum.nextElement()).putToList(list);
-            }
-        }
-    }
-
-    public int hashCode() {
-        return revision.hashCode();
-    }
-    
-    public boolean equals(Object obj) {
-        if (obj instanceof RevisionItem)
-            return this.revision.equals(((RevisionItem) obj).getRevision());
-        else
-            return false;
-    }
-    
-    public int compareTo(final java.lang.Object p1) {
-        //System.out.println(getRevision()+".compareTo("+((RevisionItem) p1).getRevision()+") = "+this.cmpRev(((RevisionItem) p1).getRevision()));
-        return -this.cmpRev(((RevisionItem) p1).getRevision());
-    }
-     */
 }
