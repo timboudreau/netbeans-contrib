3788a4ee50285a6d55ea64f7cd71160affd1c307
mentlicher@netbeans.org <mentlicher@netbeans.org>
2001-08-01 15:59:12 +0000

    A special implementation of TemplateWizard copied from org.netbeans.core.ui.MountNode.TW This is necessary for Generic VCS and all other filesystems, that do not provide their own TemplateWizard.Iterator.

--- vcscore/src/org/netbeans/modules/vcscore/actions/VcsMountFromTemplateAction.java
+++ vcscore/src/org/netbeans/modules/vcscore/actions/VcsMountFromTemplateAction.java
@@ -15,6 +15,9 @@ package org.netbeans.modules.vcscore.actions;
 
 import java.awt.*;
 import java.awt.event.*;
+import java.beans.BeanInfo;
+import java.beans.Introspector;
+import java.beans.IntrospectionException;
 import java.io.IOException;
 import java.util.*;
 import java.lang.ref.*;
@@ -24,8 +27,12 @@ import javax.swing.event.*;
 
 import org.openide.*;
 import org.openide.awt.Actions;
+import org.openide.cookies.InstanceCookie;
+import org.openide.explorer.propertysheet.PropertySheet;
 import org.openide.explorer.view.MenuView;
 import org.openide.filesystems.FileObject;
+import org.openide.filesystems.FileSystem;
+import org.openide.filesystems.FileSystemCapability;
 import org.openide.loaders.*;
 import org.openide.nodes.*;
 import org.openide.util.Mutex;
@@ -110,7 +117,7 @@ public class VcsMountFromTemplateAction extends NodeAction {
     */
     static TemplateWizard getStandardWizard () {
         if (standardWizard == null) {
-            standardWizard = new TemplateWizard ();
+            standardWizard = new TW ();
             standardWizard.setTemplatesFolder(getVCSFolder());
             standardWizard.setTargetFolder(getTargetFolder());
         }
@@ -463,4 +470,186 @@ public class VcsMountFromTemplateAction extends NodeAction {
         }
     }
 
+    /** My special version of template wizard.
+    */
+    private static final class TW extends TemplateWizard 
+    implements WizardDescriptor.Panel {
+        /** the sheet component */
+        private PropertySheet sheet;
+        /** the node to display */
+        private Node node;
+        
+        /** Panel that is used to choose target package and
+         * name of the template.
+         */
+        public Panel targetChooser() {
+            return this;
+        }
+
+        /** Provides the wizard panel with the opportunity to update the
+         * settings with its current customized state.
+         * Rather than updating its settings with every change in the GUI, it should collect them,
+         * and then only save them when requested to by this method.
+         * Also, the original settings passed to {@link #readSettings} should not be modified (mutated);
+         * rather, the (copy) passed in here should be mutated according to the collected changes.
+         * This method can be called multiple times on one instance of <code>WizardDescriptor.Panel</code>.
+         * @param settings the object representing a settings of the wizard
+         */
+        public void storeSettings (Object settings) {
+        }
+        
+        /** Help for this panel.
+         * When the panel is active, this is used as the help for the wizard dialog.
+         * @return the help or <code>null</code> if no help is supplied
+         */
+        public HelpCtx getHelp () {
+            return HelpCtx.DEFAULT_HELP;
+        }
+        
+        /** Provides the wizard panel with the current data--either
+         * the default data or already-modified settings, if the user used the previous and/or next buttons.
+         * This method can be called multiple times on one instance of <code>WizardDescriptor.Panel</code>.
+         * @param settings the object representing wizard panel state, as originally supplied to {@link WizardDescriptor#WizardDescriptor(WizardDescriptor.Iterator,Object)}
+         * @exception IllegalStateException if the the data provided
+         * by the wizard are not valid.
+         */
+        public void readSettings (Object settings) {
+            try {
+                TemplateWizard wiz = (TemplateWizard)settings;
+                wiz.putProperty ("WizardPanel_contentSelectedIndex", new Integer (2)); // NOI18N
+
+                DataObject obj = wiz.getTemplate ();
+                InstanceCookie ic = (InstanceCookie)obj.getCookie (InstanceCookie.class);
+                Object instance = ic.instanceCreate ();
+
+                node = new BeanNode (instance);
+                
+                if (instance instanceof FileSystem) {
+                    node = new CapNode (node, (FileSystem)instance);
+                }
+            } catch (java.lang.Exception ex) {
+                TopManager.getDefault().getErrorManager ().notify (ex);
+            }
+        }
+        
+        /** Get the component displayed in this panel.
+         * @return the component
+         */
+        public java.awt.Component getComponent () {
+            if (node != null && node.hasCustomizer ()) {
+                return node.getCustomizer ();
+            }
+
+            if (sheet == null) {
+                sheet = new PropertySheet ();
+
+                sheet.setName (NbBundle.getMessage (VcsMountFromTemplateAction.class, "Edit_properties"));            
+            }
+            
+            if (node != null) {
+                sheet.setNodes (new Node[] { node });
+            }
+            
+            return sheet;
+        }
+        
+        /** Add a listener to changes of the panel's validity.
+         * @param l the listener to add
+         * @see #isValid
+         */
+        public void addChangeListener (ChangeListener l) {
+        }
+        
+        /** Remove a listener to changes of the panel's validity.
+         * @param l the listener to remove
+         */
+        public void removeChangeListener (ChangeListener l) {
+        }
+        
+    } // end of TW
+
+    /** A special filter node that adds Capabilities tab to regular
+    * instance node.
+    */
+    private static final class CapNode extends FilterNode {
+        private PropertySet[] sets;
+        private FileSystem fs;
+
+        public CapNode (Node filter, FileSystem fs) {
+            super (filter);
+            this.fs = fs;
+        }
+
+        public CapNode (Node filter, FileSystem fs, org.openide.nodes.Children ch) {
+            super (filter, ch);
+            this.fs = fs;
+        }
+
+        public Node getOrig () {
+            return getOriginal ();
+        }
+
+        protected NodeListener createNodeListener () {
+            return new NodeAdapter (this) {
+                protected void propertyChange (FilterNode fn, java.beans.PropertyChangeEvent ev) {
+                    CapNode fs = (CapNode)fn;
+                    if (PROP_PROPERTY_SETS.equals (ev.getPropertyName ())) {
+                        fs.sets = null;
+                    }
+                    super.propertyChange (fn, ev);
+                }
+            };
+        }
+
+        public PropertySet[] getPropertySets () {
+            Node.PropertySet[] s = sets;
+            Node.PropertySet[] sup = super.getPropertySets ();
+
+            if (s != null && sup.length + 1 == s.length) {
+                return s;
+            }
+
+            // add the capabilities tab
+            s = sup;
+
+            FileSystemCapability cap = ((FileSystem)fs).getCapability ();
+            try {
+                if (cap != null) {
+                    BeanInfo bi = Introspector.getBeanInfo (cap.getClass (), FileSystemCapability.class);
+                    BeanNode.Descriptor d = BeanNode.computeProperties (cap, bi);
+
+                    Sheet.Set ss = new Sheet.Set ();
+                    ss.setName ("Capabilities"); // NOI18N
+                    ss.setDisplayName (NbBundle.getMessage (VcsMountFromTemplateAction.class, "PROP_Capabilities"));
+                    ss.setShortDescription (NbBundle.getMessage (VcsMountFromTemplateAction.class, "HINT_Capabilities"));
+
+                    ss.put (d.property);
+                    ss.put (d.expert);
+
+                    PropertySet[] arr = new PropertySet[s.length + 1];
+                    int from = 0;
+                    boolean placed = false;
+                    for (int i = 0; i < arr.length; i++) {
+                        if (!placed) {
+                            boolean ok = i == s.length;
+                            if (!ok) {
+                                String n = s[from].getName ();
+                                ok = n.equals ("files") || n.equals (DataFolder.SET_SORTING); // NOI18N
+                            }
+
+                            if (ok) {
+                                placed = true;
+                                arr[i] = ss;
+                                continue;
+                            }
+                        }
+                        arr[i] = s[from++];
+                    }
+                    sets = s = arr;
+                }
+            } catch (IntrospectionException e) {
+            }
+            return s;
+        }
+    }
 }
