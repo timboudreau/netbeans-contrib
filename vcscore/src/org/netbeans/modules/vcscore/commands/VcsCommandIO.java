8f17478f05b27f9f100dca8540033468f02a6fd2
mentlicher@netbeans.org <mentlicher@netbeans.org>
2001-08-01 17:27:22 +0000

    Some more default property values defined: PROPERTY_RUN_ON_MULTIPLE_FILES_IN_FOLDER PROPERTY_NEEDS_HIERARCHICAL_ORDER PROPERTY_CHECK_FOR_MODIFICATIONS PROPERTY_DISTINGUISH_BINARY_FILES PROPERTY_SUPPORTS_ADVANCED_MODE This is a fix of issue #13020.

--- vcscore/src/org/netbeans/modules/vcscore/commands/VcsCommandIO.java
+++ vcscore/src/org/netbeans/modules/vcscore/commands/VcsCommandIO.java
@@ -36,19 +36,24 @@ public class VcsCommandIO extends Object {
     private static void initDefaultPropertyValues() {
         defaultPropertyValues = new HashMap();
         defaultPropertyValues.put(VcsCommand.PROPERTY_CONCURRENT_EXECUTION, new Integer(VcsCommand.EXEC_CONCURRENT_ALL));
-        defaultPropertyValues.put(VcsCommand.PROPERTY_ON_FILE, new Boolean(true));
-        defaultPropertyValues.put(VcsCommand.PROPERTY_ON_DIR, new Boolean(true));
-        defaultPropertyValues.put(VcsCommand.PROPERTY_ON_ROOT, new Boolean(true));
-        defaultPropertyValues.put(VcsCommand.PROPERTY_REFRESH_CURRENT_FOLDER, new Boolean(false));
-        defaultPropertyValues.put(VcsCommand.PROPERTY_REFRESH_PARENT_FOLDER, new Boolean(false));
-        defaultPropertyValues.put(VcsCommand.PROPERTY_DISPLAY_PLAIN_OUTPUT, new Boolean(false));
-        defaultPropertyValues.put(VcsCommand.PROPERTY_PROCESS_ALL_FILES, new Boolean(false));
-        defaultPropertyValues.put(VcsCommand.PROPERTY_RUN_ON_MULTIPLE_FILES, new Boolean(false));
-        defaultPropertyValues.put(VcsCommand.PROPERTY_IGNORE_FAIL, new Boolean(false));
+        defaultPropertyValues.put(VcsCommand.PROPERTY_ON_FILE, Boolean.TRUE);
+        defaultPropertyValues.put(VcsCommand.PROPERTY_ON_DIR, Boolean.TRUE);
+        defaultPropertyValues.put(VcsCommand.PROPERTY_ON_ROOT, Boolean.TRUE);
+        defaultPropertyValues.put(VcsCommand.PROPERTY_REFRESH_CURRENT_FOLDER, Boolean.FALSE);
+        defaultPropertyValues.put(VcsCommand.PROPERTY_REFRESH_PARENT_FOLDER, Boolean.FALSE);
+        defaultPropertyValues.put(VcsCommand.PROPERTY_DISPLAY_PLAIN_OUTPUT, Boolean.FALSE);
+        defaultPropertyValues.put(VcsCommand.PROPERTY_PROCESS_ALL_FILES, Boolean.FALSE);
+        defaultPropertyValues.put(VcsCommand.PROPERTY_RUN_ON_MULTIPLE_FILES, Boolean.FALSE);
+        defaultPropertyValues.put(VcsCommand.PROPERTY_RUN_ON_MULTIPLE_FILES_IN_FOLDER, Boolean.FALSE);
+        defaultPropertyValues.put(VcsCommand.PROPERTY_NEEDS_HIERARCHICAL_ORDER, Boolean.FALSE);
+        defaultPropertyValues.put(VcsCommand.PROPERTY_CHECK_FOR_MODIFICATIONS, Boolean.FALSE);
+        defaultPropertyValues.put(VcsCommand.PROPERTY_IGNORE_FAIL, Boolean.FALSE);
         defaultPropertyValues.put(VcsCommand.PROPERTY_NUM_REVISIONS, new Integer(0));
-        defaultPropertyValues.put(VcsCommand.PROPERTY_CHANGING_NUM_REVISIONS, new Boolean(false));
-        defaultPropertyValues.put(VcsCommand.PROPERTY_CHANGING_REVISION, new Boolean(false));
-        defaultPropertyValues.put(VcsCommand.PROPERTY_HIDDEN, new Boolean(false));
+        defaultPropertyValues.put(VcsCommand.PROPERTY_CHANGING_NUM_REVISIONS, Boolean.FALSE);
+        defaultPropertyValues.put(VcsCommand.PROPERTY_CHANGING_REVISION, Boolean.FALSE);
+        defaultPropertyValues.put(VcsCommand.PROPERTY_DISTINGUISH_BINARY_FILES, Boolean.FALSE);
+        defaultPropertyValues.put(VcsCommand.PROPERTY_SUPPORTS_ADVANCED_MODE, Boolean.FALSE);
+        defaultPropertyValues.put(VcsCommand.PROPERTY_HIDDEN, Boolean.FALSE);
     }
     
     /**
