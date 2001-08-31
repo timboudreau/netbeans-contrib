a2b32f932cf0bcd6606c0f0f3d6a6771df076268
pnejedly@netbeans.org <pnejedly@netbeans.org>
2001-08-31 11:56:32 +0000

    Part of fix for #14943 - removed static data and initializers

--- clazz/src/org/netbeans/modules/clazz/ClassDataLoaderBeanInfo.java
+++ clazz/src/org/netbeans/modules/clazz/ClassDataLoaderBeanInfo.java
@@ -18,7 +18,9 @@ import java.awt.Image;
 import java.util.ResourceBundle;
 
 import org.openide.loaders.MultiFileLoader;
+import org.openide.TopManager;
 import org.openide.util.NbBundle;
+import org.openide.util.Utilities;
 
 
 /** BeanInfo for class loader.
@@ -31,18 +33,11 @@ public final class ClassDataLoaderBeanInfo extends SimpleBeanInfo {
         try {
             return new BeanInfo[] { Introspector.getBeanInfo (MultiFileLoader.class) };
         } catch (IntrospectionException ie) {
-            if (Boolean.getBoolean ("netbeans.debug.exceptions")) // NOI18N
-                ie.printStackTrace ();
+	    TopManager.getDefault().getErrorManager().notify(ie);
             return null;
         }
     }
 
-    /** Icons for compiler settings objects. */
-    static Image icon;
-    static Image icon32;
-
-    /** Propertydescriptors */
-    static PropertyDescriptor[] descriptors;
 
 
     /**
@@ -50,54 +45,30 @@ public final class ClassDataLoaderBeanInfo extends SimpleBeanInfo {
     * describing the editable properties supported by this bean.
     */
     public PropertyDescriptor[] getPropertyDescriptors () {
-        if (descriptors == null) initializeDescriptors();
-        return descriptors;
+	final ResourceBundle bundle =
+            NbBundle.getBundle(ClassDataLoaderBeanInfo.class);
+        try {
+	    PropertyDescriptor[] descriptors =  new PropertyDescriptor[] {
+                               new PropertyDescriptor ("extensions", ClassDataLoader.class, // NOI18N
+                                                       "getExtensions", null) // NOI18N
+                           };
+            descriptors[0].setDisplayName(bundle.getString("PROP_Extensions"));
+            descriptors[0].setShortDescription(bundle.getString("HINT_Extensions"));
+    	    return descriptors;
+        } catch (IntrospectionException e) {
+	    TopManager.getDefault().getErrorManager().notify(e);
+	    return null;
+        }
     }
 
     /** @param type Desired type of the icon
     * @return returns the Txt loader's icon
     */
     public Image getIcon(final int type) {
-        if ((type == java.beans.BeanInfo.ICON_COLOR_16x16) ||
-                (type == java.beans.BeanInfo.ICON_MONO_16x16)) {
-            if (icon == null)
-                icon = loadImage("/org/netbeans/modules/clazz/resources/class.gif"); // NOI18N
-            return icon;
+        if ((type == BeanInfo.ICON_COLOR_16x16) || (type == BeanInfo.ICON_MONO_16x16)) {
+	    return Utilities.loadImage("org/netbeans/modules/clazz/resources/class.gif"); // NOI18N
         } else {
-            if (icon32 == null)
-                icon32 = loadImage ("/org/netbeans/modules/clazz/resources/class32.gif"); // NOI18N
-            return icon32;
+            return Utilities.loadImage ("org/netbeans/modules/clazz/resources/class32.gif"); // NOI18N
         }
     }
-
-    static void initializeDescriptors () {
-        final ResourceBundle bundle =
-            NbBundle.getBundle(ClassDataLoaderBeanInfo.class);
-        try {
-            descriptors =  new PropertyDescriptor[] {
-                               new PropertyDescriptor ("extensions", ClassDataLoader.class, // NOI18N
-                                                       "getExtensions", null) // NOI18N
-                           };
-            descriptors[0].setDisplayName(bundle.getString("PROP_Extensions"));
-            descriptors[0].setShortDescription(bundle.getString("HINT_Extensions"));
-        } catch (IntrospectionException e) {
-            e.printStackTrace ();
-        }
-    }
-
 }
-
-/*
-* Log
-*  7    src-jtulach1.6         1/16/00  Jesse Glick     
-*  6    src-jtulach1.5         1/13/00  David Simonek   i18n
-*  5    src-jtulach1.4         10/23/99 Ian Formanek    NO SEMANTIC CHANGE - Sun 
-*       Microsystems Copyright in File Comment
-*  4    src-jtulach1.3         6/9/99   Ian Formanek    ---- Package Change To 
-*       org.openide ----
-*  3    src-jtulach1.2         3/22/99  Ian Formanek    Icons location fixed
-*  2    src-jtulach1.1         3/22/99  Ian Formanek    Icons moved from 
-*       modules/resources to this package
-*  1    src-jtulach1.0         2/16/99  David Simonek   
-* $
-*/
