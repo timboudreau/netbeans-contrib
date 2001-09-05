474f7b5c73a1275ab7ce4f243bac516663455212
mentlicher@netbeans.org <mentlicher@netbeans.org>
2001-09-05 10:27:59 +0000

    Regular expression match and unmatch can be set as a validator.

--- vcscore/src/org/netbeans/modules/vcscore/util/VariableInputValidator.java
+++ vcscore/src/org/netbeans/modules/vcscore/util/VariableInputValidator.java
@@ -27,6 +27,8 @@ public class VariableInputValidator extends Object {
 
     public static final String VALIDATOR = "VALIDATOR_";
     public static final String VALIDATOR_NON_EMPTY = VALIDATOR + "NON_EMPTY";
+    public static final String VALIDATOR_REGEXP_MATCH = VALIDATOR + "REGEXP_MATCH(";
+    public static final String VALIDATOR_REGEXP_UNMATCH = VALIDATOR + "REGEXP_UNMATCH(";
 
     private boolean valid;
     private String message = null;
@@ -40,6 +42,10 @@ public class VariableInputValidator extends Object {
         else {
             if (VALIDATOR_NON_EMPTY.equals(validator)) {
                 validateNonEmpty(component);
+            } else if (validator.startsWith(VALIDATOR_REGEXP_MATCH)) {
+                validateRegExpMatch(component, validator, true);
+            } else if (validator.startsWith(VALIDATOR_REGEXP_UNMATCH)) {
+                validateRegExpMatch(component, validator, false);
             } else {
                 valid = false;
                 message = g("VariableInputValidator.BadValidator", validator);
@@ -56,6 +62,44 @@ public class VariableInputValidator extends Object {
         } else {
             valid = true;
         }
+    }
+    
+    private void validateRegExpMatch(VariableInputComponent component, String validator, boolean match) {
+        int index = validator.lastIndexOf(')');
+        if (index < 0) index = validator.length();
+        String regExp;
+        if (match) {
+            regExp = validator.substring(VALIDATOR_REGEXP_MATCH.length(), index);
+        } else {
+            regExp = validator.substring(VALIDATOR_REGEXP_UNMATCH.length(), index);
+        }
+        valid = validateRegExpMatch(component, regExp);
+        if (message != null) valid = false;
+        else if (valid != match) {
+            if (match) {
+                message = g("VariableInputValidator.RegExpNotMatched", regExp, component.getLabel());
+            } else {
+                message = g("VariableInputValidator.RegExpNotUnmatched", regExp, component.getLabel());
+            }
+            variable = component.getVariable();
+            valid = false;
+        } else {
+            valid = true;
+        }
+    }
+    
+    private boolean validateRegExpMatch(VariableInputComponent component, String regExpStr) {
+        org.apache.regexp.RE regExp;
+        try {
+            regExp = new org.apache.regexp.RE(regExpStr);
+        } catch (org.apache.regexp.RESyntaxException exc) {
+            message = g("VariableInputValidator.BadRegExp", regExpStr, component.getLabel(), exc.getLocalizedMessage());
+            variable = component.getVariable();
+            return false;
+        }
+        String value = component.getValue();
+        if (value == null) value = "";
+        return regExp.match(value);
     }
 
     /** Tells whether the validation was successfull.
@@ -84,6 +128,20 @@ public class VariableInputValidator extends Object {
         return MessageFormat.format(
                 NbBundle.getBundle(VariableInputValidator.class).getString(pattern),
                 new Object[] { obj }
+            );
+    }
+
+    private static String g(String pattern, Object obj, Object obj2) {
+        return MessageFormat.format(
+                NbBundle.getBundle(VariableInputValidator.class).getString(pattern),
+                new Object[] { obj, obj2 }
+            );
+    }
+
+    private static String g(String pattern, Object obj, Object obj2, Object obj3) {
+        return MessageFormat.format(
+                NbBundle.getBundle(VariableInputValidator.class).getString(pattern),
+                new Object[] { obj, obj2, obj3 }
             );
     }
 }
