c1e5dcb50e4b4b2cbd2aab62fd9161c671b06351
mkleint@netbeans.org <mkleint@netbeans.org>
2001-09-13 15:11:03 +0000

    another virtualsdataobject tuneup.. 1. always display extension in the node. 2. refreshing issues in javacvs filesystem.

--- vcscore/src/org/netbeans/modules/vcscore/util/virtuals/VirtualsDataObject.java
+++ vcscore/src/org/netbeans/modules/vcscore/util/virtuals/VirtualsDataObject.java
@@ -47,9 +47,47 @@ public class VirtualsDataObject extends MultiDataObject {
     * @return the node representation for this data object
     */
     protected Node createNodeDelegate () {
-        DataNode node = new DataNode (this, Children.LEAF);
+        DataNode node = new VirtualsDataNode (this, Children.LEAF);
         return node;
     }
     
+    public class VirtualsDataNode extends DataNode {
+        
+        public VirtualsDataNode(MultiDataObject obj, Children childs) {
+            super(obj, childs);
+            //            setName(obj.getPrimaryFile().getNameExt());
+            //            setIconBase(
+            updateDisplayName();
+        }
+        
+        /** Changes the name of the node and may also rename the data object.
+         * If the object is renamed and file extensions are to be shown,
+         * the display name is also updated accordingly.
+         *
+         * @param name new name for the object
+         * @param rename rename the data object?
+         * @exception IllegalArgumentException if the rename failed
+         */
+        public void setName(String name, boolean rename) {
+            super.setName(name, rename);
+            if (rename) updateDisplayName();
+        }
+        
+        private void updateDisplayName() {
+            FileObject prim = getDataObject().getPrimaryFile();
+            String newDisplayName = null;
+            
+            
+            String ext = prim.getExt();
+            newDisplayName = ext == null || ext.equals("") ? // NOI18N
+                             prim.getName() : prim.getName() + '.' + ext; // NOI18N
+            if (displayFormat != null)
+                setDisplayName(displayFormat.format(new Object[] { newDisplayName }));
+            else
+                setDisplayName(newDisplayName);
+        }
+        
+        
+    }
     
 }
