48102cc1cfc7bcd187ae683269baeeb3af0e2cc9
mentlicher@netbeans.org <mentlicher@netbeans.org>
2001-10-24 17:33:47 +0000

    Refresh Even If Failed property added. This is a fix of issue #11587.

--- vcs.advanced/src/org/netbeans/modules/vcs/advanced/commands/CommandNode.java
+++ vcs.advanced/src/org/netbeans/modules/vcs/advanced/commands/CommandNode.java
@@ -84,6 +84,7 @@ public class CommandNode extends AbstractNode {
         stdandard_propertyClassTypes.put(UserCommand.PROPERTY_REFRESH_PROCESSED_FILES, Boolean.TYPE);
         stdandard_propertyClassTypes.put(VcsCommand.PROPERTY_REFRESH_CURRENT_FOLDER, Boolean.TYPE);
         stdandard_propertyClassTypes.put(VcsCommand.PROPERTY_REFRESH_PARENT_FOLDER, Boolean.TYPE);
+        stdandard_propertyClassTypes.put(VcsCommand.PROPERTY_REFRESH_ON_FAIL, Integer.TYPE);
         stdandard_propertyClassTypes.put(VcsCommand.PROPERTY_DISPLAY_PLAIN_OUTPUT, Boolean.TYPE);
         stdandard_propertyClassTypes.put(VcsCommand.PROPERTY_DISTINGUISH_BINARY_FILES, Boolean.TYPE);
         expert_propertyClassTypes.put(VcsCommand.PROPERTY_ADVANCED_NAME, String.class);
