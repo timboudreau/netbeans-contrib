3c4e8fe52f1b8f5a82076d3d7f16779640512541
mentlicher@netbeans.org <mentlicher@netbeans.org>
2001-12-07 13:06:28 +0000

    Speeding up the annotateName method. Reorder the FileObjects iff necessary. This is a fix of issue #18446.

--- vcscore/src/org/netbeans/modules/vcscore/VcsFileSystem.java
+++ vcscore/src/org/netbeans/modules/vcscore/VcsFileSystem.java
@@ -2310,9 +2310,7 @@ public abstract class VcsFileSystem extends AbstractFileSystem implements Variab
         }
 
         if (statusProvider != null) {
-            Object[] oo = VcsUtilities.reorderFileObjects(files).toArray();
-            //System.out.println(" file objects reordered = "+oo.length);
-            ArrayList importantFiles = getImportantFiles(oo);
+            ArrayList importantFiles = getImportantFiles(files.toArray());
             len = importantFiles.size();
             //if (print) System.out.println(" length of important = "+len);
             if (len == 1) {
@@ -2320,6 +2318,7 @@ public abstract class VcsFileSystem extends AbstractFileSystem implements Variab
                 //System.out.println(" fullName = "+fullName);
                 result = RefreshCommandSupport.getStatusAnnotation(name, fullName, annotationPattern, statusProvider);
             } else {
+                importantFiles = VcsUtilities.reorderFileObjects(importantFiles);
                 //if (print) System.out.println(" importantFiles = "+VcsUtilities.arrayToString((String[]) importantFiles.toArray(new String[0])));
                 result = RefreshCommandSupport.getStatusAnnotation(name, importantFiles, annotationPattern, statusProvider, multiFilesAnnotationTypes);
             }
