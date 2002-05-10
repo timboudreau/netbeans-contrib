d729cc6a4a24069f33f031167c7de4adf58364b8
mentlicher@netbeans.org <mentlicher@netbeans.org>
2002-05-10 18:31:29 +0000

    Selector for the PVCS database added. This change was made in connection with issue #22503.

---
+++ vcs.profiles.pvcs/src/org/netbeans/modules/vcs/profiles/pvcs/commands/PvcsProjectDatabaseSelector.java
@@ -0,0 +1,71 @@
+/*
+ *                 Sun Public License Notice
+ * 
+ * The contents of this file are subject to the Sun Public License
+ * Version 1.0 (the "License"). You may not use this file except in
+ * compliance with the License. A copy of the License is available at
+ * http://www.sun.com/
+ * 
+ * The Original Code is NetBeans. The Initial Developer of the Original
+ * Code is Sun Microsystems, Inc. Portions Copyright 1997-2002 Sun
+ * Microsystems, Inc. All Rights Reserved.
+ */
+
+package org.netbeans.modules.vcs.profiles.pvcs.commands;
+
+import java.util.Hashtable;
+
+import org.openide.DialogDescriptor;
+import org.openide.NotifyDescriptor;
+import org.openide.TopManager;
+
+import org.netbeans.modules.vcscore.VcsFileSystem;
+import org.netbeans.modules.vcscore.commands.*;
+import org.netbeans.modules.vcscore.cmdline.VcsAdditionalCommand;
+
+/**
+ * A selector of a PVCS project database.
+ *
+ * @author  Martin Entlicher
+ */
+public class PvcsProjectDatabaseSelector extends Object implements VcsAdditionalCommand {
+    
+    private VcsFileSystem fileSystem;
+    
+    /** Creates a new instance of PvcsProjectDatabaseSelector */
+    public PvcsProjectDatabaseSelector() {
+    }
+    
+    public void setFileSystem(VcsFileSystem fileSystem) {
+        this.fileSystem = fileSystem;
+    }
+    
+    /** This method is used to execute the command.
+     * @param vars the variables that can be passed to the command
+     * @param args the command line parametres passed to it in properties
+     * @param stdoutNRListener listener of the standard output of the command
+     * @param stderrNRListener listener of the error output of the command
+     * @param stdoutListener listener of the standard output of the command which
+     *                       satisfies regex <CODE>dataRegex</CODE>
+     * @param dataRegex the regular expression for parsing the standard output
+     * @param stderrListener listener of the error output of the command which
+     *                       satisfies regex <CODE>errorRegex</CODE>
+     * @param errorRegex the regular expression for parsing the error output
+     * @return true if the command was succesfull
+     *         false if some error occured.
+     */
+    public boolean exec(Hashtable vars, String[] args, CommandOutputListener stdoutNRListener,
+                        CommandOutputListener stderrNRListener,
+                        CommandDataOutputListener stdoutListener, String dataRegex,
+                        CommandDataOutputListener stderrListener, String errorRegex) {
+        PvcsDatabaseSelectorPanel panel = new PvcsDatabaseSelectorPanel(fileSystem, args, (String) vars.get("PROJECT_DB"));
+        if (NotifyDescriptor.OK_OPTION.equals(TopManager.getDefault().notify(new DialogDescriptor(panel, panel.getName())))) {
+            String dbLocation = panel.getSelectedDatabase();
+            if (dbLocation != null) {
+                stdoutListener.outputData(new String[] { dbLocation });
+            }
+        }
+        return true;
+    }
+    
+}
