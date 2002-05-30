b0fe7138a35724b1f6018db01160cd82786f077d
mentlicher@netbeans.org <mentlicher@netbeans.org>
2002-05-30 15:14:40 +0000

    Properties and expert help IDs added to the FS bean descriptor. This is a fix of issue #22190

--- vcs.advanced/src/org/netbeans/modules/vcs/advanced/CommandLineVcsFileSystemBeanInfo.java
+++ vcs.advanced/src/org/netbeans/modules/vcs/advanced/CommandLineVcsFileSystemBeanInfo.java
@@ -200,7 +200,8 @@ public class CommandLineVcsFileSystemBeanInfo extends SimpleBeanInfo {
         bd.setValue(VcsFileSystem.VCS_PROVIDER_ATTRIBUTE, new Boolean(true));
         bd.setValue(VcsFileSystem.VCS_FILESYSTEM_ICON_BASE, "org/netbeans/modules/vcs/advanced/vcsGeneric"); // NOI18N
         bd.setValue ("helpID", CommandLineVcsFileSystem.class.getName ()); // NOI18N
-        
+        bd.setValue ("propertiesHelpID", CommandLineVcsFileSystem.class.getName() + "_properties"); // NOI18N
+        bd.setValue ("expertHelpID", CommandLineVcsFileSystem.class.getName() + "_expert"); // NOI18N
         return bd;
     }
 
