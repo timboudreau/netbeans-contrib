cfc8c3a607f2ccaac341171d993e5777c2d0be83
tor@netbeans.org <tor@netbeans.org>
2002-12-14 22:59:16 +0000

    Back out version 1.2 of SourceCodeCommentParser since it seems to rely on JDK1.4 APIs.

--- tasklist.docscan/src/org/netbeans/modules/tasklist/docscan/SourceCodeCommentParser.java
+++ tasklist.docscan/src/org/netbeans/modules/tasklist/docscan/SourceCodeCommentParser.java
@@ -7,16 +7,12 @@
  * http://www.sun.com/
  *
  * The Original Code is NetBeans. The Initial Developer of the Original
- * Code is Sun Microsystems, Inc. Portions Copyright 1997-2002 Sun
+ * Code is Sun Microsystems, Inc. Portions Copyright 1997-2000 Sun
  * Microsystems, Inc. All Rights Reserved.
  */
 
 // @todo The current version does not handle comment tokens inside "" or ''
-//       correct! (remember that such a section may span multiple lines!!!
-// @todo When we drop support for older java vm's (<1.4) I would really like
-//       to modify this class to return a StringBuffer to the SourceScanner,
-//       since the regexp-support in java 1.4 can work directly on a
-//       StringBuffer
+//       correct! (remember that such a section may span multiple lines!!!)
 
 package org.netbeans.modules.tasklist.docscan;
 
@@ -61,7 +57,7 @@ public class SourceCodeCommentParser {
     public SourceCodeCommentParser(String blockStart, String blockEnd) {
         parser = new CommentParser(null, blockStart, blockEnd);
     }
-    
+
     /**
      * Create a new instance of SourceCodeCommentParser that supports single-
      * line comments, and multiline comments
@@ -69,9 +65,9 @@ public class SourceCodeCommentParser {
      * @param blockStart the start tag of a block comment
      * @param blockEnd the end tag of a block comment
      */
-    public SourceCodeCommentParser(String lineComment,
-                                   String blockStart,
-                                   String blockEnd) {
+    public SourceCodeCommentParser(String lineComment, 
+                            String blockStart, 
+                            String blockEnd) {
         parser = new CommentParser(lineComment, blockStart, blockEnd);
     }
     
@@ -109,7 +105,7 @@ public class SourceCodeCommentParser {
      * parser treats everything as comments, and should hence "work" for all
      * unknown file types ;)
      * Since I was to lazy to override the setDocument method in the
-     * Comment parser, I inserted the variables rest and inComment into the
+     * Comment parser, I inserted the variables rest and inComment into the 
      * base class so I could initialize them here....
      */
     private class SourceParser {
@@ -122,8 +118,6 @@ public class SourceCodeCommentParser {
             lineno = 0;
             rest = null;
             inComment = false;
-            buffer = new StringBuffer(400);
-            rest = new StringBuffer();
         }
         
         /**
@@ -134,11 +128,7 @@ public class SourceCodeCommentParser {
          *         stream.
          */
         public boolean getNextLine(CommentLine ret) throws IOException {
-            if (doc.readTrimmedLine(buffer)) {
-                ret.line = buffer.toString();
-            } else {
-                ret.line = null; // ret.line = doc.readLine();
-            }
+            ret.line = doc.readLine();
             ++lineno;
             ret.lineno = lineno;
             return ret.line != null;
@@ -151,9 +141,9 @@ public class SourceCodeCommentParser {
          */
         public void setDocument(Document doc) throws BadLocationException {
             String text = doc.getText(0, doc.getLength());
-            this.doc = new SourceReader(new BufferedReader(new StringReader(text)));
+            this.doc = new BufferedReader(new StringReader(text));
             lineno = 0;
-            rest.setLength(0);;
+            rest = null;
             inComment = false;
         }
         
@@ -162,27 +152,22 @@ public class SourceCodeCommentParser {
          * @param doc the document to parse
          */
         public void setDocument(File f) throws IOException {
-            this.doc = new SourceReader(new BufferedReader(new FileReader(f)));
+            this.doc = new BufferedReader(new FileReader(f));
             lineno = 0;
-            rest.setLength(0);
+            rest = null;
             inComment = false;
         }
         
-        /**
-         * A StringBuffer that I use towards the source reader to avoid the
-         * creation of a lot of strings...
-         */
-        protected StringBuffer buffer;
         
         /** The reader I use to read the document content from */
-        protected SourceReader doc;
+        protected BufferedReader doc;
         /** Current line in the file */
-        protected int lineno;
-        /**
+        protected int                    lineno;
+        /** 
          * If I had to split the line i just read (mix of comment and code),
          * rest contains the rest of the line I just read.
          */
-        protected StringBuffer rest;
+        protected String                 rest;
         /** Am I currently inside a comment? */
         protected boolean inComment;
     }
@@ -208,10 +193,9 @@ public class SourceCodeCommentParser {
          * @param blockStart the start token for a multiline comment block
          * @param blockEnd the end token for a multiline comment block
          */
-        public CommentParser(String lineComment,
-        String blockStart,
-        String blockEnd) {
-            super();
+        public CommentParser(String lineComment, 
+                             String blockStart, 
+                             String blockEnd) {
             this.lineComment = lineComment;
             this.blockStart = blockStart;
             this.blockEnd = blockEnd;
@@ -226,16 +210,14 @@ public class SourceCodeCommentParser {
          *         stream.
          */
         public boolean getNextLine(CommentLine ret) throws IOException {
-            StringBuffer active;
+            String line = null;
             
             // Read next line of input!
-            while (rest.length() != 0 || doc.readTrimmedLine(buffer)) {
-                if (rest.length() > 0) {
-                    // I should operate on the rest of this line...
-                    active = rest;
+            while (rest != null || (line = doc.readLine()) != null) {
+                if (rest != null) {
+                    line = rest;
+                    rest = null;
                 } else {
-                    // I should operate on the line I just read from the file..
-                    active = buffer;
                     ++lineno;
                 }
                 
@@ -243,7 +225,7 @@ public class SourceCodeCommentParser {
                     // I'm not inside a comment, but this line might contain one
                     int lineIdx = java.lang.Integer.MAX_VALUE;
                     if (lineComment != null) {
-                        lineIdx = active.indexOf(lineComment);
+                        lineIdx = line.indexOf(lineComment);
                         if (lineIdx == -1) {
                             lineIdx = java.lang.Integer.MAX_VALUE;
                         }
@@ -251,7 +233,7 @@ public class SourceCodeCommentParser {
                     
                     int blockIdx = java.lang.Integer.MAX_VALUE;
                     if (blockStart != null) {
-                        blockIdx = active.indexOf(blockStart);
+                        blockIdx = line.indexOf(blockStart);
                         if (blockIdx == -1) {
                             blockIdx = java.lang.Integer.MAX_VALUE;
                         }
@@ -261,13 +243,11 @@ public class SourceCodeCommentParser {
                     if (idx != java.lang.Integer.MAX_VALUE) {
                         // This line contains the start of a comment!!!!
                         if (idx == lineIdx) {
-                            ret.line = active.substring(idx + lineComment.length());
+                            ret.line = line.substring(idx + lineComment.length());
                             ret.lineno = lineno;
-                            active.setLength(0);
                             return true;
                         } else {
-                            // delete all characters up to the beginning of the comment
-                            active.delete(0, idx + blockStart.length());
+                            line = line.substring(idx + blockStart.length());
                             inComment = true;
                         }
                     } else {
@@ -281,15 +261,15 @@ public class SourceCodeCommentParser {
                 // null, and it should _not_ be possible to configure a
                 // setup where you have a block start and not a block end!
                 
-                int idx = active.indexOf(blockEnd);
+                int idx = line.indexOf(blockEnd);
                 
                 if (idx != -1) {
-                    rest.append(active.substring(idx + blockEnd.length()));
-                    active.delete(idx, active.length());
+                    rest = line.substring(idx + blockEnd.length());
+                    line = line.substring(0, idx);
                     inComment = false;
                 }
                 
-                ret.line = active.toString();
+                ret.line = line;
                 ret.lineno = lineno;
                 return true;
             }
@@ -317,85 +297,4 @@ public class SourceCodeCommentParser {
     
     /** The parser used by this SourceCodeCommentParser */
     private SourceParser parser;
-    
-    /**
-     * Tor told me once that there is a "big" performance penalty when I create
-     * a lot of string objects, caused by the garbage collector. I still like
-     * to be able to just scan the comment sections of my files, so instead of
-     * creating one string for each line in the file, i now create one String
-     * for each _line_ in a _comment_ section.
-     */
-    private class SourceReader {
-        private java.io.Reader input;
-        private char buffer[];
-        private int current;
-        private int buffsz;
-        
-        /**
-         * Create a new source reader from the input stream...
-         * @param in The input stream to read from
-         */
-        public SourceReader(java.io.Reader in) {
-            input = in;
-            buffer = new char[4096];
-            current = buffsz = buffer.length;
-        }
-        
-        /**
-         * Read a line of text into the buffer...
-         * @param buf the stringbuffer to insert the new trimmed line into
-         * @return false if EOF and buf should be ignored
-         */
-        private boolean readTrimmedLine(StringBuffer buf) throws IOException {
-            buf.setLength(0);
-            boolean start = true;
-            int     lastspace = Integer.MAX_VALUE;
-            int     min = current;
-            boolean done = false;
-            
-            do {
-                if (current == buffsz) {
-                    if (min != current) {
-                        buf.append(buffer, min, (current - min));
-                    }
-                    
-                    buffsz = input.read(buffer);
-                    current = 0;
-                    min = 0;
-                    
-                    if (buffsz == -1) {
-                        if (buf.length() > 0) {
-                            return true;
-                        }
-                        return false;
-                    }
-                }
-                
-                if (buffer[current] == '\r' || buffer[current] == '\n') {
-                    buf.append(buffer, min, current - min);
-                    ++current;
-                    done = true;
-                } else {
-                    if (buffer[current] == ' ') {
-                        if (start) {
-                            ++min;
-                        } else {
-                            if (lastspace == Integer.MAX_VALUE) {
-                                lastspace = current;
-                            }
-                        }
-                    } else {
-                        start = false;
-                        lastspace = Integer.MAX_VALUE;
-                    }
-                    ++current;
-                }
-            } while (!done);
-            if (lastspace != Integer.MAX_VALUE) {
-                buf.setLength(lastspace - min);
-            }
-            return true;
-        }
-    }
 }
-
