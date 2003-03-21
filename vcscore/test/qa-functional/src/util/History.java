c44c0e62915b937b78955f6fceda7fcd58f53feb
dkaspar@netbeans.org <dkaspar@netbeans.org>
2003-03-21 09:44:19 +0000

    License

--- vcscore/test/qa-functional/src/util/History.java
+++ vcscore/test/qa-functional/src/util/History.java
@@ -1,13 +1,13 @@
 /*
  *                 Sun Public License Notice
- *
+ * 
  * The contents of this file are subject to the Sun Public License
  * Version 1.0 (the "License"). You may not use this file except in
  * compliance with the License. A copy of the License is available at
  * http://www.sun.com/
- *
+ * 
  * The Original Code is NetBeans. The Initial Developer of the Original
- * Code is Sun Microsystems, Inc. Portions Copyright 1997-2002 Sun
+ * Code is Sun Microsystems, Inc. Portions Copyright 1997-2003 Sun
  * Microsystems, Inc. All Rights Reserved.
  */
 
@@ -25,14 +25,14 @@ import org.openide.nodes.Node.PropertySet;
 
 /** History class for tracing history of VCS command in Runtime tab in Explorer.
  * Typical class usage:
- * <CODE>
+ * <PRE>
  * public void testHistory () {
  *    ... get file system ...
  *    History history = new History (filesytem);
  *    ... invoke some VCS command (for example: CVS|Refresh on "data" package)...
  *    history.waitCommand ("Refresh", "data"); // waits until Refresh command finishs
  * }
- * </CODE>
+ * </PRE>
  */
 public class History {
     
