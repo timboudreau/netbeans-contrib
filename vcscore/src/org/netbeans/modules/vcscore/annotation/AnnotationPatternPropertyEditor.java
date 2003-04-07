ec614f66a4072a954828b4dce82ffe15d0954318
mentlicher@netbeans.org <mentlicher@netbeans.org>
2003-04-07 14:48:30 +0000

    Removing the introspection. The class name was a private static String anyway, thus there's no reason why the class should not be accessed directly. This BTW fix issue #32646.

--- vcscore/src/org/netbeans/modules/vcscore/annotation/AnnotationPatternPropertyEditor.java
+++ vcscore/src/org/netbeans/modules/vcscore/annotation/AnnotationPatternPropertyEditor.java
@@ -30,8 +30,6 @@ import org.openide.util.NbBundle;
 public abstract class AnnotationPatternPropertyEditor extends PropertyEditorSupport {
 
 
-    private static final String CUSTOM_EDITOR_CLASS = "org.netbeans.modules.vcscore.annotation.AnnotPatternCustomEditor";
-
     public void setValue(Object value) {
         super.setValue(value);
     }
@@ -61,22 +59,9 @@ public abstract class AnnotationPatternPropertyEditor extends PropertyEditorSupp
     public abstract String getDefaultAnnotationPattern();
     
     public java.awt.Component getCustomEditor() {
-       java.awt.Component toReturn = null;
-        try {
-            Class customEdClass = Class.forName(CUSTOM_EDITOR_CLASS, true, (NbClassLoader)Lookup.getDefault().lookup(NbClassLoader.class));
-            toReturn = (java.awt.Component)customEdClass.newInstance();
-            java.lang.reflect.Method method = customEdClass.getMethod("setCallingPropertyEditor", new Class[] {AnnotationPatternPropertyEditor.class});
-            if (method != null) {
-                method.invoke(toReturn, new Object[] {this});
-            } else {
-//                System.out.println("is null");
-            }
-        } catch (final Exception clExc) {
-//            System.out.println("exception");
-            clExc.printStackTrace();
-        }               
-       
-        return toReturn;
+        AnnotPatternCustomEditor editor = new AnnotPatternCustomEditor();
+        editor.setCallingPropertyEditor(this);
+        return editor;
     }
     
     public boolean supportsCustomEditor() {
