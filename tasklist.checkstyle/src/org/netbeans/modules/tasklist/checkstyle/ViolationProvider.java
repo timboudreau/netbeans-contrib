30fcdff7d35bb1b6ec2d8a3adb3196a2adb2baba
tor@netbeans.org <tor@netbeans.org>
2003-06-22 05:51:07 +0000

    Duh

--- tasklist.checkstyle/src/org/netbeans/modules/tasklist/checkstyle/ViolationProvider.java
+++ tasklist.checkstyle/src/org/netbeans/modules/tasklist/checkstyle/ViolationProvider.java
@@ -135,7 +135,7 @@ public class ViolationProvider extends DocumentSuggestionProvider
                     File f = org.openide.modules.InstalledFileLocator.getDefault().locate("configs/checkstyle.xml", "org.netbeans.modules.tasklist.checkstyle", false);
                     //System.out.println("FILE LOCATED = " + f);
                     if (f == null) {
-                        ErrorManager.getDefault().log("Couldn't find configs/checkstyle_packages.xml");
+                        ErrorManager.getDefault().log("Couldn't find configs/checkstyle.xml");
                         return null;
                     }
                     config = ConfigurationLoader.loadConfiguration(f.getPath(), new PropertiesExpander(props));
