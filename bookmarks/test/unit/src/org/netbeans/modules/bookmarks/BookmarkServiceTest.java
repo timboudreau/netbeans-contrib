784feaa2c0d55df89d0a00b7f36cf59180c01851
dstrupl@netbeans.org <dstrupl@netbeans.org>
2003-08-20 00:46:52 +0000

    Added tests for the bookmarks module.

---
+++ bookmarks/test/unit/src/org/netbeans/modules/bookmarks/BookmarkServiceTest.java
@@ -0,0 +1,296 @@
+/*
+ *                 Sun Public License Notice
+ * 
+ * The contents of this file are subject to the Sun Public License
+ * Version 1.0 (the "License"). You may not use this file except in
+ * compliance with the License. A copy of the License is available at
+ * http://www.sun.com/
+ * 
+ * The Original Code is NetBeans. The Initial Developer of the Original
+ * Code is Nokia. Portions Copyright 2003 Nokia.
+ * All Rights Reserved.
+ */
+
+package org.netbeans.modules.bookmarks;
+
+import javax.naming.*;
+import javax.swing.*;
+
+import junit.textui.TestRunner;
+
+import org.netbeans.junit.NbTestCase;
+import org.netbeans.junit.NbTestSuite;
+
+import org.openide.ErrorManager;
+import org.openide.filesystems.FileObject;
+import org.openide.filesystems.Repository;
+import org.openide.filesystems.FileSystem;
+import org.openide.modules.ModuleInfo;
+import org.openide.nodes.Node;
+import org.openide.util.Lookup;
+import org.openide.util.lookup.*;
+import org.openide.util.Utilities;
+import org.openide.windows.*;
+
+import org.netbeans.api.bookmarks.*;
+import org.netbeans.modules.bookmarks.actions.AddBookmarkAction;
+/** 
+ * @author David Strupl
+ */
+public class BookmarkServiceTest extends NbTestCase {
+    /** root folder FileObject */
+    private FileObject root;
+    
+    public BookmarkServiceTest(String name) {
+        super(name);
+    }
+    
+    public static void main(String[] args) {
+        TestRunner.run(new NbTestSuite(BookmarkServiceTest.class));
+    }
+
+    /**
+     * Sets up the testing environment by creating testing folders
+     * on the system file system.
+     */
+    protected void setUp () throws Exception {
+        Lookup.getDefault().lookup(ModuleInfo.class);
+        FileSystem dfs = Repository.getDefault().getDefaultFileSystem();
+        String baseFolder = BookmarkServiceImpl.BOOKMARKS_FOLDER; 
+        root = dfs.findResource(baseFolder);
+        if (root == null) {
+            String s1 = baseFolder.substring(0, baseFolder.lastIndexOf('/'));
+            FileObject f1 = dfs.findResource(s1);
+            if (f1 == null) {
+                f1 = dfs.getRoot().createFolder(s1);
+            } 
+            root = f1.createFolder(baseFolder.substring(baseFolder.lastIndexOf('/')+1));
+        }
+        
+    }
+    
+    /**
+     * This test should tests the functionality of the method
+     * BookmarkService.storeBookmark. It does so by creating a bookmark
+     * storing it and checking whether it was stored.
+     * The tests performs following steps:
+     * <OL><LI> Create a testing bookmark
+     *     <LI> Store the bookmark
+     *     <LI> Check the presence of the new file on 
+     *          the system file system
+     * </OL>
+     */
+    public void testStoreBookmark() throws Exception {
+        Bookmark b = new TestBookmark("test1");
+        BookmarkService.getDefault().storeBookmark(b);
+        
+        assertTrue("Stored item did not found", deleteFromBookmarksFolder("test1"));
+    }
+    
+    /**
+     * Tries to find and delete file with given name.
+     * The name has to only start with the param name.
+     */
+    private boolean deleteFromBookmarksFolder(String name) throws java.io.IOException {
+        boolean found = false;
+        java.util.Enumeration en = root.getChildren(false);
+        while (en.hasMoreElements()) {
+            FileObject fo = (FileObject)en.nextElement();
+            if (fo.getName().startsWith(name)) {
+                found = true;
+                fo.delete();
+            }
+        }
+        return found;
+    }
+    
+    /**
+     * Custom implementation of the Bookmark interface.
+     */
+    private static class TestBookmark implements Bookmark, java.io.Serializable {
+        private static final long serialVersionUID = 1L;
+        private String name;
+        public TestBookmark() {}
+        public TestBookmark(String name) {
+            this.name = name;
+        }
+        public java.awt.Component getToolbarPresenter() {
+            return getMenuPresenter();
+        }
+        public javax.swing.JMenuItem getMenuPresenter() {
+            return new javax.swing.JMenuItem("test Item");
+        }
+        public String getName() {
+            return name;
+        }
+        public void invoke() {
+        }
+    }
+    
+    /**
+     * This test should test the API method createDefaultBookmark.
+     * It does so by invoking the method with testing TopComponent. 
+     * The tests performs following steps:
+     * <OL><LI> Creates a testing top component
+     *     <LI> The result of createDefaultBookmark should be BookmarkImpl
+     *     <LI> creates another top component now supplying a BookmarkProvider
+     *          in its lookup
+     *     <LI> createDefaultBookmark should call through 
+                BookmarkProvider.createBookmark - the resulting bookmark should
+     *          be the one from BookmarkProvider.
+     * </OL>
+     */
+    public void testCreateDefaultBookmark() throws Exception {
+        TopComponent tc = new TopComponent();
+        Bookmark b = BookmarkService.getDefault().createDefaultBookmark(tc);
+        assertTrue("Default bookmark should be BookmarkImpl", b instanceof BookmarkImpl);
+        BookmarkImpl bi = (BookmarkImpl)b;
+        assertEquals("Top component referenced from the BookmarkImpl should be tc.", bi.getTopComponent(), tc);
+        
+        // -------------------------------------------------
+        Bookmark tb = new TestBookmark("test2");
+        TopComponent tc1 = new TestTopComponent(tb);
+        Bookmark b2 = BookmarkService.getDefault().createDefaultBookmark(tc1);
+        assertEquals("Bookmark from the test component must be tb", b2, tb);
+    }
+    
+    /**
+     * The testing top component provides a bookmark in its lookup.
+     */
+    private static class TestTopComponent extends TopComponent {
+        private Bookmark b;
+        public TestTopComponent() {
+        }
+        public TestTopComponent(Bookmark b) {
+            this.b = b;
+        }
+        public Lookup getLookup() {
+            Lookup orig = super.getLookup();
+            return new ProxyLookup(new Lookup[] {
+                orig,
+                Lookups.singleton(new BookmarkProvider() {
+                    public Bookmark createBookmark() {
+                        return b;
+                    }
+                })
+            });
+        }
+    }
+    
+    /**
+     * This test tries to verify that the AddBookmarkAction
+     * calls the createDefaultBookmark and storeBookmark methods.
+     * It does so by creating and selecting a testing top component
+     * and invoking the action.
+     * The tests performs following steps:
+     * <OL><LI> Create a TestTopComponent
+     *     <LI> Open and activate (request focus on) it
+     *     <LI> wait till the event queue dispatches all events
+     *          bound to opening the top component
+     *     <LI> verify that our top component is really activated
+     *     <LI> invoke the AddBookmarkAction
+     *     <LI> tests whether the testing bookmark bound to the
+     *          testing top component is written in the bookmarks folder
+     * </OL>
+     */
+    public void testAddBookmarkAction() throws Exception {
+        AddBookmarkAction action = new AddBookmarkAction();
+        Bookmark b = new TestBookmark("test3");
+        final TopComponent tc = new TestTopComponent(b);
+        SwingUtilities.invokeAndWait(new Runnable() {
+            public void run() {
+                tc.open();
+                tc.requestFocus();
+            }
+        });
+        final boolean [] done = new boolean[1];
+        Runnable wait = new Runnable() { 
+            public void run() {
+                done[0] = true;
+            }
+        };
+        SwingUtilities.invokeLater(wait);
+        while (! done[0]) {
+            Thread.sleep(1000);
+        }
+        assertEquals("our top component should be activated",
+            WindowManager.getDefault().getRegistry().getActivated(), tc);
+        action.actionPerformed(null);
+        assertTrue("test3 should be stored by AddBookmarkAction", deleteFromBookmarksFolder("test3"));
+        
+        // clean up
+        tc.close();
+    }
+    /**
+     * Tests whether it is possible to store the deafult
+     * bookmark with the same top component twice. 
+     * The tests performs following steps:
+     * <OL><LI> Create a new TestBookmark
+     *     <LI> store the bookmark twice
+     *     <LI> checks whether the bookmark is there
+     * </OL>
+     */
+    public void testStoreBookmarkTwice() throws Exception {
+        Bookmark ba = new TestBookmark("testA");
+        BookmarkService.getDefault().storeBookmark(ba);
+        BookmarkService.getDefault().storeBookmark(ba);
+        assertTrue("Stored item did not found", deleteFromBookmarksFolder("testA"));
+    }
+    
+    /**
+     * Test what is done when the default bookmark is invoked. It
+     * should open the saved top component.
+     * The tests performs following steps:
+     * <OL><LI> Create a special top component that records open
+     *          and request focus methods
+     *     <LI> createDefaultBookmark method is called with the created
+     *         top component
+     *     <LI> the resulting bookmark is invoked
+     *     <LI> it is verified that the bookmark invocation lead to
+     *        openning and selecting the top component
+     * </OL>
+     */
+    public void testInvokeDefaultBookmark() throws Exception {
+        final boolean [] res = new boolean[2];
+        TopComponent tc = new TopComponent() {
+            public void open() {
+                super.open();
+                res[0] = true;
+            }
+            public void requestFocus() {
+                super.requestFocus();
+                res[1] = true;
+            }
+        };
+        Bookmark b = BookmarkService.getDefault().createDefaultBookmark(tc);
+        b.invoke();
+        assertTrue("Open has not been called", res[0]);
+        assertTrue("requestFocus has not been called", res[1]);
+        
+        // clean up
+        tc.close();
+    }
+    
+    /**
+     * Test whether it is possible to create bookmark, delete it and
+     * create again with the same top component.
+     * The tests performs following steps:
+     * <OL><LI> Create TestBookmark (with name test1)
+     *     <LI> store the bookmark using storeBookmark method of BookmarkService
+     *     <LI> check whether the file test1*.* was written and delete it
+     *     <LI> Create TestBookmark (with name test1)
+     *     <LI> store the bookmark using storeBookmark method of BookmarkService
+     *     <LI> check whether the file test1*.* was written again
+     * </OL>
+     */
+    public void testBindAfterDeleting() throws Exception {
+        Bookmark b = new TestBookmark("test1");
+        BookmarkService.getDefault().storeBookmark(b);
+        
+        assertTrue("Stored item did not found", deleteFromBookmarksFolder("test1"));
+        Bookmark b2 = new TestBookmark("test1"); // this is intentionally the same as above
+        BookmarkService.getDefault().storeBookmark(b2);
+        
+        assertTrue("Stored item did not found", deleteFromBookmarksFolder("test1"));
+    }
+}
---
+++ bookmarks/test/unit/src/org/netbeans/modules/bookmarks/NavigationServiceTest.java
@@ -0,0 +1,320 @@
+/*
+ *                 Sun Public License Notice
+ * 
+ * The contents of this file are subject to the Sun Public License
+ * Version 1.0 (the "License"). You may not use this file except in
+ * compliance with the License. A copy of the License is available at
+ * http://www.sun.com/
+ * 
+ * The Original Code is NetBeans. The Initial Developer of the Original
+ * Code is Nokia. Portions Copyright 2003 Nokia.
+ * All Rights Reserved.
+ */
+
+package org.netbeans.modules.bookmarks;
+
+import javax.naming.*;
+import javax.swing.*;
+import java.beans.*;
+
+import junit.textui.TestRunner;
+
+import org.netbeans.junit.NbTestCase;
+import org.netbeans.junit.NbTestSuite;
+
+import org.openide.ErrorManager;
+import org.openide.nodes.Node;
+import org.openide.util.Lookup;
+import org.openide.util.lookup.*;
+import org.openide.util.Utilities;
+import org.openide.windows.*;
+
+import org.netbeans.api.bookmarks.*;
+
+/** 
+ * Tests in this class should test the NavigationService
+ * implementation.
+ * @author David Strupl
+ */
+public class NavigationServiceTest extends NbTestCase {
+    
+    public NavigationServiceTest(String name) {
+        super(name);
+    }
+    
+    public static void main(String[] args) {
+        TestRunner.run(new NbTestSuite(NavigationServiceTest.class));
+    }
+
+    /**
+     * This tests verifies that the NavigationService properly
+     * fires event when someone calls storeNavigationEvent.
+     * The tests performs following steps:
+     * <OL><LI> Register a listener to the NavigationService
+     *     <LI> Stores a navigation event
+     *     <LI> checks whether property change event was fired
+     * </OL>
+     */
+    public void testFiresPropertyChange() throws Exception {
+        final boolean []fired = new boolean[1];
+        NavigationService ns = NavigationService.getDefault();
+        ns.addPropertyChangeListener(new PropertyChangeListener() {
+            public void propertyChange(PropertyChangeEvent pce) {
+                fired[0] = true;
+            }
+        });
+        ns.storeNavigationEvent(new NavigationEvent() {
+            public void restoreState() {
+            }
+            public TopComponent getTopComponent() {
+                return new TopComponent();
+            }
+        });
+        assertTrue("Event not fired", fired[0]);
+    }
+    
+    /**
+     * This test checks whether we can navigate back after storing
+     * an event.
+     * <OL><LI> creates a testing top component
+     *     <LI> stores an initial navigation event
+     *     <LI> activates the top component
+     *     <LI> checks whether it really is activated
+     *     <LI> stores a navigation event with this top component
+     *     <LI> checks whether it is possible to go back
+     *     <LI> performs the step back
+     * </OL>
+     */
+    public void testCanNavigateBackwardAfterStoring() throws Exception {
+        final TopComponent tc = new TopComponent();
+        NavigationService ns = NavigationService.getDefault();
+        final boolean []restored = new boolean[1];
+        // the initial event
+        ns.storeNavigationEvent(new NavigationEvent() {
+            public void restoreState() {
+                restored[0] = true;
+            }
+            public TopComponent getTopComponent() {
+                return tc;
+            }
+        });
+        // activate the top component
+        SwingUtilities.invokeAndWait(new Runnable() {
+            public void run() {
+                tc.open();
+                tc.requestFocus();
+            }
+        });
+        final boolean [] done = new boolean[1];
+        Runnable wait = new Runnable() { 
+            public void run() {
+                done[0] = true;
+            }
+        };
+        SwingUtilities.invokeLater(wait);
+        // wait for the win sys
+        while (! done[0]) {
+            Thread.sleep(1000);
+        }
+        assertEquals("our top component should be activated",
+            WindowManager.getDefault().getRegistry().getActivated(), tc);
+
+        ns.storeNavigationEvent(new NavigationEvent() {
+            public void restoreState() {
+            }
+            public TopComponent getTopComponent() {
+                return tc;
+            }
+        });
+        assertTrue("Should be possible to navigate back", ns.canNavigateBackward());
+        
+        ns.backward();
+        assertTrue("Did not restore the state" , restored[0]);
+        
+        tc.close();
+    }
+    
+    /**
+     * This test checks whether we can navigate forward after
+     * going back.
+     * <OL><LI> creates a testing top component
+     *     <LI> stores an initial navigation event
+     *     <LI> activates the top component
+     *     <LI> checks whether it really is activated
+     *     <LI> stores a navigation event with this top component
+     *     <LI> checks whether it is possible to go back
+     *     <LI> performs the step back
+     *     <LI> checks whether it is possible to navigate forward
+     *     <LI> performs the step forward
+     *     <LI> checks whether the step forward was done
+     *     <LI> now it should be possible to go back again
+     * </OL>
+     */
+    public void testCanNavigateForwardAfterBackward() throws Exception {
+        final TopComponent tc = new TopComponent();
+        NavigationService ns = NavigationService.getDefault();
+        // the initial event
+        ns.storeNavigationEvent(new NavigationEvent() {
+            public void restoreState() {
+            }
+            public TopComponent getTopComponent() {
+                return tc;
+            }
+        });
+        // activate the top component
+        SwingUtilities.invokeAndWait(new Runnable() {
+            public void run() {
+                tc.open();
+                tc.requestFocus();
+            }
+        });
+        final boolean [] done = new boolean[1];
+        Runnable wait = new Runnable() { 
+            public void run() {
+                done[0] = true;
+            }
+        };
+        SwingUtilities.invokeLater(wait);
+        // wait for the win sys
+        while (! done[0]) {
+            Thread.sleep(1000);
+        }
+        assertEquals("our top component should be activated",
+            WindowManager.getDefault().getRegistry().getActivated(), tc);
+        
+        final boolean []restored = new boolean[1];
+        ns.storeNavigationEvent(new NavigationEvent() {
+            public void restoreState() {
+                restored[0] = true;
+            }
+            public TopComponent getTopComponent() {
+                return tc;
+            }
+        });
+        assertTrue("Should be possible to navigate back", ns.canNavigateBackward());
+        
+        ns.backward();
+        
+        assertTrue("Should be able to navigate forward now", ns.canNavigateForward());
+        
+        ns.forward();
+        
+        assertTrue("The state should be restored", restored[0]);
+        assertTrue("Should be possible to navigate back", ns.canNavigateBackward());
+        
+        tc.close();
+    }
+    
+    /**
+     * After storing the event it should be impossible to step
+     * forward.
+     * <OL><LI> creates a testing top component
+     *     <LI> stores an initial navigation event
+     *     <LI> activates the top component
+     *     <LI> checks whether it really is activated
+     *     <LI> stores a navigation event with this top component
+     *     <LI> checks whether it is impossible to go forward
+     */
+    public void testCannotForwardAfterStoring() throws Exception {
+        final TopComponent tc = new TopComponent();
+        NavigationService ns = NavigationService.getDefault();
+        // the initial event
+        ns.storeNavigationEvent(new NavigationEvent() {
+            public void restoreState() {
+            }
+            public TopComponent getTopComponent() {
+                return tc;
+            }
+        });
+        // activate the top component
+        SwingUtilities.invokeAndWait(new Runnable() {
+            public void run() {
+                tc.open();
+                tc.requestFocus();
+            }
+        });
+        final boolean [] done = new boolean[1];
+        Runnable wait = new Runnable() { 
+            public void run() {
+                done[0] = true;
+            }
+        };
+        SwingUtilities.invokeLater(wait);
+        // wait for the win sys
+        while (! done[0]) {
+            Thread.sleep(1000);
+        }
+        assertEquals("our top component should be activated",
+            WindowManager.getDefault().getRegistry().getActivated(), tc);
+
+        ns.storeNavigationEvent(new NavigationEvent() {
+            public void restoreState() {
+            }
+            public TopComponent getTopComponent() {
+                return tc;
+            }
+        });
+        assertTrue("Should be impossible to navigate forward", ! ns.canNavigateForward());
+        
+        tc.close();
+    }
+    
+    /**
+     * This test verifies that storing the navigation event does not lead
+     * to navigation when our top component is not activated.
+     * <OL><LI> activate some top componnet
+     *     <LI> create another one
+     *     <LI> Store the initial event
+     *     <LI> store one more event for the same top component
+     *     <LI> shold be impossible to navigate
+     * </OL>
+     */
+    public void testCannotNavigateWhenNotActivated() throws Exception {
+        final TopComponent tc1 = new TopComponent();
+        // activate the top component
+        SwingUtilities.invokeAndWait(new Runnable() {
+            public void run() {
+                tc1.open();
+                tc1.requestFocus();
+            }
+        });
+        final boolean [] done = new boolean[1];
+        Runnable wait = new Runnable() { 
+            public void run() {
+                done[0] = true;
+            }
+        };
+        SwingUtilities.invokeLater(wait);
+        // wait for the win sys
+        while (! done[0]) {
+            Thread.sleep(1000);
+        }
+        final TopComponent tc = new TopComponent();
+        NavigationService ns = NavigationService.getDefault();
+        assertTrue("1. Should be impossible to navigate back", ! ns.canNavigateBackward());
+        final boolean []restored = new boolean[1];
+        // the initial event
+        ns.storeNavigationEvent(new NavigationEvent() {
+            public void restoreState() {
+                restored[0] = true;
+            }
+            public TopComponent getTopComponent() {
+                return tc;
+            }
+        });
+        assertTrue("our top component should not be activated",
+            WindowManager.getDefault().getRegistry().getActivated() != tc);
+
+        ns.storeNavigationEvent(new NavigationEvent() {
+            public void restoreState() {
+            }
+            public TopComponent getTopComponent() {
+                return tc;
+            }
+        });
+        assertTrue("2. Should be impossible to navigate back", ! ns.canNavigateBackward());
+        
+        tc.close();
+        tc1.close();
+    }
+}
