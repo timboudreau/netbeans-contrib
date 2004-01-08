8b9fe146fe7f171d886b8f8996e90f1ae6979208
rgregor@netbeans.org <rgregor@netbeans.org>
2004-01-08 14:07:40 +0000

    bugfix#38430 - logout command in

---
+++ vcs.profiles.cvsprofiles/src/org/netbeans/modules/vcs/profiles/cvsprofiles/commands/CvsLogout.java
@@ -0,0 +1,62 @@
+/*
+ *                 Sun Public License Notice
+ *
+ * The contents of this file are subject to the Sun Public License
+ * Version 1.0 (the "License"). You may not use this file except in
+ * compliance with the License. A copy of the License is available at
+ * http://www.sun.com/
+ *
+ * The Original Code is NetBeans. The Initial Developer of the Original
+ * Code is Sun Microsystems, Inc. Portions Copyright 1997-2003 Sun
+ * Microsystems, Inc. All Rights Reserved.
+ */
+
+package org.netbeans.modules.vcs.profiles.cvsprofiles.commands;
+
+import java.util.*;
+import org.netbeans.modules.vcs.profiles.cvsprofiles.commands.passwd.CVSPasswd;
+
+import org.netbeans.modules.vcscore.VcsFileSystem;
+import org.netbeans.modules.vcscore.commands.CommandOutputListener;
+import org.netbeans.modules.vcscore.commands.CommandDataOutputListener;
+import org.netbeans.modules.vcscore.cmdline.*;
+
+import org.netbeans.modules.vcs.profiles.cvsprofiles.commands.passwd.CvsLoginDialog;
+import org.netbeans.modules.vcscore.Variables;
+
+/**
+ * This class is used as a CVS logout command.
+ * @author  Richard Gregor
+ */
+public class CvsLogout implements VcsAdditionalCommand {
+
+    private VcsFileSystem fileSystem = null;
+
+    public void setFileSystem(VcsFileSystem fileSystem) {
+        this.fileSystem = fileSystem;
+    }
+
+    public boolean exec(Hashtable vars, String[] args,
+                        CommandOutputListener stdoutNRListener, CommandOutputListener stderrNRListener,
+                        CommandDataOutputListener stdoutListener, String dataRegex,
+                        CommandDataOutputListener stderrListener, String errorRegex) 
+     {
+         
+         String serverName = (String) vars.get("CVS_SERVER");                           //NOI18N
+         String userName = (String) vars.get("CVS_USERNAME");                           //NOI18N
+         String cvsRoot = (String) vars.get("CVS_REPOSITORY");                          //NOI18N
+         String connectStr=":pserver:" + userName + "@" + serverName + ":" + cvsRoot;   //NOI18N
+         try{
+             CVSPasswd pasFile = new CVSPasswd((String)null);
+             pasFile.loadPassFile();
+             pasFile.remove(connectStr);
+             pasFile.savePassFile();
+             vars.put("USER_IS_LOGGED_IN", "");                                         //NOI18N
+         }catch(Exception e){
+             stderrNRListener.outputLine(e.getMessage());
+             return false;                    
+         }         
+         return true;
+    }
+}
+
