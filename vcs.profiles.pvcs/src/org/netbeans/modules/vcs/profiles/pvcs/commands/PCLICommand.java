6891ac41d7ba9943952953deb62e16c2f074de55
mentlicher@netbeans.org <mentlicher@netbeans.org>
2004-05-04 09:18:44 +0000

    An efficient executor for PCLI commands implemented. A special script is kept running and individual PVCS commands are send to it through an input stream. The output is distributed to appropriate commands. This is necessary to fix issue #34630.

---
+++ vcs.profiles.pvcs/src/org/netbeans/modules/vcs/profiles/pvcs/commands/PCLICommand.java
@@ -0,0 +1,166 @@
+/*
+ *                 Sun Public License Notice
+ * 
+ * The contents of this file are subject to the Sun Public License
+ * Version 1.0 (the "License"). You may not use this file except in
+ * compliance with the License. A copy of the License is available at
+ * http://www.sun.com/
+ * 
+ * The Original Code is NetBeans. The Initial Developer of the Original
+ * Code is Sun Microsystems, Inc. Portions Copyright 1997-2004 Sun
+ * Microsystems, Inc. All Rights Reserved.
+ */
+
+package org.netbeans.modules.vcs.profiles.pvcs.commands;
+
+import java.util.ArrayList;
+import java.util.Hashtable;
+import java.util.Iterator;
+import java.util.regex.Pattern;
+import java.util.regex.PatternSyntaxException;
+
+import org.openide.ErrorManager;
+
+import org.netbeans.modules.vcscore.cmdline.VcsAdditionalCommand;
+import org.netbeans.modules.vcscore.cmdline.exec.ExternalCommand;
+import org.netbeans.modules.vcscore.commands.CommandOutputListener;
+import org.netbeans.modules.vcscore.commands.CommandDataOutputListener;
+import org.netbeans.modules.vcscore.commands.TextOutputListener;
+
+/**
+ * PCLI command that performs efficient execution using PCLICommandExecutor.
+ *
+ * @author  Martin Entlicher
+ */
+public class PCLICommand implements VcsAdditionalCommand, VcsAdditionalCommand.ImmediateOutput {
+    
+    private String execStr;
+    private CommandOutputListener stdoutListener;
+    private CommandOutputListener stderrListener;
+    private CommandDataOutputListener stdoutDataListener;
+    private Pattern dataRegex;
+    private CommandDataOutputListener stderrDataListener;
+    private Pattern errorRegex;
+    private ArrayList stdImmediateOutListeners = new ArrayList();
+    private ArrayList stdImmediateErrListeners = new ArrayList();
+    private Boolean[] success = { null };
+    
+    /** Creates a new instance of PCLICommand */
+    public PCLICommand() {
+    }
+    
+    public boolean exec(Hashtable vars, String[] args,
+                        CommandOutputListener stdoutListener,
+                        CommandOutputListener stderrListener,
+                        CommandDataOutputListener stdoutDataListener, String dataRegex,
+                        CommandDataOutputListener stderrDataListener, String errorRegex) {
+        PCLICommandExecutor executor = PCLICommandExecutor.getDefault();
+        if (args.length < 1 || args[0] == null) {
+            stderrListener.outputLine("Expecting the PCLI command as an argument.");
+            return false;
+        }
+        this.execStr = args[0];
+        this.stdoutListener = stdoutListener;
+        this.stderrListener = stderrListener;
+        this.stdoutDataListener = stdoutDataListener;
+        this.stderrDataListener = stderrDataListener;
+        if (dataRegex != null) {
+            try {
+                this.dataRegex = Pattern.compile(dataRegex);
+            } catch (PatternSyntaxException psex) {
+                ErrorManager.getDefault().notify(psex);
+            }
+        }
+        if (errorRegex != null) {
+            try {
+                this.errorRegex = Pattern.compile(errorRegex);
+            } catch (PatternSyntaxException psex) {
+                ErrorManager.getDefault().notify(psex);
+            }
+        }
+        try {
+            return executor.runCommand(this);
+        } catch (InterruptedException iex) {
+            Thread.currentThread().interrupt();
+            return false;
+        }
+    }
+    
+    public String getStrExec() {
+        return execStr;
+    }
+    
+    /**
+     * Add a listener to the standard output, that will be noified
+     * immediately as soon as the output text is available. It does not wait
+     * for the new line and does not send output line-by-line.
+     */
+    public void addImmediateTextOutputListener(TextOutputListener l) {
+        this.stdImmediateOutListeners.add(l);
+    }
+
+    /**
+     * Add a listener to the standard error output, that will be noified
+     * immediately as soon as the output text is available. It does not wait
+     * for the new line and does not send output line-by-line.
+     */
+    public void addImmediateTextErrorListener(TextOutputListener l) {
+        this.stdImmediateErrListeners.add(l);
+    }
+
+    void stdOutput(String line) {
+        stdoutListener.outputLine(line);
+        if (dataRegex != null) {
+            stdoutDataListener.outputData(ExternalCommand.matchToStringArray(dataRegex, line));
+        }
+    }
+    
+    void immediateStdOutput(String text) {
+        Iterator it = stdImmediateOutListeners.iterator();
+        while(it.hasNext()) {
+            ((TextOutputListener) it.next()).outputLine(text);
+        }
+    }
+    
+    void errOutput(String line) {
+        stderrListener.outputLine(line);
+        if (errorRegex != null) {
+            stderrDataListener.outputData(ExternalCommand.matchToStringArray(errorRegex, line));
+        }
+    }
+    
+    void immediateErrOutput(String text) {
+        Iterator it = stdImmediateErrListeners.iterator();
+        while(it.hasNext()) {
+            ((TextOutputListener) it.next()).outputLine(text);
+        }
+        
+    }
+    
+    void setSucceeded() {
+        synchronized (success) {
+            success[0] = Boolean.TRUE;
+            success.notify();
+        }
+    }
+    
+    void setFailed() {
+        synchronized (success) {
+            success[0] = Boolean.FALSE;
+            success.notify();
+        }
+    }
+    
+    boolean succeeded() {
+        return success[0].booleanValue();
+    }
+    
+    public void waitFinished() throws InterruptedException {
+        synchronized (success) {
+            if (success[0] == null) {
+                success.wait();
+            }
+        }
+    }
+    
+}
---
+++ vcs.profiles.pvcs/src/org/netbeans/modules/vcs/profiles/pvcs/commands/PCLICommandExecutor.java
@@ -0,0 +1,380 @@
+/*
+ *                 Sun Public License Notice
+ * 
+ * The contents of this file are subject to the Sun Public License
+ * Version 1.0 (the "License"). You may not use this file except in
+ * compliance with the License. A copy of the License is available at
+ * http://www.sun.com/
+ * 
+ * The Original Code is NetBeans. The Initial Developer of the Original
+ * Code is Sun Microsystems, Inc. Portions Copyright 1997-2004 Sun
+ * Microsystems, Inc. All Rights Reserved.
+ */
+
+package org.netbeans.modules.vcs.profiles.pvcs.commands;
+
+import java.io.File;
+import java.io.IOException;
+import java.io.OutputStream;
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+
+import org.openide.ErrorManager;
+import org.openide.filesystems.FileLock;
+import org.openide.filesystems.FileObject;
+import org.openide.filesystems.FileSystem;
+import org.openide.filesystems.FileUtil;
+import org.openide.util.RequestProcessor;
+
+import org.netbeans.modules.vcscore.cmdline.exec.ExternalCommand;
+import org.netbeans.modules.vcscore.cmdline.exec.StructuredExec;
+import org.netbeans.modules.vcscore.commands.TextOutputListener;
+
+/**
+ * The efficient executor of PCLI commands.
+ * It runs a script that performs serial execution of PCLI commands without
+ * a necessity to start one JVM per each command.
+ *
+ * @author  Martin Entlicher
+ */
+public class PCLICommandExecutor implements Runnable {
+    
+    private static final String PVCS_FOLDER = "PVCS";
+    private static final String PCLI_SCRIPT_NAME = "PCLIRunScript";
+    private static final String PCLI_CMD_SUCCEEDED = PCLI_SCRIPT_NAME+" Command succeeded";
+    private static final String PCLI_CMD_FAILED = PCLI_SCRIPT_NAME+" Command failed";
+    private static final String PCLI_SCRIPT =
+        "While Test 1=1\n"+
+        "{\n"+
+        //"  echo -n \"PCLI\"\n"+
+        "  readline -vCMD\n"+
+        "  run -ns $CMD\n"+
+        "  set -vEXIT_CODE $?\n"+
+        "  if test $EXIT_CODE=0\n"+
+        "  {\n"+
+        "    echo "+PCLI_CMD_SUCCEEDED+"\n"+
+        "  }\n"+
+        "  else\n"+
+        "  {\n"+
+        "    echo "+PCLI_CMD_FAILED+"\n"+
+        "  }\n"+
+        "}";
+    private static final String[] PCLI_EXEC = { "pcli", "run", "-s" };
+    private static final String PROMPT = ">";
+    
+    private static PCLICommandExecutor instance;
+    
+    private File pcliScript;
+    private RequestProcessor.Task pcliTask;
+    private Object processLock = new Object();
+    private StructuredExec sexec;
+    private ExternalCommand cmd;
+    private Thread[] pcliThread = new Thread[] { null };
+    private volatile PCLICommand commandToProcess;
+    /** This is set to true after we have ">" prompt. */
+    private boolean[] canSendInput = new boolean[] { false };
+    
+    /** Creates a new instance of PCLICommandExecutor */
+    private PCLICommandExecutor() {
+        initScript();
+        Runtime.getRuntime().addShutdownHook(new ShutdownHook());
+    }
+    
+    public static synchronized PCLICommandExecutor getDefault() {
+        if (instance == null) {
+            instance = new PCLICommandExecutor();
+        }
+        return instance;
+    }
+    
+    private void initScript() {
+        FileSystem dfs = org.openide.filesystems.Repository.getDefault().getDefaultFileSystem();
+        FileObject vcsfo = dfs.findResource("vcs");
+        FileObject pvcsfo = vcsfo.getFileObject(PVCS_FOLDER);
+        if (pvcsfo == null) {
+            try {
+                pvcsfo = vcsfo.createFolder(PVCS_FOLDER);
+            } catch (IOException ioex) {
+                ErrorManager.getDefault().notify(ioex);
+                return ;
+            }
+        }
+        FileObject pclifo = pvcsfo.getFileObject(PCLI_SCRIPT_NAME);
+        if (pclifo == null) {
+            try {
+                pclifo = pvcsfo.createData(PCLI_SCRIPT_NAME);
+            } catch (IOException ioex) {
+                ErrorManager.getDefault().notify(ioex);
+                return ;
+            }
+            OutputStream out = null;
+            FileLock lock = null;
+            try {
+                lock = pclifo.lock();
+                out = pclifo.getOutputStream(lock);
+                out.write(PCLI_SCRIPT.getBytes());
+            } catch (IOException ioex) {
+                if (lock != null) {
+                    lock.releaseLock();
+                    lock = null;
+                }
+                try {
+                    pclifo.delete();
+                } catch (IOException ioex2) {}
+                return ;
+            } finally {
+                if (out != null) {
+                    try {
+                        out.close();
+                    } catch (IOException ioex) {}
+                }
+                if (lock != null) {
+                    lock.releaseLock();
+                }
+            }
+        }
+        pcliScript = FileUtil.toFile(pclifo);
+        PCLI_EXEC[2] += pcliScript.getAbsolutePath();
+        sexec = new StructuredExec(null, PCLI_EXEC[0],
+                                   new StructuredExec.Argument[] {
+                                       new StructuredExec.Argument(PCLI_EXEC[1], false),
+                                       new StructuredExec.Argument(PCLI_EXEC[2], false)
+                                   });
+    }
+    
+    public synchronized boolean runCommand(PCLICommand commandToProcess) throws InterruptedException {
+        if (!isPCLIProcessLoopRunning()) {
+            startPCLIProcessLoop();
+        }
+        synchronized (canSendInput) {
+            if (!canSendInput[0]) {
+                canSendInput.wait();
+                try {
+                    // wait a while for all the previous output to finish.
+                    Thread.currentThread().sleep(100);
+                } catch (InterruptedException iex) {
+                    stopPCLIProcessLoop();
+                    throw iex;
+                }
+            }
+        }
+        this.commandToProcess = commandToProcess;
+        String cmdStr = commandToProcess.getStrExec();
+        cmd.sendInput(cmdStr+"\n");
+        try {
+            commandToProcess.waitFinished();
+        } catch (InterruptedException iex) {
+            stopPCLIProcessLoop();
+            throw iex;
+        } finally {
+            this.commandToProcess = null;
+        }
+        return commandToProcess.succeeded();
+    }
+    
+    private void startPCLIProcessLoop() {
+        //pcliTask = RequestProcessor.getDefault().post(this);
+        synchronized (canSendInput) {
+            canSendInput[0] = false;
+        }
+        runScript();
+    }
+    
+    private void stopPCLIProcessLoop() {
+        if (pcliThread != null) {
+            synchronized (canSendInput) {
+                if (!canSendInput[0]) {
+                    try {
+                        canSendInput.wait();
+                    } catch (InterruptedException iex) {}
+                }
+            }
+            cmd.sendInput("exit\n");
+        }
+        pcliTask.waitFinished();
+    }
+    
+    private void kill() {
+        synchronized (pcliThread) {
+            if (pcliThread[0] != null) {
+                pcliThread[0].interrupt();
+            }
+        }
+    }
+    
+    private boolean isPCLIProcessLoopRunning() {
+        return pcliTask != null && !pcliTask.isFinished();
+    }
+    
+    private void runScript() {
+        cmd = new ExternalCommand(sexec);
+        cmd.addImmediateTextOutputListener(new PCLIStandardOutputListener());
+        cmd.addImmediateTextErrorListener(new PCLIErrorOutputListener());
+        pcliTask = RequestProcessor.getDefault().post(this);
+    }
+    
+    public void run() {
+        synchronized (pcliThread) {
+            pcliThread[0] = Thread.currentThread();
+        }
+        try {
+            cmd.exec();
+        } finally {
+            synchronized (pcliThread) {
+                pcliThread[0] = null;
+            }
+        }
+    }
+    
+    private class PCLIStandardOutputListener implements TextOutputListener {
+        
+        private String lastLine;
+        
+        // Gets immediate output
+        public void outputLine(String text) {
+            //System.out.println("PCLIStandardOutputListener.outputLine("+text+"), commandToProcess = "+commandToProcess);
+            String[] lines = buildLines(text);
+            Boolean finished = null;
+            for (int i = 0; i < lines.length; i++) {
+                String line = lines[i];
+                if (PCLI_CMD_SUCCEEDED.equals(line)) {
+                    synchronized (canSendInput) {
+                        canSendInput[0] = false;
+                    }
+                    text = removeLine(text, line);
+                    finished = Boolean.TRUE;
+                } else if (PCLI_CMD_FAILED.equals(line)) {
+                    synchronized (canSendInput) {
+                        canSendInput[0] = false;
+                    }
+                    text = removeLine(text, line);
+                    finished = Boolean.FALSE;
+                } else {
+                    if (commandToProcess != null) {
+                        commandToProcess.stdOutput(line);
+                    }
+                }
+            }
+            synchronized (canSendInput) {
+                if (!canSendInput[0] && PROMPT.equals(lastLine)) {
+                    lastLine = null;
+                    canSendInput[0] = true;
+                    text = text.substring(0, text.length() - PROMPT.length());
+                    canSendInput.notify();
+                }
+            }
+            if (commandToProcess != null) {
+                commandToProcess.immediateStdOutput(text);
+                if (finished != null) {
+                    if (finished.booleanValue()) {
+                        commandToProcess.setSucceeded();
+                    } else {
+                        commandToProcess.setFailed();
+                    }
+                }
+            }
+        }
+        
+        private List linesBuffer = new ArrayList();
+        
+        private String[] buildLines(String text) {
+            if (lastLine != null) {
+                text = lastLine + text;
+                lastLine = null;
+            }
+            for (int pos = 0; pos < text.length(); ) {
+                int newline = text.indexOf('\n', pos);
+                if (newline >= 0) {
+                    String line = text.substring(pos, newline);
+                    if (line.endsWith("\r")) line = line.substring(0, line.length() - 1);
+                    if (line.startsWith("\r")) line = line.substring(1);
+                    linesBuffer.add(line);
+                    pos = newline + 1;
+                } else {
+                    lastLine = text.substring(pos);
+                    break;
+                }
+            }
+            String[] lines = (String[]) linesBuffer.toArray(new String[0]);
+            linesBuffer.clear();
+            return lines;
+        }
+        
+        private String removeLine(String text, String line) {
+            int i = text.indexOf(line);
+            if (i >= 0) {
+                int j = i + line.length();
+                if (text.length() > j + 1 && text.charAt(j) == '\n') j++;
+                text = text.substring(0, i) + text.substring(j);
+            }
+            return text;
+        }
+        
+    }
+    
+    private class PCLIErrorOutputListener implements TextOutputListener {
+        
+        private String lastLine;
+        
+        // Gets immediate output
+        public void outputLine(String text) {
+            //System.out.println("PCLIErrorOutputListener.outputLine("+text+"), commandToProcess = "+commandToProcess);
+            if (commandToProcess == null) return ; // Throw the output if no one listens.
+            String[] lines = buildLines(text);
+            for (int i = 0; i < lines.length; i++) {
+                commandToProcess.errOutput(lines[i]);
+            }
+            commandToProcess.immediateErrOutput(text);
+        }
+        
+        private List linesBuffer = new ArrayList();
+        
+        private String[] buildLines(String text) {
+            if (lastLine != null) {
+                text = lastLine + text;
+                lastLine = null;
+            }
+            for (int pos = 0; pos < text.length(); ) {
+                int newline = text.indexOf('\n', pos);
+                if (newline >= 0) {
+                    String line = text.substring(pos, newline);
+                    if (line.endsWith("\r")) line = line.substring(0, line.length() - 1);
+                    if (line.startsWith("\r")) line = line.substring(1);
+                    linesBuffer.add(line);
+                    pos = newline + 1;
+                } else {
+                    lastLine = text.substring(pos);
+                    break;
+                }
+            }
+            String[] lines = (String[]) linesBuffer.toArray(new String[0]);
+            linesBuffer.clear();
+            return lines;
+        }
+        
+    }
+    
+    /**
+     * Shuts down the PCLI script.
+     */
+    private static class ShutdownHook extends Thread {
+        
+        public void run() {
+            Thread stopThread = new Thread() {
+                public void run() {
+                    PCLICommandExecutor.getDefault().stopPCLIProcessLoop();
+                }
+            };
+            stopThread.start();
+            try {
+                stopThread.join(5000);
+            } catch (InterruptedException iex) {}
+            if (stopThread.isAlive()) {
+                PCLICommandExecutor.getDefault().kill();
+            }
+        }
+        
+    }
+    
+}
