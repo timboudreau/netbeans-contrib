6e014201d5eff45c2781b927cbc4e808a958db66
mentlicher@netbeans.org <mentlicher@netbeans.org>
2004-05-16 17:53:03 +0000

    New Profile action added on the profiles node.

--- vcs.advanced/src/org/netbeans/modules/vcs/advanced/profilesSettings/VcsSettingsNode.java
+++ vcs.advanced/src/org/netbeans/modules/vcs/advanced/profilesSettings/VcsSettingsNode.java
@@ -16,7 +16,16 @@ package org.netbeans.modules.vcs.advanced.profilesSettings;
 import java.awt.Dialog;
 import java.awt.event.ActionListener;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.HashSet;
 import java.util.ResourceBundle;
+
+import org.netbeans.modules.vcs.advanced.ProfilesFactory;
+import org.netbeans.modules.vcs.advanced.commands.ConditionedCommands;
+import org.netbeans.modules.vcs.advanced.commands.ConditionedCommandsBuilder;
+import org.netbeans.modules.vcs.advanced.variables.ConditionedVariables;
+import org.netbeans.modules.vcscore.commands.CommandsTree;
 
 import org.openide.nodes.Node;
 import org.openide.nodes.AbstractNode;
@@ -26,11 +35,13 @@ import org.openide.nodes.CookieSet;
 import org.openide.util.HelpCtx;
 import org.openide.util.NbBundle;
 import org.openide.util.actions.SystemAction;
+import org.openide.util.datatransfer.NewType;
 import org.openide.actions.NewAction;
 import org.openide.actions.PropertiesAction;
 import org.openide.actions.ToolsAction;
 import org.openide.NotifyDescriptor;
 import org.openide.DialogDescriptor;
+import org.openide.DialogDisplayer;
 
 
 /** 
@@ -41,7 +52,7 @@ import org.openide.DialogDescriptor;
 public class VcsSettingsNode extends AbstractNode {
     
     /** Array of the actions of the java methods, constructors and fields. */
-    private static final SystemAction[] DEFAULT_ACTIONS = new SystemAction[0];
+    private static final SystemAction[] DEFAULT_ACTIONS = new SystemAction[] { SystemAction.get(NewAction.class) } ;
     
     public static final String ICON_BASE =
     "org/netbeans/modules/vcs/advanced/vcsGeneric"; // NOI18N         
@@ -75,6 +86,35 @@ public class VcsSettingsNode extends AbstractNode {
         static final long serialVersionUID =-3256331604791682300L;
         public Node getNode() {
             return new VcsSettingsNode();
+        }
+    }
+    
+    /** Get the new types that can be created in this node.
+     */
+    public NewType[] getNewTypes() {
+        //if (list == null) return new NewType[0];
+        return new NewType[] { new NewProfile() };
+    }
+    
+    private final class NewProfile extends NewType {
+
+        public String getName() {
+            return org.openide.util.NbBundle.getBundle(VcsSettingsNode.class).getString("CTL_NewProfile_ActionName");
+        }
+        
+        public void create() throws java.io.IOException {
+            NotifyDescriptor.InputLine input = new NotifyDescriptor.InputLine(
+                org.openide.util.NbBundle.getBundle(VcsSettingsNode.class).getString("CTL_NewProfileName"),
+                org.openide.util.NbBundle.getBundle(VcsSettingsNode.class).getString("CTL_NewProfileTitle")
+                );
+            //input.setInputText(org.openide.util.NbBundle.getBundle(CommandNode.class).getString("CTL_NewCommandLabel"));
+            if (DialogDisplayer.getDefault().notify(input) != NotifyDescriptor.OK_OPTION)
+                return;
+
+            String profileName = input.getInputText();
+            ProfilesFactory.getDefault().addProfile(profileName, profileName,
+                new HashSet(), new HashSet(), new ConditionedVariables(new ArrayList(), new HashMap(), new HashMap()),
+                new ConditionedCommandsBuilder(CommandsTree.EMPTY).getConditionedCommands());
         }
     }
     
