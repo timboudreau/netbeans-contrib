5245f5cf17b72403d82433549f29994c1dd8fcff
pkuzel@netbeans.org <pkuzel@netbeans.org>
2004-07-15 16:13:56 +0000

    NO SEMANTICAL CHANGE - refomatting.

--- tasklist.bugs/src/org/netbeans/modules/tasklist/bugs/bugzilla/BZBugEngine.java
+++ tasklist.bugs/src/org/netbeans/modules/tasklist/bugs/bugzilla/BZBugEngine.java
@@ -13,51 +13,15 @@
 
 package org.netbeans.modules.tasklist.bugs.bugzilla;
 
-import java.beans.PropertyChangeListener;
-import java.beans.PropertyChangeSupport;
-import java.io.File;
-import java.io.IOException;
-import java.io.OutputStream;
-import java.io.PrintWriter;
-import java.io.Writer;
-import java.io.FileWriter;
-import java.text.DateFormat;
-import java.text.ParseException;
-import java.text.SimpleDateFormat;
-import java.util.ArrayList;
 import java.util.LinkedList;
-import java.util.Date;
-import java.util.HashMap;
-import java.util.List;
-import java.util.ListIterator;
-import java.util.Set;
-import javax.swing.plaf.basic.BasicFileChooserUI;
-import java.util.SimpleTimeZone;
-import java.util.TimeZone;
-import java.util.HashMap;
 import java.net.URL;
 import java.net.MalformedURLException;
 import java.text.MessageFormat;
 
 import org.openide.ErrorManager;
-import org.openide.NotifyDescriptor;
-import org.openide.filesystems.FileLock;
-import org.openide.filesystems.Repository;
-import org.openide.filesystems.FileObject;
-import org.openide.filesystems.FileSystem;
-import org.openide.filesystems.FileUtil;
-import org.openide.loaders.DataFolder;
-import org.openide.loaders.DataObject;
-import org.openide.loaders.DataObjectNotFoundException;
-import org.openide.loaders.XMLDataObject;
 import org.openide.util.NbBundle;
-import org.openide.cookies.InstanceCookie;
 import org.openide.util.RequestProcessor;
 import org.openide.util.Utilities;
-
-import org.w3c.dom.*;
-import org.xml.sax.*;
-import org.openide.xml.*;
 
 import org.netbeans.modules.tasklist.core.TaskListView;
 import org.netbeans.modules.tasklist.bugs.*;
@@ -66,40 +30,42 @@ import org.openide.awt.StatusDisplayer;
 
 /**
  * Bridge which provides Bugzilla data to the BugList
- * This class is almost exactally the same as IZBugEngine.  
+ * This class is almost exactally the same as IZBugEngine.
+ *
  * @author Tor Norbye, serff
  */
 public class BZBugEngine implements BugEngine { // XXX remove the publicness
 
     private BugList list = null;
-    
+
     public BZBugEngine(BugList list) {
-	this.list = list;
+        this.list = list;
     }
 
-    /** Return the user name of the engine */
+    /**
+     * Return the user name of the engine
+     */
     public String getName() {
-	return (NbBundle.getMessage(BZBugEngine.class, "BugZilla")); // NOI18N;
+        return (NbBundle.getMessage(BZBugEngine.class, "BugZilla")); // NOI18N;
     }
-    
+
     public void refresh(final BugQuery query) {
         // Do in the background
         RequestProcessor.postRequest(new Runnable() {
-                public void run() {
-		    doRefresh(query);
-		}
-	    }
-        );
+            public void run() {
+                doRefresh(query);
+            }
+        });
     }
 
-    /** 
+    /**
      * TODO Change this so it adds pugs to the model one at a time.  I hate waiting...
      */
     public void doRefresh(BugQuery inQuery) {
         TaskListView v = TaskListView.getCurrent();
         BugsView view = null;
         if (v instanceof BugsView) {
-            view = (BugsView)v;
+            view = (BugsView) v;
             view.setCursor(Utilities.createProgressCursor(view));
         }
         try {
@@ -108,16 +74,14 @@ public class BZBugEngine implements BugEngine { // XXX remove the publicness
             String query = inQuery.getQueryString();
             if ((baseurl == null || baseurl.equals("")) || (query == null || query.equals(""))) {
                 //They didn't enter anything on the gui
-                StatusDisplayer.getDefault().setStatusText(
-                                    NbBundle.getMessage(BZBugEngine.class, 
-                                                  "BadQuery")); // NOI18N
+                StatusDisplayer.getDefault().setStatusText(NbBundle.getMessage(BZBugEngine.class,
+                        "BadQuery")); // NOI18N
                 return;
             }
             System.out.println("Baseurl = " + baseurl + " query = " + query);
 
-            StatusDisplayer.getDefault().setStatusText(
-                              NbBundle.getMessage(BZBugEngine.class, 
-                                                  "Refreshing")); // NOI18N
+            StatusDisplayer.getDefault().setStatusText(NbBundle.getMessage(BZBugEngine.class,
+                    "Refreshing")); // NOI18N
             URL url = null;
             try {
                 url = new URL(baseurl);
@@ -127,9 +91,8 @@ public class BZBugEngine implements BugEngine { // XXX remove the publicness
             if (url != null) {
                 Bugzilla bz = new Bugzilla(url);
                 try {
-                    StatusDisplayer.getDefault().setStatusText(
-                                    NbBundle.getMessage(BZBugEngine.class, 
-                                                  "DoingQuery")); // NOI18N
+                    StatusDisplayer.getDefault().setStatusText(NbBundle.getMessage(BZBugEngine.class,
+                            "DoingQuery")); // NOI18N
                     int bugids[] = bz.query(query);
 
                     // Provide some update
@@ -137,27 +100,25 @@ public class BZBugEngine implements BugEngine { // XXX remove the publicness
                     int n = bugids.length;
                     LinkedList issues = new LinkedList();
                     for (int i = 0; i < n; i++) {
-                        StatusDisplayer.getDefault().setStatusText(
-                                       MessageFormat.format(
-                                        NbBundle.getMessage(BZBugEngine.class, 
-                                                 "QueryingBug"), // NOI18N
-                                        new String[] { Integer.toString(bugids[i]) }));
+                        StatusDisplayer.getDefault().setStatusText(MessageFormat.format(NbBundle.getMessage(BZBugEngine.class,
+                                "QueryingBug"), // NOI18N
+                                new String[]{Integer.toString(bugids[i])}));
 
                         Issue izbug = bz.getBug(bugids[i]);
 
                         Bug bug = new Bug(Integer.toString(izbug.getId()),
-                                          izbug.getSummary(),
-                                          izbug.getPriority(),
-                                          izbug.getType(),
-                                          izbug.getComponent(),
-                                          izbug.getSubcomponent(),
-                                          izbug.getCreated(),
-                                          "",
-                                          izbug.getAssignedTo(),
-                                          izbug.getReportedBy(),
-                                          izbug.getStatus(),
-                                          izbug.getTargetMilestone(),
-                                          0);
+                                izbug.getSummary(),
+                                izbug.getPriority(),
+                                izbug.getType(),
+                                izbug.getComponent(),
+                                izbug.getSubcomponent(),
+                                izbug.getCreated(),
+                                "",
+                                izbug.getAssignedTo(),
+                                izbug.getReportedBy(),
+                                izbug.getStatus(),
+                                izbug.getTargetMilestone(),
+                                0);
                         bug.setEngine(this);
 
                         issues.add(bug);
@@ -169,11 +130,9 @@ public class BZBugEngine implements BugEngine { // XXX remove the publicness
                     ErrorManager.getDefault().notify(se);
                     System.out.println("Couldn't read bug list: sax exception");
                 } catch (java.net.UnknownHostException uhe) {
-                    StatusDisplayer.getDefault().setStatusText(
-                                       MessageFormat.format(
-                                        NbBundle.getMessage(BZBugEngine.class, 
-                                                 "NoNet"), // NOI18N
-                                        new String[] { baseurl }));
+                    StatusDisplayer.getDefault().setStatusText(MessageFormat.format(NbBundle.getMessage(BZBugEngine.class,
+                            "NoNet"), // NOI18N
+                            new String[]{baseurl}));
 
                 } catch (java.io.IOException ioe) {
                     ErrorManager.getDefault().notify(ioe);
@@ -188,7 +147,9 @@ public class BZBugEngine implements BugEngine { // XXX remove the publicness
         }
     }
 
-    /** View a particular bug. */
+    /**
+     * View a particular bug.
+     */
     public void viewBug(Bug bug, String server) {
 //	String urlstring = "http://192.168.3.3/bugzilla/show_bug.cgi?id=" + bug.getId();
 //        String urlstring = "http://bugzilla.mozilla.org/show_bug.cgi?id=" + bug.getId();
--- tasklist.bugs/src/org/netbeans/modules/tasklist/bugs/issuezilla/IZBugEngine.java
+++ tasklist.bugs/src/org/netbeans/modules/tasklist/bugs/issuezilla/IZBugEngine.java
@@ -13,57 +13,17 @@
 
 package org.netbeans.modules.tasklist.bugs.issuezilla;
 
-import java.beans.PropertyChangeListener;
-import java.beans.PropertyChangeSupport;
-import java.io.File;
-import java.io.IOException;
-import java.io.OutputStream;
-import java.io.PrintWriter;
-import java.io.Writer;
-import java.io.FileWriter;
-import java.text.DateFormat;
-import java.text.ParseException;
-import java.text.SimpleDateFormat;
-import java.util.ArrayList;
 import java.util.LinkedList;
-import java.util.Date;
-import java.util.HashMap;
-import java.util.List;
-import java.util.ListIterator;
-import java.util.Set;
-import javax.swing.plaf.basic.BasicFileChooserUI;
-import java.util.SimpleTimeZone;
-import java.util.TimeZone;
-import java.util.HashMap;
 import java.net.URL;
 import java.net.MalformedURLException;
 import java.text.MessageFormat;
 
 import org.openide.ErrorManager;
-import org.openide.NotifyDescriptor;
-import org.openide.filesystems.FileLock;
-import org.openide.filesystems.Repository;
-import org.openide.filesystems.FileObject;
-import org.openide.filesystems.FileSystem;
-import org.openide.filesystems.FileUtil;
-import org.openide.loaders.DataFolder;
-import org.openide.loaders.DataObject;
-import org.openide.loaders.DataObjectNotFoundException;
-import org.openide.loaders.XMLDataObject;
 import org.openide.util.NbBundle;
-import org.openide.cookies.InstanceCookie;
 import org.openide.util.RequestProcessor;
 import org.openide.util.Utilities;
 
-import org.w3c.dom.*;
-import org.xml.sax.*;
-import org.openide.xml.*;
-
 import org.netbeans.modules.tasklist.core.TaskListView;
-
-//import org.netbeans.nbbuild.Issuezilla;
-//import org.netbeans.nbbuild.Issue;
-
 
 import org.netbeans.modules.tasklist.bugs.*;
 import org.openide.awt.HtmlBrowser;
@@ -71,43 +31,45 @@ import org.openide.awt.StatusDisplayer;
 
 /**
  * Bridge which provides Issuezilla data to the BugList
+ *
  * @author Tor Norbye
  */
-public class IZBugEngine implements BugEngine { // XXX remove the publicness
+public final class IZBugEngine implements BugEngine { // XXX remove the publicness
 
     private BugList list = null;
-    
+
     public IZBugEngine(BugList list) {
-	this.list = list;
+        this.list = list;
     }
 
-    /** Return the user name of the engine */
+    /**
+     * Return the user name of the engine
+     */
     public String getName() {
-	return (NbBundle.getMessage(IZBugEngine.class, "IssueZilla")); // NOI18N;
+        return (NbBundle.getMessage(IZBugEngine.class, "IssueZilla")); // NOI18N;
     }
-    
+
     public void refresh(final BugQuery query) {
         // Do in the background
         RequestProcessor.postRequest(new Runnable() {
-                public void run() {
-		    doRefresh(query);
-		}
-	    }
-        );
+            public void run() {
+                doRefresh(query);
+            }
+        });
     }
 
     public void doRefresh(BugQuery inQuery) {
         TaskListView v = TaskListView.getCurrent();
         BugsView view = null;
         if (v instanceof BugsView) {
-            view = (BugsView)v;
+            view = (BugsView) v;
             view.setCursor(Utilities.createProgressCursor(view));
         }
         try {
-            
-        // Do a bug query
-        String query = null;
-        String baseurl = null;
+
+            // Do a bug query
+            String query = null;
+            String baseurl = null;
 //        query = System.getProperty("netbeans.tasklist.bugquery");
 //        if (query == null) {
 //            // TEMPORARY HACK     TODO
@@ -124,105 +86,100 @@ public class IZBugEngine implements BugEngine { // XXX remove the publicness
 //                TopManager.getDefault().notify(new NotifyDescriptor.Message("Tasklist bug summary:\nAdd -J-Dnetbeans.tasklist.bugquery=You must have the full URL for this query (Ex. http://www.netbeans.org/issues/buglist.cgi?<query>"));
 //            }
 //        }
-        baseurl = inQuery.getBaseUrl() + "/buglist.cgi?";
-        query = inQuery.getQueryString();
+            baseurl = inQuery.getBaseUrl() + "/buglist.cgi?";
+            query = inQuery.getQueryString();
 
-        if ((baseurl == null || baseurl.equals("")) || (query == null || query.equals(""))) {
-            //They didn't enter anything on the gui
-            StatusDisplayer.getDefault().setStatusText(
-                                NbBundle.getMessage(IZBugEngine.class, 
-                                              "BadQuery")); // NOI18N
-            return;
-        }
-        
-        //String query= "issue_type=DEFECT&component=projects&issue_status=UNCONFIRMED&issue_status=NEW&issue_status=STARTED&issue_status=REOPENED&version=4.0+dev&email1=&emailtype1=substring&emailassigned_to1=1&email2=&emailtype2=substring&emailreporter2=1&issueidtype=include&issue_id=&changedin=&votes=&chfieldfrom=&chfieldto=Now&chfieldvalue=&short_desc=&short_desc_type=substring&long_desc=&long_desc_type=substring&issue_file_loc=&issue_file_loc_type=substring&status_whiteboard=&status_whiteboard_type=substring&keywords=&keywords_type=anywords&field0-0-0=noop&type0-0-0=noop&value0-0-0=&cmdtype=doit&newqueryname=&order=Reuse+same+sort+as+last+time";
+            if ((baseurl == null || baseurl.equals("")) || (query == null || query.equals(""))) {
+                //They didn't enter anything on the gui
+                StatusDisplayer.getDefault().setStatusText(NbBundle.getMessage(IZBugEngine.class,
+                        "BadQuery")); // NOI18N
+                return;
+            }
+
+            //String query= "issue_type=DEFECT&component=projects&issue_status=UNCONFIRMED&issue_status=NEW&issue_status=STARTED&issue_status=REOPENED&version=4.0+dev&email1=&emailtype1=substring&emailassigned_to1=1&email2=&emailtype2=substring&emailreporter2=1&issueidtype=include&issue_id=&changedin=&votes=&chfieldfrom=&chfieldto=Now&chfieldvalue=&short_desc=&short_desc_type=substring&long_desc=&long_desc_type=substring&issue_file_loc=&issue_file_loc_type=substring&status_whiteboard=&status_whiteboard_type=substring&keywords=&keywords_type=anywords&field0-0-0=noop&type0-0-0=noop&value0-0-0=&cmdtype=doit&newqueryname=&order=Reuse+same+sort+as+last+time";
 
 
-	StatusDisplayer.getDefault().setStatusText(
-                          NbBundle.getMessage(IZBugEngine.class, 
-                                              "Refreshing")); // NOI18N
-	URL url = null;
-	try {
-	    url = new URL(baseurl);
-	} catch (MalformedURLException e) {
-	    ErrorManager.getDefault().notify(e);
-	}
-	if (url != null) {
-	    Issuezilla iz = new Issuezilla(url);
-	    try {
-		StatusDisplayer.getDefault().setStatusText(
-                                NbBundle.getMessage(IZBugEngine.class, 
-                                              "DoingQuery")); // NOI18N
+            StatusDisplayer.getDefault().setStatusText(NbBundle.getMessage(IZBugEngine.class,
+                    "Refreshing")); // NOI18N
+            URL url = null;
+            try {
+                url = new URL(baseurl);
+            } catch (MalformedURLException e) {
+                ErrorManager.getDefault().notify(e);
+            }
+            if (url != null) {
+                Issuezilla iz = new Issuezilla(url);
+                try {
+                    StatusDisplayer.getDefault().setStatusText(NbBundle.getMessage(IZBugEngine.class,
+                            "DoingQuery")); // NOI18N
 
-		int bugids[] = iz.query(query);
-		
-		// Provide some update
-		
-		int n = bugids.length;
-		LinkedList issues = new LinkedList();
-		for (int i = 0; i < n; i++) {
-		    StatusDisplayer.getDefault().setStatusText(
-                                   MessageFormat.format(
-                                    NbBundle.getMessage(IZBugEngine.class, 
-					     "QueryingBug"), // NOI18N
-				    new String[] { Integer.toString(bugids[i]) }));
-		    
-		    Issue izbug = iz.getBug(bugids[i]);
+                    int bugids[] = iz.query(query);
 
-		    Bug bug = new Bug(Integer.toString(izbug.getId()),
-				      izbug.getSummary(),
-				      izbug.getPriority(),
-				      izbug.getType(),
-				      izbug.getComponent(),
-				      izbug.getSubcomponent(),
-				      izbug.getCreated(),
-				      izbug.getKeywords(),
-				      izbug.getAssignedTo(),
-				      izbug.getReportedBy(),
-				      izbug.getStatus(),
-				      izbug.getTargetMilestone(),
-				      izbug.getVotes());
-		    bug.setEngine(this);
-		    
-		    issues.add(bug);
-		}
-		
-		// Successful list fetch -- replace the contents
-		list.setBugs(issues);
-	    } catch (org.xml.sax.SAXException se) {
-		ErrorManager.getDefault().notify(se);
-		System.out.println("Couldn't read bug list: sax exception");
-            } catch (java.net.UnknownHostException uhe) {
-                StatusDisplayer.getDefault().setStatusText(
-                                   MessageFormat.format(
-                                    NbBundle.getMessage(IZBugEngine.class, 
-					     "NoNet"), // NOI18N
-				    new String[] { baseurl }));
+                    // Provide some update
 
-	    } catch (java.io.IOException ioe) {
-		ErrorManager.getDefault().notify(ioe);
-		System.out.println("Couldn't read bug list: io exception");
-	    }
-	    StatusDisplayer.getDefault().setStatusText("");
-	}
+                    int n = bugids.length;
+                    LinkedList issues = new LinkedList();
+                    for (int i = 0; i < n; i++) {
+                        StatusDisplayer.getDefault().setStatusText(MessageFormat.format(NbBundle.getMessage(IZBugEngine.class,
+                                "QueryingBug"), // NOI18N
+                                new String[]{Integer.toString(bugids[i])}));
+
+                        Issue izbug = iz.getBug(bugids[i]);
+
+                        Bug bug = new Bug(Integer.toString(izbug.getId()),
+                                izbug.getSummary(),
+                                izbug.getPriority(),
+                                izbug.getType(),
+                                izbug.getComponent(),
+                                izbug.getSubcomponent(),
+                                izbug.getCreated(),
+                                izbug.getKeywords(),
+                                izbug.getAssignedTo(),
+                                izbug.getReportedBy(),
+                                izbug.getStatus(),
+                                izbug.getTargetMilestone(),
+                                izbug.getVotes());
+                        bug.setEngine(this);
+
+                        issues.add(bug);
+                    }
+
+                    // Successful list fetch -- replace the contents
+                    list.setBugs(issues);
+                } catch (org.xml.sax.SAXException se) {
+                    ErrorManager.getDefault().notify(se);
+                    System.out.println("Couldn't read bug list: sax exception");
+                } catch (java.net.UnknownHostException uhe) {
+                    StatusDisplayer.getDefault().setStatusText(MessageFormat.format(NbBundle.getMessage(IZBugEngine.class,
+                            "NoNet"), // NOI18N
+                            new String[]{baseurl}));
+
+                } catch (java.io.IOException ioe) {
+                    ErrorManager.getDefault().notify(ioe);
+                    System.out.println("Couldn't read bug list: io exception");
+                }
+                StatusDisplayer.getDefault().setStatusText("");
+            }
 
         } finally {
             if (view != null) {
                 view.setCursor(null);
             }
-        } 
+        }
     }
 
-    /** View a particular bug. */
+    /**
+     * View a particular bug.
+     */
     public void viewBug(Bug bug, String service) {
 //	String urlstring = "http://www.netbeans.org/issues/show_bug.cgi?id=" +
 //	    bug.getId();
-	// Show URL
-	try {
-	    URL url = new URL(new URL(service), "issues/show_bug.cgi?id=" + bug.getId());
-	    HtmlBrowser.URLDisplayer.getDefault().showURL(url);
-	} catch (MalformedURLException e) {
-	    ErrorManager.getDefault().notify(e);
-	}	
+        // Show URL
+        try {
+            URL url = new URL(new URL(service), "issues/show_bug.cgi?id=" + bug.getId());
+            HtmlBrowser.URLDisplayer.getDefault().showURL(url);
+        } catch (MalformedURLException e) {
+            ErrorManager.getDefault().notify(e);
+        }
     }
 }
