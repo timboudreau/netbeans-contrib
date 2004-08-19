f299d5c2ebfe1f97d7048dffab117e3cd329cf12
mentlicher@netbeans.org <mentlicher@netbeans.org>
2004-08-19 13:00:02 +0000

    The items in Versoining menu are back as menu presenters. VersioningExplorerAction has still an empty icon. This is basicaly a rollback of issue #46398.

--- vcscore/src/org/netbeans/modules/vcscore/actions/VcsManagerAction.java
+++ vcscore/src/org/netbeans/modules/vcscore/actions/VcsManagerAction.java
@@ -41,10 +41,6 @@ public class VcsManagerAction extends CallableSystemAction {
         return NbBundle.getBundle (VcsManagerAction.class).getString ("CTL_VcsManagerActionName");
     }
 
-    /** Returns the popup presenter in order not to show the icon and align to the left. */
-    public javax.swing.JMenuItem getMenuPresenter() {
-        return getPopupPresenter();
-    }
     /** The action's icon location.
      * @return the action's icon location
      *
