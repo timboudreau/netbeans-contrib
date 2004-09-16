37b5f14e862cf1c8bab3510977c3774244e43402
jskrivanek@netbeans.org <jskrivanek@netbeans.org>
2004-09-16 16:37:28 +0000

    Helpset and index updated.

--- jemmysupport/javahelp/org/netbeans/modules/jemmysupport/docs/jemmysupport.hs
+++ jemmysupport/javahelp/org/netbeans/modules/jemmysupport/docs/jemmysupport.hs
@@ -1,12 +1,20 @@
 <?xml version='1.0' encoding='ISO-8859-1' ?>
 <!--
-*     Copyright 2002 Sun Microsystems, Inc. All rights reserved.
-*     Use is subject to license terms.
+                Sun Public License Notice
+
+The contents of this file are subject to the Sun Public License
+Version 1.0 (the "License"). You may not use this file except in
+compliance with the License. A copy of the License is available at
+http://www.sun.com/
+
+The Original Code is NetBeans. The Initial Developer of the Original
+Code is Sun Microsystems, Inc. Portions Copyright 1997-2004 Sun
+Microsystems, Inc. All Rights Reserved.
 -->
 <!DOCTYPE helpset
-  PUBLIC "-//Sun Microsystems Inc.//DTD JavaHelp HelpSet Version 1.0//EN"
-         "http://java.sun.com/products/javahelp/helpset_1_0.dtd">
-<helpset version="1.0">
+  PUBLIC "-//Sun Microsystems Inc.//DTD JavaHelp HelpSet Version 2.0//EN"
+         "http://java.sun.com/products/javahelp/helpset_2_0.dtd">
+<helpset version="2.0">
 
 
   <!-- title -->
@@ -35,16 +43,11 @@
 
   <view>
     <name>Search</name>
-
-   <label>Search</label>
-
-   <type>javax.help.SearchView</type>
- 
-  <data engine="com.sun.java.help.search.DefaultSearchEngine">
-      JavaHelpSearch2
-
-   </data>
+    <label>Search</label>
+    <type>javax.help.SearchView</type>
+    <data engine="com.sun.java.help.search.DefaultSearchEngine">
+          JavaHelpSearch
+    </data>
   </view>
-
 
 </helpset>
