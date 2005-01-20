12e879451096277c3328001cbc947d6635b91625
pnejedly@netbeans.org <pnejedly@netbeans.org>
2005-01-20 11:26:03 +0000

    Insane module using the new engine.

--- performance.insane/src/org/netbeans/modules/performance/insane/DumpAction.java
+++ performance.insane/src/org/netbeans/modules/performance/insane/DumpAction.java
@@ -7,7 +7,7 @@
  * http://www.sun.com/
  *
  * The Original Code is NetBeans. The Initial Developer of the Original
- * Code is Sun Microsystems, Inc. Portions Copyright 1997-2004 Sun
+ * Code is Sun Microsystems, Inc. Portions Copyright 1997-2005 Sun
  * Microsystems, Inc. All Rights Reserved.
  */
 
@@ -16,8 +16,12 @@ package org.netbeans.modules.performance.insane;
 
 import javax.swing.JFileChooser;
 
+import java.io.File;
+import java.util.*;
+
 import org.openide.util.*;
-import org.netbeans.performance.insane.Insane;
+
+import org.netbeans.insane.scanner.*;
 
 /**
  * Action for UI-driven heap dump.
@@ -40,10 +44,44 @@ public final class DumpAction extends javax.swing.AbstractAction {
         if (JFileChooser.APPROVE_OPTION == chooser.showDialog(null, buttonText)) {
             java.io.File file = chooser.getSelectedFile();
             try {
-                Insane.dump(new Insane.XMLVisitor(file.getAbsolutePath()));
+                dump(file);
             } catch (Exception e) {
                 org.openide.ErrorManager.getDefault().notify(e);
             }
         }
+    }
+    
+    public static void dump(File file) throws Exception {
+        SimpleXmlVisitor visitor = new SimpleXmlVisitor(file);
+        final CountingVisitor counter = new CountingVisitor();
+        ScannerUtils.scanExclusivelyInAWT(
+            null, ScannerUtils.compoundVisitor(new Visitor[] { visitor, counter}),
+            Collections.singleton(Thread.currentThread().getContextClassLoader())
+        );
+
+        visitor.close();
+
+        Set ordered = new TreeSet(new Comparator() {
+            public int compare(Object c1, Object c2) {
+                int diff = counter.getCountForClass((Class)c2) - 
+                           counter.getCountForClass((Class)c1);
+
+                if (diff != 0 || c1 == c2) return diff;
+                return ((Class)c1).getName().compareTo(((Class)c2).getName());
+            }
+        });
+
+        ordered.addAll(counter.getClasses());
+
+        System.out.println("Usage: [instances class.Name: totalSizeInBytes]");
+        for (Iterator it = ordered.iterator(); it.hasNext();) {
+            Class cls = (Class)it.next();
+            System.out.println(counter.getCountForClass(cls) + " " +
+                            cls.getName() + ": " + counter.getSizeForClass(cls));
+        }
+
+        System.out.println("total: " + counter.getTotalSize() + " in " +
+            counter.getTotalCount() + " objects.");
+        System.out.println("Classes:" + counter.getClasses().size());
     }
 }
