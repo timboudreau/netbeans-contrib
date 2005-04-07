06c77dd4ce8ffbc49ca4975a8e5a860b5a2e0ea5
jtulach@netbeans.org <jtulach@netbeans.org>
2005-04-07 17:14:49 +0000

    Definition of the adaptable interface

---
+++ adaptable/src/org/netbeans/api/adaptable/Adaptable.java
@@ -0,0 +1,31 @@
+/*
+ *                 Sun Public License Notice
+ * 
+ * The contents of this file are subject to the Sun Public License
+ * Version 1.0 (the "License"). You may not use this file except in
+ * compliance with the License. A copy of the License is available at
+ * http://www.sun.com/
+ * 
+ * The Original Code is NetBeans. The Initial Developer of the Original
+ * Code is Sun Microsystems, Inc. Portions Copyright 1997-2003 Sun
+ * Microsystems, Inc. All Rights Reserved.
+ */
+
+package org.netbeans.api.adaptable;
+
+/** Generic adaptable interface that represents object that can be
+ * seen in various ways. For example by calling 
+ * <pre>
+ * String adatable.lookup(String.class);
+ * </pre>
+ * one can get view of the object as a string
+ *
+ * @author Jaroslav Tulach
+ */
+public interface Adaptable {
+    /** Tries to look at the adatable object as an instance of what.
+     * @param what the class one requests
+     * @return instance of the class or null if such view is not possible
+     */
+    public <T> T lookup (Class<T> what);
+}
