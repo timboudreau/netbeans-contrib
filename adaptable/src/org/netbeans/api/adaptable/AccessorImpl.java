2f1e359e4a175ccceb619cca6a03df0d51be32f4
jtulach@netbeans.org <jtulach@netbeans.org>
2005-04-07 16:38:58 +0000

    Converting aspects to adaptables

---
+++ adaptable/src/org/netbeans/api/adaptable/AccessorImpl.java
@@ -0,0 +1,27 @@
+/*
+ *                 Sun Public License Notice
+ * 
+ * The contents of this file are subject to the Sun Public License
+ * Version 1.0 (the "License"). You may not use this file except in
+ * compliance with the License. A copy of the License is available at
+ * http://www.sun.com/
+ * 
+ * The Original Code is NetBeans. The Initial Developer of the Original
+ * Code is Sun Microsystems, Inc. Portions Copyright 1997-2003 Sun
+ * Microsystems, Inc. All Rights Reserved.
+ */
+
+package org.netbeans.api.adaptable;
+
+/** Implementation of accessor for friend access to hidden features
+ * of the API.
+ *
+ * @author Jaroslav Tulach
+ */
+final class AccessorImpl extends org.netbeans.modules.adaptable.Accessor {
+    
+    public AspectProvider createAspectProvider(org.netbeans.modules.adaptable.ProviderImpl impl, Object data) {
+        return new AspectProvider (impl, data);
+    }
+    
+}
---
+++ adaptable/src/org/netbeans/modules/adaptable/Accessor.java
@@ -0,0 +1,37 @@
+/*
+ *                 Sun Public License Notice
+ * 
+ * The contents of this file are subject to the Sun Public License
+ * Version 1.0 (the "License"). You may not use this file except in
+ * compliance with the License. A copy of the License is available at
+ * http://www.sun.com/
+ * 
+ * The Original Code is NetBeans. The Initial Developer of the Original
+ * Code is Sun Microsystems, Inc. Portions Copyright 1997-2003 Sun
+ * Microsystems, Inc. All Rights Reserved.
+ */
+
+package org.netbeans.modules.adaptable;
+
+import org.netbeans.api.adaptable.*;
+
+/** Class that allows "friend" calls to api package.
+ *
+ * @author Jaroslav Tulach
+ */
+public abstract class Accessor {
+    /** instance to make calls to api package */
+    public static Accessor API;
+
+    static {
+        // forces initialization of class Aspects that initializes
+        // field API
+        Class c = org.netbeans.api.adaptable.Aspects.class;
+    }
+ 
+    /** Creates new instance of AspectProvider
+     * @param impl the impl to pass to the provider
+     */
+    public abstract AspectProvider createAspectProvider (ProviderImpl impl, Object data);
+    
+}
