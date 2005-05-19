db77bd6db54318e0a0d3beb827d026fae91334c5
thurka@netbeans.org <thurka@netbeans.org>
2005-05-19 12:46:51 +0000

    unused code removed

--- clazz/src/org/netbeans/modules/clazz/Util.java
+++ clazz/src/org/netbeans/modules/clazz/Util.java
@@ -46,130 +46,7 @@ import org.openide.src.Identifier;
 final class Util {
 
     private static RequestProcessor classProcessor;
-    
-    public static final Type getReturnType(String signature){
-        return new SignatureToType(signature).getReturnType();
-    }
-    
-    public static final MethodParameter[] getParametersType(String signature){
-        return new SignatureToType(signature).getMethodParameters();
-    }
-
-    public static class SignatureToType{
-        private char[] signature;        
-        private int len;        
-        private int i = 0;
         
-        private Type returnType = null; //return type
-        private MethodParameter[] params = null;    //return MethodParameter[]
-        
-        public SignatureToType(String signature){
-            this.signature = signature.toCharArray();//.substring(signature.indexOf(')')+1).getBytes();
-            len = this.signature.length;
-            for( ; i < len; i++ ){
-                if (this.signature[i] == '('){
-                    i++;
-                    Type t; 
-                    ArrayList list = new ArrayList();
-                    while( ( t = sigToType()) != null ){
-                        list.add(t);
-                    }                    
-                    params = new MethodParameter[list.size()];
-                    for( int n = 0; n < list.size(); n++){
-                        params[n] = new MethodParameter("", (Type)list.get(n), false);
-                    }
-                }
-                
-                returnType = sigToType();
-            }
-        }
-        
-        public Type getReturnType(){
-            return returnType;
-        }
-        
-        public MethodParameter[] getMethodParameters(){
-            return params;
-        }
-
-        Type sigToType( ) {
-            switch ((char) signature[i]) {
-            case 'B':
-                i++;
-                return Type.BYTE;
-            case 'C':
-                i++;
-                return Type.CHAR;
-            case 'D':
-                i++;
-                return Type.DOUBLE;
-            case 'F':
-                i++;
-                return Type.FLOAT;
-            case 'I':
-                i++;
-                return Type.INT;
-            case 'J':
-                i++;
-                return Type.LONG;
-            case 'L':
-                return classSigToType();
-            case 'S':
-                i++;
-                return Type.SHORT;
-            case 'V':
-                i++;
-                return Type.VOID;
-            case 'Z':
-                i++;
-                return Type.BOOLEAN;
-            case '[':
-                return createArrayType();
-            case ')':
-                i++;
-                return null;
-            default:
-                throw new IllegalArgumentException("Unknown signature"); // NOI18N
-            }
-        }
-        
-        Type classSigToType() {
-            if (signature[i++] == 'L') {
-                int start = i;
-                while (signature[i] != ';' ) i++;
-                //get FQN replace bad chars
-                String fs  = new String(signature, start, i - start ).replace('$', '.').replace('/','.');
-                String s;
-                
-                //filter java.lang
-                int idx;
-                if( fs.startsWith("java.lang") ){ // NOI18N
-                    s = fs.substring( 10 ); //filter 'java.lang.'
-                } else {
-                    s = fs;
-                }
-                i++;
-                return Type.createClass(Identifier.create(fs, s, Identifier.RESOLVED));
-            } else {
-                throw new IllegalArgumentException("Method signature has to start with >>L<<"); // NOI18N
-            }
-        }
-        
-        Type createArrayType(){
-            int depth = 0;
-            for( ; i < len; i++ ){
-                if( (char)signature[i] == '[' )
-                    depth++;
-                else
-                    break;                
-            }
-            Type t = sigToType();
-            for( int n = 0; n < depth; n++)
-                t = Type.createArray(t);
-            return t;
-        }
-    }    
-    
     static String createClassName(String signature) {
         return signature.replace('/', '.').replace('$', '.');
     }
@@ -208,14 +85,6 @@ final class Util {
         throw new IllegalArgumentException("Invalid TypeDescriptor: " + t); // NOI18N
     }
     
-    /**
-     * Finds a DataObject for the specified ClassResource given the project
-     * context
-     */
-    static DataObject findDataObject(Lookup context, Resource resource) {
-        return null;
-    }
-
     static RequestProcessor getClassProcessor() {
         if (classProcessor==null)
             classProcessor=new RequestProcessor("Clazz",5); // NOI18N
