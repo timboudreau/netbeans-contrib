4307fc0593ab17a2fdbf0b7f9ef95f02346b2a17
moser@netbeans.org <moser@netbeans.org>
2005-06-30 12:58:04 +0000

    replaced "artistic" code to add the format action into the SystemAction array for appropriate nodes when the constructor of FormatAction is called by proper entries in layer.xml. This fixes an exception which was reported on nbusers@netbeans.org: http://www.netbeans.org/servlets/ReadMsg?list=nbusers&msgId=980048

--- de.hunsicker.jalopy.plugin.netbeans/src/de/hunsicker/jalopy/plugin/netbeans/FormatAction.java
+++ de.hunsicker.jalopy.plugin.netbeans/src/de/hunsicker/jalopy/plugin/netbeans/FormatAction.java
@@ -15,8 +15,6 @@ package de.hunsicker.jalopy.plugin.netbeans;
 
 import org.openide.cookies.EditorCookie;
 import org.openide.loaders.DataFolder;
-import org.openide.loaders.DataLoader;
-import org.openide.loaders.DataLoaderPool;
 import org.openide.loaders.DataNode;
 import org.openide.loaders.DataObject;
 import org.openide.nodes.Node;
@@ -24,13 +22,9 @@ import org.openide.util.HelpCtx;
 import org.openide.util.Lookup;
 import org.openide.util.NbBundle;
 import org.openide.util.actions.CookieAction;
-import org.openide.util.actions.SystemAction;
-
-import java.util.Enumeration;
 
 import javax.swing.Action;
 import javax.swing.JEditorPane;
-import javax.swing.SwingUtilities;
 
 
 /**
@@ -54,9 +48,6 @@ public final class FormatAction extends CookieAction {
 	 * Creates a new FormatAction object.
 	 */
 	public FormatAction() {
-		// we have to register popups here, because otherwise users would have
-		// to open the "Build" menu first in order to see the popup menus
-		//		registerPopups();
 	}
 
 	/**
@@ -95,82 +86,6 @@ public final class FormatAction extends CookieAction {
 	 */
 	public boolean asynchronous() {
 		return false;
-	}
-
-	/**
-	 * Indicates whether the action should be enabled based on the currently
-	 * activated nodes. The action will only be enabled for Java data objects
-	 * (representing Java source files) and data folders.
-	 *
-	 * @param nodes the set of activated nodes.
-	 *
-	 * @return <code>true</code> if the action should be enabled.
-	 */
-	protected boolean enableOld(Node[] nodes) {
-		if (!super.enable(nodes)) {
-			return false;
-		}
-
-		boolean enabled = false;
-		boolean single = true;
-
-		if (nodes.length == 1) {
-
-			DataFolder dataFolder =
-				(DataFolder) nodes[0].getCookie(DataFolder.class);
-			DataObject dataObject =
-				(DataObject) nodes[0].getCookie(DataObject.class);
-
-			single = dataFolder == null;
-			enabled =
-				!single ||
-				(dataObject != null && NbHelper.isJavaFile(dataObject));
-		}
-		else if (nodes.length > 1) {
-
-			int javaDataObjectCount = 0;
-
-			for (int i = 0; i < nodes.length; i++) {
-
-				DataFolder dataFolder =
-					(DataFolder) nodes[i].getCookie(DataFolder.class);
-				DataObject dataObject =
-					(DataObject) nodes[i].getCookie(DataObject.class);
-
-				if (dataFolder != null) {
-					single = false;
-					enabled = true;
-				}
-
-				if (dataObject != null && NbHelper.isJavaFile(dataObject)) {
-					enabled = true;
-					javaDataObjectCount++;
-				}
-
-				if (javaDataObjectCount > 1) {
-					single = false;
-				}
-
-				if (!single && enabled) {
-					break;
-				}
-			}
-		}
-
-		if (single) {
-			_name =
-				NbBundle.getMessage(
-					FormatAction.class, "LBL_FormatSingleAction" /* NOI18N */);
-		}
-		else {
-			_name =
-				NbBundle.getMessage(
-					FormatAction.class, "LBL_FormatAllAction" /* NOI18N */);
-		}
-
-		System.out.println("name " + _name);
-		System.out.println("enabled " + enabled);
-		return enabled;
 	}
 
 	/**
@@ -250,101 +165,5 @@ public final class FormatAction extends CookieAction {
 		JEditorPane[] panes = cookie.getOpenedPanes();
 
 		return ((panes != null) && (panes.length > 0));
-	}
-
-	private void registerPopups() {
-		SwingUtilities.invokeLater(
-			new Runnable() {
-				public void run() {
-					registerPopups0();
-				}
-			});
-	}
-
-	private void registerPopups0() {
-
-		/*
-		 * @todo this code is ugly and subject to change but until the Looks
-		 *          API is in place, I really like this better than the
-		 *          SystemAction way to integrate items into the Tools submenu
-		 */
-		DataLoaderPool pool =
-			(DataLoaderPool) Lookup.getDefault().lookup(DataLoaderPool.class);
-		DataLoader loader = pool.firstProducerOf(DataFolder.class);
-		SystemAction[] actions = loader.getActions();
-
-
-
-// add our action to the popup menu of folder nodes
-SEARCH_FOLDER: 
-		for (int i = 0; i < actions.length; i++) {
-			if (actions[i] != null) // null means separator
-			 {
-
-				String actionName = actions[i].getClass().getName();
-
-				// formerly org.openide.actions.BuildAction
-				if (actionName.equals(NbHelper.REFACTORING_ACTION_CLASS_NAME)) {
-
-					SystemAction[] result =
-						new SystemAction[actions.length + 2];
-
-					System.arraycopy(actions, 0, result, 0, i + 1);
-					result[i + 2] = this; // add the action
-					result[i + 3] = null; // and a separator
-					System.arraycopy(
-						actions, i + 2, result, i + 4, actions.length - i - 2);
-					loader.setActions(result);
-
-					break SEARCH_FOLDER;
-				}
-			}
-		}
-
-		// add our action to the popup menu of Java source file nodes
-		for (Enumeration loaders = pool.allLoaders();
-					loaders.hasMoreElements();) {
-			loader = (DataLoader) loaders.nextElement();
-
-			String name = loader.getClass().getName();
-
-			/**
-			 * @todo it would be cool to be able to format FormDataNodes
-			 * 		 (org.netbeans.modules.form.FormDataLoader) as well, but I
-			 * 		 don't how to bypass the guarded sections
-			 */
-			if (
-				name.equals(NbHelper.JAVA_DATA_LOADER_CLASS_NAME) ||
-						name.equals(NbHelper.SERVLET_DATA_LOADER_CLASS_NAME)) {
-				actions = loader.getActions();
-SEARCH_FILE: 
-				for (int i = 0; i < actions.length; i++) {
-					if (actions[i] != null) // null means separator
-					 {
-
-						String actionName = actions[i].getClass().getName();
-
-						// formerly org.openide.actions.BuildAction
-						if (
-							actionName.equals(
-										NbHelper.REFACTORING_ACTION_CLASS_NAME)) {
-
-							SystemAction[] result =
-								new SystemAction[actions.length + 2];
-
-							System.arraycopy(actions, 0, result, 0, i + 1);
-							result[i + 2] = this; // add the action
-							result[i + 3] = null; // and a separator
-							System.arraycopy(
-								actions, i + 2, result, i + 4,
-								actions.length - i - 2);
-							loader.setActions(result);
-
-							break SEARCH_FILE;
-						}
-					}
-				}
-			}
-		}
 	}
 }
