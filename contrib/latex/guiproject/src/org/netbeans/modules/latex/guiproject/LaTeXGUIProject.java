05abb58f80624ac270fcd9a22575fb922bb65bbb
jlahoda@netbeans.org <jlahoda@netbeans.org>
2005-07-05 14:08:23 +0000

    Adding support for CVS lite. Upgrading dependencies for projects.

--- contrib/latex/guiproject/src/org/netbeans/modules/latex/guiproject/LaTeXGUIProject.java
+++ contrib/latex/guiproject/src/org/netbeans/modules/latex/guiproject/LaTeXGUIProject.java
@@ -15,18 +15,30 @@
 package org.netbeans.modules.latex.guiproject;
 
 import java.awt.Image;
+import java.beans.PropertyChangeEvent;
 import java.beans.PropertyChangeListener;
 import java.io.File;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Collection;
+import java.util.HashMap;
 import java.util.HashSet;
+import java.util.Iterator;
 import java.util.List;
+import java.util.Map;
+import java.util.Set;
 import javax.swing.Action;
 import javax.swing.Icon;
 import javax.swing.ImageIcon;
+import javax.swing.JSeparator;
+import javax.swing.event.ChangeEvent;
+import javax.swing.event.ChangeListener;
 import org.netbeans.api.project.Project;
 import org.netbeans.api.project.ProjectInformation;
 import org.netbeans.api.project.ProjectManager;
+import org.netbeans.api.project.ProjectUtils;
+import org.netbeans.api.project.SourceGroup;
+import org.netbeans.api.project.Sources;
 import org.netbeans.modules.latex.guiproject.ui.ProjectSettings;
 import org.netbeans.modules.latex.guiproject.ui.PropertiesDialogPanel;
 import org.netbeans.modules.latex.model.Utilities;
@@ -45,15 +57,24 @@ import org.openide.DialogDescriptor;
 import org.openide.DialogDisplayer;
 import org.openide.ErrorManager;
 import org.openide.filesystems.FileObject;
+import org.openide.filesystems.FileStateInvalidException;
+import org.openide.filesystems.FileStatusEvent;
+import org.openide.filesystems.FileStatusListener;
+import org.openide.filesystems.FileSystem;
 import org.openide.filesystems.FileUtil;
+import org.openide.filesystems.Repository;
+import org.openide.loaders.DataFolder;
 import org.openide.loaders.DataObject;
 import org.openide.loaders.DataObjectNotFoundException;
+import org.openide.loaders.FolderLookup;
 import org.openide.nodes.AbstractNode;
 import org.openide.nodes.Children;
 import org.openide.nodes.FilterNode;
 import org.openide.nodes.Node;
 import org.openide.util.Lookup;
 import org.openide.util.Mutex;
+import org.openide.util.RequestProcessor;
+import org.openide.util.WeakListeners;
 import org.openide.util.lookup.Lookups;
 import org.openide.util.lookup.ProxyLookup;
 import org.w3c.dom.Element;
@@ -269,29 +290,257 @@ public class LaTeXGUIProject implements Project, ProjectInformation, LogicalView
         
     }
     
-    private static class LaTeXGUIProjectNode extends AbstractNode {
+    private static class LaTeXGUIProjectNode extends AbstractNode implements Runnable, FileStatusListener, ChangeListener, PropertyChangeListener {
         
         private LaTeXGUIProject project;
+        
+        // icon badging >>>
+        private Set files;
+        private Map fileSystemListeners;
+        private RequestProcessor.Task task;
+        private final Object privateLock = new Object();
+        private boolean iconChange;
+        private boolean nameChange;        
+        private ChangeListener sourcesListener;
+        private Map groupsListeners;
+        // icon badging <<<
         
         public LaTeXGUIProjectNode(LaTeXGUIProject project) {
             super(project.createChildren(), Lookups.fixed(new Object[] {project}));
             setDisplayName(project.getDisplayName());
             setIconBase("org/netbeans/modules/latex/guiproject/resources/latex_gui_project_icon");
+            setProjectFiles(project);
         }
         
         public Action[] getActions(boolean context) {
-            //TODO: context???:
-            return new Action[] {
-                ActionsFactory.createShowAction(),
-                null,
-                ActionsFactory.createBuildAction(),
-                null,
-                CommonProjectActions.setAsMainProjectAction(),
-                CommonProjectActions.closeProjectAction(),
-                null,
-                CommonProjectActions.customizeProjectAction(),
-            };
+            List actions = new ArrayList();
+            
+            actions.add(ActionsFactory.createShowAction());
+            actions.add(null);
+            actions.add(ActionsFactory.createBuildAction());
+            actions.add(null);
+            actions.add(CommonProjectActions.setAsMainProjectAction());
+            actions.add(CommonProjectActions.closeProjectAction());
+            actions.add(null);
+            
+            // honor 57874 contact
+            
+            try {
+                Repository repository  = Repository.getDefault();
+                FileSystem sfs = repository.getDefaultFileSystem();
+                FileObject fo = sfs.findResource("Projects/Actions");  // NOI18N
+                if (fo != null) {
+                    DataObject dobj = DataObject.find(fo);
+                    FolderLookup actionRegistry = new FolderLookup((DataFolder)dobj);
+                    Lookup.Template query = new Lookup.Template(Object.class);
+                    Lookup lookup = actionRegistry.getLookup();
+                    Iterator it = lookup.lookup(query).allInstances().iterator();
+                    if (it.hasNext()) {
+                        actions.add(null);
+                    }
+                    while (it.hasNext()) {
+                        Object next = it.next();
+                        if (next instanceof Action) {
+                            actions.add(next);
+                        } else if (next instanceof JSeparator) {
+                            actions.add(null);
+                        }
+                    }
+                }
+            } catch (DataObjectNotFoundException ex) {
+                // data folder for exitinf fileobject expected
+                ErrorManager.getDefault().notify(ex);
+            }
+            
+            actions.add(CommonProjectActions.customizeProjectAction());
+            
+            return (Action []) actions.toArray(new Action[actions.size()]);
         }
+
+        
+        protected final void setProjectFiles(Project project) {
+            Sources sources = ProjectUtils.getSources(project);  // returns singleton
+            if (sourcesListener == null) {
+                sourcesListener = WeakListeners.change(this, sources);
+                sources.addChangeListener(sourcesListener);
+            }
+            setGroups(Arrays.asList(sources.getSourceGroups(Sources.TYPE_GENERIC)));
+        }
+        
+        private final void setGroups(Collection groups) {
+            if (groupsListeners != null) {
+                Iterator it = groupsListeners.keySet().iterator();
+                while (it.hasNext()) {
+                    SourceGroup group = (SourceGroup) it.next();
+                    PropertyChangeListener pcl = (PropertyChangeListener) groupsListeners.get(group);
+                    group.removePropertyChangeListener(pcl);
+                }
+            }
+            groupsListeners = new HashMap();
+            Set roots = new HashSet();
+            Iterator it = groups.iterator();
+            while (it.hasNext()) {
+                SourceGroup group = (SourceGroup) it.next();
+                PropertyChangeListener pcl = WeakListeners.propertyChange(this, group);
+                groupsListeners.put(group, pcl);
+                group.addPropertyChangeListener(pcl);
+                FileObject fo = group.getRootFolder();
+                roots.add(fo);
+            }
+            setFiles(roots);
+        }
+
+        protected final void setFiles(Set files) {
+            if (fileSystemListeners != null) {
+                Iterator it = fileSystemListeners.keySet().iterator();
+                while (it.hasNext()) {
+                    FileSystem fs = (FileSystem) it.next();
+                    FileStatusListener fsl = (FileStatusListener) fileSystemListeners.get(fs);
+                    fs.removeFileStatusListener(fsl);
+                }
+            }
+                        
+            fileSystemListeners = new HashMap();
+            this.files = files;
+            if (files == null) return;
+
+            Iterator it = files.iterator();
+            Set hookedFileSystems = new HashSet();
+            while (it.hasNext()) {
+                FileObject fo = (FileObject) it.next();
+                try {
+                    FileSystem fs = fo.getFileSystem();
+                    if (hookedFileSystems.contains(fs)) {
+                        continue;
+                    }
+                    hookedFileSystems.add(fs);
+                    FileStatusListener fsl = FileUtil.weakFileStatusListener(this, fs);
+                    fs.addFileStatusListener(fsl);
+                    fileSystemListeners.put(fs, fsl);
+                } catch (FileStateInvalidException e) {
+                    ErrorManager err = ErrorManager.getDefault();
+                    err.annotate(e, "Can not get " + fo + " filesystem, ignoring...");  // NO18N
+                    err.notify(ErrorManager.INFORMATIONAL, e);
+                }
+            }
+        }
+
+        public String getDisplayName () {
+            String s = super.getDisplayName ();
+
+            if (files != null && files.iterator().hasNext()) {
+                try {
+                    FileObject fo = (FileObject) files.iterator().next();
+                    s = fo.getFileSystem ().getStatus ().annotateName (s, files);
+                } catch (FileStateInvalidException e) {
+                    ErrorManager.getDefault().notify(ErrorManager.INFORMATIONAL, e);
+                }
+            }
+
+            return s;
+        }
+
+         public String getHtmlDisplayName() {
+            if (files != null && files.iterator().hasNext()) {
+                try {
+                    FileObject fo = (FileObject) files.iterator().next();
+                    FileSystem.Status stat = fo.getFileSystem().getStatus();
+                    if (stat instanceof FileSystem.HtmlStatus) {
+                        FileSystem.HtmlStatus hstat = (FileSystem.HtmlStatus) stat;
+
+                        String result = hstat.annotateNameHtml (
+                           super.getDisplayName(), files);
+
+                        //Make sure the super string was really modified
+                        if (result != null && !result.equals(getDisplayName())) {
+                            return result;
+                        }
+                    }
+                } catch (FileStateInvalidException e) {
+                    ErrorManager.getDefault().notify(ErrorManager.INFORMATIONAL, e);
+                }
+            }
+            return getDisplayName();
+        }
+         
+        public Image getIcon (int type) {
+            Image img = super.getIcon(type);
+
+            if (files != null && files.iterator().hasNext()) {
+                try {
+                    FileObject fo = (FileObject) files.iterator().next();
+                    img = fo.getFileSystem ().getStatus ().annotateIcon (img, type, files);
+                } catch (FileStateInvalidException e) {
+                    ErrorManager.getDefault().notify(ErrorManager.INFORMATIONAL, e);
+                }
+            }
+
+            return img;
+        }
+        public Image getOpenedIcon (int type) {
+            Image img = super.getOpenedIcon(type);
+
+            if (files != null && files.iterator().hasNext()) {
+                try {
+                    FileObject fo = (FileObject) files.iterator().next();
+                    img = fo.getFileSystem ().getStatus ().annotateIcon (img, type, files);
+                } catch (FileStateInvalidException e) {
+                    ErrorManager.getDefault().notify(ErrorManager.INFORMATIONAL, e);
+                }
+            }
+
+            return img;
+        }
+
+        public void run() {
+            boolean fireIcon;
+            boolean fireName;
+            synchronized (privateLock) {
+                fireIcon = iconChange;
+                fireName = nameChange;
+                iconChange = false;
+                nameChange = false;
+            }
+            if (fireIcon) {
+                fireIconChange();
+                fireOpenedIconChange();
+            }
+            if (fireName) {
+                fireDisplayNameChange(null, null);
+            }
+        }
+
+        public void annotationChanged(FileStatusEvent event) {
+            if (task == null) {
+                task = RequestProcessor.getDefault().create(this);
+            }
+
+            synchronized (privateLock) {
+                if ((iconChange == false && event.isIconChange())  || (nameChange == false && event.isNameChange())) {
+                    Iterator it = files.iterator();
+                    while (it.hasNext()) {
+                        FileObject fo = (FileObject) it.next();
+                        if (event.hasChanged(fo)) {
+                            iconChange |= event.isIconChange();
+                            nameChange |= event.isNameChange();
+                        }
+                    }
+                }
+            }
+
+            task.schedule(50);  // batch by 50 ms
+        }
+
+        // sources change
+        public void stateChanged(ChangeEvent e) {
+            setProjectFiles(project);
+        }
+
+        // group change
+        public void propertyChange(PropertyChangeEvent evt) {
+            setProjectFiles(project);
+         }
+
     }
     
     
@@ -347,10 +596,6 @@ public class LaTeXGUIProject implements Project, ProjectInformation, LogicalView
     private static class SourceFileNode extends FilterNode {
         public SourceFileNode(Node shadow, FileObject file) {
             super(shadow, Children.LEAF, new ProxyLookup(new Lookup[] {shadow.getLookup(), Lookups.singleton(file)}));
-        }
-        
-        public String getHtmlDisplayName() {
-            return null;
         }
     }
     
