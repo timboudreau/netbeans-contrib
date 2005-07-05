9c491255b913923fed93f58223be813591356ea8
jlahoda@netbeans.org <jlahoda@netbeans.org>
2005-07-05 14:01:22 +0000

    Removing use of HTML code in the nodes for the tex nodes to support CVS.

--- contrib/latex/loaders/src/org/netbeans/modules/latex/loaders/MyDataNode.java
+++ contrib/latex/loaders/src/org/netbeans/modules/latex/loaders/MyDataNode.java
@@ -7,7 +7,7 @@
  *
  * The Original Code is the LaTeX module.
  * The Initial Developer of the Original Code is Jan Lahoda.
- * Portions created by Jan Lahoda_ are Copyright (C) 2002,2003.
+ * Portions created by Jan Lahoda_ are Copyright (C) 2002-2005.
  * All Rights Reserved.
  *
  * Contributor(s): Jan Lahoda.
@@ -50,7 +50,7 @@ import org.openide.util.WeakListeners;
  *
  * @author Jan Lahoda
  */
-public class MyDataNode extends DataNode implements LaTeXSourceFactory.MainFileListener {
+public class MyDataNode extends DataNode {
     
     public static final String TEXT_SET = "Text";
     
@@ -63,55 +63,6 @@ public class MyDataNode extends DataNode implements LaTeXSourceFactory.MainFileL
     protected MyDataNode(TexDataObject obj, Children ch) {
         super(obj, ch);
         setIconBase("org/netbeans/modules/latex/loaders/MyDataIcon");
-        
-        LaTeXSourceFactory fact = (LaTeXSourceFactory) Lookup.getDefault().lookup(LaTeXSourceFactory.class);
-        
-        //TODO: the LaTeXSourceFactory in the lookup may change over time, so listen on the result!
-        fact.addMainFileListener(factoryListener = LaTeXSourceFactory.weakMainFileListener(this, fact));
-    }
-    
-    private LaTeXSourceFactory getFactory() {
-        return (LaTeXSourceFactory) Lookup.getDefault().lookup(LaTeXSourceFactory.class);
-    }
-    
-    private boolean isMainFile(Object file) {
-        return getFactory().isMainFile(file);
-    }
-    
-    private boolean isKnownFile(Object file) {
-        return getFactory().isKnownFile(file);
-    }
-    
-    public String getHtmlDisplayName() {
-        String dName = super.getDisplayName();
-        
-        if (isKnownFile(getDataObject().getPrimaryFile())) {
-            if (isMainFile(getDataObject().getPrimaryFile())) {
-                return "<html><b>" + dName + "</b></html>"; //TODO: more efficiently?
-            } else {
-                return "<html><b>" + dName + "</b></html>"; //TODO: more efficiently?
-            }
-        }
-        
-        return dName;
-    }
-    
-    public Image getIcon(int type) {
-        Image sup = super.getIcon(type);
-        String badge = null;
-            
-        if (isKnownFile(getDataObject().getPrimaryFile())) {
-            if (isMainFile(getDataObject().getPrimaryFile())) {
-                badge = "org/netbeans/modules/latex/loaders/latex_main_file_badge.gif";
-            } else {
-                badge = "org/netbeans/modules/latex/loaders/latex_incl_file_badge.gif";
-            }
-        }
-        
-        if (badge != null)
-            sup = org.openide.util.Utilities.mergeImages(sup, org.openide.util.Utilities.loadImage(badge), 16, 0);
-        
-        return sup;
     }
     
     protected TexDataObject getMyDataObject() {
@@ -135,18 +86,6 @@ public class MyDataNode extends DataNode implements LaTeXSourceFactory.MainFileL
         
         addTextProperties(textSet);
         
-//        Sheet.Set set = sheet.get(ExecutionSupport.PROP_EXECUTION);
-//        if (set == null) {
-//            set = new Sheet.Set();
-//            set.setName(ExecutionSupport.PROP_EXECUTION);
-//            set.setDisplayName(NbBundle.getMessage(MyDataNode.class, "LBL_DataNode_exec_sheet"));
-//            set.setShortDescription(NbBundle.getMessage(MyDataNode.class, "HINT_DataNode_exec_sheet"));
-//        }
-//        ((ExecutionSupport)getCookie(ExecutionSupport.class)).addProperties(set);
-        
-        // Maybe:
-//        ((CompilerSupport)getCookie(CompilerSupport.class)).addProperties(set);
-//        sheet.put(set);
         return sheet;
     }
 
@@ -219,27 +158,6 @@ public class MyDataNode extends DataNode implements LaTeXSourceFactory.MainFileL
         };
     }
 
-    public void mainFileChanged(LaTeXSourceFactory.MainFileEvent evt) {
-        System.err.println("this=" + this);
-        System.err.println("evt=" + evt);
-        
-        if (evt.getSource() == getFactory() && getDataObject().getPrimaryFile() == /*!!*/ evt.getMainFile()) {
-            System.err.println("found, firing display name changes..");
-            fireDisplayNameChange(null, null);//TODO: hopefully this updates the DN also from getHtmlDisplayName();
-            fireIconChange();
-        }
-    }
-
-//    public void propertyChange(PropertyChangeEvent evt) {
-//        System.err.println("this=" + this);
-//        System.err.println("evt=" + evt);
-//        if (evt.getSource() == getFactory() && LaTeXSourceFactory.PROP_MAIN_FILES.equals(evt.getPropertyName())) {
-//            System.err.println("found, firing display name changes..");
-//            firePropertyChange(PROP_DISPLAY_NAME, null, null);
-//            firePropertyChange(PROP_ICON, null, null);
-//        }
-//    }
-    
     // Don't use getDefaultAction(); just make that first in the data loader's getActions list
     
     private static class TexChildren extends Children.Keys implements ModelListener {
