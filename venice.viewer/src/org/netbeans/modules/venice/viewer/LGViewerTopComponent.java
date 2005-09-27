d329a0cea0b5523381756b9190119be70d3374ca
paulby@netbeans.org <paulby@netbeans.org>
2005-09-27 23:36:04 +0000

    Add HelloUniverse test Issue number: Obtained from: Submitted by: Reviewed by:

--- venice.viewer/src/org/netbeans/modules/venice/viewer/LGViewerTopComponent.java
+++ venice.viewer/src/org/netbeans/modules/venice/viewer/LGViewerTopComponent.java
@@ -1,15 +1,23 @@
 package org.netbeans.modules.venice.viewer;
 
+import com.sun.j3d.utils.geometry.ColorCube;
+import com.sun.j3d.utils.universe.SimpleUniverse;
 import java.awt.BorderLayout;
-import java.awt.Dimension;
-import java.beans.PropertyChangeEvent;
-import java.beans.PropertyChangeListener;
+import java.awt.GraphicsConfiguration;
 import java.io.Serializable;
+import javax.media.j3d.Alpha;
+import javax.media.j3d.BoundingSphere;
+import javax.media.j3d.BranchGroup;
+import javax.media.j3d.Canvas3D;
+import javax.media.j3d.RotationInterpolator;
+import javax.media.j3d.Transform3D;
+import javax.media.j3d.TransformGroup;
+import javax.vecmath.Point3d;
 import org.openide.util.NbBundle;
-import org.openide.util.RequestProcessor;
 import org.openide.util.Utilities;
+import org.openide.windows.Mode;
 import org.openide.windows.TopComponent;
-
+import org.openide.windows.WindowManager;
 /**
  * Top component which displays something.
  */
@@ -60,6 +68,8 @@ final class LGViewerTopComponent extends TopComponent {
 
     public void componentOpened() {
         // TODO add custom code on component opening
+        init();
+        System.err.println("Returning ******************************");
     }
 
     public void componentClosed() {
@@ -74,12 +84,84 @@ final class LGViewerTopComponent extends TopComponent {
     protected String preferredID() {
         return "LGViewerTopComponent";
     }
+    
+    public void open() {
+        Mode m = WindowManager.getDefault().findMode("editor");
+        if (m != null) {
+            m.dockInto(this);
+        }
+        super.open();
+    }
 
+    private SimpleUniverse u = null;
+    
+    public BranchGroup createSceneGraph() {
+	// Create the root of the branch graph
+	BranchGroup objRoot = new BranchGroup();
+
+	// Create the TransformGroup node and initialize it to the
+	// identity. Enable the TRANSFORM_WRITE capability so that
+	// our behavior code can modify it at run time. Add it to
+	// the root of the subgraph.
+	TransformGroup objTrans = new TransformGroup();
+	objTrans.setCapability(TransformGroup.ALLOW_TRANSFORM_WRITE);
+	objRoot.addChild(objTrans);
+
+	// Create a simple Shape3D node; add it to the scene graph.
+	objTrans.addChild(new ColorCube(0.4));
+
+	// Create a new Behavior object that will perform the
+	// desired operation on the specified transform and add
+	// it into the scene graph.
+	Transform3D yAxis = new Transform3D();
+	Alpha rotationAlpha = new Alpha(-1, 4000);
+
+	RotationInterpolator rotator =
+	    new RotationInterpolator(rotationAlpha, objTrans, yAxis,
+				     0.0f, (float) Math.PI*2.0f);
+	BoundingSphere bounds =
+	    new BoundingSphere(new Point3d(0.0,0.0,0.0), 100.0);
+	rotator.setSchedulingBounds(bounds);
+	objRoot.addChild(rotator);
+
+        // Have Java 3D perform optimizations on this scene graph.
+        objRoot.compile();
+
+	return objRoot;
+    }
+
+    public void init() {
+	setLayout(new BorderLayout());
+        GraphicsConfiguration config =
+           SimpleUniverse.getPreferredConfiguration();
+
+	Canvas3D c = new Canvas3D(config);
+	add("Center", c);
+
+	// Create a simple scene and attach it to the virtual universe
+	BranchGroup scene = createSceneGraph();
+	u = new SimpleUniverse(c);
+
+        // This will move the ViewPlatform back a bit so the
+        // objects in the scene can be viewed.
+        u.getViewingPlatform().setNominalViewingTransform();
+        
+        u.getViewer().getView().setMinimumFrameCycleTime(1000/30);
+
+	u.addBranchGraph(scene);
+    }
+
+    public void destroy() {
+	u.cleanup();
+    }
+    
     final static class ResolvableHelper implements Serializable {
         private static final long serialVersionUID = 1L;
         public Object readResolve() {
             return LGViewerTopComponent.getDefault();
         }
     }
+    
+    
 
 }
