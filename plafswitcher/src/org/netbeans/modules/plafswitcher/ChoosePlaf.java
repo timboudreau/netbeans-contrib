f145f21499f43bc287783ab68f305f346fbce6ce
jglick@netbeans.org <jglick@netbeans.org>
2005-09-27 23:41:36 +0000

    Misc. updates.

--- plafswitcher/src/org/netbeans/modules/plafswitcher/ChoosePlaf.java
+++ plafswitcher/src/org/netbeans/modules/plafswitcher/ChoosePlaf.java
@@ -7,7 +7,7 @@
  * http://www.sun.com/
  * 
  * The Original Code is NetBeans. The Initial Developer of the Original
- * Code is Sun Microsystems, Inc. Portions Copyright 1997-2003 Sun
+ * Code is Sun Microsystems, Inc. Portions Copyright 1997-2005 Sun
  * Microsystems, Inc. All Rights Reserved.
  *
  * Contributor(s): Petr Nejedly
@@ -15,51 +15,53 @@
 
 package org.netbeans.modules.plafswitcher;
 
+import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import java.io.IOException;
 import java.util.Iterator;
-import javax.swing.*;
-
+import javax.swing.JMenu;
+import javax.swing.JMenuItem;
+import javax.swing.LookAndFeel;
+import javax.swing.SwingUtilities;
+import javax.swing.UIManager;
+import javax.swing.UnsupportedLookAndFeelException;
+import org.openide.DialogDisplayer;
 import org.openide.ErrorManager;
 import org.openide.NotifyDescriptor;
-import org.openide.DialogDisplayer;
-
+import org.openide.awt.JMenuPlus;
+import org.openide.awt.Mnemonics;
 import org.openide.util.HelpCtx;
+import org.openide.util.Lookup;
 import org.openide.util.NbBundle;
 import org.openide.util.actions.CallableSystemAction;
-import org.openide.awt.Actions;
-import org.openide.util.Lookup;
 import org.openide.windows.WindowManager;
 
-/** Switch PLAF and remember the choice .
+/**
+ * Switch PLAF and remember the choice.
  * @author Petr Nejedly
  */
 public final class ChoosePlaf extends CallableSystemAction {
-    /** generated Serialized Version UID */
-    static final long serialVersionUID = -6L;
 
     /* Human presentable name of the action. This should be
     * presented as an item in a menu.
     * @return the name of the action
     */
     public String getName() {
-        return NbBundle.getBundle(ChoosePlaf.class).getString("LAB_PLAF");
+        return NbBundle.getMessage(ChoosePlaf.class, "LAB_PLAF");
     }
 
     /* Help context where to find more about the action.
     * @return the help context for this action
     */
     public HelpCtx getHelpCtx() {
-        return new HelpCtx (ChoosePlaf.class);
+        return null;
     }
-    
     
     /* Returns a submenu with all available L&Fs
     * @return a JMenuItem for the submenu
     */
     public JMenuItem getMenuPresenter() {
-        JMenu menu = new org.openide.awt.JMenuPlus();
-        Actions.setMenuText(menu, getName(), true);
+        JMenu menu = new JMenuPlus();
+        Mnemonics.setLocalizedText(menu, getName());
         menu.setIcon (getIcon());
         HelpCtx.setHelpIDString (menu, ChoosePlaf.class.getName ());
 
@@ -73,7 +75,7 @@ public final class ChoosePlaf extends CallableSystemAction {
             item.setToolTipText(laf.getDescription());
             HelpCtx.setHelpIDString (item, ChoosePlaf.class.getName ());
             item.addActionListener(new ActionListener() {
-                public void actionPerformed(java.awt.event.ActionEvent evt) {
+                public void actionPerformed(ActionEvent evt) {
                     try {
                         UIManager.setLookAndFeel(laf);
                         SwingUtilities.updateComponentTreeUI(WindowManager.getDefault().getMainWindow());
@@ -84,10 +86,8 @@ public final class ChoosePlaf extends CallableSystemAction {
 				NotifyDescriptor.INFORMATION_MESSAGE)
 			);
                     } catch (UnsupportedLookAndFeelException e) {
-                        // Can´t happen, we filter them, but ...
+                        // Can't happen, we filter them, but ...
                         ErrorManager.getDefault().notify(e);
-                    } catch (IOException ioe) {
-                        ErrorManager.getDefault().notify(ioe);                        
                     }
                 }
             });
@@ -98,5 +98,7 @@ public final class ChoosePlaf extends CallableSystemAction {
 
     /** Does nothing. This action is just a submenu placeholder. */
     public void performAction() {
+        assert false;
     }
+    
 }
--- plafswitcher/src/org/netbeans/modules/plafswitcher/Module.java
+++ plafswitcher/src/org/netbeans/modules/plafswitcher/Module.java
@@ -7,7 +7,7 @@
  * http://www.sun.com/
  * 
  * The Original Code is NetBeans. The Initial Developer of the Original
- * Code is Sun Microsystems, Inc. Portions Copyright 1997-2003 Sun
+ * Code is Sun Microsystems, Inc. Portions Copyright 1997-2005 Sun
  * Microsystems, Inc. All Rights Reserved.
  *
  * Contributor(s): Petr Nejedly
@@ -15,34 +15,24 @@
 
 package org.netbeans.modules.plafswitcher;
 
+import java.util.prefs.Preferences;
 import javax.swing.UIManager;
 import org.openide.ErrorManager;
-import org.openide.filesystems.Repository;
+import org.openide.modules.ModuleInstall;
 
 /**
 * Module installation class for PlafSwitcher.
 *
 * @author Petr Nejedly
 */
-public class Module extends org.openide.modules.ModuleInstall {
-    static final long serialVersionUID =-5L;
+public class Module extends ModuleInstall {
 
-    // Why such impl? Simply because:
-    // *) I want it to work under NB3.5
-    // *) I don't want to introduce (pre)deprecated SystemOption
-    //    because of such simple thing
-    // Later I may use Registry
     static String getPlaf() {
-        Object o = Repository.getDefault().getDefaultFileSystem().
-            findResource("Actions/View/org-netbeans-modules-plafswitcher-ChoosePlaf.instance").
-            getAttribute("org-netbeans-modules-plaf-Selected");
-        return (o instanceof String) ? (String)o : null;
+        return Preferences.userNodeForPackage(Module.class).get("plaf", null);
     }
     
-    static void setPlaf(String plaf) throws java.io.IOException {
-        Repository.getDefault().getDefaultFileSystem().
-            findResource("Actions/View/org-netbeans-modules-plafswitcher-ChoosePlaf.instance").
-            setAttribute("org-netbeans-modules-plaf-Selected", plaf);
+    static void setPlaf(String plaf) {
+        Preferences.userNodeForPackage(Module.class).put("plaf", plaf);
     }
 
     /** Module installed again. */
@@ -52,9 +42,9 @@ public class Module extends org.openide.modules.ModuleInstall {
             try {
                 UIManager.setLookAndFeel(plaf);
             } catch (Exception e) {
-                // Only log, don´t show to the user
-                ErrorManager.getDefault().log(ErrorManager.EXCEPTION,
-                    "Can´t set PLAF to " + plaf + ": " + e);
+                // Only log, do not show to the user
+                ErrorManager.getDefault().annotate(e, ErrorManager.UNKNOWN, "Cannot set PLAF to " + plaf, null, null, null);
+                ErrorManager.getDefault().notify(ErrorManager.INFORMATIONAL, e);
             }
         }
     }
