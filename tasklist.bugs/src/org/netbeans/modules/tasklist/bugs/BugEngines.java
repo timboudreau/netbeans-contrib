97f43b40c61f92ce9bada6e228a6ea1ac54d75bf
hair@netbeans.org <hair@netbeans.org>
2005-12-03 00:50:52 +0000

    BUG 69572 - Scarab to Tasklist-Bugs integration

--- tasklist.bugs/src/org/netbeans/modules/tasklist/bugs/BugEngines.java
+++ tasklist.bugs/src/org/netbeans/modules/tasklist/bugs/BugEngines.java
@@ -3,6 +3,7 @@ package org.netbeans.modules.tasklist.bugs;
 import org.netbeans.modules.tasklist.bugs.issuezilla.IZBugEngine;
 import org.netbeans.modules.tasklist.bugs.bugzilla.BZBugEngine;
 import org.netbeans.modules.tasklist.bugs.javanet.JavaNetEngine;
+import org.netbeans.modules.tasklist.bugs.scarab.ScarabEngine;
 
 /**
  * TODO document
@@ -17,9 +18,11 @@ public final class BugEngines {
 
     // it has fixed URL, same Sf.net etc
     static String JAVA_NET = "Java.net";
+    
+    static String SCARAB = "Scarab";
 
     static String[] list() {
-        return new String[] {IZ, BZ, JAVA_NET};
+        return new String[] {IZ, BZ, JAVA_NET, SCARAB};
     }
 
     static BugEngine get(String id) {
@@ -29,6 +32,8 @@ public final class BugEngines {
             return new BZBugEngine();
         } else if (JAVA_NET.equals(id)) {
             return new JavaNetEngine();
+        } else if (SCARAB.equals(id)) {
+            return new ScarabEngine();
         } else {
             assert false : "Invalid id " + id;
             return null;
