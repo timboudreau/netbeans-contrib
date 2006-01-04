3cc52ed2f5fe415251ae385edc9b516a66dd3ed9
mukeshgarg@netbeans.org <mukeshgarg@netbeans.org>
2006-01-04 12:12:46 +0000

    Creating directory structure for the webserver 7 plugin

---
+++ j2ee.sun.ws7/src/org/netbeans/modules/j2ee/sun/ws7/WS7LibsClassLoader.java
@@ -0,0 +1,66 @@
+/*
+ *                 Sun Public License Notice
+ *
+ * The contents of this file are subject to the Sun Public License
+ * Version 1.0 (the "License"). You may not use this file except in
+ * compliance with the License. A copy of the License is available at
+ * http://www.sun.com/
+ *
+ * The Original Code is NetBeans. The Initial Developer of the Original
+ * Code is Sun Microsystems, Inc. Portions Copyright 1997-2005 Sun
+ * Microsystems, Inc. All Rights Reserved.
+ */
+
+/*
+ * WS7LibsClassLoader.java
+ *
+ * 
+*/
+
+package org.netbeans.modules.j2ee.sun.ws7;
+
+import java.security.PermissionCollection;
+import java.security.CodeSource;
+import java.security.Permissions;
+import java.security.AllPermission;
+import java.io.File;
+import java.net.URL;
+import java.net.URLClassLoader;
+import java.net.MalformedURLException;
+
+
+
+public class WS7LibsClassLoader extends URLClassLoader {
+    
+    /** Creates new pluginClassLoader  _mapping is key=value;key=value....*/
+    public WS7LibsClassLoader() throws MalformedURLException, RuntimeException {
+        super(new URL[0]);
+    }
+    
+    public WS7LibsClassLoader( ClassLoader _loader) throws MalformedURLException, RuntimeException {
+        super(new URL[0], _loader);
+    }
+       
+        
+    public void addURL(File f) throws MalformedURLException, RuntimeException {
+            if (f.isFile()){
+                addURL(f.toURL());
+            }
+    }
+    
+public Class loadClass(String  name)throws ClassNotFoundException{
+    //System.err.println("WS7:LOADCLASS:CLASSLOADER: "+name);
+    return super.loadClass(name);
+}
+protected Class findClass(String  name) throws ClassNotFoundException{
+    //System.err.println("WS7:FINDCLASS:CLASSLOADER: "+name);
+    return super.findClass(name);
+}  
+
+    protected PermissionCollection getPermissions(CodeSource _cs) {
+        Permissions p = new Permissions();
+        p.add(new AllPermission());
+        return p;
+    }
+
+}
