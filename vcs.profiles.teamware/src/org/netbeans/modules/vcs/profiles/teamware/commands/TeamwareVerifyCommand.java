a5da08f8655ba486e6381f6aa6ecb0eeda279d08
blaukopf@netbeans.org <blaukopf@netbeans.org>
2006-02-09 18:02:20 +0000

    VCS verify group support

---
+++ vcs.profiles.teamware/src/org/netbeans/modules/vcs/profiles/teamware/commands/TeamwareVerifyCommand.java
@@ -0,0 +1,228 @@
+/*
+ * Sun Public License Notice
+ * 
+ * The contents of this file are subject to the Sun Public License
+ * Version 1.0 (the "License"). You may not use this file except in
+ * compliance with the License. A copy of the License is available at
+ * http://www.sun.com/
+ * 
+ * The Original Code is the Teamware module.
+ * The Initial Developer of the Original Code is Sun Microsystems, Inc.
+ * Portions created by Sun Microsystems, Inc. are Copyright (C) 2004.
+ * All Rights Reserved.
+ * 
+ * Contributor(s): Daniel Blaukopf.
+ */
+
+package org.netbeans.modules.vcs.profiles.teamware.commands;
+
+import java.awt.Dialog;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.Hashtable;
+import java.util.Iterator;
+import java.util.List;
+import javax.swing.JButton;
+import javax.swing.SwingUtilities;
+import org.netbeans.modules.vcs.profiles.commands.VerifyUtil;
+import org.netbeans.modules.vcscore.VcsAction;
+import org.netbeans.modules.vcscore.VcsFileSystem;
+import org.netbeans.modules.vcscore.actions.AddCommandAction;
+import org.netbeans.modules.vcscore.actions.CommandActionSupporter;
+import org.netbeans.modules.vcscore.actions.GeneralCommandAction;
+import org.netbeans.modules.vcscore.cmdline.VcsAdditionalCommand;
+import org.netbeans.modules.vcscore.commands.CommandDataOutputListener;
+import org.netbeans.modules.vcscore.commands.CommandOutputListener;
+import org.netbeans.modules.vcscore.commands.VcsCommand;
+import org.netbeans.modules.vcscore.grouping.GroupUtils;
+import org.netbeans.modules.vcscore.turbo.FileProperties;
+import org.netbeans.modules.vcscore.turbo.Statuses;
+import org.netbeans.modules.vcscore.turbo.Turbo;
+import org.netbeans.modules.vcscore.ui.NotChangedFilesPanel;
+import org.netbeans.modules.vcscore.ui.ToAddFilesPanel;
+import org.netbeans.modules.vcscore.ui.ToUpdateFilesPanel;
+import org.netbeans.modules.vcscore.ui.VerifyGroupPanel;
+import org.netbeans.modules.vcscore.util.Table;
+import org.openide.DialogDescriptor;
+import org.openide.DialogDisplayer;
+import org.openide.filesystems.FileObject;
+import org.openide.loaders.DataObject;
+import org.openide.loaders.DataObjectNotFoundException;
+import org.openide.loaders.DataShadow;
+import org.openide.util.SharedClassObject;
+
+public class TeamwareVerifyCommand implements VcsAdditionalCommand {
+
+    private ArrayList localFiles;
+    private ArrayList checkedInFiles;
+    private NotChangedFilesPanel ncfPanel;
+    private ToAddFilesPanel taPanel;
+    
+    private VcsFileSystem fileSystem;
+    
+    public void setFileSystem(VcsFileSystem fileSystem) {
+        this.fileSystem = fileSystem;
+    }
+
+    public boolean exec(final Hashtable vars, String[] args,
+                        final CommandOutputListener stdout,
+                        final CommandOutputListener stderr,
+                        final CommandDataOutputListener stdoutData, String dataRegex,
+                        final CommandDataOutputListener stderrData, String errorRegex) {
+        localFiles = new ArrayList();
+        checkedInFiles = new ArrayList();
+        try {
+            VerifyUtil.refreshFilesState("LIST", fileSystem, vars);
+        } catch (InterruptedException iexc) {
+            return false;
+        }
+        List foList = VerifyUtil.getFOs(fileSystem, vars);
+        fillFilesByState(foList);
+        showDialog();
+        return true;
+    }
+
+    private void fillFilesByState(List fos) {
+        for (Iterator it = fos.iterator(); it.hasNext(); ) {
+            FileObject fo = (FileObject) it.next();
+            FileProperties fprops = Turbo.getMeta(fo);
+            String status = FileProperties.getStatus(fprops);
+            if (status == null) {
+                continue;
+            } else if (status.equals("Local")) {
+                localFiles.add(fo);
+            } else if (status.equals("Checked in")) {
+                checkedInFiles.add(fo);
+            }
+        }
+    }
+    
+    private void showDialog() {
+        VerifyGroupPanel panel = new VerifyGroupPanel();
+        boolean nothing = true;
+        if (localFiles.size() > 0) {
+            taPanel = new ToAddFilesPanel(localFiles);
+            panel.addPanel(taPanel, "Locally Added");
+            nothing = false;
+        }
+        if (checkedInFiles.size() > 0) {
+            List dobjList = getDOForNotChanged(checkedInFiles);
+            if (dobjList.size() > 0) {
+                ncfPanel = new NotChangedFilesPanel(dobjList);
+                panel.addPanel(ncfPanel, "Checked In");
+                nothing = false;
+            }
+        }
+        DialogDescriptor dd = new DialogDescriptor(panel, "Group Verification");
+        dd.setModal(false);
+        if (nothing) {
+            panel.setDescription("No Problems were found");
+            JButton btnClose = new JButton("Close");
+            Object[] options = new Object[] {btnClose};
+            dd.setOptions(options);
+            dd.setClosingOptions(options);
+        } else {
+            panel.setDescription("The following problems were found:");
+            final JButton btnCorrect = new JButton("Correct");
+            Object[] options = new Object[] {btnCorrect, DialogDescriptor.CANCEL_OPTION};
+            dd.setOptions(options);
+            dd.setClosingOptions(options);
+            dd.setButtonListener(new ActionListener() {
+                public void actionPerformed(ActionEvent event) {
+                    if (event.getSource().equals(btnCorrect)) {
+                        correctGroup();
+                        return;
+                    }
+                }
+            });
+        }
+        final Dialog dial = DialogDisplayer.getDefault().createDialog(dd);
+        SwingUtilities.invokeLater(new Runnable() {
+            public void run() {
+                dial.setVisible(true);
+            }
+        });      
+    }
+    
+    private String getStatus(DataObject dobj) {
+        String status = null;
+        for (Iterator it = dobj.files().iterator(); it.hasNext(); ) {
+            FileObject fo = (FileObject) it.next();
+            if (!fileSystem.isProcessUnimportantFiles() &&
+                !fileSystem.isImportant(fo.getPath())) continue;
+            String foStatus = fileSystem.getStatus(fo);
+            if (status == null) {
+                status = foStatus;
+            } else if (!status.equals(foStatus)) {
+                status += foStatus;
+                break;
+            }
+        }
+        return status;
+    }
+
+    private List getDOForNotChanged(List list) {
+        Iterator it = list.iterator();
+        HashSet dobjStat = new HashSet();
+        while (it.hasNext()) {
+            FileObject fo = (FileObject)it.next();
+            DataObject dobj = null;
+            try {
+                dobj = DataObject.find(fo);
+                //System.out.println("datablject=" + dobj.getName());
+                String stat = getStatus(dobj);
+                //System.out.println("dataobject's status=" + stat);
+                if ("Checked in".equals(stat)) {
+                    dobjStat.add(dobj);
+                }
+                
+            } catch (DataObjectNotFoundException exc) {
+                continue;
+            }
+        }
+        List toReturn = new ArrayList(dobjStat.size());
+        toReturn.addAll(dobjStat);
+        return toReturn;
+    }
+   
+    private void correctGroup() {
+        if (ncfPanel != null) {
+            performNCFCorrection();
+        }
+        if (taPanel != null) {
+            performTACorrection();
+        }
+    }
+    
+    public void performNCFCorrection() {
+        List list = ncfPanel.getSelectedDataObjects();
+        if (list != null && list.size() != 0) {
+            Iterator it = list.iterator();
+            while (it.hasNext()) {
+                DataObject obj = (DataObject) it.next();
+                DataShadow shadow = GroupUtils.findDOInGroups(obj);
+                if (shadow != null) {
+                    try {
+                        shadow.delete();
+                    } catch (java.io.IOException exc) {
+                    }
+                }
+            }
+        }
+    }
+    
+    public void performTACorrection() {
+        java.util.List foList = taPanel.getFileObjects();
+        if (foList != null && foList.size() != 0) {
+            //CommandActionSupporter supp = FsCommandFactory.getFsInstance().getSupporter();
+            AddCommandAction act = (AddCommandAction) SharedClassObject.findObject(AddCommandAction.class, true);
+            FileObject[] fos = new FileObject[foList.size()];
+            fos = (FileObject[]) foList.toArray(fos);
+            CommandActionSupporter supp = (CommandActionSupporter) fos[0].getAttribute(GeneralCommandAction.VCS_ACTION_ATTRIBUTE);
+            supp.performAction(act, fos);
+        }
+    }
+    
+}
