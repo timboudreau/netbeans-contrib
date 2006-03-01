8d2e7b073fd60b09d566fe2c675b3eaaaf775036
awegscheider@netbeans.org <awegscheider@netbeans.org>
2006-03-01 01:24:29 +0000

    Adding help topics for Web Server 7 plugin

---
+++ j2ee.sun.ws7/javahelp/org/netbeans/modules/j2ee/sun/ws7/docs/wsplugin.hs
@@ -0,0 +1,34 @@
+<?xml version='1.0' encoding='ISO-8859-1'?>
+<!DOCTYPE helpset PUBLIC "-//Sun Microsystems Inc.//DTD JavaHelp HelpSet Version 2.0//EN"
+                         "http://java.sun.com/products/javahelp/helpset_2_0.dtd">
+<!-- 
+*     Copyright (c) 2006 Sun Microsystems, Inc. All rights reserved.
+*     Use is subject to license terms.
+-->
+
+
+<helpset version="2.0">
+	<title>Sun Java System Web Server Plugin Help</title>
+	<maps>
+		<homeID>wsplugin_webserver7_plugin_help</homeID>
+		<mapref location="wsplugin-map.jhm"/>
+	</maps>
+	<view mergetype="javax.help.AppendMerge">
+		<name>TOC</name>
+		<label>Table of Contents</label>
+		<type>javax.help.TOCView</type>
+		<data>wsplugin-toc.xml</data>
+	</view>
+	<view mergetype="javax.help.AppendMerge">
+		<name>Index</name>
+		<label>Index</label>
+		<type>javax.help.IndexView</type>
+		<data>wsplugin-idx.xml</data>
+	</view>
+	<view>
+		<name>Search</name>
+		<label>Search</label>
+		<type>javax.help.SearchView</type>
+		<data engine="com.sun.java.help.search.DefaultSearchEngine">JavaHelpSearch</data>
+	</view>
+</helpset>
