2deb355086942ea08ac067745f6b7a2f00082792
jtulach@netbeans.org <jtulach@netbeans.org>
2006-03-02 11:17:36 +0000

    Introducing serviceability module - right now it can list all modules and not much more, but the set of operations shall increase in future

---
+++ modulemanagement/src/org/netbeans/modules/modulemanagement/ModuleOptions.java
@@ -0,0 +1,97 @@
+/*
+ *                 Sun Public License Notice
+ * 
+ * The contents of this file are subject to the Sun Public License
+ * Version 1.0 (the "License"). You may not use this file except in
+ * compliance with the License. A copy of the License is available at
+ * http://www.sun.com/
+ * 
+ * The Original Code is NetBeans. The Initial Developer of the Original
+ * Code is Sun Microsystems, Inc. Portions Copyright 1997-2003 Sun
+ * Microsystems, Inc. All Rights Reserved.
+ */
+
+package org.netbeans.modules.modulemanagement;
+
+import java.io.PrintStream;
+import java.util.Collection;
+import java.util.Iterator;
+import org.netbeans.api.sendopts.CommandException;
+import org.netbeans.spi.sendopts.Env;
+import org.netbeans.spi.sendopts.NoArgumentProcessor;
+import org.netbeans.spi.sendopts.OneArgumentProcessor;
+import org.netbeans.spi.sendopts.Option;
+import org.netbeans.spi.sendopts.OptionProvider;
+import org.openide.modules.ModuleInfo;
+import org.openide.util.Lookup;
+import org.openide.util.NbBundle;
+
+/**
+ *
+ * @author Jaroslav Tulach
+ */
+public class ModuleOptions implements OptionProvider, NoArgumentProcessor<Void> {
+    private Option<Void> list;
+    
+    /** Creates a new instance of ModuleOptions */
+    public ModuleOptions() {
+    }
+
+    private void init() {
+        if (list != null) {
+            return;
+        }
+
+        list = Option.withoutArgument((char)-1, "listmodules", this); // NOI18N
+    }
+
+    public Option[] getOptions() {
+        init();
+        return new Option[] { list };
+    }
+
+    public Void process(Option option, Env env) throws CommandException {
+        listAllModules(env.getOutputStream());
+        return null;
+    }
+
+    private void listAllModules(PrintStream out) {
+        Collection modules = Lookup.getDefault().lookup(new Lookup.Template(ModuleInfo.class)).allInstances();
+        Integer number = new Integer(modules.size());
+        out.println(NbBundle.getMessage(ModuleOptions.class, "MSG_ModuleListHeader", number));
+        Iterator it = modules.iterator();
+        while (it.hasNext()) {
+            ModuleInfo module = (ModuleInfo) it.next();
+            Object[] args = {
+                fixedLength(module.getCodeName(), 50),
+                fixedLength(module.getCodeNameBase(), 50),
+                new Integer(module.getCodeNameRelease()),
+                fixedLength(module.getDisplayName(), 50),
+                fixedLength(module.getSpecificationVersion().toString(), 15),
+                fixedLength(module.getImplementationVersion(), 15),
+                fixedLength(module.getBuildVersion(), 15),
+                new Integer(module.isEnabled() ? 1 : 0),
+            };
+            out.println(NbBundle.getMessage(ModuleOptions.class, "MSG_ModuleListLine", args));
+        }
+        out.println(NbBundle.getMessage(ModuleOptions.class, "MSG_ModuleListFooter", number));
+        out.flush();
+    }
+
+    private static String fixedLength(String s, int len) {
+        if (s == null) {
+            return null;
+        }
+        if (s.length() >= len) {
+            return s.substring(0, len);
+        } else {
+            StringBuffer sb = new StringBuffer(len);
+            sb.append(s);
+            while (sb.length() < len) {
+                sb.append(' ');
+            }
+            return sb.toString();
+        }
+    }
+}
+
---
+++ modulemanagement/test/unit/src/org/netbeans/modules/modulemanagement/ListModulesTest.java
@@ -0,0 +1,148 @@
+/*
+ * ModuleOptionsTest.java
+ * JUnit based test
+ *
+ * Created on 2. bøezen 2006, 11:58
+ */
+
+package org.netbeans.modules.modulemanagement;
+
+import java.io.ByteArrayOutputStream;
+import java.io.File;
+import java.util.Collections;
+import java.util.HashSet;
+import java.util.Set;
+import junit.framework.TestCase;
+import junit.framework.*;
+import org.netbeans.api.sendopts.CommandException;
+import org.netbeans.api.sendopts.CommandLine;
+import org.openide.modules.ModuleInfo;
+import org.openide.modules.SpecificationVersion;
+import org.openide.util.lookup.AbstractLookup;
+import org.openide.util.lookup.InstanceContent;
+
+/**
+ *
+ * @author jarda
+ */
+public class ListModulesTest extends TestCase {
+    static {
+        System.setProperty("org.openide.util.Lookup", Lkp.class.getName());
+    }
+    
+    public ListModulesTest(String testName) {
+        super(testName);
+    }
+    
+    protected void setUp() throws Exception {
+        Lkp.ic.setPairs(Collections.EMPTY_LIST);
+        Lkp.ic.add(new ModuleOptions());
+    }
+    
+    protected void tearDown() throws Exception {
+    }
+    
+    public static Test suite() {
+        TestSuite suite = new TestSuite(ListModulesTest.class);
+        
+        return suite;
+    }
+
+    public void testAModuleIsPrinted() throws CommandException {
+        Lkp.ic.add(new MyModule("my.cnb", 5, new SpecificationVersion("1.3"), true));
+        Lkp.ic.add(new MyModule("my.snd.cnb", -1, new SpecificationVersion("5.3.1"), false));
+
+        ByteArrayOutputStream os = new ByteArrayOutputStream();
+        ByteArrayOutputStream err = new ByteArrayOutputStream();
+
+        CommandLine.getDefault().parse(new String[] { "--listmodules" }, System.in, os, err, new File("."));
+
+        assertEquals("No errors", 0, err.size());
+
+        String out = os.toString();
+        if (out.indexOf("my.cnb") == -1) {
+            fail("my.cnb should be there: " + out);
+        }
+        if (out.indexOf("my.snd.cnb") == -1) {
+            fail("snd should be there: " + out);
+        }
+        if (out.indexOf("disabled") == -1) {
+            fail("disabled should be there: " + out);
+        }
+        if (out.indexOf("enabled") == -1) {
+            fail("enabled should be there: " + out);
+        }
+        if (out.indexOf("/5") == -1) {
+            fail("5 should be there: " + out);
+        }
+        if (out.indexOf("1.3") == -1) {
+            fail("1.3 should be there: " + out);
+        }
+        if (out.indexOf("5.3.1") == -1) {
+            fail("5.3.1 should be there: " + out);
+        }
+    }
+    
+    public static final class Lkp extends AbstractLookup {
+        public static InstanceContent ic = new InstanceContent();
+        
+        public Lkp() {
+            super(ic);
+        }
+    }
+
+    private static class MyModule extends ModuleInfo {
+
+        private boolean enabled;
+        private String cnb;
+        private int r;
+        private SpecificationVersion specV;
+
+        MyModule(String cnb, int r, SpecificationVersion specV, boolean enabled) {
+            this.cnb = cnb;
+            this.r = r;
+            this.specV = specV;
+            this.enabled = enabled;
+        }
+
+        public String getCodeNameBase() {
+            return cnb;
+        }
+
+        public int getCodeNameRelease() {
+            return r;
+        }
+
+        public String getCodeName() {
+            if (r == -1) {
+                return getCodeNameBase();
+            }
+            return getCodeNameBase() + '/' + r;
+        }
+
+        public SpecificationVersion getSpecificationVersion() {
+            return specV;
+        }
+
+        public boolean isEnabled() {
+            return enabled;
+        }
+
+        public Object getAttribute(String attr) {
+            return null;
+        }
+
+        public Object getLocalizedAttribute(String attr) {
+            return null;
+        }
+
+        public Set getDependencies() {
+            return new HashSet();
+        }
+
+        public boolean owns(Class clazz) {
+            return false;
+        }
+
+    }
+}
