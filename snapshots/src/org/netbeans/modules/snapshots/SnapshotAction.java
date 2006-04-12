bcefa35d9aedf77e23fc09b50f40309b95e45ce3
tboudreau@netbeans.org <tboudreau@netbeans.org>
2006-04-12 18:27:58 +0000

    Something "perspectives" like but different.  Takes a snapshot of the current window system config and set of open projects and saves it, and you can restore to that window configuration later. Just got it working and still lots of work to do:  - Core settings aggressively tries to write to files that have been deleted and throws NPEs - needs a fix, the bug is there  - Splitter positioned not updated when restoring a snapshot  - Currently does all its work in the AWT thread, which is wrong  - Combo box is the wrong UI for the toolbar, and has problems - use the Window menu instead for now.  Need to design a pretty camera icon before I fix it. But hey, it works, and given that it deletes all the files in all subdirs of Windows2Local and replaces them, I'm amazed it does anything at all :-)

---
+++ snapshots/src/org/netbeans/modules/snapshots/SnapshotAction.java
@@ -0,0 +1,223 @@
+/*
+ * This is my template
+ *
+ * SnapshotAction.java
+ *
+ * Created on April 11, 2006, 6:45 AM
+ *
+ * To change this template, choose Tools | Template Manager
+ * and open the template in the editor.
+ */
+
+package org.netbeans.modules.snapshots;
+
+import java.awt.BorderLayout;
+import java.awt.Component;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import javax.swing.AbstractAction;
+import javax.swing.DefaultComboBoxModel;
+import javax.swing.DefaultListCellRenderer;
+import javax.swing.JComboBox;
+import javax.swing.JComponent;
+import javax.swing.JLabel;
+import javax.swing.JList;
+import javax.swing.JMenu;
+import javax.swing.JMenuItem;
+import javax.swing.JPanel;
+import javax.swing.JSeparator;
+import javax.swing.UIManager;
+import javax.swing.border.Border;
+import javax.swing.border.EmptyBorder;
+import javax.swing.border.MatteBorder;
+import org.openide.DialogDisplayer;
+import org.openide.NotifyDescriptor;
+import org.openide.util.NbBundle;
+import org.openide.util.actions.Presenter;
+
+/**
+ *
+ * @author Timothy Boudreau
+ */
+public class SnapshotAction extends AbstractAction implements Presenter.Toolbar, Presenter.Menu {
+    
+    /** Creates a new instance of SnapshotAction */
+    public SnapshotAction() {
+        putValue (NAME, "snapshots");
+    }
+
+    public void actionPerformed(ActionEvent e) {
+        throw new UnsupportedOperationException();
+    }
+
+    private JComboBox combo = null;
+    public Component getToolbarPresenter() {
+        refresh();
+        return combo;
+    }
+
+    private JMenu menu = null;
+    public JMenuItem getMenuPresenter() {
+        refresh();
+        return menu;
+    }
+
+    private JPanel pnl;
+    private boolean inRefresh = false;
+    private void refresh() {
+        if (running) {
+            System.err.println("Refresh when running - give up");
+            return;
+        }
+        inRefresh = true;
+        try {
+            if (menu == null) {
+                menu = new JMenu();
+                menu.setText (NbBundle.getMessage(SnapshotAction.class,
+                        "LBL_SnapshotsToolbar"));
+            } else {
+                menu.removeAll();
+            }
+            if (combo == null) {
+                combo = new JComboBox();
+                combo.setRenderer(new R());
+            }
+            if (pnl == null) {
+                pnl = new JPanel();
+                pnl.setLayout (new BorderLayout());
+                pnl.add (new JLabel (NbBundle.getMessage(SnapshotAction.class,
+                        "LBL_SnapshotsToolbar")), BorderLayout.LINE_START);
+                pnl.add (combo, BorderLayout.LINE_END);
+                pnl.setBorder (new EmptyBorder (5, 5, 5, 5));
+            }
+
+            String[] names = Snapshots.getNames();
+            boolean wasZero = names.length == 0;
+            if (wasZero) {
+                names = new String[] { "" };
+            } else {
+                for (int i=0; i < names.length; i++) {
+                    JMenuItem item = new JMenuItem (names[i]);
+                    item.addActionListener(al);
+                    menu.add (item);
+                }
+                menu.add (new JSeparator());
+            }
+            JMenuItem createItem = new JMenuItem (al.toString());
+            createItem.putClientProperty (CREATE, al.toString());
+            createItem.addActionListener(al);
+            menu.add (createItem);
+
+            Object[] o = new Object [ names.length + 1 ];
+
+            System.arraycopy(names, 0, o, 0, names.length);
+            o[o.length - 1] = al;
+            DefaultComboBoxModel mdl = new DefaultComboBoxModel (o);
+            combo.setModel (mdl);
+            combo.addActionListener(al);
+        } finally {
+            inRefresh = false;
+        }
+    }
+
+    private static final String CREATE = "CREATE";
+
+    public boolean running = false;
+
+    private AL al = new AL();
+    private class AL implements ActionListener {
+        public void actionPerformed(ActionEvent e) {
+            System.err.println("ap...");
+            if (running) {
+                System.err.println("Running - give up");
+                return;
+            }
+            if (inRefresh) {
+                return;
+            }
+            String name = null;
+            if (e.getSource() instanceof JComboBox) {
+                JComboBox box = (JComboBox) e.getSource();
+                System.err.println("Selected item is " + box.getSelectedItem() +
+                        " - " + box.getSelectedItem().getClass());
+                if (box.getSelectedItem() == al) {
+                    System.err.println("Calling create");
+                    create();
+                    return;
+                } else {
+                    name = ((JComboBox) e.getSource()).getSelectedItem().toString();
+                }
+            } else if (e.getSource() instanceof JMenuItem) {
+                JMenuItem item = (JMenuItem) e.getSource();
+                if (item.getClientProperty(CREATE) != null) {
+                    System.err.println("Calling create for menu");
+                    create();
+                    return;
+                }
+                name = item.getText();
+            } else {
+                throw new IllegalArgumentException (e.getSource().toString());
+            }
+            if ("".equals(name)) {
+                System.err.println("Empty name - give up");
+                return;
+            }
+            System.err.println("Restore snapshot " + name);
+            running = true;
+            try {
+                Snapshots.restoreSnapshot(name);
+            } finally {
+                running = false;
+            }
+        }
+
+        public String toString() {
+            return NbBundle.getMessage (SnapshotAction.class, "LBL_NewSnapshot");
+        }
+    }
+
+    private void create() {
+        running = true;
+        try {
+            NotifyDescriptor.InputLine line = new NotifyDescriptor.InputLine (
+                    NbBundle.getMessage(SnapshotAction.class, "LBL_TakeSnapshot"),
+                    NbBundle.getMessage(SnapshotAction.class, "TTL_TakeSnapshot")
+                    );
+            if (NotifyDescriptor.OK_OPTION.equals(DialogDisplayer.getDefault().notify(line))) {
+                String s = line.getInputText();
+                if ("".equals(s.trim()) || s.indexOf('/') >= 0 || s.indexOf(".") >= 0) {
+                    NotifyDescriptor.Message msg = new NotifyDescriptor.Message (
+                            NbBundle.getMessage(SnapshotAction.class, "MSG_BadName"));
+                    DialogDisplayer.getDefault().notify(msg);
+                    return;
+                }
+                if (Snapshots.isValid(s)) {
+                    NotifyDescriptor msg = new NotifyDescriptor (
+                            NbBundle.getMessage(SnapshotAction.class, "MSG_SnapshotExists", s),
+                            NbBundle.getMessage(SnapshotAction.class, "TTL_SnapshotExists"),
+                            NotifyDescriptor.OK_CANCEL_OPTION, NotifyDescriptor.WARNING_MESSAGE,
+                            new Object[] { NotifyDescriptor.OK_OPTION, NotifyDescriptor.CANCEL_OPTION},
+                            NotifyDescriptor.OK_OPTION);
+                    if (!NotifyDescriptor.OK_OPTION.equals(DialogDisplayer.getDefault().notify(msg))) {
+                        return;
+                    }
+                }
+                Snapshots.takeSnapshot(s);
+            }
+        } finally {
+            running = false;
+        }
+        refresh();
+    }
+
+    private class R extends DefaultListCellRenderer {
+        private Border clear = new EmptyBorder (0, 0, 1, 0);
+        private Border line = new MatteBorder (0, 0, 1, 0, UIManager.getColor("textText"));
+        public Component getListCellRendererComponent(JList list, Object value, int index, boolean isSelected, boolean cellHasFocus) {
+            JComponent result = (JComponent)
+                super.getListCellRendererComponent(list, value, index, isSelected, cellHasFocus);
+            result.setBorder(value == al ? line : clear);
+            return result;
+        }
+    }
+}
---
+++ snapshots/src/org/netbeans/modules/snapshots/Snapshots.java
@@ -0,0 +1,329 @@
+package org.netbeans.modules.snapshots;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+import org.netbeans.api.project.FileOwnerQuery;
+import org.netbeans.api.project.Project;
+import org.netbeans.api.project.ui.OpenProjects;
+import org.netbeans.core.NbTopManager;
+import org.openide.ErrorManager;
+import org.openide.filesystems.FileLock;
+import org.openide.filesystems.FileObject;
+import org.openide.filesystems.FileSystem;
+import org.openide.filesystems.FileUtil;
+import org.openide.filesystems.Repository;
+import org.openide.loaders.DataFolder;
+import org.openide.loaders.DataObject;
+import org.openide.loaders.DataShadow;
+import org.openide.util.Lookup;
+import org.openide.util.Utilities;
+
+/**
+ *
+ * @author Timothy Boudreau
+ */
+public class Snapshots {
+    
+    /** Creates a new instance of Snapshots */
+    private Snapshots() {
+    }
+
+    private static final String WIN_PATH = "snapshots/windows";
+    private static final String PRJS_PATH = "snapshots/projects";
+    public static String[] getNames() {
+        FileObject ob = getWindowsFolder(false);
+        if (ob == null) {
+            return new String[0];
+        }
+        FileObject[] fob = ob.getChildren();
+
+        List results = new ArrayList();
+        for (int i=0; i < fob.length; i++) {
+            String nm = fob[i].getName();
+            results.add(nm);
+            if (!Snapshots.isValid(nm)) {
+                try {
+                    fob[i].delete();
+                } catch (IOException ioe) {
+                    ErrorManager.getDefault().notify(ioe);
+                }
+            }
+        }
+        String[] result = (String[]) results.toArray(new String[results.size()]);
+        return result;
+    }
+
+    private static FileObject getProjectsFolder(boolean create) {
+        FileObject result = Repository.getDefault().getDefaultFileSystem().getRoot().getFileObject(PRJS_PATH);
+        if (result == null && create) {
+            try {
+                FileUtil.createFolder(Repository.getDefault().getDefaultFileSystem().getRoot(),
+                        PRJS_PATH);
+            } catch (IOException ex) {
+                ErrorManager.getDefault().notify(ex);
+            }
+        }
+        return result;
+    }
+
+    private static FileObject getWindowsFolder(boolean create) {
+        FileObject result = Repository.getDefault().getDefaultFileSystem().getRoot().getFileObject(WIN_PATH);
+        if (result == null && create) {
+            try {
+                result = FileUtil.createFolder(Repository.getDefault().getDefaultFileSystem().getRoot(),
+                        WIN_PATH);
+            } catch (IOException ex) {
+                ErrorManager.getDefault().notify(ex);
+            }
+        }
+        return result;
+    }
+
+    public static boolean isValid (String name) {
+        return getWindowsSnapshotFolder(name,  false) != null && getProjectsSnapshotFolder(name,  false) != null;
+    }
+
+    private static FileObject getWindowsSnapshotFolder (String name,  boolean create) {
+        String path = getWindowsFolderPath(name);
+        FileObject result = Repository.getDefault().getDefaultFileSystem().getRoot().getFileObject(path);
+        if (result == null && create) {
+            try {
+                result = FileUtil.createFolder(Repository.getDefault().getDefaultFileSystem().getRoot(),
+                        path);
+            } catch (IOException ex) {
+                ErrorManager.getDefault().notify(ex);
+            }
+        }
+        return result;
+    }
+
+    private static FileObject getProjectsSnapshotFolder (String name,  boolean create) {
+        String path = getProjectsFolderPath(name);
+        FileObject result = Repository.getDefault().getDefaultFileSystem().getRoot().getFileObject(path);
+        if (result == null && create) {
+            try {
+                result = FileUtil.createFolder(Repository.getDefault().getDefaultFileSystem().getRoot(),
+                        path);
+            } catch (IOException ex) {
+                ErrorManager.getDefault().notify(ex);
+            }
+        }
+        return result;
+    }
+
+    private static String getWindowsFolderPath(String name) {
+        String path = WIN_PATH + "/" + name;
+        return path;
+    }
+
+    private static String getProjectsFolderPath (String name) {
+        String path = PRJS_PATH + "/" + name;
+        return path;
+    }
+
+    public static boolean takeSnapshot(String name) {
+        try {
+            Repository.getDefault().getDefaultFileSystem().runAtomicAction(
+                new SnapshotTaker(name));
+            return true;
+        } catch (IOException ioe) {
+            ErrorManager.getDefault().notify(ioe);
+            return false;
+        }
+    }
+
+    public static boolean restoreSnapshot(String name) {
+        try {
+            Repository.getDefault().getDefaultFileSystem().runAtomicAction(
+                new SnapshotRestorer(name));
+            return true;
+        } catch (IOException ioe) {
+            ErrorManager.getDefault().notify(ioe);
+            return false;
+        }
+    }
+
+
+    private static FileObject getWindowSystemFolder() {
+        return Repository.getDefault().getDefaultFileSystem().getRoot().getFileObject("Windows2Local");
+    }
+
+    private static class SnapshotTaker implements FileSystem.AtomicAction {
+        private final String name;
+        public SnapshotTaker (String name) {
+            this.name=name;
+        }
+
+        public void run() throws IOException {
+            NbTopManager.WindowSystem wsys = (NbTopManager.WindowSystem)
+                Lookup.getDefault().lookup(NbTopManager.WindowSystem.class);
+
+            if (wsys == null) {
+                throw new IOException ("Couldn't find NbTopManager.WindowSystem" +
+                        " in the default lookup");
+            }
+            System.err.println("Begin winsys save " + System.currentTimeMillis());
+            wsys.save();
+            System.err.println("End winsys save " + System.currentTimeMillis());
+
+            FileObject winFld = getWindowsSnapshotFolder(name,  false);
+            FileObject prjFld = getProjectsSnapshotFolder(name,  false);
+            if (winFld != null) {
+                winFld.delete();
+            }
+            if (prjFld != null) {
+                prjFld.delete();
+            }
+            winFld = getWindowsSnapshotFolder(name,  true);
+            prjFld = getProjectsSnapshotFolder(name,  true);
+            if (winFld == null || prjFld == null) {
+                throw new IOException ("Failed to create folder for " + name);
+            }
+
+            FileObject winsys = getWindowSystemFolder();
+
+            if (winsys == null || !winsys.isFolder()) {
+                throw new IOException ("Window system folder missing");
+            }
+
+            System.err.println("BEGIN COPY Windows2Local to SNAPSHOT - copy " + winFld.getPath() + " to " + winsys.getPath() );
+            copy(winFld, winsys);
+
+            Project[] prjs = OpenProjects.getDefault().getOpenProjects();
+
+            DataFolder fld = DataFolder.findFolder(prjFld);
+
+            for (int i = 0; i < prjs.length; i++) {
+                FileObject dir = prjs[i].getProjectDirectory();
+                String pth = Utilities.replaceString(dir.getPath(), "/", "-");
+                DataObject orig = DataObject.find (dir);
+                DataShadow.create(fld, pth, orig);
+            }
+        }
+    }
+
+    private static void copy (FileObject to, FileObject src) throws IOException {
+        System.err.println("COPY " + src.getPath() + " to " + to.getPath());
+        FileObject result;
+        if (!src.isFolder()) {
+            result = to.getFileObject(src.getNameExt());
+            if (result == null) {
+                result = FileUtil.copyFile(src, to, src.getName(), src.getExt());
+            } else {
+                FileLock srcLock = src.lock();
+                FileUtil.copy(result.getInputStream(),
+                        src.getOutputStream(srcLock));
+                srcLock.releaseLock();
+            }
+        } else {
+            FileObject fld = to.getFileObject(src.getNameExt());
+            if (fld == null) {
+                fld = FileUtil.createFolder(to, src.getNameExt());
+            }
+            result = fld;
+            FileObject[] kidz = src.getChildren();
+            System.err.println("Start recurse copy - " + kidz.length +
+                    "children of " + fld.getPath());
+            
+            for (int i=0; i < kidz.length; i++) {
+                copy (fld, kidz[i]);
+            }
+        }
+        System.err.println("Copied " + src.getPath() + " to " + result.getPath());
+        FileUtil.copyAttributes(src, result);
+    }
+
+    private static class SnapshotRestorer implements FileSystem.AtomicAction {
+        private final String name;
+        public SnapshotRestorer (String name) {
+            this.name = name;
+        }
+
+        private void deleteChildrenOf (FileObject parent, String subfolderName) throws IOException {
+            System.err.println("Delete children of " + parent.getPath() + "/" + subfolderName );
+            FileObject fob = parent.getFileObject(subfolderName);
+            if (fob.isValid() && fob.isFolder()) {
+                FileObject[] kidz = fob.getChildren();
+                for (int i=0; i < kidz.length; i++) {
+                    kidz[i].delete();
+                }
+            }
+        }
+
+        public void run() throws IOException {
+            NbTopManager.WindowSystem wsys = (NbTopManager.WindowSystem)
+                Lookup.getDefault().lookup(NbTopManager.WindowSystem.class);
+            if (wsys == null) {
+                throw new IOException ("Couldn't find NbTopManager.WindowSystem" +
+                        " in the default lookup");
+            }
+
+            FileObject winFld = getWindowsSnapshotFolder(name,  false);
+            if (winFld == null || !winFld.isFolder()) {
+                throw new IOException ("Window system folder missing from snapshot");
+            }
+            winFld = winFld.getFileObject ("Windows2Local"); //fixme
+            if (winFld == null || !winFld.isFolder()) {
+                throw new IOException ("Windows2Local folder missing from snapshot");
+            }
+            FileObject prjFld = getProjectsSnapshotFolder(name,  false);
+
+            if (winFld == null) {
+                throw new IOException ("No saved winsys folder for " + name);
+            }
+            if (prjFld == null) {
+                throw new IOException ("No saved projects folder for " + name);
+            }
+
+            FileObject[] prjLinks = prjFld.getChildren();
+            Set prjsToOpen = new HashSet();
+            for (int i=0; i < prjLinks.length; i++) {
+                DataObject ob = DataObject.find (prjLinks[i]);
+                while (ob instanceof DataShadow) {
+                    ob = ((DataShadow) ob).getOriginal();
+                }
+                if (ob != null) {
+                    FileObject prjFolder = ob.getPrimaryFile();
+                    Project prj = FileOwnerQuery.getOwner(prjFolder);
+                    prjsToOpen.add (prj);
+                }
+            }
+
+            Set currentPrjs = new HashSet (Arrays.asList(OpenProjects.getDefault().getOpenProjects()));
+            Set toClose = new HashSet (currentPrjs);
+            toClose.removeAll (prjsToOpen);
+            prjsToOpen.removeAll(currentPrjs);
+            Project[] toCloseArr = (Project[]) toClose.toArray(new Project[toClose.size()]);
+            Project[] toOpenArr = (Project[]) prjsToOpen.toArray(new Project[prjsToOpen.size()]);
+
+            OpenProjects.getDefault().close(toCloseArr);
+            OpenProjects.getDefault().open(toOpenArr, false);
+
+            FileObject winsys = getWindowSystemFolder();
+            System.err.println("DELETING CURRENT WINDOW SYSTEM CONFIGURATION");
+            deleteChildrenOf (winsys, "Components");
+            deleteChildrenOf (winsys, "Groups");
+            deleteChildrenOf (winsys, "Modes");
+
+            FileObject wsmgr = winsys.getFileObject("WindowManager.wsmgr");
+            if (wsmgr != null) {
+                wsmgr.delete();
+            }
+            FileObject[] repl = winFld.getChildren();
+
+            System.err.println("BEGIN COPY SNAPSHOT WINSYS DATA TO Windows2Local " + repl.length + " files/folders");
+            for (int i=0; i < repl.length; i++) {
+                copy (winsys, repl[i]);
+            }
+            
+            wsys.load();
+        }
+    }
+
+
+}
+
