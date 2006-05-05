31fdda3c29bb06e1820f2de7887182d3a50fa667
sandipchitale@netbeans.org <sandipchitale@netbeans.org>
2006-05-05 23:30:08 +0000

    Show custom icons (similar to Navigator window) for local variable and fields based on public, protected, (package), private and static qualifiers. This can be turned off by setting the system property: -Dorg.netbeans.modules.debugger.localsviewenhancements.dontShowCustomIcons=true

---
+++ debugger.localsviewenhancements/src/org/netbeans/modules/debugger/localsviewenhancements/ui/models/LocalsViewNodeModelFilter.java
@@ -0,0 +1,122 @@
+/*
+ *                 Sun Public License Notice
+ *
+ * The contents of this file are subject to the Sun Public License
+ * Version 1.0 (the "License"). You may not use this file except in
+ * compliance with the License. A copy of the License is available at
+ * http://www.sun.com/
+ *
+ * The Original Code is NetBeans. The Initial Developer of the Original
+ * Code is Sun Microsystems, Inc. Portions Copyright 1997-2006 Sun
+ * Microsystems, Inc. All Rights Reserved.
+ */
+
+package org.netbeans.modules.debugger.localsviewenhancements.ui.models;
+
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+import java.lang.reflect.Modifier;
+import org.netbeans.api.debugger.jpda.Field;
+import org.netbeans.api.debugger.jpda.This;
+import org.netbeans.api.debugger.jpda.Variable;
+import org.netbeans.spi.viewmodel.ModelListener;
+import org.netbeans.spi.viewmodel.NodeModel;
+import org.netbeans.spi.viewmodel.NodeModelFilter;
+import org.netbeans.spi.viewmodel.UnknownTypeException;
+
+/**
+ *
+ * @author Sandip V. Chitale (Sandip.Chitale@Sun.Com)
+ */
+public class LocalsViewNodeModelFilter implements NodeModelFilter {
+    private static boolean dontShowCustomIcons = Boolean.getBoolean("org.netbeans.modules.debugger.localsviewenhancements.dontShowCustomIcons");
+    public LocalsViewNodeModelFilter() {
+        
+    }
+    
+    public String getDisplayName(NodeModel original, Object node) throws UnknownTypeException {
+        return original.getDisplayName(node);
+    }
+    
+    public String getIconBase(NodeModel original, Object node) throws UnknownTypeException {
+        if (!dontShowCustomIcons) {
+            if (node instanceof This) {
+                return "org/netbeans/modules/java/navigation/resources/class";
+            } else if (node instanceof Field) {
+                int modifiers = getModifiers((Field) node);
+                if (Modifier.isStatic(modifiers)) {
+                    if (Modifier.isPrivate(modifiers)){
+                        return "org/netbeans/modules/java/navigation/resources/variableStPrivate";
+                    } else if (Modifier.isProtected(modifiers)){
+                        return "org/netbeans/modules/java/navigation/resources/variableStProtected";
+                    } else if (Modifier.isPublic(modifiers)){
+                        return "org/netbeans/modules/java/navigation/resources/variableStPublic";
+                    } else {
+                        return "org/netbeans/modules/java/navigation/resources/variableStPackage";
+                    }
+                } else {
+                    if (Modifier.isPrivate(modifiers)){
+                        return "org/netbeans/modules/java/navigation/resources/variablePrivate";
+                    } else if (Modifier.isProtected(modifiers)){
+                        return "org/netbeans/modules/java/navigation/resources/variableProtected";
+                    } else if (Modifier.isPublic(modifiers)){
+                        return "org/netbeans/modules/java/navigation/resources/variablePublic";
+                    } else {
+                        return "org/netbeans/modules/java/navigation/resources/variablePackage";
+                    }
+                }
+            } else if (node instanceof Variable) {
+                return "org/netbeans/modules/java/navigation/resources/variables";
+            }
+        }
+        return original.getIconBase(node);
+    }
+    
+    public String getShortDescription(NodeModel original, Object node) throws UnknownTypeException {
+        return original.getShortDescription(node);
+    }
+    
+    public void addModelListener(ModelListener l) {
+    }
+    
+    public void removeModelListener(ModelListener l) {
+    }
+    
+    private int getModifiers(Field node) {
+        Class nodeClass = node.getClass();
+        java.lang.reflect.Field fieldField = null;
+        
+        while (nodeClass != null) {
+            try {
+                fieldField = nodeClass.getDeclaredField("field");
+            } catch (SecurityException ex) {
+                break;
+            } catch (NoSuchFieldException ex) {
+            }
+            nodeClass = nodeClass.getSuperclass();
+        }
+        
+        if (fieldField != null) {
+            fieldField.setAccessible(true);
+            try {
+                Object fieldFieldValue = fieldField.get(node);
+                Class fieldFieldValueClass = fieldFieldValue.getClass();
+                try {
+                    Method method = fieldFieldValueClass.getMethod("modifiers", new Class[0]);
+                    try {
+                        Integer modifiersInteger = (Integer) method.invoke(fieldFieldValue, new Object[0]);
+                        return modifiersInteger.intValue();
+                    } catch (IllegalArgumentException ex) {
+                    } catch (InvocationTargetException ex) {
+                    } catch (IllegalAccessException ex) {
+                    }
+                } catch (SecurityException ex) {
+                } catch (NoSuchMethodException ex) {
+                }
+            } catch (IllegalArgumentException ex) {
+            } catch (IllegalAccessException ex) {
+            }
+        }
+        return 0;
+    }
+}
