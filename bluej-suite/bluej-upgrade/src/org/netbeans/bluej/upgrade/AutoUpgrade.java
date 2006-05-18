4df193145c4ff091467dc3abd0893b85f7fb1b60
anebuzelsky@netbeans.org <anebuzelsky@netbeans.org>
2006-05-18 14:47:13 +0000

    Adding bluej-upgrade subproject into bluej-suite. This subproject is a copy of upgrader code (import settings and license dialog) from nb5.0 cluster.

---
+++ bluej-suite/bluej-upgrade/src/org/netbeans/bluej/upgrade/AutoUpgrade.java
@@ -0,0 +1,173 @@
+/*
+ *                 Sun Public License Notice
+ *
+ * The contents of this file are subject to the Sun Public License
+ * Version 1.0 (the "License"). You may not use this file except in
+ * compliance with the License. A copy of the License is available at
+ * http://www.sun.com/
+ *
+ * The Original Code is NetBeans. The Initial Developer of the Original
+ * Code is Sun Microsystems, Inc. Portions Copyright 1997-2006 Sun
+ * Microsystems, Inc. All Rights Reserved.
+ */
+
+package org.netbeans.bluej.upgrade;
+import java.io.*;
+import java.io.File;
+import java.io.IOException;
+import java.lang.reflect.Array;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.Enumeration;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Map;
+import javax.swing.JOptionPane;
+import javax.swing.text.SimpleAttributeSet;
+import javax.swing.text.StyleConstants;
+
+import org.netbeans.bluej.util.Util;
+import org.openide.ErrorManager;
+import org.openide.filesystems.FileObject;
+import org.openide.filesystems.FileUtil;
+import org.openide.filesystems.LocalFileSystem;
+import org.openide.filesystems.MultiFileSystem;
+import org.openide.filesystems.Repository;
+import org.openide.filesystems.XMLFileSystem;
+import org.openide.util.NbBundle;
+import org.xml.sax.SAXException;
+
+/** pending
+ *
+ * @author  Jiri Rechtacek
+ */
+public final class AutoUpgrade {
+
+    public static void main (String[] args) throws Exception {
+        String[] version = new String[1];
+        File sourceFolder = checkPrevious (version);
+        if (sourceFolder != null) {
+            if (!showUpgradeDialog (sourceFolder)) {
+                throw new org.openide.util.UserCancelException ();
+            }
+            doUpgrade (sourceFolder, version[0]);
+        }
+    }
+    
+    // the order of VERSION_TO_CHECK here defines the precedence of imports
+    // the first one will be choosen for import
+    final static private List VERSION_TO_CHECK = Collections.EMPTY_LIST; //Arrays.asList (new String[] { ".netbeans/5.0" });
+    
+    static private File checkPrevious (String[] version) {
+        boolean exists;
+        
+        String userHome = System.getProperty ("user.home"); // NOI18N
+        File sourceFolder = null;
+        
+        if (userHome != null) {
+            File userHomeFile = new File (userHome);
+            exists = userHomeFile.isDirectory ();
+
+            Iterator it = VERSION_TO_CHECK.iterator ();
+            String ver;
+            while (it.hasNext () && sourceFolder == null) {
+                ver = (String) it.next ();
+                sourceFolder = new File (userHomeFile.getAbsolutePath (), ver);
+                
+                if (sourceFolder.isDirectory ()) {
+                    version[0] = sourceFolder.getName();
+                    break;
+                }
+                sourceFolder = null;
+            }
+            return sourceFolder;
+        } else {
+            return null;
+        }
+    }
+    
+    private static boolean showUpgradeDialog (final File source) {
+        Util.setDefaultLookAndFeel();
+        JOptionPane p = new JOptionPane (
+            new AutoUpgradePanel (source.getAbsolutePath ()),
+            JOptionPane.QUESTION_MESSAGE,
+            JOptionPane.YES_NO_OPTION
+        );
+        javax.swing.JDialog d = p.createDialog (
+            null,
+            NbBundle.getMessage (AutoUpgrade.class, "MSG_Confirmation_Title") // NOI18N
+        );
+        d.setModal (true);
+        d.setVisible (true);
+
+        return new Integer (JOptionPane.YES_OPTION).equals (p.getValue ());
+    }
+    
+    static void doUpgrade (File source, String oldVersion) 
+    throws java.io.IOException, java.beans.PropertyVetoException {
+        
+        File userdir = new File(System.getProperty ("netbeans.user", "")); // NOI18N
+        
+        java.util.Set includeExclude;
+        try {
+            Reader r = new InputStreamReader (
+                    AutoUpgrade.class.getResourceAsStream ("copy" + oldVersion), // NOI18N
+                    "utf-8"); // NOI18N
+            includeExclude = IncludeExclude.create (r);
+            r.close ();
+        } catch (IOException ex) {
+            IOException e = new IOException ("Cannot import from version: " + oldVersion);
+            e.initCause (ex);
+            throw e;
+        }
+
+        ErrorManager.getDefault ().log (
+            ErrorManager.USER, "Import: Old version: " // NOI18N
+            + oldVersion + ". Importing from " + source + " to " + userdir // NOI18N
+        );
+        
+        File oldConfig = new File (source, "config"); // NOI18N
+        org.openide.filesystems.FileSystem old;
+        {
+            LocalFileSystem lfs = new LocalFileSystem ();
+            lfs.setRootDirectory (oldConfig);
+            
+            XMLFileSystem xmlfs = null;
+            try {
+                URL url = AutoUpgrade.class.getResource("layer" + oldVersion + ".xml"); // NOI18N
+                xmlfs = new XMLFileSystem(url);
+            } catch (SAXException ex) {
+                IOException e = new IOException ("Cannot import from version: " + oldVersion); // NOI18N
+                e.initCause (ex);
+                throw e;
+            }
+            
+            old = createLayeredSystem(lfs, xmlfs);
+        }
+        org.openide.filesystems.FileSystem mine = Repository.getDefault ().
+            getDefaultFileSystem ();
+        
+        Copy.copyDeep (old.getRoot (), mine.getRoot (), includeExclude);
+        
+    }
+
+    static MultiFileSystem createLayeredSystem(final LocalFileSystem lfs, final XMLFileSystem xmlfs) {
+        MultiFileSystem old;
+        
+        old = new MultiFileSystem (
+            new org.openide.filesystems.FileSystem[] { lfs, xmlfs }
+        ) {
+            {
+                setPropagateMasks(true);
+            }
+        };
+        return old;
+    }
+
+}
+
+
+
