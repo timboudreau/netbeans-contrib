8f1aae394fef5f62e6c7d3cd0b728abfd45cb73f
jglick@netbeans.org <jglick@netbeans.org>
2006-06-14 13:29:26 +0000

    Avoid accessing static members via instances.

--- paintcatcher/src/org/netbeans/modules/paintcatcher/PaintCatcherModule.java
+++ paintcatcher/src/org/netbeans/modules/paintcatcher/PaintCatcherModule.java
@@ -7,7 +7,7 @@
  * http://www.sun.com/
  * 
  * The Original Code is NetBeans. The Initial Developer of the Original
- * Code is Sun Microsystems, Inc. Portions Copyright 1997-2004 Sun
+ * Code is Sun Microsystems, Inc. Portions Copyright 1997-2006 Sun
  * Microsystems, Inc. All Rights Reserved.
  */
 
@@ -26,12 +26,10 @@ public class PaintCatcherModule extends ModuleInstall {
         boolean start = Boolean.getBoolean("org.netbeans.modules.paintcatcher");
         if (start) {
             System.out.println("start paint logging");
-            ConfigureAction conf = (ConfigureAction)ConfigureAction.get(ConfigureAction.class);
-            conf.classToLog = java.awt.Component.class;
-            conf.matchIfAncestor = true;
-            conf.matchIfSubclass = true;
+            ConfigureAction.classToLog = java.awt.Component.class;
+            ConfigureAction.matchIfAncestor = true;
+            ConfigureAction.matchIfSubclass = true;
             LogAction log = (LogAction)LogAction.get(LogAction.class);
-            
             log.actionPerformed(new ActionEvent(this, 0, null));
         }
     }
