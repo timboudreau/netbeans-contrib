f9c2b7ecf2781f3e653eb0ee8172b979d2739931
jtulach@netbeans.org <jtulach@netbeans.org>
2006-06-30 13:40:32 +0000

    Updating to CDDL for module which is not in standard build like a11y

--- a11y/src/org/netbeans/a11y/ui/AccPropPanel.java
+++ a11y/src/org/netbeans/a11y/ui/AccPropPanel.java
@@ -1,13 +1,19 @@
 /*
- *                 Sun Public License Notice
+ * The contents of this file are subject to the terms of the Common Development
+ * and Distribution License (the License). You may not use this file except in
+ * compliance with the License.
  *
- * The contents of this file are subject to the Sun Public License
- * Version 1.0 (the "License"). You may not use this file except in
- * compliance with the License. A copy of the License is available at
- * http://www.sun.com/
+ * You can obtain a copy of the License at http://www.netbeans.org/cddl.html
+ * or http://www.netbeans.org/cddl.txt.
  *
- * The Original Code is NetBeans. The Initial Developer of the Original
- * Code is Sun Microsystems, Inc. Portions Copyright 1997-2006 Sun
+ * When distributing Covered Code, include this CDDL Header Notice in each file
+ * and include the License file at http://www.netbeans.org/cddl.txt.
+ * If applicable, add the following below the CDDL Header, with the fields
+ * enclosed by brackets [] replaced by your own identifying information:
+ * "Portions Copyrighted [year] [name of copyright owner]"
+ *
+ * The Original Software is NetBeans. The Initial Developer of the Original
+ * Software is Sun Microsystems, Inc. Portions Copyright 1997-2006 Sun
  * Microsystems, Inc. All Rights Reserved.
  */
 
@@ -21,12 +27,12 @@ import javax.swing.JLabel;
  * @author  Marian.Mirilovic@Sun.Com
  */
 public class AccPropPanel extends javax.swing.JPanel {
-    
+
     private java.awt.Color defaultColor = java.awt.Color.blue;
     private final String NO_MNEMONIC = "no mnemonic";
     private final String NO_CHILDREN = "no children";
     private final String NULL = "null";
-    
+
     /** Creates new panel AccPropPanel */
     public AccPropPanel() {
         initComponents();
