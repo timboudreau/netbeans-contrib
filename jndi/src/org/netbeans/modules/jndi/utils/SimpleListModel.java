b4d25ffd6032a87bb1c9f3b07cfa74b885e806fe
jtulach@netbeans.org <jtulach@netbeans.org>
2006-06-30 14:03:03 +0000

    Updating to CDDL for module which is not in standard build like jndi

--- jndi/src/org/netbeans/modules/jndi/utils/SimpleListModel.java
+++ jndi/src/org/netbeans/modules/jndi/utils/SimpleListModel.java
@@ -1,13 +1,19 @@
 /*
- *                 Sun Public License Notice
- * 
- * The contents of this file are subject to the Sun Public License
- * Version 1.0 (the "License"). You may not use this file except in
- * compliance with the License. A copy of the License is available at
- * http://www.sun.com/
- * 
- * The Original Code is NetBeans. The Initial Developer of the Original
- * Code is Sun Microsystems, Inc. Portions Copyright 1997-2001 Sun
+ * The contents of this file are subject to the terms of the Common Development
+ * and Distribution License (the License). You may not use this file except in
+ * compliance with the License.
+ *
+ * You can obtain a copy of the License at http://www.netbeans.org/cddl.html
+ * or http://www.netbeans.org/cddl.txt.
+ *
+ * When distributing Covered Code, include this CDDL Header Notice in each file
+ * and include the License file at http://www.netbeans.org/cddl.txt.
+ * If applicable, add the following below the CDDL Header, with the fields
+ * enclosed by brackets [] replaced by your own identifying information:
+ * "Portions Copyrighted [year] [name of copyright owner]"
+ *
+ * The Original Software is NetBeans. The Initial Developer of the Original
+ * Software is Sun Microsystems, Inc. Portions Copyright 1997-2006 Sun
  * Microsystems, Inc. All Rights Reserved.
  */
 
@@ -19,7 +25,7 @@ import java.util.Vector;
 
 
 /** This class represent an Dat Model for JList component
- */ 
+ */
 public class SimpleListModel extends javax.swing.AbstractListModel {
 
     /** Data pool*/
@@ -32,7 +38,7 @@ public class SimpleListModel extends javax.swing.AbstractListModel {
 
     /** Adding of element to Data Model
      *  @param Object object to be inserted
-     *  @return void 
+     *  @return void
      */
     public void addElement(Object obj){
         this.data.addElement(obj);
