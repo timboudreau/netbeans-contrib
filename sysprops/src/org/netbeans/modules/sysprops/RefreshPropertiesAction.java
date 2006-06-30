b8fda2e729b9ae473dac2e030e5e497a26b52aa3
jtulach@netbeans.org <jtulach@netbeans.org>
2006-06-30 14:20:02 +0000

    Updating to CDDL for module which is not in standard build like sysprops

--- sysprops/src/org/netbeans/modules/sysprops/RefreshPropertiesAction.java
+++ sysprops/src/org/netbeans/modules/sysprops/RefreshPropertiesAction.java
@@ -1,13 +1,19 @@
 /*
- *                 Sun Public License Notice
+ * The contents of this file are subject to the terms of the Common Development
+ * and Distribution License (the License). You may not use this file except in
+ * compliance with the License.
  *
- * The contents of this file are subject to the Sun Public License
- * Version 1.0 (the "License"). You may not use this file except in
- * compliance with the License. A copy of the License is available at
- * http://www.sun.com/
+ * You can obtain a copy of the License at http://www.netbeans.org/cddl.html
+ * or http://www.netbeans.org/cddl.txt.
  *
- * The Original Code is NetBeans. The Initial Developer of the Original
- * Code is Sun Microsystems, Inc. Portions Copyright 1997-2005 Sun
+ * When distributing Covered Code, include this CDDL Header Notice in each file
+ * and include the License file at http://www.netbeans.org/cddl.txt.
+ * If applicable, add the following below the CDDL Header, with the fields
+ * enclosed by brackets [] replaced by your own identifying information:
+ * "Portions Copyrighted [year] [name of copyright owner]"
+ *
+ * The Original Software is NetBeans. The Initial Developer of the Original
+ * Software is Sun Microsystems, Inc. Portions Copyright 1997-2006 Sun
  * Microsystems, Inc. All Rights Reserved.
  *
  * Contributor(s): Jesse Glick, Michael Ruflin
@@ -25,14 +31,14 @@ import org.openide.util.actions.CallableSystemAction;
  * @author Michael Ruflin
  */
 public class RefreshPropertiesAction extends CallableSystemAction {
-    
+
     /**
      * Performs the Action.
      */
     public void performAction() {
         PropertiesNotifier.getDefault().changed();
     }
-    
+
     /**
      * Returns the Name of this Action.
      * @return a localized display name
