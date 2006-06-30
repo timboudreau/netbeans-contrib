97be12497f2235d62d5cfce8698b471193392c14
jtulach@netbeans.org <jtulach@netbeans.org>
2006-06-30 14:10:17 +0000

    Updating to CDDL for module which is not in standard build like metrics

--- metrics/src/org/netbeans/modules/metrics/NTMetric.java
+++ metrics/src/org/netbeans/modules/metrics/NTMetric.java
@@ -1,15 +1,21 @@
 /*
  * NTMetric.java
  *
- *                 Sun Public License Notice
- * 
- * The contents of this file are subject to the Sun Public License
- * Version 1.0 (the "License"). You may not use this file except in
- * compliance with the License. A copy of the License is available at
- * http://www.sun.com/
- * 
- * The Original Code is NetBeans. The Initial Developer of the Original
- * Code is Sun Microsystems, Inc. Portions Copyright 1997-2002 Sun
+ * The contents of this file are subject to the terms of the Common Development
+ * and Distribution License (the License). You may not use this file except in
+ * compliance with the License.
+ *
+ * You can obtain a copy of the License at http://www.netbeans.org/cddl.html
+ * or http://www.netbeans.org/cddl.txt.
+ *
+ * When distributing Covered Code, include this CDDL Header Notice in each file
+ * and include the License file at http://www.netbeans.org/cddl.txt.
+ * If applicable, add the following below the CDDL Header, with the fields
+ * enclosed by brackets [] replaced by your own identifying information:
+ * "Portions Copyrighted [year] [name of copyright owner]"
+ *
+ * The Original Software is NetBeans. The Initial Developer of the Original
+ * Software is Sun Microsystems, Inc. Portions Copyright 1997-2006 Sun
  * Microsystems, Inc. All Rights Reserved.
  *
  * Contributor(s): Thomas Ball
@@ -26,7 +32,7 @@ import java.util.*;
 
 /**
  * The "Number of Tramps" metric class.  A tramp is traditionally defined
- * as an unused parameter in a method.  For Java, this definition has 
+ * as an unused parameter in a method.  For Java, this definition has
  * been enhanced to ignore unused parameters for methods whose signature
  * is either defined by an interface or a superclass.  In those cases,
  * the class author does not control the method's definition.
--- metrics/src/org/netbeans/modules/metrics/WMCMetric.java
+++ metrics/src/org/netbeans/modules/metrics/WMCMetric.java
@@ -1,15 +1,21 @@
 /*
  * WMCMetric.java
  *
- *                 Sun Public License Notice
- * 
- * The contents of this file are subject to the Sun Public License
- * Version 1.0 (the "License"). You may not use this file except in
- * compliance with the License. A copy of the License is available at
- * http://www.sun.com/
- * 
- * The Original Code is NetBeans. The Initial Developer of the Original
- * Code is Sun Microsystems, Inc. Portions Copyright 1997-2002 Sun
+ * The contents of this file are subject to the terms of the Common Development
+ * and Distribution License (the License). You may not use this file except in
+ * compliance with the License.
+ *
+ * You can obtain a copy of the License at http://www.netbeans.org/cddl.html
+ * or http://www.netbeans.org/cddl.txt.
+ *
+ * When distributing Covered Code, include this CDDL Header Notice in each file
+ * and include the License file at http://www.netbeans.org/cddl.txt.
+ * If applicable, add the following below the CDDL Header, with the fields
+ * enclosed by brackets [] replaced by your own identifying information:
+ * "Portions Copyrighted [year] [name of copyright owner]"
+ *
+ * The Original Software is NetBeans. The Initial Developer of the Original
+ * Software is Sun Microsystems, Inc. Portions Copyright 1997-2006 Sun
  * Microsystems, Inc. All Rights Reserved.
  *
  * Contributor(s): Thomas Ball
@@ -25,9 +31,9 @@ import java.util.Iterator;
 
 /**
  * Class which calculates the Weighted Methods per Class metric.
- * WMC is defined as the sum of the complexity of each method; 
+ * WMC is defined as the sum of the complexity of each method;
  * unfortunately, there is no consensus as to the definition of
- * "complexity".  Most metrics tools just punt and assign a 
+ * "complexity".  Most metrics tools just punt and assign a
  * complexity value of one for each method, such that WMC equals
  * the number of methods.
  *
