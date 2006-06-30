eae1f5bad3c370968be15c4322ebfbd0fdc812ff
dkaspar@netbeans.org <dkaspar@netbeans.org>
2006-06-30 16:44:35 +0000

    SwingBorderTest - CompositeBorder usage

--- visual.examples/src/test/swingborder/SwingBorderTest.java
+++ visual.examples/src/test/swingborder/SwingBorderTest.java
@@ -17,6 +17,10 @@ import org.netbeans.api.visual.widget.LabelWidget;
 import org.netbeans.api.visual.widget.Scene;
 import org.netbeans.api.visual.widget.Widget;
 import org.netbeans.api.visual.action.ZoomAction;
+import org.netbeans.api.visual.border.CompositeBorder;
+import org.netbeans.api.visual.border.ImageBorder;
+import org.netbeans.api.visual.border.SwingBorder;
+import org.openide.util.Utilities;
 import test.SceneSupport;
 
 import javax.swing.border.BevelBorder;
@@ -30,6 +34,8 @@ import java.awt.*;
  */
 public class SwingBorderTest {
 
+    private static org.netbeans.api.visual.border.Border BORDER_SHADOW_NORMAL = new ImageBorder (new Insets (6, 6, 6, 6), Utilities.loadImage ("org/netbeans/modules/visual/resources/border/shadow_normal.png")); // NOI18N
+
     public static void main (String[] args) {
         Scene scene = new Scene ();
         scene.getActions ().addAction (new ZoomAction ());
@@ -39,6 +45,15 @@ public class SwingBorderTest {
         addLabel (scene, new BevelBorder (BevelBorder.LOWERED));
         addLabel (scene, new TitledBorder ("Titled"));
         addLabel (scene, new TitledBorder (new EtchedBorder (EtchedBorder.LOWERED), "Titled with border"));
+
+        // using SwingBorder together with CompositeBorder
+        // scene.createView has to be called because JComponent has to be created before using SwingBorder inside CompositeBorder
+        // otherwise the SwingBorder insets would not be resolved correctly
+        scene.createView ();
+        Widget label = new LabelWidget (scene, "Composite");
+        label.setBorder (new CompositeBorder (BORDER_SHADOW_NORMAL, new SwingBorder (scene, new TitledBorder (new EtchedBorder (EtchedBorder.LOWERED), "Composite")), BORDER_SHADOW_NORMAL));
+        scene.addChild (label);
+
         SceneSupport.show (scene);
     }
 
