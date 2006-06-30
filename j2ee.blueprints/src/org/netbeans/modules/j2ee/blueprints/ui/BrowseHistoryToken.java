64d6606d9cec24b710d595a49c404e20d953dd72
jtulach@netbeans.org <jtulach@netbeans.org>
2006-06-30 20:36:03 +0000

    Updating to CDDL for module j2ee

--- j2ee.blueprints/src/org/netbeans/modules/j2ee/blueprints/ui/BrowseHistoryToken.java
+++ j2ee.blueprints/src/org/netbeans/modules/j2ee/blueprints/ui/BrowseHistoryToken.java
@@ -1,13 +1,19 @@
 /*
- *                 Sun Public License Notice
- * 
- * The contents of this file are subject to the Sun Public License
- * Version 1.0 (the "License"). You may not use this file except in
- * compliance with the License. A copy of the License is available at
- * http://www.sun.com/
- * 
- * The Original Code is NetBeans. The Initial Developer of the Original
- * Code is Sun Microsystems, Inc. Portions Copyright 1997-2004 Sun
+ * The contents of this file are subject to the terms of the Common Development
+ * and Distribution License (the License). You may not use this file except in
+ * compliance with the License.
+ *
+ * You can obtain a copy of the License at http://www.netbeans.org/cddl.html
+ * or http://www.netbeans.org/cddl.txt.
+ *
+ * When distributing Covered Code, include this CDDL Header Notice in each file
+ * and include the License file at http://www.netbeans.org/cddl.txt.
+ * If applicable, add the following below the CDDL Header, with the fields
+ * enclosed by brackets [] replaced by your own identifying information:
+ * "Portions Copyrighted [year] [name of copyright owner]"
+ *
+ * The Original Software is NetBeans. The Initial Developer of the Original
+ * Software is Sun Microsystems, Inc. Portions Copyright 1997-2006 Sun
  * Microsystems, Inc. All Rights Reserved.
  */
 
@@ -20,18 +26,18 @@ package org.netbeans.modules.j2ee.blueprints.ui;
  * @author Mark Roth
  */
 public class BrowseHistoryToken {
-    
+
     private String category;
     private String article;
     // Use constants defined in BluePrintsPanel
     private String tab;
     private int scrollPosition;
-    
+
     public BrowseHistoryToken() {
     }
-    
-    public BrowseHistoryToken(String category, String article, String tab, 
-        int scrollPosition) 
+
+    public BrowseHistoryToken(String category, String article, String tab,
+        int scrollPosition)
     {
         this.category = category;
         this.article = article;
