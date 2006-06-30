b6485e7702c5329e543a16447f3debc0100e30df
jtulach@netbeans.org <jtulach@netbeans.org>
2006-06-30 21:21:23 +0000

    Updating to CDDL for module openidex

--- enode/src/org/netbeans/spi/enode/LookupContentFactoryManager.java
+++ enode/src/org/netbeans/spi/enode/LookupContentFactoryManager.java
@@ -1,13 +1,19 @@
 /*
- *                 Sun Public License Notice
- * 
- * The contents of this file are subject to the Sun Public License
- * Version 1.0 (the "License"). You may not use this file except in
- * compliance with the License. A copy of the License is available at
- * http://www.sun.com/
- * 
- * The Original Code is NetBeans. The Initial Developer of the Original
- * Code is Nokia. Portions Copyright 2003 Nokia.
+ * The contents of this file are subject to the terms of the Common Development
+ * and Distribution License (the License). You may not use this file except in
+ * compliance with the License.
+ *
+ * You can obtain a copy of the License at http://www.netbeans.org/cddl.html
+ * or http://www.netbeans.org/cddl.txt.
+ *
+ * When distributing Covered Code, include this CDDL Header Notice in each file
+ * and include the License file at http://www.netbeans.org/cddl.txt.
+ * If applicable, add the following below the CDDL Header, with the fields
+ * enclosed by brackets [] replaced by your own identifying information:
+ * "Portions Copyrighted [year] [name of copyright owner]"
+ *
+ * The Original Software is NetBeans. The Initial Developer of the Original
+ * Software is Nokia. Portions Copyright 2003 Nokia.
  * All Rights Reserved.
  */
 
@@ -21,16 +27,16 @@ import org.openide.filesystems.FileObject;
  * @author David Strupl
  */
 public final class LookupContentFactoryManager {
-    
+
     /**
      * No instances of this class should be created.
      */
     private LookupContentFactoryManager() {
     }
-    
-    /** 
+
+    /**
      * This method is intended to be called from the layer
-     * by means of the methodvalue file attribute. <UL>The following 
+     * by means of the methodvalue file attribute. <UL>The following
      * attributes of the file object are examined:
      *     <LI><EM>"implements"</EM> - comma delimited list of interfaces/classes
      *          that the resulting object implements. These are the
