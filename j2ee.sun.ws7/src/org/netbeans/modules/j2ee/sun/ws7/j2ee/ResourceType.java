32d71ddb1139d45ca2f81291fafab06921dc9e56
jtulach@netbeans.org <jtulach@netbeans.org>
2006-06-30 21:46:35 +0000

    Updating to CDDL for module serverplugins

--- j2ee.sun.ws7/src/org/netbeans/modules/j2ee/sun/ws7/j2ee/ResourceType.java
+++ j2ee.sun.ws7/src/org/netbeans/modules/j2ee/sun/ws7/j2ee/ResourceType.java
@@ -1,13 +1,19 @@
 /*
- *                 Sun Public License Notice
+ * The contents of this file are subject to the terms of the Common Development
+ * and Distribution License (the License). You may not use this file except in
+ * compliance with the License.
  *
- * The contents of this file are subject to the Sun Public License
- * Version 1.0 (the "License"). You may not use this file except in
- * compliance with the License. A copy of the License is available at
- * http://www.sun.com/
+ * You can obtain a copy of the License at http://www.netbeans.org/cddl.html
+ * or http://www.netbeans.org/cddl.txt.
  *
- * The Original Code is NetBeans. The Initial Developer of the Original
- * Code is Sun Microsystems, Inc. Portions Copyright 1997-2005 Sun
+ * When distributing Covered Code, include this CDDL Header Notice in each file
+ * and include the License file at http://www.netbeans.org/cddl.txt.
+ * If applicable, add the following below the CDDL Header, with the fields
+ * enclosed by brackets [] replaced by your own identifying information:
+ * "Portions Copyrighted [year] [name of copyright owner]"
+ *
+ * The Original Software is NetBeans. The Initial Developer of the Original
+ * Software is Sun Microsystems, Inc. Portions Copyright 1997-2006 Sun
  * Microsystems, Inc. All Rights Reserved.
  */
 
@@ -22,12 +28,12 @@ package org.netbeans.modules.j2ee.sun.ws7.j2ee;
  * @author Administrator
  */
 public class ResourceType {
-    
+
     private static final int ENUM_JDBC=1;
     private static final int ENUM_EXT_JNDI=2;
     private static final int ENUM_CUSTOM=3;
     private static final int ENUM_MAIL=4;
-    
+
     public static final ResourceType JDBC =new ResourceType(ENUM_JDBC);
     public static final ResourceType JNDI =new ResourceType(ENUM_EXT_JNDI);
     public static final ResourceType CUSTOM =new ResourceType(ENUM_CUSTOM);
--- j2ee.sun.ws7/src/org/netbeans/modules/j2ee/sun/ws7/serverresources/dd/WS70CustomResource.java
+++ j2ee.sun.ws7/src/org/netbeans/modules/j2ee/sun/ws7/serverresources/dd/WS70CustomResource.java
@@ -1,13 +1,19 @@
 /*
- *                 Sun Public License Notice
+ * The contents of this file are subject to the terms of the Common Development
+ * and Distribution License (the License). You may not use this file except in
+ * compliance with the License.
  *
- * The contents of this file are subject to the Sun Public License
- * Version 1.0 (the "License"). You may not use this file except in
- * compliance with the License. A copy of the License is available at
- * http://www.sun.com/
+ * You can obtain a copy of the License at http://www.netbeans.org/cddl.html
+ * or http://www.netbeans.org/cddl.txt.
  *
- * The Original Code is NetBeans. The Initial Developer of the Original
- * Code is Sun Microsystems, Inc. Portions Copyright 1997-2005 Sun
+ * When distributing Covered Code, include this CDDL Header Notice in each file
+ * and include the License file at http://www.netbeans.org/cddl.txt.
+ * If applicable, add the following below the CDDL Header, with the fields
+ * enclosed by brackets [] replaced by your own identifying information:
+ * "Portions Copyrighted [year] [name of copyright owner]"
+ *
+ * The Original Software is NetBeans. The Initial Developer of the Original
+ * Software is Sun Microsystems, Inc. Portions Copyright 1997-2006 Sun
  * Microsystems, Inc. All Rights Reserved.
  */
 
@@ -19,10 +25,10 @@
 package org.netbeans.modules.j2ee.sun.ws7.serverresources.dd;
 
 /**
- * Code reused from Appserver common API module 
+ * Code reused from Appserver common API module
  */
 public interface WS70CustomResource {
-        
+
         public static final String JNDINAME = "JndiName";	// NOI18N
 	public static final String RESTYPE = "ResType";	// NOI18N
 	public static final String FACTORYCLASS = "FactoryClass";	// NOI18N
