183bddab421492a3ee2379b49c6460f5b85eff8a
jtulach@netbeans.org <jtulach@netbeans.org>
2006-07-01 08:53:00 +0000

    Final touches for CDDL upgrade in module core

--- o.n.swing.scrollbars/src/org/netbeans/swing/scrollbars/spi/MarkingModel.java
+++ o.n.swing.scrollbars/src/org/netbeans/swing/scrollbars/spi/MarkingModel.java
@@ -1,13 +1,19 @@
 /*
- *                 Sun Public License Notice
+ * The contents of this file are subject to the terms of the Common Development
+ * and Distribution License (the License). You may not use this file except in
+ * compliance with the License.
  *
- * The contents of this file are subject to the Sun Public License
- * Version 1.0 (the "License"). You may not use this file except in
- * compliance with the License. A copy of the License is available at
- * http://www.sun.com/
+ * You can obtain a copy of the License at http://www.netbeans.org/cddl.html
+ * or http://www.netbeans.org/cddl.txt.
  *
- * The Original Code is NetBeans. The Initial Developer of the Original
- * Code is Sun Microsystems, Inc. Portions Copyright 1997-2004 Sun
+ * When distributing Covered Code, include this CDDL Header Notice in each file
+ * and include the License file at http://www.netbeans.org/cddl.txt.
+ * If applicable, add the following below the CDDL Header, with the fields
+ * enclosed by brackets [] replaced by your own identifying information:
+ * "Portions Copyrighted [year] [name of copyright owner]"
+ *
+ * The Original Software is NetBeans. The Initial Developer of the Original
+ * Software is Sun Microsystems, Inc. Portions Copyright 1997-2006 Sun
  * Microsystems, Inc. All Rights Reserved.
  */
 
@@ -31,12 +37,12 @@ import java.util.Enumeration;
 public interface MarkingModel {
     /** Find out how many marks this model contains.
      * @return The count
-     */    
+     */
     public int size();
     /** Get a mark at a specific index
      * @param i The index
      * @return The Mark
-     */    
+     */
     public Mark getMark (int i);
     /** Get an enumeration of all marks.
      * @return The enumeration
