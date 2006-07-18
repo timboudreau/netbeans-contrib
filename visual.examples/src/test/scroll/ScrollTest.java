3fe369e64a23ed855dcd6241be695656ea243000
dkaspar@netbeans.org <dkaspar@netbeans.org>
2006-07-18 16:31:38 +0000

    ScrollWidget added with test

---
+++ visual.examples/src/test/scroll/ScrollTest.java
@@ -0,0 +1,73 @@
+/*
+ *                 Sun Public License Notice
+ *
+ * The contents of this file are subject to the Sun Public License
+ * Version 1.0 (the "License"). You may not use this file except in
+ * compliance with the License. A copy of the License is available at
+ * http://www.sun.com/
+ *
+ * The Original Code is NetBeans. The Initial Developer of the Original
+ * Code is Sun Microsystems, Inc. Portions Copyright 1997-2006 Sun
+ * Microsystems, Inc. All Rights Reserved.
+ */
+package test.scroll;
+
+import org.netbeans.api.visual.action.ResizeAction;
+import org.netbeans.api.visual.action.ZoomAction;
+import org.netbeans.api.visual.border.ResizeBorder;
+import org.netbeans.api.visual.layout.SerialLayout;
+import org.netbeans.api.visual.widget.*;
+import org.openide.util.Utilities;
+import test.SceneSupport;
+
+import java.awt.*;
+
+/**
+ * @author David Kaspar
+ */
+public class ScrollTest extends Scene {
+
+    public ScrollTest () {
+        getActions ().addAction (new ZoomAction ());
+        LayerWidget layer = new LayerWidget (this);
+        addChild (layer);
+
+        ScrollWidget scroll = new ScrollWidget (this);
+        scroll.setBorder (new ResizeBorder (8, Color.BLUE, false));
+        scroll.setPreferredLocation (new Point (50, 50));
+        scroll.setMinimalSize (new Dimension (100, 200));
+        scroll.setMaximalSize (new Dimension (500, 500));
+        layer.addChild (scroll);
+
+        Widget view = new Widget (this);
+        view.setLayout (new SerialLayout (SerialLayout.Orientation.VERTICAL));
+
+        view.addChild (new LabelWidget (this, "Shrink the area for showing scroll bars"));
+        view.addChild (new LabelWidget (this, "Drag scroll bars to move the view"));
+        view.addChild (new LabelWidget (this, "Click on arrow and slider to perform unit and block scroll of the view"));
+        view.addChild (new SeparatorWidget (this, SeparatorWidget.Orientation.HORIZONTAL));
+        view.addChild (new ImageWidget (this, Utilities.loadImage ("test/resources/displayable_64.png")));
+        view.addChild (new LabelWidget (this, "Long Long Long Long Long Long Label 1"));
+        view.addChild (new LabelWidget (this, "Label 1"));
+        view.addChild (new LabelWidget (this, "Label 2"));
+        view.addChild (new LabelWidget (this, "Label 3"));
+        view.addChild (new LabelWidget (this, "Label 4"));
+        view.addChild (new LabelWidget (this, "Label 5"));
+        view.addChild (new LabelWidget (this, "Long Long Long Long Long Long Label 5"));
+        view.addChild (new LabelWidget (this, "Label 6"));
+        view.addChild (new LabelWidget (this, "Label 7"));
+        view.addChild (new LabelWidget (this, "Label 8"));
+        view.addChild (new LabelWidget (this, "Label 9"));
+        view.addChild (new LabelWidget (this, "Label 0"));
+        view.addChild (new LabelWidget (this, "Long Long Long Long Long Long Label 0"));
+
+        scroll.setView (view);
+
+        scroll.getActions ().addAction (new ResizeAction ());
+    }
+
+    public static void main (String[] args) {
+        SceneSupport.show (new ScrollTest ());
+    }
+
+}
