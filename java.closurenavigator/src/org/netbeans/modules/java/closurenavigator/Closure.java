47d5051d81637cf126d7c9708855dfce89fde655
tboudreau@netbeans.org <tboudreau@netbeans.org>
2006-08-02 07:12:19 +0000

    A quick and dirty module for listing the closure of a given source file, based on some code from the unit tests for the classfile module.  Right now just prints a list to the output window. What it *really* ought to do is offer to generate a new project, extract the chosen class and its closure, move everything into the new project and fix up the dependencies.  This would be very useful for doing things like migrating an app to the NB platform or developing an integration of an existing app, where you only need some of the logic but the original app was never factored into many pieces.

---
+++ java.closurenavigator/src/org/netbeans/modules/java/closurenavigator/Closure.java
@@ -0,0 +1,187 @@
+/*
+ * Closure.java
+ *
+ * The contents of this file are subject to the terms of the Common Development
+ * and Distribution License (the License). You may not use this file except in
+ * compliance with the License.
+ *
+ * You can obtain a copy of the License at http://www.netbeans.org/cddl.html
+ * or http://www.netbeans.org/cddl.txt.
+ *
+ * When distributing Covered Code, include this CDDL Header Notice in each file
+ * and include the License file at http://www.netbeans.org/cddl.txt.
+ * If applicable, add the following below the CDDL Header, with the fields
+ * enclosed by brackets [] replaced by your own identifying information:
+ * "Portions Copyrighted [year] [name of copyright owner]"
+ *
+ * Contributor(s): Thomas Ball
+ */
+package org.netbeans.modules.java.closurenavigator;
+import org.netbeans.modules.classfile.*;
+import java.io.*;
+import java.util.*;
+import org.netbeans.api.java.classpath.ClassPath;
+import org.openide.ErrorManager;
+import org.openide.awt.StatusDisplayer;
+import org.openide.filesystems.FileObject;
+import org.openide.loaders.DataObject;
+import org.openide.windows.IOProvider;
+import org.openide.windows.InputOutput;
+
+/**
+ * Closure:  report all classes which this file references in one
+ * way or another.  Note: this utility won't find classes which are
+ * dynamically loaded.
+ *
+ * @author Thomas Ball
+ */
+public class Closure {
+    String thisClass;
+    Set closure;
+    private final ClassPath classPath;
+
+    Closure(ClassPath classPath, String thisClass) throws IOException {
+        this.classPath = classPath;
+        this.thisClass = thisClass;
+        buildClosure (false);
+    }
+    
+    //TODO:  Exclude class files in library jars from search
+    //TODO:  Figure out how to put the whole shebang on the clipboard when
+    //we're done, or better, offer to create a new project, extract the
+    //class and its closure, update dependencies everywhere, including 
+    //libraries...
+
+    public void buildClosure(boolean includeJDK)
+      throws IOException {
+        if (closure != null)
+            return;
+        closure = new HashSet();
+        Set visited = new HashSet();
+        Stack stk = new Stack();
+        ClassName thisCN = ClassName.getClassName(thisClass.replace('.', '/'));
+        stk.push(thisCN);
+        visited.add(thisCN.getExternalName());
+
+        while (!stk.empty()) {
+            // Add class to closure.
+            ClassName cn = (ClassName)stk.pop();
+            InputStream is = findClassStream(cn.getType());
+	    if (is == null) {
+		System.err.println("couldn't find class: " + 
+                                   cn.getExternalName());
+		continue;
+	    }
+            ClassFile cfile = new ClassFile(is);
+            if (cfile.getSourceFileName() != null) {
+                String srcName = cfile.getName().getPackage().replace('.', '/') + 
+                        '/' + cfile.getSourceFileName();
+                System.err.println("SFN " + srcName);
+                sourceFileNames.add (srcName);
+            }
+            closure.add(cfile.getName().getExternalName());
+            
+            ConstantPool pool = cfile.getConstantPool();
+            Iterator refs = pool.getAllClassNames().iterator();
+            while (refs.hasNext()) {
+                ClassName cnRef = (ClassName)refs.next();
+                String cname = cnRef.getExternalName();
+                if (cname.indexOf('[') != -1) {
+                    // skip arrays
+                } else if (!includeJDK && 
+                           (cname.startsWith("java.") || 
+                            cname.startsWith("javax.") ||
+                            cname.startsWith("sun.") ||
+                            cname.startsWith("com.sun.corba") ||
+                            cname.startsWith("com.sun.image") ||
+                            cname.startsWith("com.sun.java.swing") ||
+                            cname.startsWith("com.sun.naming") ||
+                            cname.startsWith("com.sun.security"))) {
+                    // if directed, skip JDK references
+                } else {
+                    boolean isNew = visited.add(cname);
+                    if (isNew)
+                        stk.push(cnRef);
+                }
+            }
+        }
+    }
+
+    InputOutput dumpClosure() {
+        InputOutput io = IOProvider.getDefault().getIO("Closure of " + 
+                thisClass, false);
+        PrintWriter out = io.getOut();
+        io.select();
+        Iterator iter = new TreeSet(closure).iterator();
+        while (iter.hasNext()) {
+            out.println((String)iter.next());
+        }
+        out.close();
+        return io;
+    }
+
+    Iterator dependencies() {
+        return closure.iterator();
+    }
+
+    private Set <String> sourceFileNames = new HashSet();
+    private Set <String> notFound = null;
+//    private Set <FileObject> binaries = new HashSet();
+    private InputStream findClassStream(String className) throws IOException {
+        String nm = className.replace('.','/') + ".class";
+        FileObject ob = classPath.findResource(nm);
+        if (ob == null) {
+            StatusDisplayer.getDefault().setStatusText("Could not find resource " +
+                    "for " + className);
+//            IOException e = new IOException ("Could not find resource " + 
+//                    className);
+//            ErrorManager.getDefault().annotate (e, ErrorManager.USER,
+//                    "Failed at " + className, "Could not find resource " +
+//                    className, null, null);
+            if (notFound == null) {
+                notFound = new HashSet();
+            }
+            notFound.add (nm);
+            return null;
+//            throw e;
+        } else {
+//            binaries.add (ob);
+            return new BufferedInputStream (ob.getInputStream());
+        }
+    }
+        
+    Set <DataObject> sources = null;
+    Set getSources(ClassPath srcPath) throws IOException {
+        if (sourceFileNames.isEmpty() || (sources != null && sources.isEmpty())) {
+            throw new IOException ("Source file names not present.  Recompile "+
+                    "with debug info");
+        }
+        if (sources == null) {
+            sources = new HashSet <DataObject> ();
+            for (String nm : sourceFileNames) {
+                FileObject ob = srcPath.findResource(nm);
+                if (ob == null) {
+                    if (notFound == null) {
+                        notFound = new HashSet();
+                    }
+                    notFound.add (nm);
+//                    sources.clear();
+//                    throw new IOException ("Failed to find file for " + nm + 
+//                            ". I give up.");
+                } else {
+                    sources.add (DataObject.find(ob));
+                }
+            }
+        }
+        return sources;
+    }
+    
+    String[] getMissingSources () {
+        if (notFound == null) {
+            return new String[0];
+        } else {
+            String[] result=(String[]) notFound.toArray(new String[notFound.size()]);
+            return result;
+        }
+    }
+}
---
+++ java.closurenavigator/src/org/netbeans/modules/java/closurenavigator/ViewClosure.java
@@ -0,0 +1,133 @@
+/*
+ * The contents of this file are subject to the terms of the Common Development
+ * and Distribution License (the License). You may not use this file except in
+ * compliance with the License.
+ * You can obtain a copy of the License at http://www.netbeans.org/cddl.html
+ * or http://www.netbeans.org/cddl.txt.
+ * When distributing Covered Code, include this CDDL Header Notice in each file
+ * and include the License file at http://www.netbeans.org/cddl.txt.
+ * If applicable, add the following below the CDDL Header, with the fields
+ * enclosed by brackets [] replaced by your own identifying information:
+ * "Portions Copyrighted [year] [name of copyright owner]"
+ */
+package org.netbeans.modules.java.closurenavigator;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.List;
+import org.netbeans.api.java.classpath.ClassPath;
+import org.netbeans.api.project.FileOwnerQuery;
+import org.netbeans.api.project.Project;
+import org.netbeans.modules.java.JavaDataObject;
+import org.netbeans.spi.java.classpath.ClassPathProvider;
+import org.openide.ErrorManager;
+import org.openide.awt.StatusDisplayer;
+import org.openide.cookies.SourceCookie;
+import org.openide.loaders.DataObject;
+import org.openide.nodes.Node;
+import org.openide.util.HelpCtx;
+import org.openide.util.NbBundle;
+import org.openide.util.actions.CookieAction;
+import org.openide.windows.InputOutput;
+
+public final class ViewClosure extends CookieAction {
+    
+    protected void performAction(Node[] activatedNodes) {
+        JavaDataObject javaDataObject = (JavaDataObject) 
+                activatedNodes[0].getLookup().lookup(JavaDataObject.class);
+        Project p = FileOwnerQuery.getOwner(
+                javaDataObject.getPrimaryFile());
+        if (p != null) {
+            ClassPathProvider prov=(ClassPathProvider) p
+                    .getLookup().lookup (ClassPathProvider.class);
+            if (prov != null) {
+                ClassPath cp = prov.findClassPath(
+                        javaDataObject.getPrimaryFile(),
+                        ClassPath.EXECUTE);
+                //expedient - really shouldn't be using sourceCookie, but the
+                //alternatives are equally bad these days...
+                SourceCookie src = (SourceCookie) javaDataObject.getCookie(
+                        SourceCookie.class);
+                if (src != null) {
+                    String nm = 
+                        src.getSource().getClasses()[0].getName().getFullName();
+                    try {
+                        Closure closure = new Closure (cp, nm);
+                        InputOutput io = closure.dumpClosure();
+                        ClassPath srcPath = prov.findClassPath(
+                                javaDataObject.getPrimaryFile(),
+                                ClassPath.SOURCE);
+                        Collection <DataObject> obs = closure.getSources(
+                                srcPath);
+//                        for (DataObject dob : obs) {
+//                            Transferable t = dob.getNodeDelegate().clipboardCopy();
+//                            //xxx and do what?  How do I programmaticaly
+                              //multi-select-and-copy?
+//                        }
+                        String[] missing = closure.getMissingSources();
+                        if (missing.length > 0) {
+                            io.getOut().println("-----------------------" +
+                                    "------------------------");
+                            io.getOut().println("MISSING SOURCES FOR");
+                            for (int i = 0; i < missing.length; i++) {
+                                io.getOut().println ("  " + missing[i]);
+                            }
+                        }
+                        
+                        io.getOut().println("*****************************" +
+                                "********************\n" + obs.size() + 
+                                " SOURCES FOUND:\n");
+                        List <String> names = new ArrayList <String> (obs.size());
+                        for (DataObject dob : obs) {
+                            names.add (dob.getPrimaryFile().getPath());
+                        }
+                        Collections.sort (names);
+                        for (String s : names) {
+                            io.getOut().println(s);
+                        }
+                        io.getOut().close();
+                        return;
+                    } catch (IOException ioe) {
+                        ErrorManager.getDefault().notify (ioe);
+                        return;
+                    }
+                }
+            }
+        }
+        StatusDisplayer.getDefault().setStatusText("Could not find " +
+                ".class file for " + 
+                javaDataObject.getNodeDelegate().getDisplayName() +
+                " to compute closure.  Maybe it is not compiled?");
+    }
+    
+    protected int mode() {
+        return CookieAction.MODE_EXACTLY_ONE;
+    }
+    
+    public String getName() {
+        return NbBundle.getMessage(ViewClosure.class, "CTL_ViewClosure");
+    }
+    
+    protected Class[] cookieClasses() {
+        return new Class[] {
+            JavaDataObject.class
+        };
+    }
+    
+    protected void initialize() {
+        super.initialize();
+        // see org.openide.util.actions.SystemAction.iconResource() javadoc for more details
+        putValue("noIconInMenu", Boolean.TRUE);
+    }
+    
+    public HelpCtx getHelpCtx() {
+        return HelpCtx.DEFAULT_HELP;
+    }
+    
+    protected boolean asynchronous() {
+        return true;
+    }
+}
+
