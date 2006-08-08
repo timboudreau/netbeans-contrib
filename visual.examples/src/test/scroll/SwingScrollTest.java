af5e8b5ce2c2d5b1f25692b38b0d54c16bac8314
dkaspar@netbeans.org <dkaspar@netbeans.org>
2006-08-08 12:24:00 +0000

    SwingScrollWidget with SwingScrollTest added

---
+++ visual.examples/src/test/scroll/SwingScrollTest.java
@@ -0,0 +1,64 @@
+/*
+ * Copyright (c) 2006 Your Corporation. All Rights Reserved.
+ */
+package test.scroll;
+
+import org.netbeans.api.visual.action.ResizeAction;
+import org.netbeans.api.visual.action.ZoomAction;
+import org.netbeans.api.visual.border.BorderFactory;
+import org.netbeans.api.visual.layout.LayoutFactory;
+import org.netbeans.api.visual.widget.*;
+import org.openide.util.Utilities;
+import test.SceneSupport;
+
+import java.awt.*;
+
+/**
+ * @author David Kaspar
+ */
+public class SwingScrollTest extends Scene {
+
+    public SwingScrollTest () {
+        getActions ().addAction (new ZoomAction ());
+        LayerWidget layer = new LayerWidget (this);
+        addChild (layer);
+
+        SwingScrollWidget scroll = new SwingScrollWidget (this);
+        scroll.setBorder (BorderFactory.createResizeBorder (8, Color.BLUE, false));
+        scroll.setPreferredLocation (new Point (50, 50));
+        scroll.setMinimalSize (new Dimension (100, 200));
+        scroll.setMaximalSize (new Dimension (500, 500));
+        layer.addChild (scroll);
+
+        Widget view = new Widget (this);
+        view.setLayout (LayoutFactory.createVerticalLayout ());
+
+        view.addChild (new LabelWidget (this, "Shrink the area for showing scroll bars"));
+        view.addChild (new LabelWidget (this, "Drag scroll bars to move the view"));
+        view.addChild (new LabelWidget (this, "Click on arrow and slider to perform unit and block scroll of the view"));
+        view.addChild (new SeparatorWidget (this, SeparatorWidget.Orientation.HORIZONTAL));
+        view.addChild (new ImageWidget (this, Utilities.loadImage ("test/resources/displayable_64.png")));
+        view.addChild (new LabelWidget (this, "Long Long Long Long Long Long Label 1"));
+        view.addChild (new LabelWidget (this, "Label 1"));
+        view.addChild (new LabelWidget (this, "Label 2"));
+        view.addChild (new LabelWidget (this, "Label 3"));
+        view.addChild (new LabelWidget (this, "Label 4"));
+        view.addChild (new LabelWidget (this, "Label 5"));
+        view.addChild (new LabelWidget (this, "Long Long Long Long Long Long Label 5"));
+        view.addChild (new LabelWidget (this, "Label 6"));
+        view.addChild (new LabelWidget (this, "Label 7"));
+        view.addChild (new LabelWidget (this, "Label 8"));
+        view.addChild (new LabelWidget (this, "Label 9"));
+        view.addChild (new LabelWidget (this, "Label 0"));
+        view.addChild (new LabelWidget (this, "Long Long Long Long Long Long Label 0"));
+
+        scroll.setView (view);
+
+        scroll.getActions ().addAction (new ResizeAction ());
+    }
+
+    public static void main (String[] args) {
+        SceneSupport.show (new SwingScrollTest ());
+    }
+
+}
