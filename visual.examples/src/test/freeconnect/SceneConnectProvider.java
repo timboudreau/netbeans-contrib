5b9cce04cbd87eaed17f4f1479775d37e1e99451
alex_grk <none@none>
2006-09-07 17:25:47 +0000

    some minor changes

--- visual.examples/src/test/freeconnect/SceneConnectProvider.java
+++ visual.examples/src/test/freeconnect/SceneConnectProvider.java
@@ -22,44 +22,46 @@ import java.awt.*;
  * @author alex
  */
 public class SceneConnectProvider implements ConnectProvider {
-
-        private String source = null;
-        private String target = null;
-
-        private GraphScene.StringGraph scene;
-
-        public SceneConnectProvider(GraphScene.StringGraph scene){
-            this.scene=scene;
-        }
-        
-        public boolean isSourceWidget (Widget sourceWidget) {
-            Object object = scene.findObject (sourceWidget);
-            source = scene.isNode (object) ? (String) object : null;
-            return source != null;
-        }
-
-        public ConnectorState isTargetWidget (Widget sourceWidget, Widget targetWidget) {
-            Object object = scene.findObject (targetWidget);
-            target = scene.isNode (object) ? (String) object : null;
-            if (target != null)
-                return ! source.equals (target) ? ConnectorState.ACCEPT : ConnectorState.REJECT_AND_STOP;
-            return object != null ? ConnectorState.REJECT_AND_STOP : ConnectorState.REJECT;
-        }
-
-        public boolean hasCustomTargetWidgetResolver (Scene scene) {
-            return false;
-        }
-
-        public Widget resolveTargetWidget (Scene scene, Point sceneLocation) {
-            return null;
-        }
-
-        int edgeCounter;
-        public void createConnection (Widget sourceWidget, Widget targetWidget) {
-            String edge = "edge" + edgeCounter ++;
-            scene.addEdge (edge);
-            scene.setEdgeSource (edge, source);
-            scene.setEdgeTarget (edge, target);
-        }
-
+    
+    private String source = null;
+    private String target = null;
+    
+    private GraphScene.StringGraph scene;
+    
+    public SceneConnectProvider(GraphScene.StringGraph scene){
+        this.scene=scene;
     }
+    
+    public boolean isSourceWidget(Widget sourceWidget) {
+        Object object = scene.findObject(sourceWidget);
+        source = scene.isNode(object) ? (String) object : null;
+        return source != null;
+    }
+    
+    public ConnectorState isTargetWidget(Widget sourceWidget, Widget targetWidget) {
+        Object object = scene.findObject(targetWidget);
+        target = scene.isNode(object) ? (String) object : null;
+        if (target != null)
+            return ! source.equals(target) ? ConnectorState.ACCEPT : ConnectorState.REJECT_AND_STOP;
+        return object != null ? ConnectorState.REJECT_AND_STOP : ConnectorState.REJECT;
+    }
+    
+    public boolean hasCustomTargetWidgetResolver(Scene scene) {
+        return false;
+    }
+    
+    public Widget resolveTargetWidget(Scene scene, Point sceneLocation) {
+        return null;
+    }
+    
+    int edgeCounter;
+    public void createConnection(Widget sourceWidget, Widget targetWidget) {
+        String edge = "edge" + edgeCounter ++;
+        if(isSourceWidget(sourceWidget) && isTargetWidget(sourceWidget,targetWidget).equals(ConnectorState.ACCEPT)){
+            scene.addEdge(edge);
+            scene.setEdgeSource(edge, source);
+            scene.setEdgeTarget(edge, target);
+        }
+    }
+    
+}
