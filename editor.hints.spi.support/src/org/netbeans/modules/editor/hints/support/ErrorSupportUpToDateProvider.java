e9791212eaa6adc9ace5f3d84e91c41f07ef8c7f
jlahoda@netbeans.org <jlahoda@netbeans.org>
2006-10-02 11:32:20 +0000

    Moving the generic up-to-date-status provider into spisupport (finishing previous commit).

---
+++ editor.hints.spi.support/src/org/netbeans/modules/editor/hints/support/ErrorSupportUpToDateProvider.java
@@ -0,0 +1,46 @@
+/*
+ * The contents of this file are subject to the terms of the Common Development
+ * and Distribution License (the License). You may not use this file except in
+ * compliance with the License.
+ *
+ * You can obtain a copy of the License at http://www.netbeans.org/cddl.html
+ * or http://www.netbeans.org/cddl.txt.
+ *
+ * When distributing Covered Code, include this CDDL Header Notice in each file
+ * and include the License file at http://www.netbeans.org/cddl.txt.
+ * If applicable, add the following below the CDDL Header, with the fields
+ * enclosed by brackets [] replaced by your own identifying information:
+ * "Portions Copyrighted [year] [name of copyright owner]"
+ *
+ * The Original Software is NetBeans. The Initial Developer of the Original
+ * Software is Sun Microsystems, Inc. Portions Copyright 1997-2006 Sun
+ * Microsystems, Inc. All Rights Reserved.
+ */
+package org.netbeans.modules.editor.hints.support;
+
+import org.netbeans.spi.editor.errorstripe.UpToDateStatus;
+import org.netbeans.spi.editor.errorstripe.UpToDateStatusProvider;
+
+/**
+ *
+ * @author Jan Lahoda
+ */
+public class ErrorSupportUpToDateProvider extends UpToDateStatusProvider {
+
+    private UpToDateStatus status;
+
+    /** Creates a new instance of ErrorSupportUpToDateProvider */
+    public ErrorSupportUpToDateProvider() {
+        status = UpToDateStatus.UP_TO_DATE_OK;
+    }
+
+    public UpToDateStatus getUpToDate() {
+        return status;
+}
+    
+    public void setUpToDate(UpToDateStatus status) {
+        this.status = status;
+        firePropertyChange(PROP_UP_TO_DATE, null, status);
+    }
+    
+}
