ef7b492e411b1dde40ab563c0c1b0e09dedf3e38
sandipchitale@netbeans.org <sandipchitale@netbeans.org>
2006-10-20 14:48:36 +0000

    Better diagnostics.

--- java.tools.nbjad/src/org/netbeans/modules/java/tools/nbjad/actions/DecompileAction.java
+++ java.tools.nbjad/src/org/netbeans/modules/java/tools/nbjad/actions/DecompileAction.java
@@ -51,23 +51,26 @@ import org.openide.windows.InputOutput;
  * @author Sandip V. Chitale (Sandip.Chitale@Sun.Com)
  */
 public final class DecompileAction extends CookieAction {
-
+    
     private static Date latestFileDate = new Date();
-
+    
     private static String nbjadOutputDirectory=
             System.getProperty("org.netbeans.modules.java.tools.nbjad.output.directory",
             System.getProperty("user.home") + File.separator + ".nbjad");
-
+    
     private NbjadSettings settings;
-
+    
     public DecompileAction() {
         settings =(NbjadSettings)SharedClassObject.findObject(NbjadSettings.class, true);
     }
-
+    
     protected void performAction(Node[] activatedNodes) {
+        // Get DataObject
         DataObject dataObject = (DataObject) activatedNodes[0].getCookie(DataObject.class);
         if (dataObject != null) {
+            // Get primary FileoBject
             FileObject fileObject = dataObject.getPrimaryFile();
+            // Is it a .class file
             if (fileObject != null && fileObject.getExt().equals("class")) {
                 // Ensure output directory
                 File nbjadOutputDirectoryFile = new File(nbjadOutputDirectory);
@@ -80,20 +83,22 @@ public final class DecompileAction extends CookieAction {
                     ErrorManager.getDefault().log("Could not create NBJAD output directory: " + nbjadOutputDirectory);
                     return;
                 }
-
+                
                 // Output directory FileObject
                 FileObject nbjadOutputDirectoryFileObject = FileUtil.toFileObject(nbjadOutputDirectoryFile);
-
+                
                 try {
                     // Check for Jar file system
                     FileSystem fileSystem = fileObject.getFileSystem();
                     if (fileSystem instanceof JarFileSystem) {
                         try {
+                            // Copy contents of .class file in jar file to output directory
                             fileObject =
                                     FileUtil.copyFile(fileObject,
                                     nbjadOutputDirectoryFileObject,
                                     fileObject.getName(),
                                     fileObject.getExt());
+                            // Make sure file is queued for clean up on exiting IDE
                             FileUtil.toFile(fileObject).deleteOnExit();
                         } catch (IOException ex) {
                             ErrorManager.getDefault().log("Could not create physical class file on disk.");
@@ -106,31 +111,41 @@ public final class DecompileAction extends CookieAction {
                 // Now get the java.io.File for the class file
                 File file = FileUtil.toFile(fileObject);
                 if (file != null) {
+                    // Log
+                    InputOutput io = IOProvider.getDefault().getIO("Decompile: " + fileObject.getNameExt(), true);
+                    PrintWriter pw = new PrintWriter(io.getOut());
+                    
+                    // Build the jad command
+                    List jadCommand = new ArrayList();
+                    jadCommand.add(settings.getJadLocation()); // JAD executable
+                    jadCommand.add("-o");                      // overwrite
+                    jadCommand.add("-r");                      // create package directory structure
+                    jadCommand.add("-s");                      // suffix .java
+                    jadCommand.add(".java");
+                    jadCommand.add("-d");                      //  output directoryo
+                    jadCommand.add(nbjadOutputDirectory);
+                    
+                    // Additional options
+                    String jadOptions = settings.getJadOptions().trim();
+                    if (jadOptions != null && jadOptions.length() > 0) {
+                        jadCommand.addAll(Arrays.asList(jadOptions.split(" ")));
+                    }
+                    jadCommand.add(file.getAbsolutePath());
+                    
+                    pw.println(jadCommand);
+                    
                     try {
-                        List jadCommand = new ArrayList();
-                        jadCommand.add(settings.getJadLocation()); // JAD executable
-                        jadCommand.add("-o");                      // overwrite
-                        jadCommand.add("-r");                      // create package directory structure
-                        jadCommand.add("-s");                      // suffix .java
-                        jadCommand.add(".java");
-                        jadCommand.add("-d");                      //  output directoryo
-                        jadCommand.add(nbjadOutputDirectory);
-                        // Additional options
-                        String jadOptions = settings.getJadOptions().trim();
-                        if (jadOptions != null && jadOptions.length() > 0) {
-                            jadCommand.addAll(Arrays.asList(jadOptions.split(" ")));
-                        }
-                        jadCommand.add(file.getAbsolutePath());
-
+                        // Run jad decompiler
                         FilterProcess filterProcess =
                                 new FilterProcess((String[])jadCommand.toArray(new String[0]));
                         PrintWriter in = filterProcess.exec();
                         in.close();
-
-                        if (filterProcess.waitFor() == 0) {
-                            InputOutput io = IOProvider.getDefault().getIO("Decompile: " + fileObject.getNameExt(), true);
-                            PrintWriter pw = new PrintWriter(io.getOut());
-                            pw.println(jadCommand);
+                        
+                        
+                        // Did it exit normally
+                        int status = filterProcess.waitFor();
+                        if (status == 0) {
+                            
                             String[] linesText = filterProcess.getStdOutOutput();
                             if (linesText != null) {
                                 for (int i = 0; i < linesText.length; i++) {
@@ -144,12 +159,24 @@ public final class DecompileAction extends CookieAction {
                                     pw.println(linesText[i]);
                                 }
                             }
+                        } else {
+                            // Failed
+                            pw = new PrintWriter(io.getErr());
+                            pw.println("Decompilation exited with status: " + status);
                         }
+                        
+                        // CLeanup process
                         filterProcess.destroy();
-
+                        
                         // Open newser files
                         openNewFilesIn(nbjadOutputDirectoryFileObject, nbjadOutputDirectoryFile);
+                        
                     } catch (IOException fe) {
+                        // Failed
+                        pw = new PrintWriter(io.getErr());
+                        pw.println("Decompilation failed.");
+                        pw.println(fe.getMessage());
+                        pw.println("Make sure you have configured the location of Jad correctly using Tools:Options:Decompile panel.");
                         DialogDisplayer.getDefault().notify(
                                 new NotifyDescriptor.Message(fe.getMessage() +
                                 "\nMake sure you have configured the location of Jad correctly using Tools:Options:Decompile panel."));
@@ -158,23 +185,30 @@ public final class DecompileAction extends CookieAction {
             }
         }
     }
-
+    
     private static void openNewFilesIn(FileObject nbjadOutputDirectoryFileObject, File nbjadOutputDirectoryFile) {
         openNewFilesIn(nbjadOutputDirectoryFileObject, nbjadOutputDirectoryFile, latestFileDate);
         latestFileDate = new Date(System.currentTimeMillis());
     }
-
+    
+    // Recursively open files later than last sampled time
     private static void openNewFilesIn(FileObject root, File directory, Date laterThan) {
         File[] files = directory.listFiles();
         for (int i = 0; i < files.length; i++) {
             File file = files[i];
             if (file.isDirectory()) {
+                // Recurse into child folders
                 openNewFilesIn(root, file, laterThan);
                 continue;
             }
+            
+            // Get FileObject
             FileObject fileObject = FileUtil.toFileObject(file);
+            
+            // Is it newer
             if (fileObject.lastModified().after(latestFileDate)) {
                 if (fileObject.getPath().endsWith(".java")) {
+                    // Not required really as we are recursing from the root
                     if (FileUtil.isParentOf(root, fileObject)) {
                         DataObject dataObject;
                         try {
@@ -191,15 +225,15 @@ public final class DecompileAction extends CookieAction {
             }
         }
     }
-
+    
     protected int mode() {
         return CookieAction.MODE_EXACTLY_ONE;
     }
-
+    
     public String getName() {
         return NbBundle.getMessage(DecompileAction.class, "CTL_DecompileAction");
     }
-
+    
     protected boolean enable(Node[] activatedNodes) {
         if (activatedNodes != null && activatedNodes.length > 0) {
             DataObject dataObject = (DataObject) activatedNodes[0].getCookie(DataObject.class);
@@ -212,21 +246,21 @@ public final class DecompileAction extends CookieAction {
         }
         return false;
     }
-
+    
     protected Class[] cookieClasses() {
         return new Class[] {
             DataObject.class
         };
     }
-
+    
     protected String iconResource() {
         return "org/netbeans/modules/java/tools/nbjad/actions/nbjad.gif";
     }
-
+    
     public HelpCtx getHelpCtx() {
         return HelpCtx.DEFAULT_HELP;
     }
-
+    
     protected boolean asynchronous() {
         return false;
     }
