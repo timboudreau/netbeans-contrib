485a233aa4aee33cd313d08e6f2458b27511a40d
dkaspar@netbeans.org <dkaspar@netbeans.org>
2006-10-25 14:19:27 +0000

    VMDAnchorTest added

--- visual.examples/src/org/netbeans/modules/visual/examples/RunDialog.java
+++ visual.examples/src/org/netbeans/modules/visual/examples/RunDialog.java
@@ -57,6 +57,7 @@ public class RunDialog extends javax.swing.JFrame {
         "test.scroll.SwingScrollTest",
         "test.swingborder.SwingBorderTest",
         "test.tool.ToolTest",
+        "test.vmd.VMDAnchorTest",
         "test.vmd.VMDCollisionTest",
         "test.vmd.VMDTest",
     };
