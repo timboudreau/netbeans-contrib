b18787a9e3862fbd4bb302c02c0d3a0036b08d74
ramos@netbeans.org <ramos@netbeans.org>
2006-11-04 09:11:48 +0000

    clean up, formatting

--- ramos.localhistory/src/ramos/localhistory/LocalHistoryAction.java
+++ ramos.localhistory/src/ramos/localhistory/LocalHistoryAction.java
@@ -19,7 +19,7 @@
  */
 package ramos.localhistory;
 
-import javax.swing.JOptionPane;
+import java.io.File;
 import org.openide.filesystems.FileObject;
 import org.openide.filesystems.FileUtil;
 
@@ -27,23 +27,21 @@ import org.openide.loaders.DataObject;
 
 import org.openide.nodes.Node;
 
-import org.openide.util.ContextAwareAction;
 import org.openide.util.HelpCtx;
 import org.openide.util.Lookup;
 import org.openide.util.NbBundle;
 import org.openide.util.actions.CookieAction;
 
-import org.openide.windows.WindowManager;
-
 import ramos.localhistory.ui.LocalHistoryTopComponent;
+import ramos.localhistory.ui.VersionNode;
 
-import java.awt.Cursor;
 import java.awt.event.ActionEvent;
 
-import javax.swing.AbstractAction;
-import javax.swing.Action;   
 import java.util.Collection;
-import ramos.localhistory.ui.VersionNode;
+
+import javax.swing.AbstractAction;
+import javax.swing.Action;
+import javax.swing.JOptionPane;
 
 
 /**
@@ -52,191 +50,188 @@ import ramos.localhistory.ui.VersionNode;
  * @author $author$
  * @version $Revision$
  */
-public final class LocalHistoryAction extends CookieAction {
-   /**
-    * DOCUMENT ME!
-    *
-    * @param actionContext DOCUMENT ME!
-    *
-    * @return DOCUMENT ME!
-    */
-   public Action createContextAwareInstance(final Lookup actionContext) {
-      return new ContextAction(actionContext);
-   }
+public final class LocalHistoryAction
+  extends CookieAction {
+  /**
+   * DOCUMENT ME!
+   *
+   * @param actionContext DOCUMENT ME!
+   *
+   * @return DOCUMENT ME!
+   */
+  public Action createContextAwareInstance(final Lookup actionContext) {
+    return new ContextAction(actionContext);
+  }
 
-   /**
-    * DOCUMENT ME!
-    *
-    * @return DOCUMENT ME!
-    */
-   public HelpCtx getHelpCtx() {
-      return HelpCtx.DEFAULT_HELP;
-   }
+  /**
+   * DOCUMENT ME!
+   *
+   * @return DOCUMENT ME!
+   */
+  public HelpCtx getHelpCtx() {
+    return HelpCtx.DEFAULT_HELP;
+  }
 
-   /**
-    * DOCUMENT ME!
-    *
-    * @return DOCUMENT ME!
-    */
-   public String getName() {
-      return NbBundle.getMessage(LocalHistoryAction.class,
-         "CTL_LocalHistoryAction");
-   }
+  /**
+   * DOCUMENT ME!
+   *
+   * @return DOCUMENT ME!
+   */
+  public String getName() {
+    return NbBundle.getMessage(LocalHistoryAction.class,
+      "CTL_LocalHistoryAction");
+  }
 
-   /**
-    * DOCUMENT ME!
-    *
-    * @return DOCUMENT ME!
-    */
-   protected boolean asynchronous() {
-      return false;
-   }
+  /**
+   * DOCUMENT ME!
+   *
+   * @return DOCUMENT ME!
+   */
+  protected boolean asynchronous() {
+    return false;
+  }
 
-   /**
-    * DOCUMENT ME!
-    *
-    * @return DOCUMENT ME!
-    */
-   protected Class[] cookieClasses() {
-      return new Class[] { DataObject.class };
-   }
+  /**
+   * DOCUMENT ME!
+   *
+   * @return DOCUMENT ME!
+   */
+  protected Class[] cookieClasses() {
+    return new Class[] { DataObject.class };
+  }
 
-   /**
-    * DOCUMENT ME!
-    */
-   protected void initialize() {
-      super.initialize();
-      // see org.openide.util.actions.SystemAction.iconResource() javadoc for more details
-      putValue("noIconInMenu", Boolean.TRUE);
-   }
+  /**
+   * DOCUMENT ME!
+   */
+  protected void initialize() {
+    super.initialize();
+    // see org.openide.util.actions.SystemAction.iconResource() javadoc for more details
+    putValue("noIconInMenu", Boolean.TRUE);
+  }
 
-   /**
-    * DOCUMENT ME!
-    *
-    * @return DOCUMENT ME!
-    */
-   protected int mode() {
-      return CookieAction.MODE_EXACTLY_ONE;
-   }
+  /**
+   * DOCUMENT ME!
+   *
+   * @return DOCUMENT ME!
+   */
+  protected int mode() {
+    return CookieAction.MODE_EXACTLY_ONE;
+  }
 
-   /**
-    * DOCUMENT ME!
-    *
-    * @param activatedNodes DOCUMENT ME!
-    */
-   protected void performAction(final Node[] activatedNodes) {
-      DataObject c = (DataObject) activatedNodes[0].getCookie(DataObject.class);
-      performAction(c);
-   }
+  /**
+   * DOCUMENT ME!
+   *
+   * @param activatedNodes DOCUMENT ME!
+   */
+  protected void performAction(final Node[] activatedNodes) {
+    DataObject c = (DataObject) activatedNodes[0].getCookie(DataObject.class);
+    performAction(c);
+  }
 
-   /**
-    * DOCUMENT ME!
-    *
-    * @param c DOCUMENT ME!
-    */
-   protected void performAction(final DataObject c) {
-      //DataObject c = (DataObject) activatedNodes[0].getCookie(DataObject.class);
-      final FileObject file = c.getPrimaryFile();
-//      Cursor old = WindowManager.getDefault().getMainWindow().getCursor();
-//      WindowManager.getDefault().getMainWindow()
-//                   .setCursor(Cursor.getPredefinedCursor(Cursor.WAIT_CURSOR));
+  /**
+   * DOCUMENT ME!
+   *
+   * @param c DOCUMENT ME!
+   */
+  protected void performAction(final DataObject c) {
+    final FileObject fileObject = c.getPrimaryFile();
 
-      final LocalHistoryTopComponent win = LocalHistoryTopComponent.findInstance();
-      Collection<VersionNode> col = win.fillNodeList((FileUtil.toFile(file)));
-      if (col.size() == 0) {
-          //WindowManager.getDefault().getMainWindow().setCursor(old);
-          JOptionPane.showMessageDialog(null,"No revisions in local history");
-          return;
-      }
-      win.setFile(FileUtil.toFile(file),col);
-      win.open();
-      win.requestActive();
-      win.setDisplayName("Local History of " + file.getNameExt());
-      //      Runnable run = new Runnable() {
-      //         public void run() {
-      //            win.setFile(FileUtil.toFile(file));
-      //            win.revalidate();
-      //         }
-      //      };
-      //      new Thread(run).start();
-      //WindowManager.getDefault().getMainWindow().setCursor(old);
-   }
+    final LocalHistoryTopComponent localHistoryTopComponent =
+      LocalHistoryTopComponent.findInstance();
+    File file = FileUtil.toFile(fileObject);
+    Collection<VersionNode> collection =
+      localHistoryTopComponent.fillRevisionsList(file);
 
-   /**
-    * DOCUMENT ME!
-    *
-    * @param dataObject DOCUMENT ME!
-    *
-    * @return DOCUMENT ME!
-    */
-   private boolean enable(final DataObject dataObject) {
-      assert dataObject != null;
+    if (collection.size() != 0) {
+      localHistoryTopComponent.setFile(file, collection);
+      localHistoryTopComponent.open();
+      localHistoryTopComponent.requestActive();
+      localHistoryTopComponent.setDisplayName(
+          "Local History of " + fileObject.getNameExt());
+    } else {
+      JOptionPane.showMessageDialog(null, "No revisions in local history");
+    }
+  }
 
-      //DataObject dataObject = n.getLookup().lookup(DataObject.class);
-      //System.out.println("dataObject ="+dataObject);
-      return ((dataObject != null) && dataObject.getPrimaryFile().isData());
-   }
+  /**
+   * DOCUMENT ME!
+   *
+   * @param dataObject DOCUMENT ME!
+   *
+   * @return DOCUMENT ME!
+   */
+  private boolean enable(final DataObject dataObject) {
+    assert dataObject != null;
 
-   /**
-    * DOCUMENT ME!
-    *
-    * @author $author$
-    * @version $Revision$
-    */
-   private class ContextAction extends AbstractAction {
-      DataObject classNode = null;
+    //DataObject dataObject = n.getLookup().lookup(DataObject.class);
+    //System.out.println("dataObject ="+dataObject);
+    return ((dataObject != null) && dataObject.getPrimaryFile()
+                                              .isData());
+  }
 
-      /**
-       * Creates a new ContextAction object.
-       *
-       * @param context DOCUMENT ME!
-       */
-      public ContextAction(final Lookup context) {
-         //System.out.println("init CA");
-         //Node _classNode = context.getDefault().lookup(Node.class);
-         DataObject _classNode = (DataObject) context.lookup(DataObject.class);
-         //System.out.println("_classNode = "+_classNode);
-         classNode = ((_classNode != null) && enable(_classNode)) ? _classNode
-                                                                  : null;
-      }
+  /**
+   * DOCUMENT ME!
+   *
+   * @author $author$
+   * @version $Revision$
+   */
+  private class ContextAction
+    extends AbstractAction {
+    DataObject classNode = null;
 
-      /**
-       * DOCUMENT ME!
-       *
-       * @param e DOCUMENT ME!
-       */
-      public void actionPerformed(final ActionEvent e) {
-         performAction(classNode);
-      }
+    /**
+     * Creates a new ContextAction object.
+     *
+     * @param context DOCUMENT ME!
+     */
+    public ContextAction(final Lookup context) {
+      //System.out.println("init CA");
+      //Node _classNode = context.getDefault().lookup(Node.class);
+      DataObject _classNode = (DataObject) context.lookup(DataObject.class);
+      //System.out.println("_classNode = "+_classNode);
+      classNode =
+        ((_classNode != null) && enable(_classNode))
+        ? _classNode
+        : null;
+    }
 
-      /**
-       * Gets the <code>Object</code> associated with the specified
-       * key.
-       *
-       * @param key a string containing the specified <code>key</code>
-       *
-       * @return the binding <code>Object</code> stored with this key; if there
-       *         are no keys, it will return <code>null</code>
-       *
-       * @see Action#getValue
-       */
-      public Object getValue(final String key) {
-         if (key.equals(AbstractAction.NAME)) {
-            return getName();
-         }
+    /**
+     * DOCUMENT ME!
+     *
+     * @param e DOCUMENT ME!
+     */
+    public void actionPerformed(final ActionEvent e) {
+      performAction(classNode);
+    }
 
-         return super.getValue(key);
+    /**
+     * Gets the <code>Object</code> associated with the specified
+     * key.
+     *
+     * @param key a string containing the specified <code>key</code>
+     *
+     * @return the binding <code>Object</code> stored with this key; if there
+     *         are no keys, it will return <code>null</code>
+     *
+     * @see Action#getValue
+     */
+    public Object getValue(final String key) {
+      if (key.equals(AbstractAction.NAME)) {
+        return getName();
       }
 
-      /**
-       * DOCUMENT ME!
-       *
-       * @return DOCUMENT ME!
-       */
-      public boolean isEnabled() {
-         //         System.out.println("CA isEnabled");
-         //         System.out.println("classNode = "+classNode);
-         return classNode != null;
-      }
-   }
+      return super.getValue(key);
+    }
+
+    /**
+     * DOCUMENT ME!
+     *
+     * @return DOCUMENT ME!
+     */
+    public boolean isEnabled() {
+      //         System.out.println("CA isEnabled");
+      //         System.out.println("classNode = "+classNode);
+      return classNode != null;
+    }
+  }
 }
