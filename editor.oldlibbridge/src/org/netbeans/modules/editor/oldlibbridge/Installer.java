e586e5b11d3b4b78049e25715348fecf2dc11c79
vstejskal@netbeans.org <vstejskal@netbeans.org>
2007-01-19 05:21:11 +0000

    Integrating Highlighting SPI from editor_api_delivery_200701191221

---
+++ editor.oldlibbridge/src/org/netbeans/modules/editor/oldlibbridge/Installer.java
@@ -0,0 +1,25 @@
+package org.netbeans.modules.editor.oldlibbridge;
+
+import java.util.logging.Logger;
+import org.netbeans.editor.Registry;
+import org.openide.modules.ModuleInstall;
+
+/**
+ * Manages a module's lifecycle. Remember that an installer is optional and
+ * often not needed at all.
+ */
+public class Installer extends ModuleInstall {
+
+    private static final Logger LOG = Logger.getLogger(Installer.class.getName());
+    
+//    /** The flag that determines if the new highlighting API should be used. */
+//    private static final boolean HIGHLIGHTING = Boolean.getBoolean("org-netbeans-spi-editor-highlighting");
+    
+    public void restored() {
+//        LOG.warning("Highlighting is " + (HIGHLIGHTING ? "on" : "off") + ".");
+//        if (HIGHLIGHTING) {
+            Registry.addChangeListener(HighlightingDrawLayer.INJECTOR);
+//        }
+    }
+    
+}
