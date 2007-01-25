7b0e07e3794884560b74128b6f70064a81e311bc
lebedkov@netbeans.org <lebedkov@netbeans.org>
2007-01-25 20:57:57 +0000

    Some improvements to the ObjectList

--- tasklist.core/src/org/netbeans/modules/tasklist/core/util/ObjectList.java
+++ tasklist.core/src/org/netbeans/modules/tasklist/core/util/ObjectList.java
@@ -21,146 +21,34 @@ package org.netbeans.modules.tasklist.core.util;
 
 import java.util.AbstractList;
 import java.util.ArrayList;
-import java.util.EventListener;
-import java.util.EventObject;
 import java.util.List;
 import javax.swing.event.EventListenerList;
 
 /**
- * A list of objects that allows listening for changes
+ * A list of objects that allows listening for changes.
+ * 
+ * @author tl
  */
-public class ObjectList<E> extends AbstractList<E> {
-    /**
-     * Owner of an ObjectList
-     */
-    public static interface Owner {
-        /**
-         * Returns the ObjectList
-         *
-         * @return objects
-         */
-        public ObjectList getObjectList();
-    }
-    
-    /**
-     * This interface could be implemented by elements of an ObjectList.
-     */
-    public static interface Element {
-        /**
-         * Returns the ObjectList that contains this element
-         *
-         * @return ObjectList that contains this element.
-         */
-        public ObjectList getParentList();
-    }
-	
-    /**
-     * An event for changes in an ObjectList
-     */
-    public static class Event extends EventObject {
-        /** a range of items was removed */
-        public static final int EVENT_REMOVED = 0;
-        
-        /** new items added */
-        public static final int EVENT_ADDED = 1;
-        
-        /** the objects changed completely */
-        public static final int EVENT_STRUCTURE_CHANGED = 2;
-        
-        /** objects were just reordered. Not removed and not added. */
-        public static final int EVENT_REORDERED = 3;
-
-        private int op;
-        private int indices[];
-        private Object objects[];
-        
-        /**
-         * Constructor
-         *
-         * @param source source for this event. Typically an ObjectList
-         * @param indices indices of the changed values. May be null if
-         *   op == EVENT_REORDERED.
-         * @param objects changed values. May be null if
-         *   op == EVENT_REORDERED.
-         * @param op Operation. One of the EVENT_* constants from this class
-         */
-        public Event(Object source, int op, int[] indices, Object[] objects) {
-            super(source);
-            
-            assert op == EVENT_ADDED || op == EVENT_REMOVED ||
-                op == EVENT_REORDERED || op == EVENT_STRUCTURE_CHANGED : 
-                "Wrong operation"; // NOI18N
-            
-            assert indices != null || op == EVENT_REORDERED;
-            assert objects != null || op == EVENT_REORDERED;
-            
-            this.op = op;
-            this.indices = indices;
-            this.objects = objects;
-        }
-        
-        /**
-         * Returns the type of the event
-         *
-         * @return one of the EVENT_* constants from this class
-         */
-        public int getType() {
-            return op;
-        }
-        
-        /**
-         * Returns changed objects
-         * 
-         * @return array of changed objects. May be null if
-         *   op == EVENT_REORDERED
-         */
-        public Object[] getObjects() {
-            return objects;
-        }
-        
-        /**
-         * Returns indices of changed objects
-         * 
-         * @return indices. May be null if op == EVENT_REORDERED
-         */
-        public int[] getIndices() {
-            return indices;
-        }
-    }
-    
-    /**
-     * A listener for an ObjectList. Objects from this list could 
-     * implement this interface. Such an itemy will not be informed about
-     * all changes. It will be informed about changes associated
-     * with it.
-     */
-    public static interface Listener extends EventListener {
-        /**
-         * A change in the list occured.
-         *
-         * @param e an event
-         */
-        public void listChanged(Event e);
-    }
-    
+public class ObjectList<E> extends AbstractList<E> {    
     private EventListenerList listeners = null;
     
     /** internal representation */
     private List<E> objects = null;
     
     /**
-     * Creates a new instance of ObjectList
+     * Creates a new instance of ObjectList with an ArrayList as underlying 
+     * implementation class.
      */
     public ObjectList() {
     }
     
     /**
-     * Returns the owner of this list
+     * Creates a new instance of ObjectList.
      * 
-     * @return owner or null
+     * @param objects observed list
      */
-    public ObjectList.Owner getOwner() {
-    	return null;
+    public ObjectList(List<E> objects) {
+        this.objects = objects;
     }
     
     public int size() {
@@ -178,9 +66,9 @@ public class ObjectList<E> extends AbstractList<E> {
         
         E old = objects.set(index, obj);
         if (hasListeners()) {
-            fireEvent(new Event(this, Event.EVENT_REMOVED, 
+            fireEvent(new ObjectListEvent(this, ObjectListEvent.EVENT_REMOVED, 
                 new int[] {index}, new Object[]{old}));
-            fireEvent(new Event(this, Event.EVENT_ADDED, 
+            fireEvent(new ObjectListEvent(this, ObjectListEvent.EVENT_ADDED, 
                 new int[] {index}, new Object[]{obj}));
         }
         return old;
@@ -191,10 +79,10 @@ public class ObjectList<E> extends AbstractList<E> {
      *
      * @param listener a listener
      */
-    public void addListener(Listener listener) {
+    public void addListener(ObjectListListener listener) {
         if (listeners == null)
             listeners = new EventListenerList();
-        listeners.add(Listener.class, listener);
+        listeners.add(ObjectListListener.class, listener);
     }
     
     /**
@@ -202,9 +90,9 @@ public class ObjectList<E> extends AbstractList<E> {
      *
      * @param listener a listener
      */
-    public void removeListener(Listener listener) {
+    public void removeListener(ObjectListListener listener) {
         if (listeners != null)
-            listeners.remove(Listener.class, listener);
+            listeners.remove(ObjectListListener.class, listener);
         if (listeners.getListenerCount() == 0)
             this.listeners = null;
     }
@@ -223,7 +111,7 @@ public class ObjectList<E> extends AbstractList<E> {
      *
      * @param e an event
      */
-    protected void fireEvent(Event e) {
+    protected void fireEvent(ObjectListEvent e) {
         // Guaranteed to return a non-null array
         Object[] l = listeners.getListenerList();
         
@@ -231,7 +119,7 @@ public class ObjectList<E> extends AbstractList<E> {
         // those that are interested in this event
         for (int i = l.length - 2; i >= 0; i -= 2) {
             // Lazily create the event:
-            ((Listener) l[i+1]).listChanged(e);
+            ((ObjectListListener) l[i+1]).listChanged(e);
         }
     }
     
@@ -249,13 +137,14 @@ public class ObjectList<E> extends AbstractList<E> {
         
         objects.add(index, element);
         
-        if (hasListeners() || element instanceof Listener) {
-            Event e = new Event(this, Event.EVENT_ADDED, new int[] {index},
-                new Object[] {element});
+        if (hasListeners() || element instanceof ObjectListListener) {
+            ObjectListEvent e = new ObjectListEvent(this, 
+                    ObjectListEvent.EVENT_ADDED, new int[] {index},
+                    new Object[] {element});
             if (hasListeners())
                 fireEvent(e);
-            if (element instanceof Listener)
-                ((Listener) element).listChanged(e);
+            if (element instanceof ObjectListListener)
+                ((ObjectListListener) element).listChanged(e);
         }
     }
     
@@ -263,13 +152,14 @@ public class ObjectList<E> extends AbstractList<E> {
 	if (objects == null)
             throw new IndexOutOfBoundsException("Empty list"); // NOI18N
         E obj = objects.remove(index);
-        if (hasListeners() || obj instanceof Listener) {
-            Event e = new Event(this, Event.EVENT_REMOVED, 
-                new int[] {index}, new Object[] {obj});
+        if (hasListeners() || obj instanceof ObjectListListener) {
+            ObjectListEvent e = new ObjectListEvent(this, 
+                    ObjectListEvent.EVENT_REMOVED, 
+                    new int[] {index}, new Object[] {obj});
             if (hasListeners())
                 fireEvent(e);
-            if (obj instanceof Listener)
-                ((Listener) obj).listChanged(e);
+            if (obj instanceof ObjectListListener)
+                ((ObjectListListener) obj).listChanged(e);
         }
         if (objects.size() == 0)
             objects = null;
@@ -287,7 +177,8 @@ public class ObjectList<E> extends AbstractList<E> {
             throw new IndexOutOfBoundsException("Empty list"); // NOI18N
         objects.add(newIndex, objects.remove(index));
         if (hasListeners()) {
-            Event e = new Event(this, Event.EVENT_REORDERED, null, null);
+            ObjectListEvent e = new ObjectListEvent(this, 
+                    ObjectListEvent.EVENT_REORDERED, null, null);
             fireEvent(e);
         }
     }
