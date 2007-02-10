2d2ad56d925f69af4d9eaa00c743fa7a012b0caf
tboudreau@netbeans.org <tboudreau@netbeans.org>
2007-02-10 22:50:00 +0000

    CDDL, sorting

--- javanavigators/src/org/netbeans/modules/javanavigators/AsynchListModel.java
+++ javanavigators/src/org/netbeans/modules/javanavigators/AsynchListModel.java
@@ -1,5 +1,17 @@
 /*
-*/
+ * The contents of this file are subject to the terms of the Common Development
+ * and Distribution License (the License). You may not use this file except in
+ * compliance with the License.
+ *
+ * You can obtain a copy of the License at http://www.netbeans.org/cddl.html
+ * or http://www.netbeans.org/cddl.txt.
+ *
+ * When distributing Covered Code, include this CDDL Header Notice in each file
+ * and include the License file at http://www.netbeans.org/cddl.txt.
+ * If applicable, add the following below the CDDL Header, with the fields
+ * enclosed by brackets [] replaced by your own identifying information:
+ * "Portions Copyrighted [year] [name of copyright owner]"
+ */
 package org.netbeans.modules.javanavigators;
 
 import java.awt.EventQueue;
@@ -50,10 +62,10 @@ public final class AsynchListModel <T extends Object> implements GenerifiedListM
     public void setComparator (Comparator <T> c) {
         if (c != this.comparator) {
             this.comparator = c;
-            List <T> old = new ArrayList <T> (contents);
+            List <T> nue = new ArrayList <T> (contents);
             if (comparator != null) {
-                Collections.sort (contents, comparator);
-                Diff diff = ListDiff.createDiff(old, contents);
+                Collections.sort (nue, comparator);
+                Diff diff = ListDiff.createDiff(contents, nue);
                 updateOnEventQueue (diff);
             }
         }
--- javanavigators/src/org/netbeans/modules/javanavigators/Description.java
+++ javanavigators/src/org/netbeans/modules/javanavigators/Description.java
@@ -1,12 +1,21 @@
 /*
- * Description.java
+ * The contents of this file are subject to the terms of the Common Development
+ * and Distribution License (the License). You may not use this file except in
+ * compliance with the License.
  *
- * Created on February 9, 2007, 11:12 PM
+ * You can obtain a copy of the License at http://www.netbeans.org/cddl.html
+ * or http://www.netbeans.org/cddl.txt.
  *
- * To change this template, choose Tools | Template Manager
- * and open the template in the editor.
+ * When distributing Covered Code, include this CDDL Header Notice in each file
+ * and include the License file at http://www.netbeans.org/cddl.txt.
+ * If applicable, add the following below the CDDL Header, with the fields
+ * enclosed by brackets [] replaced by your own identifying information:
+ * "Portions Copyrighted [year] [name of copyright owner]"
+ *
+ * The Original Software is NetBeans. The Initial Developer of the Original
+ * Software is Sun Microsystems, Inc. Portions Copyright 1997-2006 Sun
+ * Microsystems, Inc. All Rights Reserved.
  */
-
 package org.netbeans.modules.javanavigators;
 
 import java.util.Comparator;
@@ -107,6 +116,15 @@ public class Description {
         return sb.toString();
     }
     
+    long getPosition() {
+        long result = pos;
+        while (parent != null) {
+            result += parent.pos;
+            parent = parent.parent;
+        }
+        return result;
+    }
+    
     public int hashCode() {
         int hash = 7;
         hash = 29 * hash + (this.name != null ? this.name.hashCode() : 0);
@@ -133,18 +151,21 @@ public class Description {
                 return d1.name.compareTo(d2.name);
             }
             else {
-                return d1.pos == d2.pos ? 0 : d1.pos < d2.pos ? -1 : 1;
+                long p1 = d1.getPosition();
+                long p2 = d2.getPosition();
+//                return p1 == p2 ? 0 : p1 < p2 ? -1 : 1;
+                return (int) (p1 - p2);
             }
         }
 
         int k2i( ElementKind kind ) {
             switch( kind ) {
                 case CONSTRUCTOR:
-                    return 1;
-                case METHOD:
-                    return 2;
-                case FIELD:
                     return 3;
+                case METHOD:
+                    return 1;
+                case FIELD:
+                    return 2;
                 case CLASS:
                 case INTERFACE:
                 case ENUM:
