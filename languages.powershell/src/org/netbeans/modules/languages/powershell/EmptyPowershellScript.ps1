436a7720efc860abc431807647af9890fefb9702
sandipchitale@netbeans.org <sandipchitale@netbeans.org>
2007-02-25 06:46:02 +0000

    Initial check in. Editor Support for Windows powershell script using the Generic Language Framework.

---
+++ languages.powershell/src/org/netbeans/modules/languages/powershell/EmptyPowershellScript.ps1
@@ -0,0 +1,10 @@
+# Windows powershell script.
+#
+# You can execute this script using the following command:
+#
+# %SystemRoot%\system32\windowspowershell\v1.0\powershell.exe &"thisscript.ps1"
+#
+# Windows powershell documentation:
+#
+# http://msdn2.microsoft.com/en-us/library/aa139691.aspx
+#
