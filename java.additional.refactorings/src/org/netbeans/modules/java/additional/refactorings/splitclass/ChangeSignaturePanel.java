24c29942f979b33107e22e8693cfc52ca91b3071
tboudreau@netbeans.org <tboudreau@netbeans.org>
2007-05-25 09:32:45 +0000

    cddl

--- java.additional.refactorings/src/org/netbeans/modules/java/additional/refactorings/splitclass/ChangeSignaturePanel.java
+++ java.additional.refactorings/src/org/netbeans/modules/java/additional/refactorings/splitclass/ChangeSignaturePanel.java
@@ -1,9 +1,19 @@
 /*
- * ChangeSignaturePanel.java
- *
- * Created on May 22, 2007, 5:33 PM
+ * The contents of this file are subject to the terms of the Common Development
+ * and Distribution License (the License). You may not use this file except in
+ * compliance with the License.
+ * 
+ * You can obtain a copy of the License at http://www.netbeans.org/cddl.html
+ * or http://www.netbeans.org/cddl.txt.
+ * 
+ * When distributing Covered Code, include this CDDL Header Notice in each file
+ * and include the License file at http://www.netbeans.org/cddl.txt.
+ * If applicable, add the following below the CDDL Header, with the fields
+ * enclosed by brackets [] replaced by your own identifying information:
+ * "Portions Copyrighted [year] [name of copyright owner]"
+ * 
+ * Portions Copyrighted 2007 Sun Microsystems, Inc.
  */
-
 package org.netbeans.modules.java.additional.refactorings.splitclass;
 
 import java.awt.Component;
