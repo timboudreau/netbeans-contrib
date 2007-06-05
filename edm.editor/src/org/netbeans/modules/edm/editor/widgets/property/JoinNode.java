654c3fcdc3ca8094d08687739822a0c903b2dcfd
karthik_s <none@none>
2007-06-05 09:31:29 +0000

    Adding Property Sheet to Join and Table nodes

--- edm.editor/src/org/netbeans/modules/edm/editor/widgets/property/JoinNode.java
+++ edm.editor/src/org/netbeans/modules/edm/editor/widgets/property/JoinNode.java
@@ -2,22 +2,23 @@
  * The contents of this file are subject to the terms of the Common Development
  * and Distribution License (the License). You may not use this file except in
  * compliance with the License.
- * 
- * You can obtain a copy of the License at http://www.netbeans.org/cddl.html
+// *
+// * You can obtain a copy of the License at http://www.netbeans.org/cddl.html
  * or http://www.netbeans.org/cddl.txt.
- * 
+ *
  * When distributing Covered Code, include this CDDL Header Notice in each file
  * and include the License file at http://www.netbeans.org/cddl.txt.
  * If applicable, add the following below the CDDL Header, with the fields
  * enclosed by brackets [] replaced by your own identifying information:
  * "Portions Copyrighted [year] [name of copyright owner]"
- * 
+ *
  * The Original Software is NetBeans. The Initial Developer of the Original
  * Software is Sun Microsystems, Inc. Portions Copyright 1997-2007 Sun
  * Microsystems, Inc. All Rights Reserved.
  */
 package org.netbeans.modules.edm.editor.widgets.property;
 
+import org.netbeans.modules.edm.editor.dataobject.MashupDataObject;
 import org.openide.ErrorManager;
 import org.openide.nodes.AbstractNode;
 import org.openide.nodes.Children;
@@ -33,9 +34,12 @@ import org.netbeans.modules.sql.framework.model.SQLJoinOperator;
 public class JoinNode extends AbstractNode {
     
     private SQLJoinOperator joinOp;
-
-    public JoinNode(SQLJoinOperator obj) {
+    
+    private MashupDataObject mObj;
+    
+    public JoinNode(SQLJoinOperator obj, MashupDataObject dObj) {
         super(Children.LEAF);
+        mObj = dObj;
         joinOp = obj;
     }
     
@@ -57,21 +61,37 @@ public class JoinNode extends AbstractNode {
     @Override
     public boolean canDestroy() {
         return true;
-    }    
+    }
     
     /** Creates a property sheet. */
     protected Sheet createSheet() {
         Sheet sheet = Sheet.createDefault();
-        Sheet.Set set = sheet.createPropertiesSet();        
+        Sheet.Set set = sheet.createPropertiesSet();
         try {
-            Property conditionProp = new PropertySupport.Reflection(this.joinOp.getJoinCondition(), String.class,
-                    "getConditionText", "setConditionText");                    
-            conditionProp.setName("Join Condition");
-            set.put(conditionProp);
+            PropertySupport.Reflection joinConditionProp = new PropertySupport.Reflection(this.joinOp.getJoinCondition(), String.class,
+                    "getConditionText", "setConditionText");
+            joinConditionProp.setName("Join Condition");
+            joinConditionProp.setPropertyEditorClass(PropertyEditorManager.getPropertyEditor("JOINCONDITION"));
+            set.put(joinConditionProp);
+            
+            PropertySupport.Reflection joinTypeProp = new PropertySupport.Reflection(this.joinOp, String.class,
+                    "getJoinTypeString", "setJoinType");
+            joinTypeProp.setName("Join Type");
+            joinTypeProp.setPropertyEditorClass(PropertyEditorManager.getPropertyEditor("JOINTYPE"));
+            set.put(joinTypeProp);
+            
         } catch (Exception ex) {
             ErrorManager.getDefault().notify(ex);
         }
         sheet.put(set);
         return sheet;
-    }   
+    }
+    
+    public SQLJoinOperator getJoinOperator() {
+        return this.joinOp;
+    }
+    
+    public MashupDataObject getMashupDataObject() {
+        return mObj;
+    }
 }
--- edm.editor/src/org/netbeans/modules/edm/editor/widgets/property/TableNode.java
+++ edm.editor/src/org/netbeans/modules/edm/editor/widgets/property/TableNode.java
@@ -2,16 +2,16 @@
  * The contents of this file are subject to the terms of the Common Development
  * and Distribution License (the License). You may not use this file except in
  * compliance with the License.
- * 
+ *
  * You can obtain a copy of the License at http://www.netbeans.org/cddl.html
  * or http://www.netbeans.org/cddl.txt.
- * 
+ *
  * When distributing Covered Code, include this CDDL Header Notice in each file
  * and include the License file at http://www.netbeans.org/cddl.txt.
  * If applicable, add the following below the CDDL Header, with the fields
  * enclosed by brackets [] replaced by your own identifying information:
  * "Portions Copyrighted [year] [name of copyright owner]"
- * 
+ *
  * The Original Software is NetBeans. The Initial Developer of the Original
  * Software is Sun Microsystems, Inc. Portions Copyright 1997-2007 Sun
  * Microsystems, Inc. All Rights Reserved.
@@ -33,7 +33,7 @@ import org.netbeans.modules.sql.framework.model.SQLDBTable;
 public class TableNode extends AbstractNode {
     
     private SQLDBTable dbTable;
-
+    
     public TableNode(SQLDBTable obj) {
         super(Children.LEAF);
         dbTable = obj;
@@ -57,27 +57,33 @@ public class TableNode extends AbstractNode {
     @Override
     public boolean canDestroy() {
         return true;
-    }    
+    }
     
     /** Creates a property sheet. */
     protected Sheet createSheet() {
         Sheet sheet = Sheet.createDefault();
-        Sheet.Set set = sheet.createPropertiesSet();        
+        Sheet.Set set = sheet.createPropertiesSet();
         try {
-            Property nameProp = new PropertySupport.Reflection(this.dbTable, 
-                    String.class, "getFullyQualifiedName", null);                    
+            PropertySupport.Reflection nameProp = new PropertySupport.Reflection(this.dbTable,
+                    String.class, "getFullyQualifiedName", null);
             nameProp.setName("Table Name");
-            set.put(nameProp);            
+            set.put(nameProp);
             
-            Property conditionProp = new PropertySupport.Reflection(
-                    this.dbTable.getParent().getConnectionDefinition(), 
-                    String.class, "getConnectionURL", null);                    
+            PropertySupport.Reflection conditionProp = new PropertySupport.Reflection(
+                    this.dbTable.getParent().getConnectionDefinition(),
+                    String.class, "getConnectionURL", null);
             conditionProp.setName("Connection URL");
-            set.put(conditionProp);           
+            set.put(conditionProp);
+            
+            PropertySupport.Reflection modelNameProp = new PropertySupport.Reflection(
+                    this.dbTable.getParent(), String.class,"getModelName",null);
+            modelNameProp.setName("Model Name");
+            set.put(modelNameProp);
+            
         } catch (Exception ex) {
             ErrorManager.getDefault().notify(ex);
         }
         sheet.put(set);
         return sheet;
-    }   
+    }
 }
