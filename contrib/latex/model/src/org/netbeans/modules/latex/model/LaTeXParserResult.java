0d1afccb125d228ebc87ee649b834ac6fd4fcd7f
jlahoda@netbeans.org <jlahoda@netbeans.org>
2007-07-28 16:30:55 +0000

    Errors in the ParserResult - forgotten file.

--- contrib/latex/model/src/org/netbeans/modules/latex/model/LaTeXParserResult.java
+++ contrib/latex/model/src/org/netbeans/modules/latex/model/LaTeXParserResult.java
@@ -19,12 +19,16 @@
 
 package org.netbeans.modules.latex.model;
 
+import java.util.Collection;
+import java.util.Collections;
+import java.util.LinkedList;
 import org.netbeans.api.gsf.Element;
 import org.netbeans.api.gsf.ParserFile;
 import org.netbeans.api.gsf.ParserResult;
 import org.netbeans.modules.latex.model.command.CommandUtilities;
 import org.netbeans.modules.latex.model.command.DocumentNode;
 import org.netbeans.modules.latex.model.structural.StructuralElement;
+import org.netbeans.spi.editor.hints.ErrorDescription;
 import org.openide.filesystems.FileObject;
 
 /**
@@ -37,13 +41,15 @@ public final class LaTeXParserResult extends ParserResult {
     private StructuralElement structuralRoot;
     private CommandUtilities utils;
     private FileObject mainFile;
+    private Collection<ErrorDescription> errors;
             
-    public LaTeXParserResult(ParserFile file, FileObject mainFile, DocumentNode root, StructuralElement structuralRoot, CommandUtilities utils) {
+    public LaTeXParserResult(ParserFile file, FileObject mainFile, DocumentNode root, StructuralElement structuralRoot, CommandUtilities utils, Collection<ErrorDescription> errors) {
         super(file);
         this.root = root;
         this.structuralRoot = structuralRoot;
         this.utils = utils;
         this.mainFile = mainFile;
+        this.errors = Collections.unmodifiableCollection(new LinkedList<ErrorDescription>(errors));
     }
 
     public Element getRoot() {
@@ -68,6 +74,10 @@ public final class LaTeXParserResult extends ParserResult {
 
     public StructuralElement getStructuralRoot() {
         return structuralRoot;
+    }
+
+    public Collection<ErrorDescription> getErrors() {
+        return errors;
     }
 
 }
