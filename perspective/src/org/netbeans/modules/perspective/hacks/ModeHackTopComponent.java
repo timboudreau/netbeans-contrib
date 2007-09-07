6834122e5dbfd59fde45a4015a05c605ac849788
theanuradha@netbeans.org <theanuradha@netbeans.org>
2007-09-07 11:50:23 +0000

    org.netbeans.modules.perspective --> org.netbeans.modules.perspective/1 and remove unused bundle

--- perspective/src/org/netbeans/modules/perspective/hacks/ModeHackTopComponent.java
+++ perspective/src/org/netbeans/modules/perspective/hacks/ModeHackTopComponent.java
@@ -19,10 +19,9 @@
 package org.netbeans.modules.perspective.hacks;
 
 import java.io.Serializable;
-import org.openide.util.NbBundle;
 import org.openide.windows.TopComponent;
 import org.openide.windows.WindowManager;
-//import org.openide.util.Utilities;
+
 
 /**
  * Top component which displays something.
@@ -31,14 +30,10 @@ public final class ModeHackTopComponent extends TopComponent {
 
     private static final long serialVersionUID = 1l;
     private static ModeHackTopComponent instance;
-    /** path to the icon used by the component and its open action */
-//    static final String ICON_PATH = "SET/PATH/TO/ICON/HERE";
     private static String PREFERRED_ID = "ModeHackTopComponent";
 
     public ModeHackTopComponent() {
         initComponents();
-        setName(NbBundle.getMessage(ModeHackTopComponent.class, "CTL_ModeHackTopComponent"));
-        setToolTipText(NbBundle.getMessage(ModeHackTopComponent.class, "HINT_ModeHackTopComponent"));
     }
 
     /** This method is called from within the constructor to
@@ -81,36 +76,36 @@ public final class ModeHackTopComponent extends TopComponent {
     public static synchronized ModeHackTopComponent findInstance() {
         TopComponent win = WindowManager.getDefault().findTopComponent(PREFERRED_ID);
         if (win == null) {
-//            Logger.getLogger(ModeHackTopComponent.class.getName()).warning(
-//                    "Cannot find " + PREFERRED_ID + " component. It will not be located properly in the window system.");
-            return getDefault();
+           return getDefault();
         }
         if (win instanceof ModeHackTopComponent) {
             return (ModeHackTopComponent) win;
         }
-//        Logger.getLogger(ModeHackTopComponent.class.getName()).warning(
-//                "There seem to be multiple components with the '" + PREFERRED_ID +
-//                "' ID. That is a potential source of errors and unexpected behavior.");
         return getDefault();
     }
 
+    @Override
     public int getPersistenceType() {
         return TopComponent.PERSISTENCE_ALWAYS;
     }
 
+    @Override
     public void componentOpened() {
         // TODO add custom code on component opening
     }
 
+    @Override
     public void componentClosed() {
         // TODO add custom code on component closing
     }
 
     /** replaces this in object stream */
+    @Override
     public Object writeReplace() {
         return new ResolvableHelper();
     }
 
+    @Override
     protected String preferredID() {
         return PREFERRED_ID;
     }
