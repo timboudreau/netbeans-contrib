7a7159b62143d1cdcd3832cc26b52d5d1e39615a
mzlamal@netbeans.org <mzlamal@netbeans.org>
2007-09-27 13:34:38 +0000

    Update license to CDDL+GPL2 w CP exception

--- debugger.callstackviewenhancements/src/org/netbeans/modules/debugger/callstackviewenhancements/ui/GridBagHelper.java
+++ debugger.callstackviewenhancements/src/org/netbeans/modules/debugger/callstackviewenhancements/ui/GridBagHelper.java
@@ -1,14 +1,25 @@
 /*
  *                 Sun Public License Notice
- * 
+ *
  * The contents of this file are subject to the Sun Public License
  * Version 1.0 (the "License"). You may not use this file except in
  * compliance with the License. A copy of the License is available at
  * http://www.sun.com/
- * 
+ *
  * The Original Code is NetBeans. The Initial Developer of the Original
  * Code is Sun Microsystems, Inc. Portions Copyright 1997-2005 Sun
  * Microsystems, Inc. All Rights Reserved.
+
+If you wish your version of this file to be governed by only the CDDL
+or only the GPL Version 2, indicate your decision by adding
+"[Contributor] elects to include this software in this distribution
+under the [CDDL or GPL Version 2] license." If you do not indicate a
+single choice of license, a recipient has the option to distribute
+your version of this file under either the CDDL, the GPL Version 2 or
+to extend the choice of license to its licensees as provided above.
+However, if you add GPL Version 2 code and therefore, elected the GPL
+Version 2 license, then the option applies only if the new code is
+made subject to such option by the copyright holder.
  */
 
 package org.netbeans.modules.debugger.callstackviewenhancements.ui;
@@ -24,7 +35,7 @@ import java.awt.event.*;
  *
  * The class <code>GridBagHelper</code>
  * For example:
- * 
+ *
  * The following table is based on the Scott's (Swing course instructor)
  * tip for taming the GridBagLayout. The only difference is that the row
  * and column from his tip are exchanged. i.e. the constraints are in a row
