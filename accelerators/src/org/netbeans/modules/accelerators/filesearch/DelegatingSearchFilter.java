4436150cb34d9bafbfd484d641a79addb43379c1
mzlamal@netbeans.org <mzlamal@netbeans.org>
2007-09-27 09:05:15 +0000

    Update license to CDDL+GPL2 w CP exception

--- accelerators/src/org/netbeans/modules/accelerators/filesearch/DelegatingSearchFilter.java
+++ accelerators/src/org/netbeans/modules/accelerators/filesearch/DelegatingSearchFilter.java
@@ -1,16 +1,27 @@
 /*
  *                 Sun Public License Notice
- * 
+ *
  * The contents of this file are subject to the Sun Public License
  * Version 1.0 (the "License"). You may not use this file except in
  * compliance with the License. A copy of the License is available at
  * http://www.sun.com/
- * 
- * The Original Code is the Accelerators module. 
- * The Initial Developer of the Original Code is Andrei Badea. 
+ *
+ * The Original Code is the Accelerators module.
+ * The Initial Developer of the Original Code is Andrei Badea.
  * Portions Copyright 2005-2006 Andrei Badea.
  * All Rights Reserved.
- * 
+
+If you wish your version of this file to be governed by only the CDDL
+or only the GPL Version 2, indicate your decision by adding
+"[Contributor] elects to include this software in this distribution
+under the [CDDL or GPL Version 2] license." If you do not indicate a
+single choice of license, a recipient has the option to distribute
+your version of this file under either the CDDL, the GPL Version 2 or
+to extend the choice of license to its licensees as provided above.
+However, if you add GPL Version 2 code and therefore, elected the GPL
+Version 2 license, then the option applies only if the new code is
+made subject to such option by the copyright holder.
+ *
  * Contributor(s): Andrei Badea
  */
 
@@ -24,13 +35,13 @@ import org.openide.filesystems.FileObject;
  * @author Andrei Badea
  */
 public class DelegatingSearchFilter implements SearchFilter {
-    
+
     final SearchFilter[] delegates;
-    
+
     public DelegatingSearchFilter(SearchFilter delegate1, SearchFilter delegate2) {
         this(new SearchFilter[] { delegate1, delegate2 });
     }
-    
+
     public DelegatingSearchFilter(SearchFilter[] delegates) {
         this.delegates = delegates;
     }
