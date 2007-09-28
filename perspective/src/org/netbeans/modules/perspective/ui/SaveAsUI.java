2bd40c8866f312f58176332cb890a06e94a12e8c
theanuradha@netbeans.org <theanuradha@netbeans.org>
2007-09-28 05:49:48 +0000

    mnemonics

--- perspective/src/org/netbeans/modules/perspective/ui/SaveAsUI.java
+++ perspective/src/org/netbeans/modules/perspective/ui/SaveAsUI.java
@@ -50,6 +50,7 @@ import java.awt.event.ActionEvent;
 import java.util.List;
 import javax.swing.AbstractAction;
 import javax.swing.DefaultListModel;
+import javax.swing.JButton;
 import javax.swing.event.ListSelectionEvent;
 import javax.swing.event.ListSelectionListener;
 import org.netbeans.modules.perspective.utils.PerspectiveManagerImpl;
@@ -146,8 +147,8 @@ public class SaveAsUI extends javax.swing.JDialog {
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
 
-        btnCancel = new javax.swing.JButton();
-        btnOK = new javax.swing.JButton();
+        btnCancel = new JButton(new CancelAction());
+        btnOK = new JButton(new SaveAs());
         jScrollPane1 = new javax.swing.JScrollPane();
         modeList = new javax.swing.JList();
         lblName = new javax.swing.JLabel();
@@ -163,11 +164,9 @@ public class SaveAsUI extends javax.swing.JDialog {
         setTitle(org.openide.util.NbBundle.getMessage(SaveAsUI.class, "SaveAsUI.title")); // NOI18N
         setResizable(false);
 
-        btnCancel.setAction(new CancelAction());
-        btnCancel.setMnemonic('C');
+        org.openide.awt.Mnemonics.setLocalizedText(btnCancel, org.openide.util.NbBundle.getMessage(SaveAsUI.class, "Cancel")); // NOI18N
 
-        btnOK.setAction(new SaveAs());
-        btnOK.setMnemonic('O');
+        org.openide.awt.Mnemonics.setLocalizedText(btnOK, org.openide.util.NbBundle.getMessage(SaveAsUI.class, "OK")); // NOI18N
 
         modeList.setSelectionMode(javax.swing.ListSelectionModel.SINGLE_SELECTION);
         jScrollPane1.setViewportView(modeList);
@@ -246,7 +245,7 @@ public class SaveAsUI extends javax.swing.JDialog {
                 .add(layout.createParallelGroup(org.jdesktop.layout.GroupLayout.BASELINE)
                     .add(before)
                     .add(after))
-                .addPreferredGap(org.jdesktop.layout.LayoutStyle.RELATED, 20, Short.MAX_VALUE)
+                .addPreferredGap(org.jdesktop.layout.LayoutStyle.RELATED, org.jdesktop.layout.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
                 .add(jLabel1)
                 .addPreferredGap(org.jdesktop.layout.LayoutStyle.RELATED)
                 .add(jScrollPane1, org.jdesktop.layout.GroupLayout.PREFERRED_SIZE, 163, org.jdesktop.layout.GroupLayout.PREFERRED_SIZE)
