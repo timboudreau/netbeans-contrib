73e7ef8e3bc24a2c225a66da506cfd91dcf77c64
radval@netbeans.org <radval@netbeans.org>
2007-10-05 06:35:35 +0000

    get handle to BPELModel from set consistency module for editing consistency rule.

--- o.ucla.netbeans.module.bpel.setconsistency/src/org/ucla/netbeans/module/bpel/setconsistency/actions/EditSetConsistencyRuleAction.java
+++ o.ucla.netbeans.module.bpel.setconsistency/src/org/ucla/netbeans/module/bpel/setconsistency/actions/EditSetConsistencyRuleAction.java
@@ -9,11 +9,18 @@
 
 package org.ucla.netbeans.module.bpel.setconsistency.actions;
 
+import org.netbeans.modules.bpel.model.api.BpelModel;
+import org.netbeans.modules.bpel.model.spi.BpelModelFactory;
+import org.netbeans.modules.xml.retriever.catalog.Utilities;
+import org.netbeans.modules.xml.xam.ModelSource;
 import org.openide.loaders.DataObject;
 import org.openide.nodes.Node;
 import org.openide.util.HelpCtx;
+import org.openide.util.Lookup;
 import org.openide.util.NbBundle;
 import org.openide.util.actions.CookieAction;
+import org.openide.windows.IOProvider;
+import org.openide.windows.OutputWriter;
 
 /**
  *
@@ -33,7 +40,21 @@ public class EditSetConsistencyRuleAction extends CookieAction {
 
     @Override
     protected void performAction(Node[] activatedNodes) {
-        
+        OutputWriter writer =  IOProvider.getDefault().getStdOut();
+        writer.println("In EditSetConsistencyRuleAction");
+        if(activatedNodes.length > 0) {
+            DataObject bpelDataObject = activatedNodes[0].getCookie(DataObject.class);
+            
+            if(bpelDataObject != null) {
+                writer.println("In bpel file "+ bpelDataObject.getName());
+                
+                BpelModel model = getModel(bpelDataObject);
+                
+                if(model != null) {
+                    writer.println("Got BPEL Model");
+                }
+            }
+        }
     }
 
     @Override
@@ -46,4 +67,14 @@ public class EditSetConsistencyRuleAction extends CookieAction {
         return HelpCtx.DEFAULT_HELP;
     }
 
+    private BpelModel getModel(DataObject bpelDataObject) {
+        ModelSource modelSource = Utilities.getModelSource(bpelDataObject
+                .getPrimaryFile(), true);
+        return getModelFactory().getModel(modelSource);
+    }
+    private BpelModelFactory getModelFactory() {
+        BpelModelFactory factory = (BpelModelFactory) Lookup.getDefault()
+                .lookup(BpelModelFactory.class);
+        return factory;
+    }
 }
