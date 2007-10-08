6776a6daec397f1ef105302c274462ae21ca6ad3
radval@netbeans.org <radval@netbeans.org>
2007-10-08 18:08:21 +0000

    Added Comment support in bpel model add process level. Allow user to add a new comment then set the comment text, allow user to remove comment. Made changes in set consistency module to demo how to add comment, set comment text with rules etc

--- o.ucla.netbeans.module.bpel.setconsistency/src/org/ucla/netbeans/module/bpel/setconsistency/actions/EditSetConsistencyRuleAction.java
+++ o.ucla.netbeans.module.bpel.setconsistency/src/org/ucla/netbeans/module/bpel/setconsistency/actions/EditSetConsistencyRuleAction.java
@@ -9,10 +9,14 @@
 
 package org.ucla.netbeans.module.bpel.setconsistency.actions;
 
+import java.util.Iterator;
+import java.util.List;
 import org.netbeans.modules.bpel.model.api.BpelModel;
+import org.netbeans.modules.bpel.model.api.XmlComment;
 import org.netbeans.modules.bpel.model.spi.BpelModelFactory;
 import org.netbeans.modules.xml.retriever.catalog.Utilities;
 import org.netbeans.modules.xml.xam.ModelSource;
+import org.netbeans.modules.xml.xam.dom.AbstractDocumentComponent;
 import org.openide.loaders.DataObject;
 import org.openide.nodes.Node;
 import org.openide.util.HelpCtx;
@@ -21,6 +25,7 @@ import org.openide.util.NbBundle;
 import org.openide.util.actions.CookieAction;
 import org.openide.windows.IOProvider;
 import org.openide.windows.OutputWriter;
+import org.ucla.netbeans.module.bpel.setconsistency.Util;
 
 /**
  *
@@ -48,15 +53,33 @@ public class EditSetConsistencyRuleAction extends CookieAction {
             if(bpelDataObject != null) {
                 writer.println("In bpel file "+ bpelDataObject.getName());
                 
-                BpelModel model = getModel(bpelDataObject);
+                BpelModel model = Util.getModel(bpelDataObject);
                 
                 if(model != null) {
                     writer.println("Got BPEL Model");
+                    
+                    org.netbeans.modules.bpel.model.api.Process process = model.getProcess();
+                    if(process != null) {
+                        writer.println("Got BPEL process" + process.getName());
+                       
+                        //print existing comments
+                        String message1 = "Printing Existing Comments";
+                        printComments(process, model, writer, message1);
+                        //add new comment
+                        String message2 = "Adding a new set consistency rule in a new comment";
+                        writer.println(message2);
+                        addNewComment(process);
+                        //print comments again
+                        String message3 = "Printing All Comments Again";
+                        printComments(process, model, writer, message3);
+                        
+                    }
                 }
             }
         }
     }
 
+    
     @Override
     public String getName() {
         return NbBundle.getMessage(EditSetConsistencyRuleAction.class, "EditSetConsistencyRuleAction_DisplayName");
@@ -67,14 +90,58 @@ public class EditSetConsistencyRuleAction extends CookieAction {
         return HelpCtx.DEFAULT_HELP;
     }
 
-    private BpelModel getModel(DataObject bpelDataObject) {
-        ModelSource modelSource = Utilities.getModelSource(bpelDataObject
-                .getPrimaryFile(), true);
-        return getModelFactory().getModel(modelSource);
+    
+
+    private void addComment() {
+
     }
-    private BpelModelFactory getModelFactory() {
-        BpelModelFactory factory = (BpelModelFactory) Lookup.getDefault()
-                .lookup(BpelModelFactory.class);
-        return factory;
+
+    private void printComments(org.netbeans.modules.bpel.model.api.Process process,
+                                          BpelModel model,
+                                          OutputWriter writer,
+                                          String message) {
+      
+       List<XmlComment> comments = process.getXmlComments();
+       Iterator<XmlComment> it = comments.iterator();
+       int i = 0;
+       model.startTransaction();
+       writer.println(message);
+       while(it.hasNext()) {
+           XmlComment comment = it.next();
+           String commentStr = comment.getCommentText();
+           writer.println("comment: " + i + " : " + comment.getCommentText());
+           i++;
+       }
+
+       model.endTransaction();
+    }
+
+    private XmlComment getMatchingComment(org.netbeans.modules.bpel.model.api.Process process,
+                                          BpelModel model) {
+        XmlComment matchingComment = null;                                  
+
+        List<XmlComment> comments = process.getXmlComments();
+       Iterator<XmlComment> it = comments.iterator();
+       while(it.hasNext()) {
+           XmlComment comment = it.next();
+           String commentStr = comment.getCommentText();
+           if(commentStr.startsWith("<SetConsistencyRules")) {
+                matchingComment = comment;
+                break;
+           }
+       }
+        
+       return matchingComment;
+    }
+
+    private XmlComment addNewComment(org.netbeans.modules.bpel.model.api.Process process) {
+        XmlComment newComment = null;                                  
+        try {
+            newComment =process.addXmlComment("<SetConsistencyRules><SetConsistencyRule name=\"firstRule\" /> </SetConsistencyRules>");
+        } catch(Exception ex) {
+            ex.printStackTrace();;
+        }
+       
+        return newComment;
     }
 }
--- o.ucla.netbeans.module.bpel.setconsistency/src/org/ucla/netbeans/module/bpel/setconsistency/actions/RunSetConsistencyValidatorAction.java
+++ o.ucla.netbeans.module.bpel.setconsistency/src/org/ucla/netbeans/module/bpel/setconsistency/actions/RunSetConsistencyValidatorAction.java
@@ -9,10 +9,16 @@
 
 package org.ucla.netbeans.module.bpel.setconsistency.actions;
 
+import org.netbeans.modules.bpel.model.api.BpelModel;
+import org.openide.loaders.DataObject;
 import org.openide.nodes.Node;
 import org.openide.util.HelpCtx;
 import org.openide.util.NbBundle;
 import org.openide.util.actions.NodeAction;
+import org.openide.windows.IOProvider;
+import org.openide.windows.OutputWriter;
+import org.ucla.netbeans.module.bpel.setconsistency.Util;
+import org.ucla.netbeans.module.bpel.setconsistency.validator.SetConsistencyValidatorVisitor;
 
 /**
  *
@@ -22,6 +28,20 @@ public class RunSetConsistencyValidatorAction extends NodeAction {
 
     @Override
     protected void performAction(Node[] activatedNodes) {
+        OutputWriter writer =  IOProvider.getDefault().getStdOut();
+        writer.println("In RunSetConsistencyValidatorAction");
+        
+        DataObject bpelDataObject = activatedNodes[0].getCookie(DataObject.class);
+            
+            if(bpelDataObject != null) {
+                writer.println("In bpel file "+ bpelDataObject.getName());
+                BpelModel model = Util.getModel(bpelDataObject);
+                
+                if(model != null && model.getProcess() != null) {
+                    SetConsistencyValidatorVisitor visitor = new SetConsistencyValidatorVisitor();
+                    model.getProcess().accept(visitor);
+                }
+            }
         
     }
 
