7376df027251dfbff246e1b6b99230db811472d0
jlahoda@netbeans.org <jlahoda@netbeans.org>
2007-10-22 16:02:10 +0000

    Making the module compilable.

--- java.fscompletion/src/org/netbeans/modules/java/fscompletion/CompletionProviderImpl.java
+++ java.fscompletion/src/org/netbeans/modules/java/fscompletion/CompletionProviderImpl.java
@@ -41,24 +41,21 @@
 package org.netbeans.modules.java.fscompletion;
 
 import java.io.IOException;
-import javax.swing.text.BadLocationException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import javax.swing.text.Document;
 import javax.swing.text.JTextComponent;
 import org.netbeans.api.java.classpath.ClassPath;
-import org.netbeans.editor.BaseDocument;
-import org.netbeans.editor.SyntaxSupport;
-import org.netbeans.editor.TokenItem;
-import org.netbeans.editor.ext.java.JavaTokenContext;
+import org.netbeans.api.java.lexer.JavaTokenId;
+import org.netbeans.api.lexer.TokenHierarchy;
+import org.netbeans.api.lexer.TokenSequence;
 import org.netbeans.modules.editor.fscompletion.spi.support.FSCompletion;
-import org.netbeans.modules.editor.java.NbJavaJMISyntaxSupport;
-import org.netbeans.modules.javacore.api.JavaModel;
-import org.netbeans.modules.javacore.internalapi.JavaMetamodel;
 import org.netbeans.spi.editor.completion.CompletionProvider;
 import org.netbeans.spi.editor.completion.CompletionResultSet;
 import org.netbeans.spi.editor.completion.CompletionTask;
 import org.netbeans.spi.editor.completion.support.AsyncCompletionQuery;
 import org.netbeans.spi.editor.completion.support.AsyncCompletionTask;
-import org.openide.ErrorManager;
+import org.netbeans.spi.java.classpath.support.ClassPathSupport;
 import org.openide.filesystems.FileObject;
 import org.openide.loaders.DataObject;
 
@@ -74,40 +71,47 @@ public class CompletionProviderImpl implements CompletionProvider {
 
     public CompletionTask createTask(int queryType, JTextComponent component) {
         return new AsyncCompletionTask(new AsyncCompletionQuery() {
-            protected void query(CompletionResultSet resultSet, Document doc, int caretOffset) {
+            protected void query(CompletionResultSet resultSet, final Document doc, final int caretOffset) {
+                try {
                 DataObject od = (DataObject) doc.getProperty(Document.StreamDescriptionProperty);
                 FileObject file = od.getPrimaryFile();
                 
-                JavaModel.getJavaRepository().beginTrans(false);
+                final int[] offset = new int[1];
+                final String[] string = new String[1];
                 
-                try {
-                    if (doc instanceof BaseDocument) {
-                        SyntaxSupport sup = ((BaseDocument) doc).getSyntaxSupport();
-                        NbJavaJMISyntaxSupport nbJavaSup = (NbJavaJMISyntaxSupport)sup.get(NbJavaJMISyntaxSupport.class);
+                doc.render(new Runnable() {
+                    public void run() {
+                        TokenHierarchy th = TokenHierarchy.get(doc);
+                        TokenSequence<JavaTokenId> ts = th.tokenSequence(JavaTokenId.language());
                         
-                        TokenItem item = nbJavaSup.getTokenChain(caretOffset, caretOffset);
-                        
-                        if (item != null && item.getTokenID() == JavaTokenContext.STRING_LITERAL) {
-                            int start = item.getOffset() + 1;
+                        if (ts != null) {
+                            ts.move(caretOffset);
                             
-                            JavaModel.setClassPath(file);
-                            
-                            ClassPath cp = JavaMetamodel.getManager().getClassPath();
-                            FileObject[] roots = cp.getRoots();
-                            
-                            String text = doc.getText(start, caretOffset - start);
-                            
-                            resultSet.addAllItems(FSCompletion.completion(roots, roots, text, caretOffset - text.length()));
-                            
-                            resultSet.setTitle(text);
+                            if (ts.moveNext() && ts.token().id() == JavaTokenId.STRING_LITERAL) {
+                                offset[0] = ts.offset();
+                                string[0] = ts.token().text().toString();
+                            }
                         }
                     }
+                });
+                
+                if (string[0] != null) {
+                    ClassPath boot = ClassPath.getClassPath(file, ClassPath.BOOT);
+                    ClassPath exec = ClassPath.getClassPath(file, ClassPath.EXECUTE);
+                    
+                    if (boot != null && exec != null) {
+                        ClassPath cp = ClassPathSupport.createProxyClassPath(boot, exec);
+                        FileObject[] roots = cp.getRoots();
+
+                        String text = string[0].substring(1, caretOffset - offset[0]);
+
+                        resultSet.addAllItems(FSCompletion.completion(roots, roots, text, caretOffset - text.length()));
+                    }
+                }
+                
                 } catch (IOException e) {
-                    ErrorManager.getDefault().notify(e);
-                } catch (BadLocationException e) {
-                    ErrorManager.getDefault().notify(e);
+                    Logger.getLogger(CompletionProviderImpl.class.getName()).log(Level.WARNING, null, e);
                 } finally {
-                    JavaModel.getJavaRepository().endTrans();
                     resultSet.finish();
                 }
             }
