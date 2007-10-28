01aea8e2b263838ee91dd9e210843a5ac2d47e82
sandipchitale@netbeans.org <sandipchitale@netbeans.org>
2007-10-28 22:34:48 +0000

    Fix icons and action labels. Correct the license headers.

--- fileopenserver/src/org/netbeans/modules/fileopenserver/StartFileOpenServerAction.java
+++ fileopenserver/src/org/netbeans/modules/fileopenserver/StartFileOpenServerAction.java
@@ -1,14 +1,70 @@
 /*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
+ * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS HEADER.
+ *
+ * Copyright 1997-2007 Sun Microsystems, Inc. All rights reserved.
+ *
+ * The contents of this file are subject to the terms of either the GNU
+ * General Public License Version 2 only ("GPL") or the Common
+ * Development and Distribution License("CDDL") (collectively, the
+ * "License"). You may not use this file except in compliance with the
+ * License. You can obtain a copy of the License at
+ * http://www.netbeans.org/cddl-gplv2.html
+ * or nbbuild/licenses/CDDL-GPL-2-CP. See the License for the
+ * specific language governing permissions and limitations under the
+ * License.  When distributing the software, include this License Header
+ * Notice in each file and include the License file at
+ * nbbuild/licenses/CDDL-GPL-2-CP.  Sun designates this
+ * particular file as subject to the "Classpath" exception as provided
+ * by Sun in the GPL Version 2 section of the License file that
+ * accompanied this code. If applicable, add the following below the
+ * License Header, with the fields enclosed by brackets [] replaced by
+ * your own identifying information:
+ * "Portions Copyrighted [year] [name of copyright owner]"
+ *
+ * Contributor(s):
+ *
+ * The Original Software is NetBeans. The Initial Developer of the Original
+ * Software is Sun Microsystems, Inc. Portions Copyright 1997-2006 Sun
+ * Microsystems, Inc. All Rights Reserved.
+ *
+ * If you wish your version of this file to be governed by only the CDDL
+ * or only the GPL Version 2, indicate your decision by adding
+ * "[Contributor] elects to include this software in this distribution
+ * under the [CDDL or GPL Version 2] license." If you do not indicate a
+ * single choice of license, a recipient has the option to distribute
+ * your version of this file under either the CDDL, the GPL Version 2 or
+ * to extend the choice of license to its licensees as provided above.
+ * However, if you add GPL Version 2 code and therefore, elected the GPL
+ * Version 2 license, then the option applies only if the new code is
+ * made subject to such option by the copyright holder.
  */
+
 package org.netbeans.modules.fileopenserver;
 
+import javax.swing.Icon;
+import javax.swing.ImageIcon;
 import org.openide.util.HelpCtx;
 import org.openide.util.NbBundle;
+import org.openide.util.Utilities;
 import org.openide.util.actions.BooleanStateAction;
 
 public final class StartFileOpenServerAction extends BooleanStateAction {
+    private static final Icon START_ICON = new ImageIcon(Utilities.loadImage("org/netbeans/modules/fileopenserver/resources/StartFileOpenServerIcon.gif"));
+    private static final Icon STOP_ICON = new ImageIcon(Utilities.loadImage("org/netbeans/modules/fileopenserver/resources/StopFileOpenServerIcon.gif"));
+
+    @Override
+    protected void initialize() {
+        super.initialize();
+        final boolean started = FileOpenServer.getFileOpenServer().isStarted();
+        if (started) {
+            putValue(SMALL_ICON, STOP_ICON);
+            putValue(NAME, NbBundle.getMessage(StartFileOpenServerAction.class, "CTL_StopFileOpenServerAction"));
+        } else {
+            putValue(SMALL_ICON, getIcon());
+            putValue(NAME, getName());
+        }
+        putProperty(PROP_BOOLEAN_STATE, started);
+    }
 
     @Override
     public void setBooleanState(boolean start) {
@@ -20,13 +76,15 @@ public final class StartFileOpenServerAction extends BooleanStateAction {
             } else {
                 fileOpenServer.stopServer();
             }
+            final boolean started = fileOpenServer.isStarted();
+            if (started) {
+                putValue(SMALL_ICON, STOP_ICON);
+                putValue(NAME, NbBundle.getMessage(StartFileOpenServerAction.class, "CTL_StopFileOpenServerAction"));
+            } else {
+                putValue(SMALL_ICON, getIcon());
+                putValue(NAME, getName());
+            }
         }
-    }        
-    
-    @Override
-    protected void initialize() {
-        super.initialize();
-        putProperty(PROP_BOOLEAN_STATE, FileOpenServer.getFileOpenServer().isStarted());
     }
 
     public String getName() {
@@ -35,7 +93,12 @@ public final class StartFileOpenServerAction extends BooleanStateAction {
 
     @Override
     protected String iconResource() {
-        return "org/netbeans/modules/fileopenserver/resources/StartFileOpenServerIcon.gif";
+        final boolean started = FileOpenServer.getFileOpenServer().isStarted();
+        if (started) {
+            return "org/netbeans/modules/fileopenserver/resources/StopFileOpenServerIcon.gif";
+        } else {
+            return "org/netbeans/modules/fileopenserver/resources/StartFileOpenServerIcon.gif";
+        }
     }
 
     public HelpCtx getHelpCtx() {
