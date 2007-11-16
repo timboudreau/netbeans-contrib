8e2f3eb79d8ef696e1dfeef9763b0bb3e44a1e59
martin_adamek <none@none>
2007-11-16 18:56:41 +0000

    Fixing template names

---
+++ groovy.groovyproject/src/org/netbeans/modules/groovy/groovyproject/resources/GroovyClass.groovy
@@ -0,0 +1,10 @@
+// Groovy source file
+
+class Greet {
+    def name
+    Greet(who) { name = who[0].toUpperCase() + who[1..-1] }
+    def salute() { println "Hello $name!" }
+}
+
+g = new Greet('world')  // create object
+g.salute()              // Output "Hello World!"
