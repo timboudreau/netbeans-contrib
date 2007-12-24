e9577501a9cb2ada8eadeefe98ada23a51791e28
dcaoyuan@netbeans.org <dcaoyuan@netbeans.org>
2007-12-24 15:25:45 +0000

    -completion for keyword

--- scala.editing/src/org/netbeans/modules/scala/editing/Scala.java
+++ scala.editing/src/org/netbeans/modules/scala/editing/Scala.java
@@ -41,11 +41,27 @@
 
 package org.netbeans.modules.scala.editing;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Map;
+import javax.swing.text.AbstractDocument;
+import org.netbeans.api.languages.ASTItem;
+import org.netbeans.api.languages.ASTNode;
 import org.netbeans.api.languages.ASTToken;
 import org.netbeans.api.languages.CharInput;
+import org.netbeans.api.languages.CompletionItem;
+import org.netbeans.api.languages.Context;
 import org.netbeans.api.languages.Language;
 import org.netbeans.api.languages.LanguageDefinitionNotFoundException;
 import org.netbeans.api.languages.LanguagesManager;
+import org.netbeans.api.languages.LibrarySupport;
+import org.netbeans.api.languages.SyntaxContext;
+import org.netbeans.api.lexer.Token;
+import org.netbeans.api.lexer.TokenHierarchy;
+import org.netbeans.api.lexer.TokenSequence;
 
 /**
  *
@@ -53,7 +69,7 @@ import org.netbeans.api.languages.LanguagesManager;
  */
 public class Scala {
 
-    private static final String DOC = "org/netbeans/modules/languages/erlang/Documentation.xml";
+    private static final String DOC = "org/netbeans/modules/scala/editing/Documentation.xml";
     private static final String MIME_TYPE = "text/x-scala";
 
     public static Object[] parseXmlStart(CharInput input) {
@@ -109,4 +125,144 @@ public class Scala {
             return null;
         }
     }
+    
+    // code completion .........................................................
+    
+    public static List<CompletionItem> completionItems (Context context) {
+        if (context instanceof SyntaxContext) {
+            List<CompletionItem> result = new ArrayList<CompletionItem> ();
+            return result;
+        }
+        
+        AbstractDocument document = (AbstractDocument) context.getDocument ();
+        document.readLock ();
+        try {
+            TokenSequence tokenSequence = getTokenSequence (context);
+            List<CompletionItem> result = new ArrayList<CompletionItem> ();
+            Token token = previousToken (tokenSequence);
+            String tokenText = token.text ().toString ();
+            String libraryContext = null;
+            if (tokenText.equals ("new")) {
+                result.addAll (getLibrary ().getCompletionItems ("constructor"));
+                return result;
+            }
+            if (tokenText.equals (".")) {
+                token = previousToken (tokenSequence);
+                if (token.id ().name ().endsWith ("id"))
+                    libraryContext = token.text ().toString ();
+            } else
+            if (token.id ().name ().endsWith ("id") ) {
+                token = previousToken (tokenSequence);
+                if (token.text ().toString ().equals (".")) {
+                    token = previousToken (tokenSequence);
+                    if (token.id ().name ().endsWith ("id"))
+                        libraryContext = token.text ().toString ();
+                } else
+                if (token.text ().toString ().equals ("new")) {
+                    result.addAll (getLibrary ().getCompletionItems ("constructor"));
+                    return result;
+                }
+            }
+
+            if (libraryContext != null) {
+                result.addAll (getLibrary ().getCompletionItems (libraryContext));
+                result.addAll (getLibrary ().getCompletionItems ("member"));
+            } else
+                result.addAll (getLibrary ().getCompletionItems ("root"));
+            return result;
+        } finally {
+            document.readUnlock ();
+        }
+    }
+    
+    private static TokenSequence getTokenSequence (Context context) {
+        TokenHierarchy tokenHierarchy = TokenHierarchy.get (context.getDocument ());
+        TokenSequence ts = tokenHierarchy.tokenSequence ();
+        while (true) {
+            ts.move (context.getOffset ());
+            if (!ts.moveNext ()) return ts;
+            TokenSequence ts2 = ts.embedded ();
+            if (ts2 == null) return ts;
+            ts = ts2;
+        }
+    }
+    
+    private static List<CompletionItem> merge (List<CompletionItem> items) {
+        Map<String,CompletionItem> map = new HashMap<String,CompletionItem> ();
+        Iterator<CompletionItem> it = items.iterator ();
+        while (it.hasNext ()) {
+            CompletionItem completionItem = it.next ();
+            CompletionItem current = map.get (completionItem.getText ());
+            if (current != null) {
+                String library = current.getLibrary ();
+                if (library == null) library = "";
+                if (completionItem.getLibrary () != null &&
+                    library.indexOf (completionItem.getLibrary ()) < 0
+                )
+                    library += ',' + completionItem.getLibrary ();
+                completionItem = CompletionItem.create (
+                    current.getText (),
+                    current.getDescription (),
+                    library,
+                    current.getType (),
+                    current.getPriority ()
+                );
+            }
+            map.put (completionItem.getText (), completionItem);
+        }
+        return new ArrayList<CompletionItem> (map.values ());
+    }
+    
+    private static Token previousToken (TokenSequence ts) {
+        do {
+            if (!ts.movePrevious ()) return ts.token ();
+        } while (
+            ts.token ().id ().name ().endsWith ("whitespace") ||
+            ts.token ().id ().name ().endsWith ("comment")
+        );
+        return ts.token ();
+    }
+    
+    // helper methods ..........................................................
+    
+    
+    private static LibrarySupport library;
+    
+    private static LibrarySupport getLibrary () {
+        if (library == null)
+            library = LibrarySupport.create (
+                Arrays.asList (new String[] {DOC})
+            );
+        return library;
+    }
+    
+    private static String getParametersAsText (ASTNode params) {
+        if (params == null) return "";
+        StringBuffer buf = new StringBuffer();
+        for (ASTItem item : params.getChildren()) {
+            if (item instanceof ASTNode) {
+                String nt = ((ASTNode) item).getNT();
+                if ("Parameter".equals(nt)) {
+                     Iterator<ASTItem> iter = ((ASTNode) item).getChildren().iterator();
+                     if (iter.hasNext()) {
+                         item = iter.next();
+                     }
+                }
+            }
+            if (!(item instanceof ASTToken)) {
+                continue;
+            }
+            ASTToken token = (ASTToken)item;
+            String type = token.getTypeName ();
+            if ("whitespace".equals(type) || "comment".equals(type)) {
+                continue;
+            }
+            String id = token.getIdentifier();
+            buf.append(id);
+            if (",".equals(id)) {
+                buf.append(' ');
+            }
+        }
+        return buf.toString();
+    }    
 }
