909711668f40b8a500c6a73a04157afa9dbee30c
jlahoda@netbeans.org <jlahoda@netbeans.org>
2007-12-31 20:53:42 +0000

    Replacing old HighlightLayer usage by the new Highlighting SPI. Getting the search patterns from the core/tasklist/todo module. Removing the color customizer, can be customized in the std. Fonts&Colors panel now. Expanding the highlight til the end of the line.

--- showtodos/src/org/netbeans/modules/showtodos/HighlightTodos.java
+++ showtodos/src/org/netbeans/modules/showtodos/HighlightTodos.java
@@ -24,7 +24,7 @@
  * Contributor(s):
  *
  * The Original Software is NetBeans. The Initial Developer of the Original
- * Software is Sun Microsystems, Inc. Portions Copyright 1997-2006 Sun
+ * Software is Sun Microsystems, Inc. Portions Copyright 1997-2007 Sun
  * Microsystems, Inc. All Rights Reserved.
  *
  * If you wish your version of this file to be governed by only the CDDL
@@ -40,37 +40,34 @@
  */
 package org.netbeans.modules.showtodos;
 
-import java.awt.Color;
-import java.awt.Font;
-import java.beans.PropertyChangeEvent;
-import java.beans.PropertyChangeListener;
-import java.util.ArrayList;
+import java.util.LinkedList;
 import java.util.List;
-import java.util.Set;
-import java.util.prefs.BackingStoreException;
-import java.util.prefs.Preferences;
 
-import javax.swing.SwingUtilities;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.swing.JEditorPane;
+import javax.swing.text.AttributeSet;
 import javax.swing.text.BadLocationException;
+import javax.swing.text.Document;
 import javax.swing.text.JTextComponent;
 
+import org.netbeans.api.editor.mimelookup.MimeLookup;
+import org.netbeans.api.editor.settings.FontColorSettings;
 import org.netbeans.api.lexer.Token;
 import org.netbeans.api.lexer.TokenHierarchy;
-import org.netbeans.api.lexer.TokenId;
 import org.netbeans.api.lexer.TokenSequence;
 import org.netbeans.editor.BaseDocument;
-import org.netbeans.editor.Coloring;
-import org.netbeans.editor.DrawContext;
-import org.netbeans.editor.DrawLayer;
-import org.netbeans.editor.EditorUI;
 import org.netbeans.editor.FinderFactory;
-import org.netbeans.editor.MarkFactory;
-import org.netbeans.editor.Registry;
 import org.netbeans.editor.Utilities;
-import org.openide.ErrorManager;
-import org.openide.util.Mutex;
-import org.openide.util.NbPreferences;
-import org.openide.util.WeakSet;
+import org.netbeans.modules.tasklist.todo.settings.Settings;
+import org.netbeans.spi.editor.highlighting.HighlightsChangeListener;
+import org.netbeans.spi.editor.highlighting.HighlightsContainer;
+import org.netbeans.spi.editor.highlighting.HighlightsLayer;
+import org.netbeans.spi.editor.highlighting.HighlightsLayerFactory;
+import org.netbeans.spi.editor.highlighting.HighlightsSequence;
+import org.netbeans.spi.editor.highlighting.ZOrder;
+import org.openide.util.Exceptions;
+import org.openide.util.Lookup;
 
 
 /**
@@ -79,312 +76,165 @@ import org.openide.util.WeakSet;
  *
  * @author Andrei Badea
  * @author Tor Norbye
+ * @author Jan Lahoda
  */
-public class HighlightTodos implements PropertyChangeListener {
-    private static final HighlightTodos DEFAULT = new HighlightTodos();
-    private final Set<EditorUI> INSTALLED = new WeakSet();
+public class HighlightTodos implements HighlightsContainer {
 
-    private HighlightTodos() {
-        ShowTodosOptions.getDefault().addPropertyChangeListener(this);
+    private static final Logger LOG = Logger.getLogger(HighlightTodos.class.getName());
+    
+    private BaseDocument doc;
+    private Lookup lookup;
+
+    public HighlightTodos(BaseDocument doc, Lookup lookup) {
+        this.doc = doc;
+        this.lookup = lookup;
     }
+    
+    public HighlightsSequence getHighlights(int startOffset, int endOffset) {
+        FontColorSettings fcs = lookup.lookup(FontColorSettings.class);
+        
+        if (fcs == null) {
+            return HighlightsSequence.EMPTY;
+        }
+        
+        AttributeSet coloring = fcs.getTokenFontColors("todo-tasks"); // NOI18N
+        
+        if (coloring == null) {
+            return HighlightsSequence.EMPTY;
+        }
+        
+        List<int[]> highlights = new LinkedList<int[]>();
 
-    public static HighlightTodos getDefault() {
-        return DEFAULT;
-    }
+        TokenHierarchy th = TokenHierarchy.get(doc);
+        TokenSequence ts = null;
 
-    public void install(final JTextComponent component) {
-        if (component == null) {
-            //No editors open
-            return;
+        if (th != null) {
+            ts = th.tokenSequence();
         }
 
-        Mutex.EVENT.readAccess(new Runnable() {
-                public void run() {
-                    EditorUI editorUI = Utilities.getEditorUI(component);
+        try {
+            startOffset = Utilities.getRowStart(doc, startOffset);
+            endOffset = Math.min(doc.getLength(), endOffset);
+        
+            for (String word : Settings.getDefault().getPatterns()) {
+                int pos = startOffset;
 
-                    if (editorUI == null) {
-                        return;
-                    }
-
-                    Layer layer = (Layer)editorUI.findLayer(Layer.NAME);
-
-                    if (layer == null) {
-                        layer = new Layer();
-                        editorUI.addLayer(layer, Layer.VISIBILITY);
-                        INSTALLED.add(editorUI);
-
-                        // needed in order to repaint the active component when the module
-                        // is installed/enabled through Module Manager/Update Center
-                        editorUI.repaint(0);
-                    }
-                }
-            });
-    }
-
-    public void uninstall() {
-        Mutex.EVENT.readAccess(new Runnable() {
-                public void run() {
-                    for (EditorUI editorUI : INSTALLED) {
-                        editorUI.removeLayer(Layer.NAME);
-                    }
-
-                    INSTALLED.clear();
-
-                    // repainting the active component when the module
-                    // is installed/enabled through Module Manager/Update Center
-                    repaintMostActiveComponent();
-                }
-            });
-    }
-
-    public void setColor(final JTextComponent component, final Color color) {
-        Mutex.EVENT.readAccess(new Runnable() {
-                public void run() {
-                    EditorUI editorUI = Utilities.getEditorUI(component);
-
-                    if (editorUI == null) {
-                        return;
-                    }
-
-                    Layer layer = (Layer)editorUI.findLayer(Layer.NAME);
-
-                    if (layer == null) {
-                        return;
-                    }
-
-                    layer.setColor(color);
-                    editorUI.repaint(0);
-                }
-            });
-    }
-
-    private void repaintMostActiveComponent() {
-        assert SwingUtilities.isEventDispatchThread();
-
-        javax.swing.text.JTextComponent component = Registry.getMostActiveComponent();
-
-        if (component == null) {
-            return;
-        }
-
-        EditorUI editorUI = Utilities.getEditorUI(component);
-
-        if (editorUI != null) {
-            editorUI.repaint(0);
-        }
-    }
-
-    public void propertyChange(PropertyChangeEvent event) {
-        String propertyName = event.getPropertyName();
-
-        if (propertyName.equals(ShowTodosOptions.PROP_HIGHLIGHTING_COLOR)) {
-            Mutex.EVENT.readAccess(new Runnable() {
-                    public void run() {
-                        for (EditorUI editorUI : INSTALLED) {
-                            Layer layer = (Layer)editorUI.findLayer(Layer.NAME);
-
-                            if (layer != null) {
-                                layer.colorChanged();
-                            }
-                        }
-
-                        repaintMostActiveComponent();
-                    }
-                });
-        }
-    }
-
-    private static final class Layer extends DrawLayer.AbstractLayer {
-        private static final String NAME = "show-todos-layer"; // NOI18N
-
-        // just below the highlight whitespace layer (see whitespace highlighting module)
-        private static final int VISIBILITY = 7994;
-        private Coloring coloring;
-        private int[] blocks = new int[] { -1, -1 };
-        private int curInd = 0;
-        private String[] markers;
-
-        public Layer() {
-            super(NAME);
-        }
-
-        /**
-         * Compute the set of markers to scan for in the user source code.
-         * The code tries to look for the same markers used by the TODO module
-         * in case the user has customized the set. (However, it is doing this
-         * by peeking at the Preferences possibly left by the docscan module,
-         * rather than having a contract API with it, based on
-         * tasklist/docscan/src/org/netbeans/modules/tasklist/docscan/Settings.java)
-         */
-        private String[] getTodoMarkers() {
-            if (markers == null) {
-                final String MARKER_PREFIX = "Tag"; // NOI18N
-                final int MARKER_PREFIX_LENGTH = MARKER_PREFIX.length();
-                List<String> markerList = new ArrayList<String>();
-
-                try {
-                    Preferences preferences =
-                        NbPreferences.root().node("org/netbeans/modules/tasklist/docscan"); // NOI18N
-                    String[] keys = preferences.keys();
-
-                    for (int i = 0; i < keys.length; i++) {
-                        String key = keys[i];
-
-                        if ((key != null) && key.startsWith(MARKER_PREFIX)) {
-                            markerList.add(key.substring(MARKER_PREFIX_LENGTH));
-                        }
-                    }
-                } catch (BackingStoreException bse) {
-                    ErrorManager.getDefault().notify(bse);
-                }
-
-                if (markerList.size() > 0) {
-                    markerList.remove("@todo"); // Applies to javadoc, and these tags are now colorized separately
-                    markers = markerList.toArray(new String[markerList.size()]);
-                } else {
-                    // Additional candidates: HACK, WORKAROUND, REMOVE, OLD
-                    markers = new String[] { "TODO", "FIXME", "XXX", "PENDING" }; // NOI18N
-                }
-            }
-
-            return markers;
-        }
-
-        public Preferences getDocscanPreferences() {
-            return NbPreferences.root().node("org/netbeans/modules/tasklist/docscan");
-        }
-
-        public void setColor(Color color) {
-            coloring = new Coloring(null, color, null);
-        }
-
-        public void colorChanged() {
-            coloring = null;
-
-            // will be recreated in updateContext
-        }
-
-        public void init(DrawContext ctx) {
-            BaseDocument doc = (BaseDocument)ctx.getEditorUI().getDocument();
-            int blockIndex = 0;
-            curInd = 0;
-
-            TokenHierarchy th = TokenHierarchy.get(doc);
-            TokenSequence<TokenId> ts = null;
-
-            if (th != null) {
-                ts = th.tokenSequence();
-            }
-
-            try {
-                for (String word : getTodoMarkers()) {
-                    int beginPos = ctx.getStartOffset();
-                    int endPos = ctx.getEndOffset();
-                    int pos = beginPos;
-
-                    // Search from pos to endPos for TODO markers.
-                    for (;;) {
-                        FinderFactory.WholeWordsFwdFinder finder =
+                // Search from pos to endPos for TODO markers.
+                while (pos < endOffset) {
+                    FinderFactory.WholeWordsFwdFinder finder =
                             new FinderFactory.WholeWordsFwdFinder(doc, word, true);
-                        int next = doc.find(finder, pos, endPos);
-                        int nextEnd;
+                    int next = doc.find(finder, pos, endOffset);
 
-                        if ((next >= beginPos) && (next < endPos)) {
-                            nextEnd = next + word.length();
+                    if ((next >= startOffset) && (next < endOffset)) {
+                        // See if it looks like a token we care about (comments)
+                        if (ts != null) {
+                            ts.move(next);
 
-                            // See if it looks like a token we care about (comments)
-                            if (ts != null) {
-                                ts.move(next);
+                            if (ts.moveNext()) {
+                                Token token = ts.token();
+                                pos = Math.min(Utilities.getRowEnd(doc, next), ts.offset() + token.length());
 
-                                if (ts.index() == -1) {
-                                    ts.moveNext();
-                                }
+                                if (token != null) {
+                                    String category = token.id().primaryCategory();
 
-                                if (ts.tokenCount() > 0) {
-                                    Token token = ts.token();
-
-                                    if (token != null) {
-                                        String category = token.id().primaryCategory();
-
-                                        if ("comment".equals(category)) { // NOI18N
-                                            blockIndex = addBlock(blockIndex, next, nextEnd);
-                                        }
+                                    if ("comment".equals(category)) { // NOI18N
+                                        highlights.add(new int[]{next, pos});
                                     }
                                 }
+                            } else {
+                                pos = next + word.length();
                             }
                         } else {
-                            break;
+                            pos = next + word.length();
                         }
-
-                        pos = nextEnd;
+                    } else {
+                        break;
                     }
                 }
-            } catch (BadLocationException e) {
-                Utilities.annotateLoggable(e);
-            } finally {
-                blockIndex = addBlock(blockIndex, -1, -1);
             }
+        } catch (BadLocationException e) {
+            Exceptions.printStackTrace(e);
         }
+        
+        return new SeqImpl(highlights, coloring);
+    }
 
-        private int addBlock(int blockIndex, int start, int end) {
-            if (blockIndex > (blocks.length - 2)) {
-                int[] newBlocks = new int[(blocks.length > 0) ? (blocks.length * 2) : 2];
-                System.arraycopy(blocks, 0, newBlocks, 0, blocks.length);
-                blocks = newBlocks;
+    public void addHighlightsChangeListener(HighlightsChangeListener listener) {
+    }
 
-                // we never shrink the blocks array, but in practice it
-                // doesn't seem to matter, because the editor calls init() for each line,
-                // so the max length of the array will be 4 (ws start, ws end, -1, -1)
-            }
+    public void removeHighlightsChangeListener(HighlightsChangeListener listener) {
+    }
+    
+    private static final class SeqImpl implements HighlightsSequence {
+        
+        private int[] current;
+        private List<int[]> highlights;
+        private AttributeSet as;
 
-            blocks[blockIndex++] = start;
-            blocks[blockIndex++] = end;
-
-            return blockIndex;
-        }
-
-        public boolean isActive(DrawContext ctx, MarkFactory.DrawMark mark) {
-            boolean active;
-
-            int pos = ctx.getFragmentOffset();
-
-            if (pos == blocks[curInd]) {
-                active = true;
-                setNextActivityChangeOffset(blocks[curInd + 1]);
-            } else if (pos == blocks[curInd + 1]) {
-                active = false;
-                curInd += 2;
-                setNextActivityChangeOffset(blocks[curInd]);
-
-                if (pos == blocks[curInd]) { // just follows
-                    setNextActivityChangeOffset(blocks[curInd + 1]);
-                    active = true;
+        public SeqImpl(List<int[]> highlights, AttributeSet as) {
+            this.highlights = highlights;
+            this.as = as;
+            
+            if (LOG.isLoggable(Level.FINE)) {
+                LOG.fine("SeqImpl:"); // NOI18N
+                for (int[] span : highlights) {
+                    LOG.fine("span: " + span[0] + "-" + span[1]); // NOI18N
                 }
+            }
+        }
+
+        public boolean moveNext() {
+            if (highlights.isEmpty())
+                return false;
+            
+            current = highlights.remove(0);
+            
+            return true;
+        }
+
+        public int getStartOffset() {
+            return current[0];
+        }
+
+        public int getEndOffset() {
+            return current[1];
+        }
+
+        public AttributeSet getAttributes() {
+            return as;
+        }
+    }
+    
+    public static final class FactoryImpl implements HighlightsLayerFactory {
+
+        public HighlightsLayer[] createLayers(Context context) {
+            Document doc = context.getDocument();
+            
+            if (!(doc instanceof BaseDocument)) {
+                return new HighlightsLayer[0];
+            }
+            
+            String mimeType;
+            Object docMT = doc.getProperty("mimeType"); // NOI18N
+            
+            if (docMT instanceof String) {
+                mimeType = (String) docMT;
             } else {
-                setNextActivityChangeOffset(blocks[curInd]);
-                active = false;
-            }
+                JTextComponent pane = context.getComponent();
 
-            return active;
-        }
-
-        public void updateContext(DrawContext ctx) {
-            int pos = ctx.getFragmentOffset();
-
-            if ((pos >= blocks[curInd]) && (pos < blocks[curInd + 1])) {
-                if (coloring == null) {
-                    Font font = ctx.getFont();
-
-                    if (font != null) {
-                        font = font.deriveFont(Font.BOLD);
-                    }
-
-                    coloring = new Coloring(font,
-                            ShowTodosOptions.getDefault().getHighlightingColor(), null);
+                if (pane instanceof JEditorPane) {
+                    mimeType = ((JEditorPane) pane).getContentType();
+                } else {
+                    mimeType = "text/base"; // NOI18N
                 }
-
-                coloring.apply(ctx);
             }
+            
+            HighlightsContainer c = new HighlightTodos((BaseDocument) doc, MimeLookup.getLookup(mimeType));
+            
+            return new HighlightsLayer[] {
+                HighlightsLayer.create(HighlightsLayer.class.getName(), ZOrder.SYNTAX_RACK.forPosition(5000), false, c),
+            };
         }
+        
     }
 }
