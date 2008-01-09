dddec55a727f943fd5ce4e911a817a8429445534
satyaranjan@netbeans.org <satyaranjan@netbeans.org>
2008-01-09 07:45:01 +0000

    Dummy portlet taglib implementation for taglib code completion

---
+++ portalpack.portlets.genericportlets/src/org/netbeans/modules/portalpack/portlets/taglib/PortletURLTag.java
@@ -0,0 +1,52 @@
+/*
+ * CDDL HEADER START
+ * The contents of this file are subject to the terms
+ * of the Common Development and Distribution License
+ * (the License). You may not use this file except in
+ * compliance with the License.
+ *
+ * You can obtain a copy of the License at
+ * http://www.sun.com/cddl/cddl.html and legal/CDDLv1.0.txt
+ * See the License for the specific language governing
+ * permission and limitations under the License.
+ *
+ * When distributing Covered Code, include this CDDL
+ * Header Notice in each file and include the License file
+ * at legal/CDDLv1.0.txt.
+ * If applicable, add the following below the CDDL Header,
+ * with the fields enclosed by brackets [] replaced by
+ * your own identifying information:
+ * "Portions Copyrighted [year] [name of copyright owner]"
+ *
+ * Copyright 2006 Sun Microsystems Inc. All Rights Reserved
+ * CDDL HEADER END
+ */
+
+
+package org.netbeans.modules.portalpack.portlets.taglib;
+
+
+
+
+/**
+ * This class provides methods that are common to both ActionURL and RenderURL.
+ * 
+ * This class applicable for Portlet v2.0
+ */
+public class PortletURLTag extends BaseURLTag
+{
+    public void setPortletMode(String mode) {
+    
+    }
+
+    public void setWindowState(String state) {
+    
+    }
+
+    public void setCopyCurrentRenderParameters(boolean copyCurrentRenderParameters) {
+    
+    }
+
+}
+
+   
