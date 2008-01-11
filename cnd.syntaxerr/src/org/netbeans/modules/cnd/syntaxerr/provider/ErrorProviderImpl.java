3bc3e94df8d47a086636d441a358a2810821f248
vkvashin@netbeans.org <vkvashin@netbeans.org>
2008-01-11 22:43:16 +0000

    Launches appropriatre compiler - Sun or Gnu

--- cnd.syntaxerr/src/org/netbeans/modules/cnd/syntaxerr/provider/ErrorProviderImpl.java
+++ cnd.syntaxerr/src/org/netbeans/modules/cnd/syntaxerr/provider/ErrorProviderImpl.java
@@ -50,6 +50,165 @@ import org.openide.loaders.DataObject;
 // package-local
 class ErrorProviderImpl extends ErrorProvider {
 
+    private static class ErrorBag {
+	
+	Map<Integer, ErrorInfoImpl> errors = new HashMap<Integer, ErrorInfoImpl>();
+	Map<Integer, ErrorInfoImpl> warnings = new HashMap<Integer, ErrorInfoImpl>();
+	
+	public void add(String message, boolean error, int line, int column) {
+	    ErrorInfoImpl info = new ErrorInfoImpl(message, error, line, column);
+	    Map<Integer, ErrorInfoImpl> map = error ? errors : warnings;
+	    ErrorInfoImpl existent = map.get(line);
+	    if( existent == null ) {
+		map.put(line, info);
+	    }
+	    else {
+		existent.adsorb(info);
+	    }
+	}
+	
+	public Collection<ErrorInfo> getResult() {
+	    Collection<ErrorInfo> result = new ArrayList<ErrorInfo>(errors.size() + warnings.size());
+	    for( ErrorInfo info : errors.values() ) {
+		result.add(info);
+	    }
+	    for( ErrorInfo info : warnings.values() ) {
+		result.add(info);
+	    }
+	    return result;
+	}
+    }
+    
+    private interface CompilerOutputParser {
+        void parseCompilerOutput(InputStream stream, String interestingFileName, ErrorBag errorBag) throws IOException;
+    }
+    
+    private static class CompilerInfo {
+        
+        private String path;
+        private CompilerOutputParser parser;
+        
+        public CompilerInfo(String path, CompilerOutputParser parser) {
+            this.path = path;
+            this.parser = parser;
+        }
+
+        public String getPath() {
+            return path;
+        }
+
+        public CompilerOutputParser getParser() {
+            return parser;
+        }
+    }
+    
+    private static abstract class BaseParser implements CompilerOutputParser {
+        
+        public void parseCompilerOutput(InputStream stream, String interestingFileName, ErrorBag errorBag) throws IOException {
+            BufferedReader reader = new BufferedReader(new InputStreamReader(stream));
+            for( String line = reader.readLine(); line != null; line = reader.readLine() ) {
+                try {
+                    parseCompilerOutputLine(line, interestingFileName, errorBag);
+                } catch( Exception e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+        protected abstract void parseCompilerOutputLine(String line, String interestingFileName, ErrorBag errorBag);
+    }
+    
+    private static class GnuParser extends BaseParser {
+
+        protected void parseCompilerOutputLine(String line, String interestingFileName, ErrorBag errorBag) {
+            if( DebugUtils.TRACE ) System.err.printf("\tPARSING: \t%s\n", line);
+            findErrorOrWarning(line, ": error: ", true, interestingFileName, errorBag); // NOI18N
+            findErrorOrWarning(line, ": warning: ", false, interestingFileName, errorBag); // NOI18N
+        }
+
+        private void findErrorOrWarning(String line, String keyword, boolean error, String interestingFileName, ErrorBag errorBag) {
+            // the example of the message:
+            // var/tmp/file_158662.cc:10: error: `qwe' undeclared (first use this function)
+            int pos = line.indexOf(keyword);
+            if( pos > 0 ) {
+                int beforeErrPos = pos;
+                int afterErrPos = pos + keyword.length();
+                int fileEndPos = line.indexOf(':');
+                if( fileEndPos > 0 ) {
+                    String fileName = line.substring(0, fileEndPos);
+                    if( fileName.equals(interestingFileName)) {
+                        String strPosition = line.substring(fileEndPos+1, beforeErrPos);
+                        int lineNum;
+                        int colNum = -1;
+                        int colonPos = strPosition.indexOf(':');
+                        if( colonPos < 0 ) {
+                            lineNum = Integer.parseInt(strPosition);
+                        }
+                        else {
+                            lineNum = Integer.parseInt(strPosition.substring(0, colonPos));
+                            colNum = Integer.parseInt(strPosition.substring(colonPos+1));
+                        }
+                        String message = line.substring(afterErrPos);
+                        if( DebugUtils.TRACE ) System.err.printf("\t\tFILE: %s LINE: %8d COL: %d MESSAGE: %s\n", fileName, lineNum, colNum, message);
+                        errorBag.add(message, error, lineNum, colNum);
+                    }
+                }
+            }
+        }
+        
+    }
+    
+
+    private static class SunParser extends BaseParser {
+
+        protected void parseCompilerOutputLine(String line, String interestingFileName, ErrorBag errorBag) {
+            if( DebugUtils.TRACE ) System.err.printf("\tPARSING: \t%s\n", line);
+            findErrorOrWarning(line, ": Error: ", true, interestingFileName, errorBag); // NOI18N
+            findErrorOrWarning(line, ": Warning: ", false, interestingFileName, errorBag); // NOI18N
+        }
+
+        private void findErrorOrWarning(String line, String keyword, boolean error, String interestingFileName, ErrorBag errorBag) {
+            // the example of the message
+            // "/var/tmp/file_158661.cc", line 8: Error: qwe is not defined.
+            
+            int keywordPos = line.indexOf(keyword);
+            if( keywordPos <= 0 ) return;
+            
+            String message = line.substring(keywordPos + keyword.length());
+            
+            if (! line.startsWith("\"")) return;
+            
+            int fileEndPos = 1; // right after '"'
+            boolean backSlash = false;
+            while (fileEndPos < line.length()) {
+                char c = line.charAt(fileEndPos);
+                if (c == '"' && ! backSlash) {
+                    break;
+                }
+                backSlash = (c == '\\');
+                fileEndPos++;
+            }
+            // fileEndPos now points trailinng '"'
+            
+            String fileName = line.substring(1, fileEndPos);
+            if (!fileName.equals(interestingFileName)) return;
+            
+            int lineStartPos = line.indexOf("line", fileEndPos);
+            if (lineStartPos < 0) return;
+            lineStartPos += "line".length()+1; // "line" length+1
+            
+            int lineEndPos = line.indexOf(":", lineStartPos);
+            if (lineEndPos < 0) return;
+            
+            String strPosition = line.substring(lineStartPos, lineEndPos).trim();
+            int lineNum = Integer.parseInt(strPosition);
+            int colNum = -1;
+
+            if( DebugUtils.TRACE ) System.err.printf("\t\tFILE: %s LINE: %8d COL: %d MESSAGE: %s\n", fileName, lineNum, colNum, message);
+            errorBag.add(message, error, lineNum, colNum);
+        }
+        
+    }
+    
     /**
      * ErrorProvider implementation.
      * It is supposed to be called in a separate thread
@@ -81,8 +240,8 @@ class ErrorProviderImpl extends ErrorProvider {
 	    }
 	}
 	
-        String compiler = getCompiler(dao);
-        if( compiler != null ) {
+        CompilerInfo compilerInfo = getCompilerInfo(dao);
+        if( compilerInfo != null && compilerInfo.getPath() != null &&  compilerInfo.getPath().length() > 0 ) {
 	    
             ErrorBag result = new ErrorBag();
             FileObject fo = dao.getPrimaryFile();
@@ -104,12 +263,12 @@ class ErrorProviderImpl extends ErrorProvider {
             writer.close();
 	    
             // TODO: set correct options
-            String command = compiler + " -c -o /dev/null -I . " + getOptions(dao) + ' ' + tmpFile.getAbsolutePath(); // NOI18N
+            String command = compilerInfo.getPath() + " -c -o /dev/null -I . " + getOptions(dao) + ' ' + tmpFile.getAbsolutePath(); // NOI18N
             if( DebugUtils.SLEEP_ON_PARSE ) DebugUtils.sleep(3000);
             if( DebugUtils.TRACE ) System.err.printf("\n\nRUNNING %s\n", command);
             Process compilerProcess = Runtime.getRuntime().exec(command, null, FileUtil.toFile(fo.getParent()));
             InputStream stream = compilerProcess.getErrorStream();
-            parseCompilerOutput(stream, tmpFile.getAbsolutePath(), result);
+            compilerInfo.getParser().parseCompilerOutput(stream, tmpFile.getAbsolutePath(), result);
 //	    result = merge(result);
             stream.close();
             if( DebugUtils.CLEAN_TMP ) {
@@ -121,35 +280,6 @@ class ErrorProviderImpl extends ErrorProvider {
         return Collections.emptyList();
     }
 
-    private static class ErrorBag {
-	
-	Map<Integer, ErrorInfoImpl> errors = new HashMap<Integer, ErrorInfoImpl>();
-	Map<Integer, ErrorInfoImpl> warnings = new HashMap<Integer, ErrorInfoImpl>();
-	
-	public void add(String message, boolean error, int line, int column) {
-	    ErrorInfoImpl info = new ErrorInfoImpl(message, error, line, column);
-	    Map<Integer, ErrorInfoImpl> map = error ? errors : warnings;
-	    ErrorInfoImpl existent = map.get(line);
-	    if( existent == null ) {
-		map.put(line, info);
-	    }
-	    else {
-		existent.adsorb(info);
-	    }
-	}
-	
-	public Collection<ErrorInfo> getResult() {
-	    Collection<ErrorInfo> result = new ArrayList<ErrorInfo>(errors.size() + warnings.size());
-	    for( ErrorInfo info : errors.values() ) {
-		result.add(info);
-	    }
-	    for( ErrorInfo info : warnings.values() ) {
-		result.add(info);
-	    }
-	    return result;
-	}
-    }
-    
     private String getOptions(DataObject dao) {
         // FIXUP: a temporary varyant that allows to get *something*
         // TODO: think over, what if there are several items?
@@ -172,51 +302,40 @@ class ErrorProviderImpl extends ErrorProvider {
         return sb.toString();
     }
 
-    // FIXUP: a temporary implementation, just to try how it looks like
-    private void parseCompilerOutput(InputStream stream, String interestingFileName, ErrorBag errorBag) throws IOException {
-        BufferedReader reader = new BufferedReader(new InputStreamReader(stream));
-        for( String line = reader.readLine(); line != null; line = reader.readLine() ) {
-            parseCompilerOutputLine(line, interestingFileName, errorBag);
-        }
-    }
-    
-    private void parseCompilerOutputLine(String line, String interestingFileName, ErrorBag errorBag) {
-        if( DebugUtils.TRACE ) System.err.printf("\tPARSING: \t%s\n", line);
-        findErrorOrWarning(line, ": error: ", true, interestingFileName, errorBag); // NOI18N
-        findErrorOrWarning(line, ": warning: ", false, interestingFileName, errorBag); // NOI18N
-    }
-    
-    private void findErrorOrWarning(String line, String keyword, boolean error, String interestingFileName, ErrorBag errorBag) {
-        int pos = line.indexOf(keyword);
-        if( pos > 0 ) {
-            int beforeErrPos = pos;
-            int afterErrPos = pos + keyword.length();
-            int fileEndPos = line.indexOf(':');
-            if( fileEndPos > 0 ) {
-                String fileName = line.substring(0, fileEndPos);
-                if( fileName.equals(interestingFileName)) {
-                    String strPosition = line.substring(fileEndPos+1, beforeErrPos);
-		    int lineNum;
-		    int colNum = -1;
-		    int colonPos = strPosition.indexOf(':');
-		    if( colonPos < 0 ) {
-                        lineNum = Integer.parseInt(strPosition);
-		    }
-		    else {
-			lineNum = Integer.parseInt(strPosition.substring(0, colonPos));
-			colNum = Integer.parseInt(strPosition.substring(colonPos+1));
-		    }
-                    String message = line.substring(afterErrPos);
-                    if( DebugUtils.TRACE ) System.err.printf("\t\tFILE: %s LINE: %8d COL: %d MESSAGE: %s\n", fileName, lineNum, colNum, message);
-		    errorBag.add(message, error, lineNum, colNum);
-                }
-            }
-        }
-    }
 
     // FIXUP: a temporary implementation
-    private String getCompiler(DataObject dao) {
-        String baseName = dao.getPrimaryFile().getMIMEType().endsWith("/x-c++") ? "g++" : "gcc"; // NOI18N
+    private CompilerInfo getCompilerInfo(DataObject dao) {
+        NativeFileItem item = getNativeFileItem(dao);
+        if( isSun(dao, item) ) {
+            String baseName = isCpp(dao, item) ? "CC" : "cc"; // NOI18N
+            String path = findInPath(baseName);
+            return new CompilerInfo(path, new SunParser());
+        }
+        else {
+            String baseName = isCpp(dao, item) ? "g++" : "gcc"; // NOI18N
+            String path = findInPath(baseName);
+            return new CompilerInfo(path, new GnuParser());
+        }
+        
+    }
+    
+    private boolean isCpp(DataObject dao, NativeFileItem item) {
+        return dao.getPrimaryFile().getMIMEType().endsWith("/x-c++");
+    }
+    
+    private boolean isSun(DataObject dao, NativeFileItem item) {
+        for (String macro : item.getSystemMacroDefinitions()) {
+            if (macro.startsWith("__SUNPRO_C")) {
+                return true;
+            }
+            else if (macro.startsWith("__SUNPRO_CC")) {
+                return true;
+            }
+        }
+        return false;
+    }
+    
+    private String findInPath(String baseName) {
         String path = System.getenv("PATH"); // NOI18N
         for (StringTokenizer tokenizer = new StringTokenizer(path, File.pathSeparator); tokenizer.hasMoreTokens();) {
             String pathElement = tokenizer.nextToken();
@@ -227,5 +346,9 @@ class ErrorProviderImpl extends ErrorProvider {
         }
         return null;
     }
-    
+
+    private NativeFileItem getNativeFileItem(DataObject dao) {
+        Collection<NativeFileItem> items = dao.getLookup().lookup(NativeFileItemSet.class).getItems();
+        return items.isEmpty() ? null : items.iterator().next();
+    }
 }
