d3a72d0f233faf0bb5f7f8eeab286d6c8a3604a7
Tomas Stupka <tstupka@netbeans.org>
2008-02-06 20:08:01 +0100

    new status tests

--- clearcase/test/unit/src/org/netbeans/modules/clearcase/client/status/ListStatusTest.java
+++ clearcase/test/unit/src/org/netbeans/modules/clearcase/client/status/ListStatusTest.java
@@ -52,6 +52,7 @@ import org.netbeans.modules.clearcase.FileInformation;
 import org.netbeans.modules.clearcase.FileStatusCache;
 import org.netbeans.modules.clearcase.client.ClearcaseCommand;
 import org.netbeans.modules.clearcase.client.test.DummyCleartool;
+import org.openide.filesystems.FileUtil;
 
 /**
  *
@@ -63,14 +64,19 @@ public class ListStatusTest extends TestCase {
     private FileStatusCache cache;
     
     public ListStatusTest(String testName) {
-        super(testName);
+        super(testName);        
     }            
 
     @Override
     protected void setUp() throws Exception {
         super.setUp();        
+        
         cache = Clearcase.getInstance().getFileStatusCache();
-        createFileInformation = cache.getClass().getMethod("createFileInformation", FileEntry.class);
+        createFileInformation = cache.getClass().getDeclaredMethod("createFileInformation", FileEntry.class);        
+        createFileInformation.setAccessible(true);
+        
+        Method m = cache.getClass().getDeclaredMethod("isIgnored", File.class);        
+        m.setAccessible(true);        
     }
 
     @Override
@@ -80,66 +86,131 @@ public class ListStatusTest extends TestCase {
 
     public void testUptodate() throws IOException, ClearcaseException, IllegalAccessException, IllegalArgumentException, InvocationTargetException {
         String rawOutput = "version                Main.java@@/main/1                     Rule: element * /main/LATEST";
-
         List<FileEntry> entryList = execList(rawOutput);        
-        assertEquals(1, entryList.size());
-        FileEntry entry = entryList.get(0);
-        assertListOutput(entry, null, new File("Main.java"), "/main", 1L, "/main/1", null, -1, null, false, "version");                
         
+        assertEquals(1, entryList.size());        
+        assertListOutput(entryList.get(0), null, new File("Main.java"), "/main", 1L, "/main/1", null, -1, null, false, "version");                
+        
+        FileEntry entry = convert(entryList.get(0));    
         FileInformation info = createFileInformation(entry);
+        assertEquals(FileInformation.STATUS_VERSIONED_UPTODATE, info.getStatus());
     }
         
-    public void testViewPrivate() throws IOException, ClearcaseException {
+    public void testViewPrivate() throws IOException, ClearcaseException, IllegalAccessException, IllegalArgumentException, InvocationTargetException { 
         String rawOutput = "view private object    file0";
         List<FileEntry> entryList = execList(rawOutput);
         
-        assertEquals(1, entryList.size());
-        assertListOutput(entryList.get(0), null, new File("file0"), null, -1, null, null, -1, null, false, "view private object");        
-    }
-
-    public void testCheckedout() throws IOException, ClearcaseException {
-        String rawOutput = "version                README@@/main/CHECKEDOUT from /main/3  Rule: element * CHECKEDOUT";
-        List<FileEntry> entryList = execList(rawOutput);
-        
         assertEquals(1, entryList.size());        
-        assertListOutput(entryList.get(0), null, new File("README"), "/main", FileVersionSelector.CHECKEDOUT_VERSION, "/main/CHECKEDOUT", "/main", 3L, "/main/3", true, "version");                
+        assertListOutput(entryList.get(0), null, new File("file0"), null, -1, null, null, -1, null, false, "view private object");        
+        
+        FileEntry entry = convert(entryList.get(0));        
+        FileInformation info = createFileInformation(entry);
+        assertEquals(info.getStatus(), FileInformation.STATUS_NOTVERSIONED_NEWLOCALLY);        
     }
 
-    public void testCheckedoutButRemoved() throws IOException, ClearcaseException {
-        String[] rawOutput = new String[] {"version                README@@/main/CHECKEDOUT from /main/1 [checkedout but removed]",
-                                           "version                test1@@/main/CHECKEDOUT from /main/2 [not loaded, checkedout but removed]"};
-        List<FileEntry> entryList = execList(rawOutput);
+    public void testCheckedout() throws IOException, ClearcaseException, IllegalAccessException, IllegalArgumentException, InvocationTargetException { 
+        String lsRawOutput = "version                README@@/main/CHECKEDOUT from /main/3  Rule: element * CHECKEDOUT";
+        String lscoRawOutput = "README<~=~>cctomas<~=~>reserved";
+        List<FileEntry> entryList = execList(lsRawOutput, lscoRawOutput);
         
-        assertEquals(2, entryList.size());        
+        assertEquals(1, entryList.size());                
+        assertListOutput(entryList.get(0), null, new File("README"), "/main", FileVersionSelector.CHECKEDOUT_VERSION, "/main/CHECKEDOUT", "/main", 3L, "/main/3", true, "version");                
+        
+        FileEntry entry = convert(entryList.get(0));  
+        FileInformation info = createFileInformation(entry);
+        assertEquals(FileInformation.STATUS_VERSIONED_CHECKEDOUT, info.getStatus());                
+        assertEquals("Reserved", info.getShortStatusText());
+        assertEquals("Reserved", info.getStatusText());
+    }
+
+    public void testCheckedoutUnreserved() throws IOException, ClearcaseException, IllegalAccessException, IllegalArgumentException, InvocationTargetException { 
+        String lsRawOutput = "version                README@@/main/CHECKEDOUT from /main/1  Rule: element * CHECKEDOUT";
+        String lscoRawOutput = "README<~=~>cctomas<~=~>unreserved";
+
+        List<FileEntry> entryList = execList(lsRawOutput, lscoRawOutput);
+        
+        assertEquals(1, entryList.size());                
+        assertListOutput(entryList.get(0), null, new File("README"), "/main", FileVersionSelector.CHECKEDOUT_VERSION, "/main/CHECKEDOUT", "/main", 1L, "/main/1", true, "version");                
+        
+        FileEntry entry = convert(entryList.get(0));  
+        FileInformation info = createFileInformation(entry);
+        assertEquals(FileInformation.STATUS_VERSIONED_CHECKEDOUT | FileInformation.STATUS_UNRESERVED, info.getStatus());                
+        assertEquals("Unreserved", info.getShortStatusText());        
+        assertEquals("Unreserved", info.getStatusText());        
+    }
+    
+    public void testCheckedoutButRemoved() throws IOException, ClearcaseException, IllegalAccessException, IllegalArgumentException, InvocationTargetException {
+        String[] lsRawOutput = new String[] {"version                README@@/main/CHECKEDOUT from /main/1 [checkedout but removed]",
+                                             "version                test1@@/main/CHECKEDOUT from /main/2 [not loaded, checkedout but removed]"};        
+        String[] lscoRawOutput = new String[] {"README<~=~>cctomas<~=~>reserved",
+                                               "test1<~=~>cctomas<~=~>reserved"};
+        
+        List<FileEntry> entryList = execList(lsRawOutput, lscoRawOutput);
+        
+        assertEquals(2, entryList.size());                 
         assertListOutput(entryList.get(0), "[checkedout but removed]", new File("README"), "/main", FileVersionSelector.CHECKEDOUT_VERSION, "/main/CHECKEDOUT", "/main", 1L, "/main/1", true, "version");                
         assertListOutput(entryList.get(1), "[not loaded, checkedout but removed]", new File("test1"), "/main", FileVersionSelector.CHECKEDOUT_VERSION, "/main/CHECKEDOUT", "/main", 2L, "/main/2", true, "version");                        
-    }
         
-    public void testLoadedButMissing() throws IOException, ClearcaseException {
+        FileEntry entry = convert(entryList.get(0));                    
+        FileInformation info = createFileInformation(entry);
+        assertEquals(info.getStatus(), FileInformation.STATUS_VERSIONED_CHECKEDOUT_BUT_REMOVED);                        
+        
+        entry = convert(entryList.get(1));
+        info = createFileInformation(entry);
+        assertEquals(info.getStatus(), FileInformation.STATUS_VERSIONED_CHECKEDOUT_BUT_REMOVED);                        
+    }
+
+    public void testCheckedoutButRemovedUnreserved() throws IOException, ClearcaseException, IllegalAccessException, IllegalArgumentException, InvocationTargetException {
+        String[] lsRawOutput = new String[] {"version                README@@/main/CHECKEDOUT from /main/1 [checkedout but removed]"};        
+        String[] lscoRawOutput = new String[] {"README<~=~>cctomas<~=~>unreserved"};
+        
+        List<FileEntry> entryList = execList(lsRawOutput, lscoRawOutput);
+        
+        assertEquals(1, entryList.size());                 
+        assertListOutput(entryList.get(0), "[checkedout but removed]", new File("README"), "/main", FileVersionSelector.CHECKEDOUT_VERSION, "/main/CHECKEDOUT", "/main", 1L, "/main/1", true, "version");                        
+        
+        FileEntry entry = convert(entryList.get(0));                    
+        FileInformation info = createFileInformation(entry);
+        assertEquals(info.getStatus(), FileInformation.STATUS_VERSIONED_CHECKEDOUT_BUT_REMOVED | FileInformation.STATUS_UNRESERVED);                                
+    }
+    
+    public void testLoadedButMissing() throws IOException, ClearcaseException, IllegalAccessException, IllegalArgumentException, InvocationTargetException { 
         String rawOutput = "version                test1@@/main/2 [loaded but missing]    Rule: element * /main/LATEST";
         List<FileEntry> entryList = execList(rawOutput);
         
-        assertEquals(1, entryList.size());        
+        assertEquals(1, entryList.size());                 
         assertListOutput(entryList.get(0), "[loaded but missing]", new File("test1"), "/main", 2L, "/main/2", null, -1, null, false, "version");                
+        
+        FileEntry entry = convert(entryList.get(0));
+        FileInformation info = createFileInformation(entry);
+        assertEquals(info.getStatus(), FileInformation.STATUS_VERSIONED_LOADED_BUT_MISSING);        
     }        
 
-    public void testHijacked() throws IOException, ClearcaseException {
+    public void testHijacked() throws IOException, ClearcaseException , IllegalAccessException, IllegalArgumentException, InvocationTargetException {
         String rawOutput = "version                test1@@/main/2 [hijacked]              Rule: element * /main/LATEST";
         List<FileEntry> entryList = execList(rawOutput);
         
-        assertEquals(1, entryList.size());        
+        assertEquals(1, entryList.size());                   
         assertListOutput(entryList.get(0), "[hijacked]", new File("test1"), "/main", 2L, "/main/2", null, -1, null, false, "version");                
+        
+        FileEntry entry = convert(entryList.get(0));       
+        FileInformation info = createFileInformation(entry);
+        assertEquals(info.getStatus(), FileInformation.STATUS_VERSIONED_HIJACKED);        
     }        
 
-    public void testEclipsed() throws IOException, ClearcaseException {
+    public void testEclipsed() throws IOException, ClearcaseException, IllegalAccessException, IllegalArgumentException, InvocationTargetException {
         String[] rawOutput = new String[] {            
             "file element           Makefile@@ [eclipsed]",
             "view private object    Makefile"
         };
         List<FileEntry> entryList = execList(rawOutput);
         
-        assertEquals(1, entryList.size());                
+        assertEquals(1, entryList.size()); 
         assertListOutput(entryList.get(0), "[eclipsed]", new File("Makefile"), null, -1, null, null, -1, null, false, "file element");                        
+        
+        FileEntry entry = convert(entryList.get(0));       
+        FileInformation info = createFileInformation(entry);
+        assertNotSame(info.getStatus(), FileInformation.STATUS_NOTVERSIONED_ECLIPSED);
         
         rawOutput = new String[] {                        
             "view private object    Makefile",
@@ -148,12 +219,11 @@ public class ListStatusTest extends TestCase {
         entryList = execList(rawOutput);
         
         assertEquals(1, entryList.size());                
-        assertListOutput(entryList.get(0), "[eclipsed]", new File("Makefile"), null, -1, null, null, -1, null, false, "file element");                        
-        
+        assertListOutput(entryList.get(0), "[eclipsed]", new File("Makefile"), null, -1, null, null, -1, null, false, "file element");                                
     }        
     
     public void testCrap() throws IOException, ClearcaseException {
-        List<FileEntry> entryList = execList(null);
+        List<FileEntry> entryList = execList(null, new String[]{});
         assertEquals(entryList.size(), 0);       
         
         String[] rawOutput = new String[] {
@@ -203,10 +273,22 @@ public class ListStatusTest extends TestCase {
         }                                
         assertEquals(type, fe.getType());
     }
-    
-    public static List<FileEntry> execList(String ...rawOutput) throws IOException, ClearcaseException {
 
-        ListCommandExecutor executor = new ListCommandExecutor(rawOutput);
+    public static List<FileEntry> execList(String lsRawOutput) throws IOException, ClearcaseException {
+        return execList(new String[] { lsRawOutput }, new String[] {});
+    }
+    
+    public static List<FileEntry> execList(String[] lsRawOutput) throws IOException, ClearcaseException {
+        return execList(lsRawOutput, new String[] {});
+    }
+
+    public static List<FileEntry> execList(String lsRawOutput, String lscoRawOutput) throws IOException, ClearcaseException {
+        return execList(new String[] { lsRawOutput }, new String[] { lscoRawOutput });
+    }
+    
+    public static List<FileEntry> execList(String[] lsRawOutput, String[] lscoRawOutput) throws IOException, ClearcaseException {
+
+        ListCommandExecutor executor = new ListCommandExecutor(lsRawOutput, lscoRawOutput);
         DummyCleartool ct = new DummyCleartool(executor);
 
         ListStatus lf = new ListStatus(new File(""), false);
@@ -217,26 +299,39 @@ public class ListStatusTest extends TestCase {
         List<FileEntry> entryList = new  ArrayList<FileEntry>(lf.getOutput());
         return entryList;
     }
-
+    
     private static class ListCommandExecutor implements DummyCleartool.CommandExecutor {
-        final private String[] output;
+        final private String[] lsRawOutput;
+        final private String[] lscoRawOutput;
 
-        public ListCommandExecutor(String ...output) {
-            this.output = output;
+        public ListCommandExecutor(String[] lsRawOutput, String[] lscoRawOutput) {
+            this.lsRawOutput = lsRawOutput;
+            this.lscoRawOutput = lscoRawOutput;
         }
         
         public void exec(ClearcaseCommand command) {
-            if(output == null) {
+            if(lsRawOutput == null) {
                 command.outputText(null);
                 return;
             }
-            for (String string : output) {
-                command.outputText(string);                        
+            if(command.getClass().getName().indexOf("ListCommand") > -1) {
+                for (String string : lsRawOutput) {
+                    command.outputText(string);                        
+                }    
+            } else if(command.getClass().getName().indexOf("LSCOCommand") > -1) {
+                for (String string : lscoRawOutput) {
+                    command.outputText(string);                        
+                }                    
             }
+            
         }        
     }    
     
     private FileInformation createFileInformation(FileEntry entry) throws IllegalAccessException, IllegalArgumentException, InvocationTargetException {
         return (FileInformation) createFileInformation.invoke(cache, new Object[] {entry});
+    }
+    
+    private FileEntry convert(FileEntry entry) {
+        return new FileEntry(entry.getType(), FileUtil.normalizeFile(entry.getFile()), entry.getOriginVersion(), entry.getVersion(), entry.getAnnotation(), entry.isReserved(), "");
     }
 }
