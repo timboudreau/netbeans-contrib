988ec7b1fe2e860fc2683cd002248165c8e7948f
igor_nikiforov@netbeans.org <igor_nikiforov@netbeans.org>
2008-03-03 15:40:44 +0300

    Linux native packages support

--- ssinstaller/engine/src/org/netbeans/installer/utils/nativepackages/FakeNativePackageInstaller.java
+++ ssinstaller/engine/src/org/netbeans/installer/utils/nativepackages/FakeNativePackageInstaller.java
@@ -19,31 +19,35 @@ import org.netbeans.installer.utils.helper.FileEntry;
 public class FakeNativePackageInstaller implements NativePackageInstaller {
 
 //    public static String FAKE_PACKAGES_DIR = System.getProperty("user.home") + File.separator + "fake_packages";
-    public static String FAKE_PACKAGES_DIR = "/tmp/installed_fake_packages";
-
+    public static final String FAKE_PACKAGES_DIR = "/tmp/installed_fake_packages/";
+    public static final String FAKE_PACKAGES_COUNTER = "fake_packages_counter";
+    public static final String FAKE_PACKAGE = "fake_package.";
+    
     public boolean install(String pathToPackage, Product product) {
         try {
             LogManager.log("executing command: touch " + FAKE_PACKAGES_DIR + extractFileName(pathToPackage));
-            Process p = new ProcessBuilder("touch", FAKE_PACKAGES_DIR + extractFileName(pathToPackage)).start();
-            return p.waitFor() == 0;
+            int counter = parseInteger(product.getProperty(FAKE_PACKAGES_COUNTER)) + 1;            
+            String packageName = extractFileName(pathToPackage);
+            Process p = new ProcessBuilder("touch", FAKE_PACKAGES_DIR + packageName).start();
+            if (p.waitFor() != 0) return false;
+            product.setProperty(FAKE_PACKAGE + String.valueOf(counter), packageName);        
+            product.setProperty(FAKE_PACKAGES_COUNTER, String.valueOf(counter));                    
         } catch (InterruptedException ex) {
             Logger.getLogger(FakeNativePackageInstaller.class.getName()).log(Level.SEVERE, null, ex);
         } catch (IOException ex) {
             Logger.getLogger(FakeNativePackageInstaller.class.getName()).log(Level.SEVERE, null, ex);
         }
-        return false;
+        return true;
     }
 
     public boolean uninstall(Product product) {
-        if (product == null) return true;
         try {
-            for (FileEntry installedFile : product.getInstalledFiles()) {
-                if (!installedFile.isDirectory()) {
-                    LogManager.log("executing command: rm " + FAKE_PACKAGES_DIR + extractFileName(installedFile.getName()));
-                    Process p = new ProcessBuilder("rm", FAKE_PACKAGES_DIR + extractFileName(installedFile.getName())).start();
-                    LogManager.logExit("Finish uninstalling fake native package");
-                    if (p.waitFor() != 0) return false;
-                }
+            int counter = parseInteger(product.getProperty(FAKE_PACKAGES_COUNTER));            
+            for(int i=1; i<=counter; i++) {
+                String packageName = product.getProperty(FAKE_PACKAGE + String.valueOf(i));
+                LogManager.log("executing command: rm " + FAKE_PACKAGES_DIR + packageName);
+                Process p = new ProcessBuilder("rm", FAKE_PACKAGES_DIR + packageName).start();
+                if (p.waitFor() != 0) return false;
             }
         } catch (InterruptedException ex) {
             Logger.getLogger(FakeNativePackageInstaller.class.getName()).log(Level.SEVERE, null, ex);
@@ -53,11 +57,16 @@ public class FakeNativePackageInstaller implements NativePackageInstaller {
         return true;
     }
 
+    private int parseInteger(String value) {
+        return (value == null || value.length() == 0)? 0: Integer.parseInt(value);
+    }        
+    
     private String extractFileName(String pathToPackage) {
-        return pathToPackage.substring(pathToPackage.lastIndexOf(File.separatorChar));
+        return pathToPackage.substring(pathToPackage.lastIndexOf(File.separatorChar) + 1);
     }
 
     public boolean isCorrectPackageFile(String pathToPackage) {
-        return true;
+        return !pathToPackage.contains("LICENSE") && !(new File(pathToPackage).isDirectory());
     }
+
 }
