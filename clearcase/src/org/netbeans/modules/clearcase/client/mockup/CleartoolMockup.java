994400531ce3c2190dccd3233f7f1b6fbfe734e8
Tomas Stupka <tstupka@netbeans.org>
2008-03-11 18:40:04 +0100

    adding lstype command

--- clearcase/src/org/netbeans/modules/clearcase/client/mockup/CleartoolMockup.java
+++ clearcase/src/org/netbeans/modules/clearcase/client/mockup/CleartoolMockup.java
@@ -189,6 +189,8 @@ public class CleartoolMockup extends Process implements Runnable {
              processMV(args);            
         } else if(ctCommand.equals("annotate")) {
              processANNOTATE(args);            
+        } else if(ctCommand.equals("lstype")) {
+             processLSTYPE(args);            
         } else if(ctCommand.equals("lsvtree") ||
                 ctCommand.equals("describe")  ||
                 ctCommand.equals("merge")     ||
@@ -449,6 +451,21 @@ public class CleartoolMockup extends Process implements Runnable {
         return sb;
     }
 
+    private void processLSTYPE(String[] args) {
+        String kind = null;
+        for (int i = 1; i < args.length; i++) {
+            String arg = args[i];
+            if(arg.equals("-short")) {              
+                // ignore
+            } else if(arg.equals("-kind")) {
+                kind = args[++i];
+            } 
+        }
+        if(kind != null && kind.equals("lbtype")) {
+            inputStream.setDelegate(new ByteArrayInputStream("BORING\nSAD\nLIFE\n".getBytes()));            
+        }
+    }
+
     private void processMV(String[] args) {
         
         List<File> files = new ArrayList<File>();
@@ -620,7 +637,7 @@ public class CleartoolMockup extends Process implements Runnable {
             String line = null;
             StringBuffer sb = new StringBuffer();
             while((line = br.readLine()) != null) {
-                sb.append("####  2008-04-01 vajcak    ");
+                sb.append("####  2008-04-01 Arnold    ");
                 sb.append(File.separator);
                 sb.append("main");
                 sb.append(File.separator);
