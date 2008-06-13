444049ad665d7d5414fd04c66853a3a396ca3f8d
mesnik@netbeans.org <mesnik@netbeans.org>
2008-06-13 15:57:50 +0400

    The istallation and uninstallation process was reorganized.

--- ssinstaller/components/products/nb-base/src/org/netbeans/installer/products/nb/base/ConfigurationLogic.java
+++ ssinstaller/components/products/nb-base/src/org/netbeans/installer/products/nb/base/ConfigurationLogic.java
@@ -39,10 +39,12 @@ package org.netbeans.installer.products.nb.base;
 import java.io.File;
 import java.io.IOException;
 import java.util.List;
-import org.netbeans.installer.product.Registry;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import org.netbeans.installer.product.components.ProductConfigurationLogic;
 import org.netbeans.installer.product.components.Product;
 import org.netbeans.installer.utils.FileProxy;
+import org.netbeans.installer.utils.FileUtils;
 import org.netbeans.installer.utils.LogManager;
 import org.netbeans.installer.utils.SystemUtils;
 import org.netbeans.installer.utils.applications.JavaUtils;
@@ -53,7 +55,6 @@ import org.netbeans.installer.utils.exceptions.InstallationException;
 import org.netbeans.installer.utils.exceptions.NativeException;
 import org.netbeans.installer.utils.exceptions.UninstallationException;
 import org.netbeans.installer.utils.helper.FilesList;
-import org.netbeans.installer.utils.helper.Status;
 import org.netbeans.installer.utils.helper.Text;
 import org.netbeans.installer.utils.progress.Progress;
 import org.netbeans.installer.utils.system.shortcut.FileShortcut;
@@ -262,90 +263,20 @@ public class ConfigurationLogic extends ProductConfigurationLogic {
         
         /////////////////////////////////////////////////////////////////////////////
         try {
-            progress.setDetail(getString("CL.install.netbeans.conf")); // NOI18N
-            
-            NetBeansUtils.updateNetBeansHome(installLocation);
-            
-            // final long xmx = NetBeansUtils.getJvmMemorySize(
-            //         installLocation,
-            //         NetBeansUtils.MEMORY_XMX);
-            // if (xmx < REQUIRED_XMX_VALUE) {
-            //     NetBeansUtils.setJvmMemorySize(
-            //            installLocation,
-            //             NetBeansUtils.MEMORY_XMX,
-            //            REQUIRED_XMX_VALUE);
-            // }
+            progress.setDetail(getString("CL.install.netbeans.conf")); // NOI18N            
+            NetBeansUtils.updateNetBeansHome(installLocation);   
         } catch (IOException e) {
             throw new InstallationException(
                     getString("CL.install.error.netbeans.conf"), // NOI18N
                     e);
-        }
-        
-        /////////////////////////////////////////////////////////////////////////////
-        try {
-            progress.setDetail(getString("CL.install.glassfish.integration")); // NOI18N
-            
-            final List<Product> glassfishes =
-                    Registry.getInstance().getProducts("glassfish");
-            for (Product glassfish: glassfishes) {
-                if (glassfish.getStatus() == Status.INSTALLED) {
-                    final File gfLocation = glassfish.getInstallationLocation();
-                    
-                    if (gfLocation != null) {
-                        NetBeansUtils.setJvmOption(
-                                installLocation,
-                                GLASSFISH_JVM_OPTION_NAME,
-                                gfLocation.getAbsolutePath(),
-                                true);
-                        break;
-                    }
-                }
-            }
-        } catch (IOException e) {
-            throw new InstallationException(
-                    getString("CL.install.error.glassfish.integration"), // NOI18N
-                    e);
-        }
-        
-        /////////////////////////////////////////////////////////////////////////////
-        try {
-            progress.setDetail(getString("CL.install.tomcat.integration")); // NOI18N
-            
-            final List<Product> tomcats =
-                    Registry.getInstance().getProducts("tomcat");
-            for (Product tomcat: tomcats) {
-                if (tomcat.getStatus() == Status.INSTALLED) {
-                    final File tcLocation = tomcat.getInstallationLocation();
-                    
-                    if (tcLocation != null) {
-                        NetBeansUtils.setJvmOption(
-                                installLocation,
-                                TOMCAT_JVM_OPTION_NAME_HOME,
-                                tcLocation.getAbsolutePath(),
-                                true);
-                        NetBeansUtils.setJvmOption(
-                                installLocation,
-                                TOMCAT_JVM_OPTION_NAME_TOKEN,
-                                "" + System.currentTimeMillis(),
-                                true);
-                        break;
-                    }
-                }
-            }
-        } catch (IOException e) {
-            throw new InstallationException(
-                    getString("CL.install.error.tomcat.integration"), // NOI18N
-                    e);
-        }
-        
-        /////////////////////////////////////////////////////////////////////////////
+        }            
         progress.setPercentage(Progress.COMPLETE);
     }
     
     public void uninstall(final Progress progress) throws UninstallationException {
         final Product product = getProduct();
         final File installLocation = product.getInstallationLocation();
-        
+        LogManager.log("uninstalling NB from " + installLocation.getAbsolutePath());
         NetBeansUtils.warnNetbeansRunning(installLocation);
         /////////////////////////////////////////////////////////////////////////////
         try {
@@ -373,32 +304,35 @@ public class ConfigurationLogic extends ProductConfigurationLogic {
         }
         
         /////////////////////////////////////////////////////////////////////////////
-        if (!SystemUtils.isMacOS()) {
-            try {
-                progress.setDetail(getString("CL.uninstall.desktop")); // NOI18N
-                
-                final String shortcutLocation = getProduct().getProperty(
-                        DESKTOP_SHORTCUT_LOCATION_PROPERTY);
-                
-                if ((shortcutLocation == null) ||
-                        shortcutLocation.equals(CURRENT_USER_PROPERTY_VALUE)) {
-                    SystemUtils.removeShortcut(
-                            getDesktopShortcut(installLocation),
-                            LocationType.CURRENT_USER_DESKTOP,
-                            false);
-                } else {
-                    SystemUtils.removeShortcut(
-                            getDesktopShortcut(installLocation),
-                            LocationType.ALL_USERS_DESKTOP,
-                            false);
-                }
-            } catch (NativeException e) {
-                LogManager.log(
-                        getString("CL.uninstall.error.desktop"), // NOI18N
-                        e);
-            }
-        }
         
+        try {
+            progress.setDetail(getString("CL.uninstall.desktop")); // NOI18N
+
+            final String shortcutLocation = getProduct().getProperty(
+                    DESKTOP_SHORTCUT_LOCATION_PROPERTY);
+
+            if ((shortcutLocation == null) ||
+                    shortcutLocation.equals(CURRENT_USER_PROPERTY_VALUE)) {
+                SystemUtils.removeShortcut(
+                        getDesktopShortcut(installLocation),
+                        LocationType.CURRENT_USER_DESKTOP,
+                        false);
+            } else {
+                SystemUtils.removeShortcut(
+                        getDesktopShortcut(installLocation),
+                        LocationType.ALL_USERS_DESKTOP,
+                        false);
+            }
+        } catch (NativeException e) {
+            LogManager.log(
+                    getString("CL.uninstall.error.desktop"), // NOI18N
+                    e);
+        }
+        try {
+            FileUtils.deleteFile(installLocation, true);
+        } catch (IOException ex) {
+            Logger.getLogger(ConfigurationLogic.class.getName()).log(Level.SEVERE, null, ex);
+        }
         /////////////////////////////////////////////////////////////////////////////
         progress.setPercentage(Progress.COMPLETE);
     }
@@ -424,22 +358,12 @@ public class ConfigurationLogic extends ProductConfigurationLogic {
     
     @Override
     public String getExecutable() {
-        if (SystemUtils.isWindows()) {
-            return EXECUTABLE_WINDOWS;
-        } else {
-            return EXECUTABLE_UNIX;
-        }
+        return EXECUTABLE_UNIX;        
     }
     
     @Override
     public String getIcon() {
-        if (SystemUtils.isWindows()) {
-            return ICON_WINDOWS;
-        } else if (SystemUtils.isMacOS()) {
-            return ICON_MACOSX;
-        } else {
-            return ICON_UNIX;
-        }
+        return ICON_UNIX;        
     }
 
     @Override
@@ -478,20 +402,9 @@ public class ConfigurationLogic extends ProductConfigurationLogic {
             final File location) {
         final File icon;
         final File executable;
-        
-        if (SystemUtils.isWindows()) {
-            icon = new File(location, ICON_WINDOWS);
-        } else if (SystemUtils.isMacOS()) {
-            icon = new File(location, ICON_MACOSX);
-        } else {
-            icon = new File(location, ICON_UNIX);
-        }
-        
-        if (SystemUtils.isWindows()) {
-            executable = new File(location, EXECUTABLE_WINDOWS);
-        } else {
-            executable = new File(location, EXECUTABLE_UNIX);
-        }
+               
+        icon = new File(location, ICON_UNIX);
+        executable = new File(location, EXECUTABLE_UNIX);        
         
         final FileShortcut shortcut = new FileShortcut(name, executable);
         
@@ -505,6 +418,13 @@ public class ConfigurationLogic extends ProductConfigurationLogic {
         
         return shortcut;
     }
+
+    @Override
+    public boolean registerInSystem() {
+        return super.registerInSystem();
+    }
+    
+    
     
     
     /////////////////////////////////////////////////////////////////////////////////
