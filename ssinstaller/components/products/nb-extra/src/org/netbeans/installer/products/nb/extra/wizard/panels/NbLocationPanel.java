1aedc3379fb344e1199f2b39f9ac1cf3b48cd53f
mesnik@netbeans.org <mesnik@netbeans.org>
2008-07-03 13:27:58 +0400

    The Netbeans default location was changed.

--- ssinstaller/components/products/nb-extra/src/org/netbeans/installer/products/nb/extra/wizard/panels/NbLocationPanel.java
+++ ssinstaller/components/products/nb-extra/src/org/netbeans/installer/products/nb/extra/wizard/panels/NbLocationPanel.java
@@ -39,11 +39,11 @@ package org.netbeans.installer.products.nb.extra.wizard.panels;
 import java.io.File;
 import java.io.FilenameFilter;
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 import org.netbeans.installer.product.Registry;
 import org.netbeans.installer.product.components.Product;
 import org.netbeans.installer.utils.ResourceUtils;
+import org.netbeans.installer.utils.SystemUtils;
 import org.netbeans.installer.utils.helper.Status;
 import org.netbeans.installer.wizard.components.panels.ApplicationLocationPanel;
 import org.netbeans.installer.wizard.components.panels.sunstudio.PreInstallSummaryPanel;
@@ -59,7 +59,7 @@ public class NbLocationPanel extends ApplicationLocationPanel {
     private List<File> locations;
     private List<String> labels;
 
-    public NbLocationPanel() {        
+    public NbLocationPanel() {            
         setProperty(LOCATION_LABEL_TEXT_PROPERTY, 
                 LOCATION_LABEL_TEXT);        
         setProperty(LIST_LABEL_TEXT_PROPERTY, 
@@ -72,6 +72,7 @@ public class NbLocationPanel extends ApplicationLocationPanel {
         setProperty(DESCRIPTION_PROPERTY,
                 DEFAULT_DESCRIPTION);
         
+      
         locations = new ArrayList();
         labels = new ArrayList<String>();
         fetchLocationsFromRegistry(locations);
@@ -79,13 +80,19 @@ public class NbLocationPanel extends ApplicationLocationPanel {
         
         for(File location : locations) {
             labels.add(readNBDescription(location));
-        }        
-        if (locations.size() > 0 ) {
-            selectedLocation = locations.get(0);
-        } else {
-            selectedLocation = new File(DEFAULT_LOCATION);
-        }
+        }              
     }
+
+    @Override
+    public void initialize() {
+        super.initialize();
+        final Product product = (Product) getWizard().
+                getContext().
+                get(Product.class);
+        selectedLocation = resolvePath(product.getProperty(Product.INSTALLATION_LOCATION_PROPERTY));
+    }
+    
+    
     
     @Override
     public List<File> getLocations() {
@@ -203,7 +210,8 @@ public class NbLocationPanel extends ApplicationLocationPanel {
                 }
             });        
             for (File location : candidates) {
-                if (!locations.contains(location)) {
+                if (!locations.contains(location)
+                        && location.canWrite()) {
                     locations.add(location);
                 }
             }
@@ -240,348 +248,10 @@ public class NbLocationPanel extends ApplicationLocationPanel {
     public static final String LIST_LABEL_TEXT = 
             ResourceUtils.getString(NbLocationPanel.class, 
             "NBP.list.label.text"); // NOI18N
-    public static final String DEFAULT_LOCATION = 
-            ResourceUtils.getString(NbLocationPanel.class, 
-            "NBP.default.location"); // NOI18N
+  
 
       // public static final String ERROR_FAILED_VERIFY_INPUT_TEXT =
         //    ResourceUtils.getString(NbLocationPanel.class,
           //  "NBP.error.failed.input.verify");//NOI18N
 }
 
-/*
-public class NbLocationPanel extends DestinationPanel {
-    /////////////////////////////////////////////////////////////////////////////////
-    // Instance
-    private JdkLocationPanel jdkLocationPanel;
-    
-    public NbLocationPanel() {
-        jdkLocationPanel = new JdkLocationPanel();
-        
-        setProperty(TITLE_PROPERTY,
-                DEFAULT_TITLE);
-        setProperty(DESCRIPTION_PROPERTY,
-                DEFAULT_DESCRIPTION);
-        
-        setProperty(DESTINATION_LABEL_TEXT_PROPERTY,
-                DEFAULT_DESTINATION_LABEL_TEXT);
-        setProperty(DESTINATION_BUTTON_TEXT_PROPERTY,
-                DEFAULT_DESTINATION_BUTTON_TEXT);
-        
-        setProperty(JDK_LOCATION_LABEL_TEXT_PROPERTY,
-                DEFAULT_JDK_LOCATION_LABEL_TEXT);
-        setProperty(BROWSE_BUTTON_TEXT_PROPERTY,
-                DEFAULT_BROWSE_BUTTON_TEXT);
-        
-        setProperty(JdkLocationPanel.MINIMUM_JDK_VERSION_PROPERTY,
-                DEFAULT_MINIMUM_JDK_VERSION);
-        setProperty(JdkLocationPanel.MAXIMUM_JDK_VERSION_PROPERTY,
-                DEFAULT_MAXIMUM_JDK_VERSION);
-    }
-    
-    @Override
-    public WizardUi getWizardUi() {
-        if (wizardUi == null) {
-            wizardUi = new NbBaseDestinationPanelUi(this);
-        }
-        
-        return wizardUi;
-    }
-    
-    @Override
-    public void initialize() {
-        super.initialize();
-        
-        jdkLocationPanel.setWizard(getWizard());
-        
-        jdkLocationPanel.setProperty(
-                JdkLocationPanel.MINIMUM_JDK_VERSION_PROPERTY,
-                getProperty(JdkLocationPanel.MINIMUM_JDK_VERSION_PROPERTY));
-        jdkLocationPanel.setProperty(
-                JdkLocationPanel.MAXIMUM_JDK_VERSION_PROPERTY,
-                getProperty(JdkLocationPanel.MAXIMUM_JDK_VERSION_PROPERTY));
-        
-        if (getProperty(JdkLocationPanel.PREFERRED_JDK_VERSION_PROPERTY) != null) {
-            jdkLocationPanel.setProperty(
-                    JdkLocationPanel.PREFERRED_JDK_VERSION_PROPERTY,
-                    getProperty(JdkLocationPanel.PREFERRED_JDK_VERSION_PROPERTY));
-        }
-        List <Product> toInstall = Registry.getInstance().getProductsToInstall();
-        jdkLocationPanel.setJreAllowed(true);
-        for(Product product : toInstall) {
-            String uid = product.getUid();
-            if(uid.startsWith("nb-") && !uid.matches("nb-(base|cnd|php|ruby)")) {
-                jdkLocationPanel.setJreAllowed(false);
-                break;
-            }
-        }
-        
-        jdkLocationPanel.initialize();
-        
-        //This makes it possible to perform silent installation with emptry state files 
-        //that means that JDK_LOCATION_PROPERTY property is explicitely set to the first location
-        //that fits the requirements
-        //TODO: Investigate the prons&cons and side affects of moving
-        //this code to the end of JdkLocationPanel.initialize() method        
-        File jdkLocation = jdkLocationPanel.getSelectedLocation();        
-        if(jdkLocation!=null && !jdkLocation.getPath().equals(StringUtils.EMPTY_STRING)) {
-            jdkLocationPanel.setLocation(jdkLocation);
-        }        
-    }
-    
-    public JdkLocationPanel getJdkLocationPanel() {
-        return jdkLocationPanel;
-    }
-    
-    /////////////////////////////////////////////////////////////////////////////////
-    // Inner Classes
-    public static class NbBaseDestinationPanelUi extends DestinationPanelUi {
-        protected NbLocationPanel panel;
-        
-        public NbBaseDestinationPanelUi(NbLocationPanel panel) {
-            super(panel);
-            
-            
-            this.panel = panel;
-        }
-        
-        public SwingUi getSwingUi(SwingContainer container) {
-            if (swingUi == null) {
-                swingUi = new NbBaseDestinationPanelSwingUi(panel, container);
-            }
-            
-            return super.getSwingUi(container);
-        }
-    }
-    
-    public static class NbBaseDestinationPanelSwingUi extends DestinationPanelSwingUi {
-        protected NbLocationPanel panel;
-        
-        private NbiLabel jdkLocationLabel;
-        private NbiComboBox jdkLocationComboBox;
-        private NbiButton browseButton;
-        private NbiLabel statusLabel;
-        
-        private NbiTextField jdkLocationField;
-        
-        private NbiDirectoryChooser fileChooser;
-        
-        public NbBaseDestinationPanelSwingUi(
-                final NbLocationPanel panel,
-                final SwingContainer container) {
-            super(panel, container);
-            
-            this.panel = panel;
-            
-            initComponents();
-        }
-        
-        // protected ////////////////////////////////////////////////////////////////
-        @Override
-        protected void initialize() {
-            jdkLocationLabel.setText(
-                    panel.getProperty(JDK_LOCATION_LABEL_TEXT_PROPERTY));
-            
-            final JdkLocationPanel jdkLocationPanel = panel.getJdkLocationPanel();
-            
-            if (jdkLocationPanel.getLocations().size() == 0) {
-                final Version minVersion = Version.getVersion(jdkLocationPanel.getProperty(
-                        JdkLocationPanel.MINIMUM_JDK_VERSION_PROPERTY));
-                final Version maxVersion = Version.getVersion(jdkLocationPanel.getProperty(
-                        JdkLocationPanel.MAXIMUM_JDK_VERSION_PROPERTY));
-                
-                statusLabel.setText(StringUtils.format(
-                        jdkLocationPanel.getProperty(JdkLocationPanel.ERROR_NOTHING_FOUND_PROPERTY),
-                        minVersion.toJdkStyle(),
-                        minVersion.toJdkStyle()));
-            } else {
-                statusLabel.clearText();
-                statusLabel.setVisible(false);
-            }
-            
-            final List<File> jdkLocations = jdkLocationPanel.getLocations();                        
-            final List<String> jdkLabels = jdkLocationPanel.getLabels();
-            
-            final LocationsComboBoxModel model = new LocationsComboBoxModel(
-                    jdkLocations,
-                    jdkLabels);            
-            
-            ((LocationsComboBoxEditor) jdkLocationComboBox.getEditor()).setModel(
-                    model);
-            jdkLocationComboBox.setModel(
-                    model);
-            
-            final File selectedLocation = jdkLocationPanel.getSelectedLocation();
-            final int index = jdkLocations.indexOf(selectedLocation);
-            String selectedItem;
-            if(index != -1) {
-                  selectedItem = jdkLabels.get(index);  
-            } else {
-                  selectedItem = selectedLocation.toString();
-            }  
-            model.setSelectedItem(selectedItem);                        
-            browseButton.setText(
-                    panel.getProperty(BROWSE_BUTTON_TEXT_PROPERTY));
-            
-            super.initialize();
-        }
-        
-        @Override
-        protected void saveInput() {
-            super.saveInput();
-            
-            panel.getJdkLocationPanel().setLocation(
-                    new File(jdkLocationField.getText()));
-        }
-        
-        @Override
-        protected String validateInput() {
-            String errorMessage = super.validateInput();
-            if (errorMessage == null) {
-                errorMessage = panel.getJdkLocationPanel().validateLocation(
-                        jdkLocationField.getText());
-            }
-            
-            return errorMessage;
-        }
-        
-        // private //////////////////////////////////////////////////////////////////
-        private void initComponents() {
-            // selectedLocationField ////////////////////////////////////////////////
-            jdkLocationField = new NbiTextField();
-            jdkLocationField.getDocument().addDocumentListener(
-                    new DocumentListener() {
-                public void insertUpdate(DocumentEvent e) {
-                    updateErrorMessage();
-                }
-                
-                public void removeUpdate(DocumentEvent e) {
-                 //   updateErrorMessage();  
-                }
-                
-                public void changedUpdate(DocumentEvent e) {
-                    updateErrorMessage();
-                }
-            });
-            
-            // jdkLocationComboBox //////////////////////////////////////////////////
-            final LocationValidator validator = new LocationValidator() {
-                public void validate(String location) {
-                    jdkLocationField.setText(location);
-                }
-            };
-            
-            jdkLocationComboBox = new NbiComboBox();
-            jdkLocationComboBox.setEditable(true);
-            jdkLocationComboBox.setEditor(new LocationsComboBoxEditor(validator));
-            jdkLocationComboBox.addItemListener(new ItemListener() {
-                public void itemStateChanged(ItemEvent event) {
-                    final ComboBoxModel model = jdkLocationComboBox.getModel();
-                    
-                    if (model instanceof LocationsComboBoxModel) {
-                        jdkLocationField.setText(
-                                ((LocationsComboBoxModel) model).getLocation());
-                    }
-                }
-            });
-            
-            // jdkLocationLabel /////////////////////////////////////////////////////
-            jdkLocationLabel = new NbiLabel();
-            jdkLocationLabel.setLabelFor(jdkLocationComboBox);
-            
-            // browseButton /////////////////////////////////////////////////////////
-            browseButton = new NbiButton();
-            browseButton.addActionListener(new ActionListener() {
-                public void actionPerformed(ActionEvent event) {
-                    browseButtonPressed();
-                }
-            });
-            
-            // statusLabel //////////////////////////////////////////////////////////
-            statusLabel = new NbiLabel();
-            
-            // fileChooser //////////////////////////////////////////////////////////
-            fileChooser = new NbiDirectoryChooser();
-            
-            // this /////////////////////////////////////////////////////////////////
-            add(jdkLocationLabel, new GridBagConstraints(
-                    0, 2,                             // x, y
-                    2, 1,                             // width, height
-                    1.0, 0.0,                         // weight-x, weight-y
-                    GridBagConstraints.LINE_START,    // anchor
-                    GridBagConstraints.HORIZONTAL,    // fill
-                    new Insets(11, 11, 0, 11),        // padding
-                    0, 0));                           // padx, pady - ???
-            add(jdkLocationComboBox, new GridBagConstraints(
-                    0, 3,                             // x, y
-                    1, 1,                             // width, height
-                    1.0, 0.0,                         // weight-x, weight-y
-                    GridBagConstraints.LINE_START,    // anchor
-                    GridBagConstraints.HORIZONTAL,    // fill
-                    new Insets(4, 11, 0, 0),          // padding
-                    0, 0));                           // padx, pady - ???
-            add(browseButton, new GridBagConstraints(
-                    1, 3,                             // x, y
-                    1, 1,                             // width, height
-                    0.0, 0.0,                         // weight-x, weight-y
-                    GridBagConstraints.LINE_START,    // anchor
-                    GridBagConstraints.HORIZONTAL,    // fill
-                    new Insets(4, 4, 0, 11),          // padding
-                    0, 0));                           // padx, pady - ???
-            add(statusLabel, new GridBagConstraints(
-                    0, 4,                             // x, y
-                    2, 1,                             // width, height
-                    1.0, 0.0,                         // weight-x, weight-y
-                    GridBagConstraints.LINE_START,    // anchor
-                    GridBagConstraints.HORIZONTAL,    // fill
-                    new Insets(4, 11, 0, 11),         // padding
-                    0, 0));                           // padx, pady - ???
-        }
-        
-        private void browseButtonPressed() {
-            fileChooser.setSelectedFile(new File(jdkLocationField.getText()));
-            
-            if (fileChooser.showOpenDialog(this) == JFileChooser.APPROVE_OPTION) {
-                jdkLocationComboBox.getModel().setSelectedItem(
-                        fileChooser.getSelectedFile().getAbsolutePath());
-            }
-        }
-    }
-    
-    /////////////////////////////////////////////////////////////////////////////////
-    // Constants
-    public static final String JDK_LOCATION_LABEL_TEXT_PROPERTY =
-            "nbBase.location.label.text"; // NOI18N
-    public static final String BROWSE_BUTTON_TEXT_PROPERTY =
-            "browse.button.text"; // NOI18N
-    
-    public static final String DEFAULT_TITLE =
-            ResourceUtils.getString(NbLocationPanel.class,
-            "NBP.title"); // NOI18N
-    public static final String DEFAULT_DESCRIPTION =
-            ResourceUtils.getString(NbLocationPanel.class,
-            "NBP.description"); // NOI18N
-    
-    public static final String DEFAULT_DESTINATION_LABEL_TEXT =
-            ResourceUtils.getString(NbLocationPanel.class,
-            "NBP.destination.label.text"); // NOI18N
-    public static final String DEFAULT_DESTINATION_BUTTON_TEXT =
-            ResourceUtils.getString(NbLocationPanel.class,
-            "NBP.destination.button.text"); // NOI18N
-    
-    public static final String DEFAULT_JDK_LOCATION_LABEL_TEXT =
-            ResourceUtils.getString(NbLocationPanel.class,
-            "NBP.nbBase.location.label.text"); // NOI18N
-    public static final String DEFAULT_BROWSE_BUTTON_TEXT =
-            ResourceUtils.getString(NbLocationPanel.class,
-            "NBP.browse.button.text"); // NOI18N
-    
-    public static final String DEFAULT_MINIMUM_JDK_VERSION =
-            ResourceUtils.getString(NbLocationPanel.class,
-            "NBP.minimum.nbBase.version"); // NOI18N
-    public static final String DEFAULT_MAXIMUM_JDK_VERSION =
-            ResourceUtils.getString(NbLocationPanel.class,
-            "NBP.maximum.nbBase.version"); // NOI18N
-}
-
-*/
