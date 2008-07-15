2da78d2d86d029fc5236705a7f885f3bd7a7f43e
joshna138@netbeans.org <joshna138@netbeans.org>
2008-07-15 14:34:34 +0530

    ContentsByTypeId tag is added

---
+++ portalpack.cms/src/org/netbeans/modules/portalpack/cms/palette/items/ContentsByTypeId.java
@@ -0,0 +1,24 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package org.netbeans.modules.portalpack.cms.palette.items;
+
+import org.netbeans.modules.portalpack.cms.palette.*;
+
+/**
+ *
+ * @author Joshna
+ */
+public class ContentsByTypeId extends TagLibDropDefault {
+    
+     @Override
+    public String getTemplateName() {
+        return "contents-by-type-id.template";
+    }
+   
+ 
+    
+}
+
