676ce411adbcdba7a787a028abb503cfe1ed7c24
Caoyuan Deng <dcaoyuan@netbeans.org>
2008-07-25 22:34:20 +0800

    Added Scala JUnit Test template

---
+++ scala.project/src/org/netbeans/modules/scala/project/ui/resources/EmptyScalaJUnitTest.scala
@@ -0,0 +1,29 @@
+/*
+ * __NAME__.scala
+ *
+ * To change this template, choose Tools | Template Manager
+ * and open the template in the editor.
+ */
+
+package __PACKAGE__
+
+import org.junit.After
+import org.junit.Before
+import org.junit.Test
+import org.junit.Assert._
+
+class __NAME__ {
+
+    @Before
+    def setUp(): Unit = {
+    }
+
+    @After
+    def tearDown(): Unit = {
+    }
+
+    @Test
+    def example() = {
+    }
+
+}
