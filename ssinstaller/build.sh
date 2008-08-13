8f47c1260444923ec666fe9ce07f1236040e8cc7
mesnik@netbeans.org <mesnik@netbeans.org>
2008-08-13 20:05:12 +0400

    SS installer build was fixed. SizeOf temporary removed.

--- ssinstaller/build.sh
+++ ssinstaller/build.sh
@@ -1,7 +1,14 @@
-ant -f BuildHelper/build.xml
+#export PATH=/set/java-sqe/tools/apache-ant-1.6.5/bin:$PATH
+export JAVA_HOME=/usr/java
+which ant
+cd BuildHelper
+$JAVA_HOME/bin/xjc -d src -p org.netbeans.xml.schema.productdescription xml-resources/jaxb/description/ProductDescription.xsd
+ant -f build2.xml
+cd -
 java -cp BuildHelper/dist/BuildHelper.jar buildhelper.BuildHelper ProductDescription.xml . toolchain
+
 cd infra
 bash build.sh
 cd - 
 cd toolchain
-bash build.sh
+#bash build.sh
