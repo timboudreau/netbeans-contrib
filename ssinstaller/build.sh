7da78bf950406ca92ea68409882622f35b447ed5
mesnik@netbeans.org <mesnik@netbeans.org>
2008-08-13 22:27:07 +0400

    The build environment was updated.

--- ssinstaller/build.sh
+++ ssinstaller/build.sh
@@ -1,6 +1,5 @@
-#export PATH=/set/java-sqe/tools/apache-ant-1.6.5/bin:$PATH
-export JAVA_HOME=/usr/java
 which ant
+which java
 cd BuildHelper
 $JAVA_HOME/bin/xjc -d src -p org.netbeans.xml.schema.productdescription xml-resources/jaxb/description/ProductDescription.xsd
 ant -f build2.xml
