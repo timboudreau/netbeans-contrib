8efeff4b7dfe6416bff1ab063d655fd870b22c3f
Jindrich Sedek <jsedek@netbeans.org>
2009-01-23 13:05:09 +0100

    adding basic selenium support for maven projects

---
+++ selenium.maven/src/org/netbeans/modules/selenium/maven/SeleneseTestWizardOperator.java
@@ -0,0 +1,169 @@
+/*
+ * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS HEADER.
+ *
+ * Copyright 2009 Sun Microsystems, Inc. All rights reserved.
+ *
+ * The contents of this file are subject to the terms of either the GNU
+ * General Public License Version 2 only ("GPL") or the Common
+ * Development and Distribution License("CDDL") (collectively, the
+ * "License"). You may not use this file except in compliance with the
+ * License. You can obtain a copy of the License at
+ * http://www.netbeans.org/cddl-gplv2.html
+ * or nbbuild/licenses/CDDL-GPL-2-CP. See the License for the
+ * specific language governing permissions and limitations under the
+ * License.  When distributing the software, include this License Header
+ * Notice in each file and include the License file at
+ * nbbuild/licenses/CDDL-GPL-2-CP.  Sun designates this
+ * particular file as subject to the "Classpath" exception as provided
+ * by Sun in the GPL Version 2 section of the License file that
+ * accompanied this code. If applicable, add the following below the
+ * License Header, with the fields enclosed by brackets [] replaced by
+ * your own identifying information:
+ * "Portions Copyrighted [year] [name of copyright owner]"
+ *
+ * If you wish your version of this file to be governed by only the CDDL
+ * or only the GPL Version 2, indicate your decision by adding
+ * "[Contributor] elects to include this software in this distribution
+ * under the [CDDL or GPL Version 2] license." If you do not indicate a
+ * single choice of license, a recipient has the option to distribute
+ * your version of this file under either the CDDL, the GPL Version 2 or
+ * to extend the choice of license to its licensees as provided above.
+ * However, if you add GPL Version 2 code and therefore, elected the GPL
+ * Version 2 license, then the option applies only if the new code is
+ * made subject to such option by the copyright holder.
+ *
+ * Contributor(s):
+ *
+ * Portions Copyrighted 2008 Sun Microsystems, Inc.
+ */
+package org.netbeans.modules.selenium.maven;
+
+import java.io.IOException;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Set;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.event.ChangeListener;
+import org.netbeans.api.java.project.JavaProjectConstants;
+import org.netbeans.api.project.Project;
+import org.netbeans.api.project.ProjectUtils;
+import org.netbeans.api.project.SourceGroup;
+import org.netbeans.api.project.Sources;
+import org.netbeans.spi.java.project.support.ui.templates.JavaTemplates;
+import org.netbeans.spi.project.ui.templates.support.Templates;
+import org.openide.WizardDescriptor;
+import org.openide.filesystems.FileObject;
+import org.openide.loaders.DataFolder;
+import org.openide.loaders.DataObject;
+import org.openide.util.ChangeSupport;
+import org.openide.util.NbBundle;
+
+/**
+ *
+ * @author Jindrich Sedek
+ */
+public class SeleneseTestWizardOperator implements WizardDescriptor.InstantiatingIterator {
+
+    private ChangeSupport changeSupport = new ChangeSupport(this);
+    private static final String DEFAULT_SERVER_PORT = "80";         // NOI18N
+    private transient WizardDescriptor.Panel panel;
+    private transient WizardDescriptor wiz;
+
+
+    public static WizardDescriptor.InstantiatingIterator create(){
+        return new SeleneseTestWizardOperator();
+    }
+
+    public Set<FileObject> instantiate() throws IOException {
+        FileObject dir = Templates.getTargetFolder(wiz);
+        String targetName = Templates.getTargetName(wiz);
+
+        DataFolder df = DataFolder.findFolder(dir);
+        FileObject template = Templates.getTemplate(wiz);
+
+        FileObject createdFile = null;
+        DataObject dTemplate = DataObject.find(template);
+        Object serverPort = null;
+        if (serverPort == null){
+            serverPort = DEFAULT_SERVER_PORT;
+        }
+        Map<String, Object> params = new HashMap<String, Object>();
+        params.put("server_port", serverPort);   //NOI18N
+
+        DataObject dobj = dTemplate.createFromTemplate(df, targetName, params);
+        createdFile = dobj.getPrimaryFile();
+
+        Project project = Templates.getProject(wiz);
+        SeleniumMavenSupport.prepareProject(project);
+
+        return Collections.singleton(createdFile);
+    }
+
+    
+    public void initialize(WizardDescriptor wiz) {
+        this.wiz = wiz;
+        Project project = Templates.getProject(wiz);
+        panel = createPanel(wiz);
+        panel.getComponent();
+    }
+
+    public void uninitialize(WizardDescriptor wiz) {
+        this.wiz = null;
+        panel = null;
+    }
+
+    public String name() {
+        return NbBundle.getMessage(SeleneseTestWizardOperator.class, "SELENESE_TEMPLATE_WIZARD_TITLE");
+    }
+
+    public boolean hasNext() {
+        return false;
+    }
+
+    public boolean hasPrevious() {
+        return false;
+    }
+
+    public void nextPanel() {
+          assert(false);
+    }
+
+    public void previousPanel() {
+          assert(false);
+    }
+
+    public WizardDescriptor.Panel current() {
+        return panel;
+    }
+
+    private WizardDescriptor.Panel createPanel(WizardDescriptor wizardDescriptor) {
+        // Ask for Java folders
+        Project project = Templates.getProject(wizardDescriptor);
+        Sources sources = ProjectUtils.getSources(project);
+        SourceGroup[] groups = sources.getSourceGroups(JavaProjectConstants.SOURCES_TYPE_JAVA);
+        assert groups != null : "Cannot return null from Sources.getSourceGroups: " + sources;
+        if (groups.length == 0) {
+            groups = sources.getSourceGroups(Sources.TYPE_GENERIC);
+            return Templates.createSimpleTargetChooser(project, groups);
+        } else {
+            FileObject testDir = SeleniumMavenSupport.getTestRoot(project);
+            for (SourceGroup selGroup : groups) {
+                if (selGroup.getRootFolder().equals(testDir)){
+                    return JavaTemplates.createPackageChooser(project, new SourceGroup[]{selGroup});
+                }
+            }
+            return JavaTemplates.createPackageChooser(project, groups);
+        }
+
+    }
+
+    public void addChangeListener(ChangeListener l) {
+        changeSupport.addChangeListener(l);
+    }
+
+    public void removeChangeListener(ChangeListener l) {
+        changeSupport.removeChangeListener(l);
+    }
+}
---
+++ selenium.maven/src/org/netbeans/modules/selenium/maven/SeleniumMavenSupport.java
@@ -0,0 +1,182 @@
+/*
+ * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS HEADER.
+ *
+ * Copyright 2009 Sun Microsystems, Inc. All rights reserved.
+ *
+ * The contents of this file are subject to the terms of either the GNU
+ * General Public License Version 2 only ("GPL") or the Common
+ * Development and Distribution License("CDDL") (collectively, the
+ * "License"). You may not use this file except in compliance with the
+ * License. You can obtain a copy of the License at
+ * http://www.netbeans.org/cddl-gplv2.html
+ * or nbbuild/licenses/CDDL-GPL-2-CP. See the License for the
+ * specific language governing permissions and limitations under the
+ * License.  When distributing the software, include this License Header
+ * Notice in each file and include the License file at
+ * nbbuild/licenses/CDDL-GPL-2-CP.  Sun designates this
+ * particular file as subject to the "Classpath" exception as provided
+ * by Sun in the GPL Version 2 section of the License file that
+ * accompanied this code. If applicable, add the following below the
+ * License Header, with the fields enclosed by brackets [] replaced by
+ * your own identifying information:
+ * "Portions Copyrighted [year] [name of copyright owner]"
+ *
+ * If you wish your version of this file to be governed by only the CDDL
+ * or only the GPL Version 2, indicate your decision by adding
+ * "[Contributor] elects to include this software in this distribution
+ * under the [CDDL or GPL Version 2] license." If you do not indicate a
+ * single choice of license, a recipient has the option to distribute
+ * your version of this file under either the CDDL, the GPL Version 2 or
+ * to extend the choice of license to its licensees as provided above.
+ * However, if you add GPL Version 2 code and therefore, elected the GPL
+ * Version 2 license, then the option applies only if the new code is
+ * made subject to such option by the copyright holder.
+ *
+ * Contributor(s):
+ *
+ * Portions Copyrighted 2009 Sun Microsystems, Inc.
+ */
+package org.netbeans.modules.selenium.maven;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.Collections;
+import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import org.apache.maven.project.MavenProject;
+import org.netbeans.api.project.Project;
+import org.netbeans.modules.maven.api.ModelUtils;
+import org.netbeans.modules.maven.api.NbMavenProject;
+import org.netbeans.modules.maven.model.ModelOperation;
+import org.netbeans.modules.maven.model.Utilities;
+import org.netbeans.modules.maven.model.pom.Configuration;
+import org.netbeans.modules.maven.model.pom.Dependency;
+import org.netbeans.modules.maven.model.pom.POMModel;
+import org.netbeans.modules.maven.model.pom.POMModelFactory;
+import org.netbeans.modules.maven.model.pom.Plugin;
+import org.netbeans.modules.maven.model.pom.PluginExecution;
+import org.netbeans.modules.maven.model.pom.Repository;
+import org.netbeans.modules.xml.xam.ModelSource;
+import org.openide.filesystems.FileObject;
+import org.openide.filesystems.FileUtil;
+
+/**
+ *
+ * @author Jindrich Sedek
+ */
+final class SeleniumMavenSupport {
+
+    static FileObject getTestRoot(Project project) {
+        NbMavenProject nbProject = project.getLookup().lookup(NbMavenProject.class);
+        MavenProject mvp = nbProject.getMavenProject();
+        List<String> testRoots = mvp.getTestCompileSourceRoots();
+        if (testRoots.isEmpty()) {
+            return null;
+        }
+        File testRoot = new File(testRoots.get(0));
+        FileObject result = null;
+        try {
+            result = FileUtil.createData(testRoot);
+        } catch (IOException ex) {
+            Logger.getLogger(SeleniumMavenSupport.class.getName()).log(Level.SEVERE, "Impossible to create test root file object", ex);
+        }
+        return result;
+    }
+
+    static void prepareProject(Project project) {
+        assert (isMavenProject(project));
+        if (isProjectReady(project)) {
+            return;
+        }
+
+        NbMavenProject nbProject = project.getLookup().lookup(NbMavenProject.class);
+        MavenProject mvp = nbProject.getMavenProject();
+
+        ModelOperation<POMModel> operation = new SeleniumOperation(mvp);
+        org.netbeans.modules.maven.model.Utilities.performPOMModelOperations(getPomFile(project), Collections.singletonList(operation));
+
+    }
+
+    private static class SeleniumOperation implements ModelOperation<POMModel> {
+
+        private final MavenProject mvp;
+
+        public SeleniumOperation(MavenProject mvp) {
+            this.mvp = mvp;
+        }
+
+        public void performOperation(POMModel model) {
+            Repository rep = ModelUtils.addModelRepository(mvp, model, "http://nexus.openqa.org/content/repositories/releases");
+            rep.setId("openqa-releases");
+            rep.setLayout("default");
+            rep.setName("Openqa Release Repository");
+            rep.setReleases(model.getFactory().createReleaseRepositoryPolicy());
+
+            Dependency dep = ModelUtils.checkModelDependency(model,
+                    "org.openqa.selenium.client-drivers", "selenium-java-client-driver", true);
+            dep.setScope("test");
+            dep.setVersion("1.0-beta-1");
+
+            model.getProject().getBuild().addPlugin(createSeleniumPlugin(model));
+            model.getProject().getBuild().addPlugin(createSureFirePlugin(model));
+        }
+
+        private Plugin createSeleniumPlugin(POMModel model) {
+            Plugin plugin = model.getFactory().createPlugin();
+            plugin.setGroupId("org.codehaus.mojo");
+            plugin.setArtifactId("selenium-maven-plugin");
+            PluginExecution startServer = model.getFactory().createExecution();
+            startServer.setId("start");
+            startServer.addGoal("start-server");
+            startServer.setPhase("pre-integration-test");
+            Configuration serverConf = model.getFactory().createConfiguration();
+            serverConf.setSimpleParameter("background", "true");
+            startServer.setConfiguration(serverConf);
+            plugin.addExecution(startServer);
+            
+            PluginExecution stopServer = model.getFactory().createExecution();
+            stopServer.setId("stop");
+            stopServer.addGoal("stop-server");
+            stopServer.setPhase("post-integration-test");
+            plugin.addExecution(stopServer);
+            return plugin;
+        }
+
+        private Plugin createSureFirePlugin(POMModel model) {
+            Plugin plugin = model.getFactory().createPlugin();
+            plugin.setGroupId("org.apache.maven.plugins");
+            plugin.setArtifactId("maven-surefire-plugin");
+            Configuration conf = model.getFactory().createConfiguration();
+            conf.setSimpleParameter("skip", "true");
+            plugin.setConfiguration(conf);
+            PluginExecution execution = model.getFactory().createExecution();
+            Configuration execConf = model.getFactory().createConfiguration();
+            execConf.setSimpleParameter("skip", "false");
+            execution.addGoal("test");
+            execution.setPhase("integration-test");
+            execution.setConfiguration(execConf);
+            plugin.addExecution(execution);
+            return plugin;
+        }
+    }
+
+    private static boolean isProjectReady(Project project) {
+        POMModel model = getPOMModel(project);
+        return ModelUtils.hasModelDependency(model, "org.openqa.selenium.client-drivers", "selenium-java-client-driver");
+    }
+
+    public static boolean isMavenProject(Project project) {
+        return project.getLookup().lookup(NbMavenProject.class) != null;
+    }
+
+    private static POMModel getPOMModel(Project project) {
+        FileObject pom = getPomFile(project);
+        ModelSource source = Utilities.createModelSource(pom);
+        return POMModelFactory.getDefault().getModel(source);
+    }
+
+    private static FileObject getPomFile(Project project) {
+        return project.getProjectDirectory().getFileObject("pom.xml");
+    }
+}
