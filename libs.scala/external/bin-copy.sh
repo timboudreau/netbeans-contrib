e16baedc13b3ad62b127ec1a6e5400d02e8be57d
Caoyuan Deng <dcaoyuan@netbeans.org>
2009-10-21 15:49:22 +0800

    Upgrated to Scala-2.8.0.r19180

---
+++ libs.scala/external/bin-copy.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+SHA1=
+SHA2=
+SHA3=
+SHA4=
+
+cp scala-compiler-2.8.0.jar ~/.hgexternalcache/$SHA1-scala-compiler-2.8.0.jar
+cp scala-dbc-2.8.0.jar      ~/.hgexternalcache/$SHA2-scala-dbc-2.8.0.jar
+cp scala-library-2.8.0.jar  ~/.hgexternalcache/$SHA3-scala-library-2.8.0.jar
+cp scala-swing-2.8.0.jar    ~/.hgexternalcache/$SHA4-scala-swing-2.8.0.jar
