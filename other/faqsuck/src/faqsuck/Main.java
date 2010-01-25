7968ef43eac0fbd8dedba61455a1a0f082f26040
Jesse Glick <jglick@netbeans.org>
2010-01-25 14:22:49 -0500

    Fixing main class.

--- other/faqsuck/src/faqsuck/Main.java
+++ other/faqsuck/src/faqsuck/Main.java
@@ -58,7 +58,7 @@ public class Main {
 
     public static void main(String[] args) throws Exception {
         if (args.length < 2) {
-            System.err.println("Usage: java -jar jfaqsuck.jar url baseurl [outputfile]");
+            System.err.println("Usage: java -jar faqsuck.jar url baseurl [outputfile]");
             System.exit(1);
         }
         String faqUrl = args[0];
