e7bf100d7cd9babee58b64db40f334c5fbf7e0ba
Jesse Glick <jglick@netbeans.org>
2011-05-19 13:59:37 -0400

    Scala support seems to have been moved to: http://java.net/projects/nbscala

--- libs.scala/external/bin-copy.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-SHA1=1ED2057FFC31D07AA7149F20A5073B6D627C0D2F
-SHA2=84960D60BAB643FC5174AA13CCDC9679100463CE
-SHA3=362B35537BEAE22833FCB982B69F9484908A92FF
-
-cp scala-compiler-2.8.0.jar ~/.hgexternalcache/$SHA1-scala-compiler-2.8.0.jar
-cp scala-library-2.8.0.jar  ~/.hgexternalcache/$SHA2-scala-library-2.8.0.jar
-cp scala-swing-2.8.0.jar    ~/.hgexternalcache/$SHA3-scala-swing-2.8.0.jar
