c806d7625ce755314d709bb34b357d1e69f74540
Jesse Glick <jglick@netbeans.org>
2011-07-21 11:12:44 -0400

    Avoid dep on NB sources.

--- other/signatures/test/org/netbeans/signatures/SignatureTaskTest.java
+++ other/signatures/test/org/netbeans/signatures/SignatureTaskTest.java
@@ -48,7 +48,6 @@ import java.io.Reader;
 import junit.framework.TestCase;
 import org.apache.tools.ant.BuildEvent;
 import org.apache.tools.ant.BuildListener;
-import org.apache.tools.ant.BuildLogger;
 import org.apache.tools.ant.Project;
 import org.apache.tools.ant.types.FileSet;
 
@@ -63,8 +62,6 @@ public class SignatureTaskTest extends TestCase {
     
     private File antJar;
     private File antLauncherJar;
-    private File antModuleJar;
-    private File nbdev;
     
     protected void setUp() throws Exception {
         super.setUp();
@@ -72,10 +69,6 @@ public class SignatureTaskTest extends TestCase {
         assertTrue(antJar.getAbsolutePath(), antJar.isFile());
         antLauncherJar = new File(antJar.getParentFile(), "ant-launcher.jar");
         assertTrue(antLauncherJar.getAbsolutePath(), antLauncherJar.isFile());
-        nbdev = new File(antJar.getParentFile().getParentFile().getParentFile().getParentFile(), "nbbuild/netbeans");
-        assertTrue(nbdev.isDirectory());
-        antModuleJar = new File(nbdev, "ide8/modules/org-apache-tools-ant-module.jar");
-        assertTrue(antModuleJar.getAbsolutePath(), antModuleJar.isFile());
     }
     
     public void testExecuteAnt() throws Exception {
@@ -104,35 +97,6 @@ public class SignatureTaskTest extends TestCase {
                 String l = b.readLine();
                 assertNotNull("found matching line in " + out, l);
                 if (l.equals("{Class _ = org.apache.tools.ant.Task.class;}")) {
-                    break;
-                }
-            }
-        } finally {
-            r.close();
-        }
-    }
-    
-    public void testExecuteNB() throws Exception {
-        Project p = new Project();
-        p.addBuildListener(new Listener());
-        SignatureTask task = new SignatureTask();
-        task.setProject(p);
-        File out = File.createTempFile("signatures", ".java");
-        task.setOut(out);
-        FileSet fs = new FileSet();
-        fs.setProject(p);
-        fs.setDir(nbdev);
-        fs.setIncludes("*/modules/*.jar,*/lib/*.jar,*/core/*.jar");
-        task.addFileSet(fs);
-        task.execute();
-        assertTrue(out.isFile());
-        Reader r = new FileReader(out);
-        try {
-            BufferedReader b = new BufferedReader(r);
-            while (true) {
-                String l = b.readLine();
-                assertNotNull("found matching line in " + out, l);
-                if (l.equals("{Class _ = javax.help.HelpSet.class;}")) {
                     break;
                 }
             }
