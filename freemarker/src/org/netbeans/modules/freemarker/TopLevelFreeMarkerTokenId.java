8ec48d04ecac52bc7a9e01ddcf2dffce548fa1f6
Jan Lahoda <jlahoda@netbeans.org>
2011-10-06 18:45:11 +0200

    Join sections for the language embedded in the freemarker template, to get correct coloring for things like block comments.

--- freemarker/src/org/netbeans/modules/freemarker/TopLevelFreeMarkerTokenId.java
+++ freemarker/src/org/netbeans/modules/freemarker/TopLevelFreeMarkerTokenId.java
@@ -1,7 +1,7 @@
 /*
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS HEADER.
  * 
- * Copyright 1997-2007 Sun Microsystems, Inc. All rights reserved.
+ * Copyright 1997-2011 Sun Microsystems, Inc. All rights reserved.
  * 
  * The contents of this file are subject to the terms of either the GNU
  * General Public License Version 2 only ("GPL") or the Common
@@ -34,7 +34,7 @@
  * 
  * Contributor(s):
  * 
- * Portions Copyrighted 2007 Sun Microsystems, Inc.
+ * Portions Copyrighted 2007-2011 Sun Microsystems, Inc.
  */
 
 package org.netbeans.modules.freemarker;
@@ -104,7 +104,7 @@ public enum TopLevelFreeMarkerTokenId implements TokenId {
                         Language<?> language = (Language) Language.find(embeddedMimeType);
 
                         if (language != null) {
-                            return LanguageEmbedding.create(language, 0, 0);
+                            return LanguageEmbedding.create(language, 0, 0, true);
                         }
                     }
 
@@ -125,7 +125,6 @@ public enum TopLevelFreeMarkerTokenId implements TokenId {
     }.language();
     
     public static Language<TopLevelFreeMarkerTokenId> language() {
-        Thread.dumpStack();
         return language;
     }
 }
