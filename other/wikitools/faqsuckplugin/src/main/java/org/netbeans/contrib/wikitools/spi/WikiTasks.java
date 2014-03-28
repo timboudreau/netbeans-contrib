4eeb3ebb7a8289d3e9217eca2a87cc9401e39db8
Eric Barboni <skygo@netbeans.org>
2014-03-28 20:04:02 +0100

    minor jdoc

--- other/wikitools/faqsuckplugin/src/main/java/org/netbeans/contrib/wikitools/spi/WikiTasks.java
+++ other/wikitools/faqsuckplugin/src/main/java/org/netbeans/contrib/wikitools/spi/WikiTasks.java
@@ -17,9 +17,27 @@ import org.netbeans.contrib.faqsuck.FaqsuckMojo;
  */
 public interface WikiTasks {
 
+    /**
+     * Wiki version string
+     * @return wiki version as in meta
+     */
     String getVersion();
 
+    /**
+     * 
+     * @param baseUrl
+     * @param tocContent
+     * @param categories
+     * @param mojo 
+     */
     void feedCategory(URL baseUrl, Document tocContent, List<Category> categories, FaqsuckMojo mojo);
 
+    /**
+     * 
+     * @param doc
+     * @param mojos
+     * @param ent
+     * @return 
+     */
     String strip(Document doc, FaqsuckMojo mojos, Entry ent);
 }
